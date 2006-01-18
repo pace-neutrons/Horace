b22fe992039a7b4c212e5d2a49e370e4cc0eeb4e
Joost van Duijn <()>
2006-01-18 16:09:03 +0000

    bose.m: Bose function bose_inv.m: Inverse of the Bose function
    Both can be called by user_func
    
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@90 1cb955da-4463-431d-88dd-2af2f5550de3

---
+++ functions/bose_inv.m
@@ -0,0 +1,56 @@
+function dout= bose_inv(din,temp)
+% Inverse of Bose function. Multiply S(Q,w) by this function to get X''(Q,w)
+% Is called using the dnd_user_func routine. It assumes that the energy 
+% axis is always the last axis in the dataset.
+%
+% syntax:
+%   >> wout= user_func(win, @bose, temperature(K))
+%
+% Input:
+% ------
+%   din     input dataset that needs to be Bose corrected
+%   t       temperature (K)
+%
+% Output:
+% -------
+%   dout    output data structure containing dout.s, dout.e, dout.title
+%
+%   dout.s= din.s*(1-exp(-hw/KbT))
+
+% Original author: J. van Duijn
+%
+% $Revision$ ($Date$)
+%
+% Horace v0.1   J. van Duijn, T.G.Perring
+
+% Check and extract input paramaters
+if nargin==1,
+    dout.s=din.s;
+    dout.e=din.e;
+    dout.title=din.title;
+    return
+elseif ~isnumeric(temp) | length(temp)>1
+    error('ERROR: temperature needs to be given for the Bose correction to work');
+end
+
+% obtain the energy axis
+np= find(din.pax==4);
+en=eval(['din.p',num2str(np),';']);
+
+% Apply the Bose correction
+naxis=length(din.pax);
+for i=1:length(en)-1
+    hw= (en(i)+en(i+1))/2;
+    Bose= 1-exp(-hw*11.604/temp);
+    if naxis==1,    % 1D dataset
+        dout.s(i,1)= din.s(i)*Bose;
+    elseif naxis==2    % 2D dataset
+        dout.s(:,i)= din.s(:,i)*Bose;
+    elseif naxis==3    % 3D dataset
+        dout.s(:,:,i)= din.s(:,:,i)*Bose;
+    else    % 4D dataset
+        dout.s(:,:,:,i)= din.s(:,:,:,i)*Bose;
+    end
+end
+dout.title= [din.title, ' inverse Bose function(T= ', num2str(temp), ')'];
+dout.e= zeros(size(din.e));
