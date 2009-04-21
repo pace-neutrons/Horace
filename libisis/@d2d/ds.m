c9e092290fefe482d7fa4291cece55a4dcb89421
Russell Ewings <russell.ewings@stfc.ac.uk>
2009-04-21 15:53:28 +0000

    trivial fix of libisis/@d2d/ds method so that it calls ds and not da refs #65
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/branches/sqw@200 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 6394c7d94a3b5b4d130f8edd4adc08c71ab5f1ed

--- libisis/@d2d/ds.m
+++ libisis/@d2d/ds.m
@@ -1,14 +1,14 @@
 function ds(win,varargin)
 % Surface plot for 2D dataset
 %
-%   >> da(win)
-%   >> da(win,xlo,xhi);
-%   >> da(win,xlo,xhi,ylo,yhi);
+%   >> ds(win)
+%   >> ds(win,xlo,xhi);
+%   >> ds(win,xlo,xhi,ylo,yhi);
 % Or:
-%   >> da(win,'xlim',[xlo,xhi],'ylim',[ylo,yhi],'colormap','jet');
+%   >> ds(win,'xlim',[xlo,xhi],'ylim',[ylo,yhi],'colormap','jet');
 %
 % See help for libisis/ds for more details of other options
 
 % R.A. Ewings 14/10/2008
 
-da(sqw(win),varargin{:});
+ds(sqw(win),varargin{:});
