ba6cf5b1e7229b39ff3aa9335d2978ceeccde484
Alex Buts <alex.buts@stfc.ac.uk>
2009-11-10 19:22:26 +0000

    refs #24 version function has been added to Horace, similarly to Libisis
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@307 1cb955da-4463-431d-88dd-2af2f5550de3

---
+++ utilities/version.m
@@ -0,0 +1,6 @@
+function current=version()
+% the function returns the version of horace, which should correspond to
+% distinctive tag version on the SVN server. 
+%
+% 
+current='3.0.1.(Horace $Revision: $ from ($Date: $))';
