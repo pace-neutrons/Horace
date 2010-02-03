8776b018732251d3ec22ddb8196ba34afb2c24bd
Alex Buts <alex.buts@stfc.ac.uk>
2010-02-03 13:32:27 +0000

    refs #74 make the horace_version.m function unique as the copy of it existed in utilities/horace_version.m too.
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@345 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: fb09a4554959afe1912313b347f4ecaf9c3506ea

--- horace_version.m
+++ horace_version.m
@@ -1,5 +1,40 @@
-function application=horace_version
+function [application,Matlab_SVN,Fortran_version]=horace_version(varargin)
+% the function returns the version of horace, which should correspond to
+% the distinctive tag version from the SVN server. 
+%
+% An pre-commit hook script has to be enabled to support valid Matlab
+% versioning. The script should modify the data of this file before
+% committing. 
+% The variable below introduced to allow the commit hook touching this file and 
+% make this touches available to the svn (may be it is a cumbersome solution, but is 
+% the best and most portable I can think in a five minutes time). 
+%
+% if horace_version is called with parameter, the function
+% returns revision data as numbers rather then strings (convenient for version analysis)
+%
+%
+% $COMMIT_COUNTER:: 1 $
+%
+% No variable below this one should resemble COMMIT_COUNTER, as their values will 
+% be modified and probably corrupted at commit
+% after the couter changed, the row below will be updated to the latest svn version while committing
+% 
+application.name='horace';
+application.version=2.0;  % not used at the moment -- when would, should be modified sensibly
+
+Matlab_SVN='$Revision::      $ ($Date::                                              $)';
+%
 % Information about name and version of application
 
-application.name='horace';
-application.version=1.0;
+Fortran_version  ='Not implemented                                         ';
+if nargin>0
+    hd     =str2double(Matlab_SVN(12:17));
+    Matlab_SVN =hd(1);
+    if regexp(Fortran_version,'Not implemented')
+        Fortran_version  = -1;
+    else
+        hd  =str2double(Fortran_version(12:17));
+        Fortran_version=hd(1);
+    end
+end
+
--- tortoseHoracehook.bat
+++ tortoseHoracehook.bat
@@ -1,5 +1,5 @@
 @echo off
 set SCRDIR=%CD%
 rem pause
-"%MATLABDIR%\sys\perl\win32\bin\perl.exe" "%SCRDIR%\..\Libisis_Dev\Libisis\ISIS_utilities\svn_matlab_commit_hook.pl" "%SCRDIR%\utilities\horace_version.m"
+"%MATLABDIR%\sys\perl\win32\bin\perl.exe" "%SCRDIR%\..\Libisis_Dev\Libisis\ISIS_utilities\svn_matlab_commit_hook.pl" "%SCRDIR%\horace_version.m"
 exit 0;
