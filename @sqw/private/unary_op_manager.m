28479ccc399df3b417e587c65506944aab16d59a
Russell Ewings <russell.ewings@stfc.ac.uk>
2010-03-24 12:24:24 +0000

    change sqw/unary_op_manager to prevent error when dealing with arrays of sqw objects by checking is_sqw_type for each object in array, rather than all of them refs #24
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@400 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: d580e478177dc341df05046ec4288588a255e030

--- @sqw/private/unary_op_manager.m
+++ @sqw/private/unary_op_manager.m
@@ -13,7 +13,7 @@ function w = unary_op_manager (w1, unary_op)
 
 w = w1;
 for i=1:numel(w1)
-    if is_sqw_type(w1)
+    if is_sqw_type(w1(i))
         result = unary_op(sigvar(w1(i).data.pix(8,:), w1(i).data.pix(9,:)));   % Apply operation to pixel data
         w(i).data.pix(8:9,:) = [result.s;result.e];
         w(i) = recompute_bin_data (w(i));
