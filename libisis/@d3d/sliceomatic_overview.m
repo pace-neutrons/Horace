cdd29cd9f8e54a1561f97de72b350dbb0c5c66a1
Russell Ewings <russell.ewings@stfc.ac.uk>
2010-03-25 17:33:14 +0000

    sliceomatic_overview function added refs #89
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@405 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 351c27b272154224d6d8a8203d64ec4123f3f145

---
+++ libisis/@d3d/sliceomatic_overview.m
@@ -0,0 +1,97 @@
+function sliceomatic_overview(win,varargin)
+%
+% sliceomatic_overview(win)
+% OR
+% sliceomatic_overview(win,axis)
+% 
+% where win is a 3d object (sqw or d3d)
+%       axis is an integer in the range 1 to 3, to specify which axis we
+%       want to view along
+%
+% Do a sliceomatic plot, but set the axes so that we look straight down the
+% 3rd (vertical) axis, so that when the slider is moved we get a series of
+% what appear to be 2d slices.
+%
+% RAE 25/3/2010
+%
+
+%This is quite simple really - we just have to run sliceomatic and then set
+%the camera position appropriately.
+
+if isa(win,'d3d')
+    nopix=true;
+elseif isa(win,'sqw') && dimensions(win)==3
+    nopix=false;
+else
+    error('Sliceomatic only valid for 3d Horace objects');
+end
+
+if ~(nargin==1 || nargin==2)
+    error('Check the number of input arguments');
+end
+
+if nargin==2
+    axis=varargin{1};
+else
+    axis=3;
+end
+    
+   
+if nopix
+    plot(win);
+    if axis==1
+        ycen=median(win.p{win.dax(2)});
+        zcen=median(win.p{win.dax(3)});
+        xmax=max(win.p{win.dax(1)});
+        %
+        %Camera pos vector:
+        camposvec=[100.*xmax,ycen,zcen];
+    elseif axis==2
+        xcen=median(win.p{win.dax(1)});
+        zcen=median(win.p{win.dax(3)});
+        ymax=max(win.p{win.dax(2)});
+        %
+        %Camera pos vector:
+        camposvec=[xcen,100.*ymax,zcen];
+    elseif axis==3
+        xcen=median(win.p{win.dax(1)});
+        ycen=median(win.p{win.dax(2)});
+        zmax=max(win.p{win.dax(3)});
+        %
+        %Camera pos vector:
+        camposvec=[xcen,ycen,zmax];
+    else
+        error('Axis argument must be an integer in the range 1 to 3');
+    end
+    %Set the camera position
+    set(gca,'CameraPosition',camposvec);
+else
+    if axis==1
+            ycen=median(win.data.p{win.data.dax(2)});
+            zcen=median(win.data.p{win.data.dax(3)});
+            xmax=max(win.data.p{win.data.dax(1)});
+            %
+            %Camera pos vector:
+            camposvec=[100.*xmax,ycen,zcen];
+    elseif axis==2
+            xcen=median(win.data.p{win.data.dax(1)});
+            zcen=median(win.data.p{win.data.dax(3)});
+            ymax=max(win.data.p{win.data.dax(2)});
+            %
+            %Camera pos vector:
+            camposvec=[xcen,100.*ymax,zcen];
+    elseif axis==3
+            xcen=median(win.data.p{win.data.dax(1)});
+            ycen=median(win.data.p{win.data.dax(2)});
+            zmax=max(win.data.p{win.data.dax(3)});
+            %
+            %Camera pos vector:
+            camposvec=[xcen,ycen,zmax];
+    else
+            error('Axis argument must be an integer in the range 1 to 3');
+    end
+    plot(win);
+    %
+    %Set the camera position
+    set(gca,'CameraPosition',camposvec);
+end
