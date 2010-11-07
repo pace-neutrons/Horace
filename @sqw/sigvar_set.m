b1e5cac0ae16801d57b3e39aa866ac8a7dba0d30
Toby Perring <toby.perring@stfc.ac.uk>
2010-11-07 14:36:48 +0000

    Fix bug in sigvar_set that allowed signal and error arrays to be replaced with arrays that are inconsistent with the corresponding sqw or dnd arrays.
    Fix error that did not correctly fill the pixel values in sqw objects is sigvar_set.
    
    refs #24
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@498 1cb955da-4463-431d-88dd-2af2f5550de3

--- @sqw/sigvar_set.m
+++ @sqw/sigvar_set.m
@@ -7,11 +7,15 @@ function w = sigvar_set(w,sigvarobj)
 %
 % $Revision$ ($Date$)
 
+if ~isequal(size(w.data.s),size(sigvarobj.s))
+    error('sqw object and sigvar object have inconsistent sizes')
+end
+
 w.data.s=sigvarobj.s;
 w.data.e=sigvarobj.e;
 stmp = replicate_array(w.data.s, w.data.npix)';
 etmp = replicate_array(w.data.e, w.data.npix)';
-wout.data.pix(8:9,:) = [stmp;etmp]; % propagate signal into the pixel data
+w.data.pix(8:9,:) = [stmp;etmp]; % propagate signal into the pixel data
 
 % If no pixels, then our convention is that signal and error set to zero
 nopix=(w.data.npix==0);
