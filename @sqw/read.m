554edcd837f047044772d6865494567222d66b21
Russell Ewings <russell.ewings@stfc.ac.uk>
2010-11-29 13:33:19 +0000

    Alter read so that it does not fail when attempting to parse data refs #91
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@504 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: b45163a3617d1b0614bac909503bc9b60d2f0d91

--- @sqw/read.m
+++ @sqw/read.m
@@ -12,7 +12,15 @@ function varargout = read(varargin)
 %
 % $Revision$ ($Date$)
 
-[data_source, args, source_is_file, sqw_type, ndims] = parse_data_source (varargin{:});
+%First must get the file input into the correct format for
+%parse_data_source to deal with it:
+try
+    [sqw_type, nd, data_source_1, mess] = is_sqw_type_file(varargin{:});
+catch
+    error('Error reading sqw file - ensure that read called with a dummy sqw object as the first argument');
+end
+    
+[data_source, args, source_is_file, sqw_type, ndims] = parse_data_source (varargin{1},data_source_1);
 if ~isempty(args)
     error('Check number of arguments')
 end
@@ -32,7 +40,9 @@ end
 % varargout
 
 % In this case, there is only one output argument
-if source_is_file
+if source_is_file && isfield(data_source_1,'keyword') && strcmp(data_source_1.keyword,'$file_data')
+    varargout{1}=w;
+elseif source_is_file
     varargout{1}={w};
 else
     varargout{1}=w;
