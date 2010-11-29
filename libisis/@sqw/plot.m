54d04b7d16c789b991acd778be96647d39034f3d
Russell Ewings <russell.ewings@stfc.ac.uk>
2010-11-29 15:15:30 +0000

    Update a slew of sqw and dnd methods so that they do not fail when called on an array of Horace ojects refs #3
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@506 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: ed2f7e4ee573c13cc3d6a4af4bd4de045a945a21

--- libisis/@sqw/plot.m
+++ libisis/@sqw/plot.m
@@ -13,21 +13,26 @@ function [figureHandle, axesHandle, plotHandle] = plot(win,varargin)
 
 % R.A. Ewings 14/10/2008
 
-nd=dimensions(win); %find out what dimensionality dataset we have.
+for i=1:numel(win)
+    nd=dimensions(win(i)); %find out what dimensionality dataset we have.
 
-if nd==0 || nd>=4
-    error('Dataset is neither 1d, 2d, nor 3d, so cannot be plotted');
+    if nd==0 || nd>=4
+        error('Dataset is neither 1d, 2d, nor 3d, so cannot be plotted');
+    end
+
+    if nd==1 && i==1
+        [figureHandle_, axesHandle_, plotHandle_] = dp(win(i),varargin{:});
+    elseif nd==1 && i>1
+        [figureHandle_, axesHandle_, plotHandle_] = pp(win(i),varargin{:});
+    elseif nd==2
+        [figureHandle_, axesHandle_, plotHandle_] = da(win(i),varargin{:});
+    else
+        sliceomatic(win(i),varargin{:});
+    end
+
+    % Output only if requested. 
+    if nargout>=1, figureHandle=figureHandle_; end
+    if nargout>=2, axesHandle=axesHandle_; end
+    if nargout>=3, plotHandle=plotHandle_; end
+
 end
-
-if nd==1
-    [figureHandle_, axesHandle_, plotHandle_] = dp(win,varargin{:});
-elseif nd==2
-    [figureHandle_, axesHandle_, plotHandle_] = da(win,varargin{:});
-else
-    sliceomatic(win,varargin{:});
-end
-
-% Output only if requested
-if nargout>=1, figureHandle=figureHandle_; end
-if nargout>=2, axesHandle=axesHandle_; end
-if nargout>=3, plotHandle=plotHandle_; end
