81b660b1c39f971597ce659209d5c1fbc95d64c8
Russell Ewings <russell.ewings@stfc.ac.uk>
2010-12-08 10:07:54 +0000

    Modified sqw/sigvar_set method to remove logical inconsistency for the case when binary operations are performed on two sqw objects of dnd-type refs #24
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@515 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 49087943d49fd9b01a9f958f1f9161405ba63797

--- @sqw/sigvar_set.m
+++ @sqw/sigvar_set.m
@@ -15,7 +15,16 @@ w.data.s=sigvarobj.s;
 w.data.e=sigvarobj.e;
 stmp = replicate_array(w.data.s, w.data.npix)';
 etmp = replicate_array(w.data.e, w.data.npix)';
-w.data.pix(8:9,:) = [stmp;etmp]; % propagate signal into the pixel data
+
+%======
+%RAE edit 8/12/2010: logical inconsistency previously here. Consider the
+%case of adding two dnd-type sqw objects. The output created here will have
+%a pix array. Normally this does not matter, but it does matter for e.g.
+%fit_func on a dnd object.
+if is_sqw_type(w)
+    w.data.pix(8:9,:) = [stmp;etmp]; % propagate signal into the pixel data
+end
+%=====
 
 % If no pixels, then our convention is that signal and error set to zero
 nopix=(w.data.npix==0);
