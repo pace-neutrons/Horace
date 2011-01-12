e3a31013f604744fea8af4a10d9602477e18da9f
Toby Perring <toby.perring@stfc.ac.uk>
2011-01-12 13:34:50 +0000

    Correct problem with replicate if the reference object is an sqw object Documentation editing for a number of methods (not yet complete)
    refs #24
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@520 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: a5f633d8db67e561cfefd6b13a367b01f1ec2f43

--- @sqw/replicate.m
+++ @sqw/replicate.m
@@ -5,41 +5,35 @@ function wout = replicate (win,wref)
 %
 % $Revision$ ($Date$)
 
-%Do some tests on win and wref, and then loop if win is an array:
+% Do some tests on win and wref, and then loop if win is an array:
 if numel(wref)>1
     error('Reference object is an array of objects - it must be a single object for replicate to work');
 end
 
-%Initialise the output
-for i=1:numel(win)
-    wout(i)=wref;
-end
-
-for i=1:numel(win)
-    if isa(wref,'sqw') % Must handle case that wref is sqw object, but win(i) is not.
-        if isa(win(i),'d0d')||isa(win(i),'d1d')||isa(win(i),'d2d')||isa(win(i),'d3d')||isa(win(i),'d4d')
-            %wout(i) = wref;
-            wout(i).data = replicate_dnd(struct(win(i)),wref.data);
-        elseif isa(win(i),'sqw')
-            if ~is_sqw_type(win(i))
-                %wout(i) = wref;
-                wout(i).data = replicate_dnd(win(i).data,wref.data);
-            else
-                error('Replication of sqw data to higher dimensions not yet implemented. Convert to corresponding dnd object and replicate that.')
-            end
-        else
-            error('Check input arguments to replicate - first argument must be dnd object')
-        end
-    else    % must be that win(i) is sqw object and wref is not
-        if ~is_sqw_type(win(i))
-            if isa(wref,'d0d')||isa(wref,'d1d')||isa(wref,'d2d')||isa(wref,'d3d')||isa(wref,'d4d')
-                %wout(i) = sqw(wref);
-                wout(i).data = replicate_dnd(win(i).data,struct(wref));
-            else
-                error('Check input arguments to replicate - first argument must be dnd object')
-            end
+% All elements of win have the same class, so do the loop over elements at the innermost line, not the outermost
+if isa(wref,'sqw') % Must handle case that wref is sqw object, but win is not.
+    if isa(win,'d0d')||isa(win,'d1d')||isa(win,'d2d')||isa(win,'d3d')||isa(win,'d4d')
+        wout = repmat(wref,size(win)); % do not use for loop to initialise output - very inefficient!
+        for i=1:numel(win), wout(i).data = replicate_dnd(struct(win(i)),wref.data); end
+    elseif isa(win,'sqw')
+        if ~is_sqw_type(win)
+            wout = repmat(wref,size(win));
+            for i=1:numel(win), wout(i).data = replicate_dnd(win(i).data,wref.data); end
         else
             error('Replication of sqw data to higher dimensions not yet implemented. Convert to corresponding dnd object and replicate that.')
         end
+    else
+        error('Check input arguments to replicate - first argument must be dnd object')
+    end
+else    % must be that win is sqw object and wref is not
+    if ~is_sqw_type(win)
+        if isa(wref,'d0d')||isa(wref,'d1d')||isa(wref,'d2d')||isa(wref,'d3d')||isa(wref,'d4d')
+            wout = repmat(sqw(wref),size(win));
+            for i=1:numel(win), wout(i).data = replicate_dnd(win(i).data,struct(wref)); end
+        else
+            error('Check input arguments to replicate - first argument must be dnd object')
+        end
+    else
+        error('Replication of sqw data to higher dimensions not yet implemented. Convert to corresponding dnd object and replicate that.')
     end
 end
