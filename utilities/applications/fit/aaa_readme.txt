46d495b90d844900bf3020b1161ebdbbaa77998b
Toby Perring <toby.perring@stfc.ac.uk>
2011-04-04 06:56:26 +0000

    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@22 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

--- utilities/applications/fit/aaa_readme.txt
+++
@@ -1,3 +0,0 @@
-The function 'fit' is included for backwards compatibility with older user programs
-that perform fitting. It is superceeded by multifit, which is much more flexible and
-has a number of significant improvements.
--- utilities/applications/multifit/private/arraystr.m
+++
@@ -1,9 +0,0 @@
-function str=arraystr(sz,i)
-% Make a string of the form '[2,3,1]' from a size array and single index
-ind=cell(1,numel(sz));
-[ind{:}]=ind2sub(sz,i);
-str='[';
-for j=1:numel(ind)
-    str=[str,num2str(ind{j}),','];
-end
-str(end:end)=']';
