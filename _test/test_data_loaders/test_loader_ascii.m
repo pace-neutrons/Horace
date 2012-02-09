c888bb16376ee8009f1af233bf97541508ad972e
Alex Buts <alex.buts@stfc.ac.uk>
2012-02-09 21:26:40 +0000

    minor changes to unit tests to pass on Liunx
    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@142 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

--- _test/test_data_loaders/test_loader_ascii.m
+++ _test/test_data_loaders/test_loader_ascii.m
@@ -63,7 +63,7 @@ classdef test_loader_ascii< TestCase
             set(herbert_config,'use_mex',1);
             [par,loader] = load_par(loader,'demo_par.par','-horace');
             set(herbert_config,'use_mex',old_state);
-            assertEqual(loader.par_file_name,'demo_par.par');            
+            assertEqual(loader.par_file_name,'demo_par.PAR');            
             
             assertTrue(all(ismember({'filename','filepath','x2','phi','azim','width','height','group'},fields(par))));
             assertTrue(all(ismember(fields(par),{'filename','filepath','x2','phi','azim','width','height','group'})));            
@@ -77,7 +77,7 @@ classdef test_loader_ascii< TestCase
             set(herbert_config,'use_mex',0);
             [par,loader] = load_par(loader,'demo_par.par','-hor');
             set(herbert_config,'use_mex',old_state);
-            assertEqual(loader.par_file_name,'demo_par.par');            
+            assertEqual(loader.par_file_name,'demo_par.PAR');            
             
             assertTrue(all(ismember({'filename','filepath','x2','phi','azim','width','height','group'},fields(par))));
             assertTrue(all(ismember(fields(par),{'filename','filepath','x2','phi','azim','width','height','group'})));            
@@ -94,7 +94,7 @@ classdef test_loader_ascii< TestCase
         function test_mslice_par(this)                       
             [par,loader]=load_par(loader_ascii,'demo_par.par');          
             assertEqual([6,28160],size(par));
-            assertEqual(loader.par_file_name,'demo_par.par');            
+            assertEqual(loader.par_file_name,'demo_par.PAR');            
         end 
 % DEFINED FIELDS        
         function test_spe_fields_defined(this)
@@ -107,7 +107,7 @@ classdef test_loader_ascii< TestCase
             [par,loader]=load_par(loader,'demo_par.par');
             fields = defined_fields(loader);
             assertEqual({'det_par'},fields);
-            assertEqual(loader.par_file_name,'demo_par.par');
+            assertEqual(loader.par_file_name,'demo_par.PAR');
        end
         function test_all_fields_defined(this)
             loader=loader_ascii('spe_wrong.spe','demo_par.par');
