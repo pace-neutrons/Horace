55721b4a9b3eba63c7932b1a50057abce7ca1e56
Alex Buts <alex.buts@stfc.ac.uk>
2012-02-10 13:54:05 +0000

    refs #24 minor modifications to use_herbert.m
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@602 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: dd122df70a31fd51f8bdd8929585977f8e046998

--- admin/use_herbert.m
+++ admin/use_herbert.m
@@ -1,14 +1,27 @@
 function use_herbert(on_off)
-% utility to switch herbert package off or on
+% utility to switch between herbert and libisis package to use in Horace
 %
+%Usage:
 %>> use_herbert(['on'/'off'])
-%switches herbert on if used without arguments
-%>> use_herbert('off') 
-%switches herbert off and initates libisis
 %
-%works only when packages were registered into toolbox area using
-%install_isis('Package_name') script
 %
+%>>use_herbert('off') or 
+%>>use_herbert off 
+%    switches herbert off and initates libisis, reinitiates Horace
+%>>use_herbert on    
+%>>use_herbert('on')
+%>>use_herbert
+%   switches libisis off and initates herbert, reinitiates Horace
+%
+% works only when packages were registered into toolbox area using
+% install_isis('Package_name') script or in other sityations 
+% when libisis_on and herebrt_on
+% utilites are registered to be always on the path. 
+%
+%
+% $Revision: 1853 $  ($Date: 2009-10-01 16:02:29 +0100 (Thu, 01 Oct 2009) $)
+%
+
 hor_root=fileparts(which('horace_init.m'));
 if nargin == 0 || (nargin == 1 && strncmpi(on_off,'on',2))
     try
@@ -18,7 +31,8 @@ if nargin == 0 || (nargin == 1 && strncmpi(on_off,'on',2))
 	try
 		herbert_on();    
 	catch
-		error('use_herbert:wrong_arguments','this function relies on herbert_on function to be permanently availible on path and apparently it is not');
+		error('use_herbert:wrong_arguments',...
+             'this function relies on herbert_on function to be permanently availible on path and apparently it is not');
 	end
     rmpath(fullfile(hor_root,'libisis'));
     addpath(fullfile(hor_root,'herbert'));
