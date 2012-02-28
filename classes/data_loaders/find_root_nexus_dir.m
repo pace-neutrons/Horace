2714f06672f617e463bd96c4e27e7920128d2585
Alex Buts <alex.buts@stfc.ac.uk>
2012-02-28 11:03:24 +0000

    refs pace-neutrons/Herbert#2 better comments to find_root_nexus_dir.m function
    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@166 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

--- classes/data_loaders/find_root_nexus_dir.m
+++ classes/data_loaders/find_root_nexus_dir.m
@@ -1,14 +1,29 @@
 function [root_nxspe_path,data_version,data_structure] = find_root_nexus_dir(hdf_fileName,nexus_application_name)
-% function identifies the path to the root folder correspondent data file
-% for further usage by hdf5read/hdf5write functions
-%e.g. 
+% function identifies the path to the root folder in the input NeXus data file
+% This folder can be later used in path for data, read by hdf5read/hdf5write functions
 %
+%Usage:
 %>>root_folder=find_root_nexus_dir(nexus_file_name,'NXSPE')
-%                  -- obtain the position of an nxspe data if they are present in current nexus file
-%                    -- if the data are absent, function returns empty
-%                       string
-%         
-% The root folder is a folder where all nxspe data are related to. 
+%             -- returns the path to a nxspe data if they are present in current NeXus file
+%             -- if the data are absent, function returns empty  string
+%Inputs:
+% hdf_fileName           -- the name of hdf5 NeXus file 
+% nexus_application_name -- the name of the NeXus application (in NeXus
+%                            sence), which describes specific data
+%                            structure. The procedure has been tested on
+%                            NXSPE data (application) though should work
+%                            for any data organized as NeXus application
+%
+%Outputs:
+%root_folder   --    The root folder is a hdf5 folder where all application's
+%                    (nxspe) data are related to. 
+%data_version  --     the version of the NeXus application's data (may
+%                      define the data format)
+%data_structure --   The structure  describes the arrangement of the hdf5 
+%                    data folders and attribures within the file hdf5 file. 
+%                    This structure can be (and was) obtained by hdf5info
+%                    function and returned by this procedure for
+%                    efficiency reasons. 
 %
 % $Author: Alex Buts; 20/10/2011
 %
@@ -16,7 +31,7 @@ function [root_nxspe_path,data_version,data_structure] = find_root_nexus_dir(hdf
 %
 %
 data_structure =  hdf5info(hdf_fileName,'ReadAttributes',true);
-groups = data_structure.GroupHierarchy.Groups(:);
+groups         = data_structure.GroupHierarchy.Groups(:);
 
 n_nxspe_entries=0;
 nxspe_folders=cell(1,1);
@@ -30,7 +45,7 @@ for i=1:numel(groups)
                 definition = hdf5read(hdf_fileName,nexus_folder.Datasets(j).Name);
                 if strcmp(definition.Data,nexus_application_name)
                     n_nxspe_entries=n_nxspe_entries+1;
-                    nxspe_folders{n_nxspe_entries}=nexus_folder.Name;
+                    nxspe_folders{n_nxspe_entries}= nexus_folder.Name;
                     nxspe_version{n_nxspe_entries}= read_nxspe_version(hdf_fileName,nexus_folder.Datasets(j));
                 end
             end
