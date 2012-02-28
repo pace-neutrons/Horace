4cd57c92b620ab72c688279d405451fc89a1c874
Alex Buts <alex.buts@stfc.ac.uk>
2012-02-28 10:47:57 +0000

    refs pace-neutrons/Herbert#16 modifications to config_file_name to allow it working with Matlab 2007b
    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@165 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

--- configuration/@config/private/config_file_name.m
+++ configuration/@config/private/config_file_name.m
@@ -3,8 +3,15 @@ function file_name = config_file_name (config_name)
 
 % $Revision$ ($Date$)
 
-
-root_config_name = mfilename('class');
+%--> the block to provide compartibility between matlab 2008a and 2007b where
+% mfilename behaviour changes
+[fd,ff]=fileparts(mfilename('class'));
+if isempty(fd) 
+    root_config_name = ff;
+else
+    root_config_name = fd;
+end
+%<--
 fetch_default=false;
 config_data = config_store(root_config_name,fetch_default);
 file_name=fullfile(config_data.config_folder_path, [config_name,'.mat']);
