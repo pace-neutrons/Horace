b8c718c7ef0e0dec91edf35e35963f6d8e17fa1a
Toby Perring <toby.perring@stfc.ac.uk>
2012-07-02 14:12:20 +0000

    IMprove tests of change crystal orientation
    refs #43
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@636 1cb955da-4463-431d-88dd-2af2f5550de3

--- test/test_change_crystal/test_rlu_corr_to_lattice.m
+++ test/test_change_crystal/test_rlu_corr_to_lattice.m
@@ -1,4 +1,6 @@
 function test_rlu_corr_to_lattice
+% This function can be used to test the private function rlu_corr_to_lattice, but only
+% by changing the working directly to the location of 
 
 eps=1e-12;
 
@@ -15,7 +17,15 @@ rotmat=rotvec_to_rotmat2(rotvec);
 rlu_corr=b\rotmat*b0;
 
 % Now test:
-[alatt_out,angdeg_out,rotmat_out]=rlu_corr_to_lattice(rlu_corr,alatt0,angdeg0);
+tmp=which('rlu_corr_to_lattice','-all');
+funcfolder=fileparts(tmp{1});
+current_path=pwd;
+try
+    cd(funcfolder)
+    [alatt_out,angdeg_out,rotmat_out]=rlu_corr_to_lattice(rlu_corr,alatt0,angdeg0);
+catch
+end
+cd(current_path);
 
 if max(abs(alatt_out(:)-alatt(:)))>eps, error('Lattice parameters not recovered'), end
 if max(abs(angdeg_out(:)-angdeg(:)))>eps, error('Lattice angless not recovered'), end
