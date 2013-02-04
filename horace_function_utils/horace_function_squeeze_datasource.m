2ac10c40071f30df4d3d4771e93f43af8ced0002
Toby Perring <toby.perring@stfc.ac.uk>
2013-02-04 13:47:18 +0000

    Add refine_crystal as a method of Horace data objects
    refs #116
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@677 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 5688f4e36a72d6fe0bf9d18b628606b3cc15b0a5

--- horace_function_utils/horace_function_squeeze_datasource.m
+++ horace_function_utils/horace_function_squeeze_datasource.m
@@ -28,17 +28,14 @@ elseif is_horace_data_object(w)
     if ~isempty(mess)
         error(mess)
     end
+elseif is_filename(w)
+    if ~iscellstr(w)    
+        wout=cellstr(w);
+    else
+        wout=w;
+    end
 else
     error('Invalid data source')
-end
-
-%------------------------------------------------------------------------------------------------------------
-function ok=is_horace_data_object(w)
-% Determine if an argument is a Horace object (sqw, d0d, d1d, d2d, d3d, d4d)
-if isa(w,'sqw')||isa(w,'d0d')||isa(w,'d1d')||isa(w,'d2d')||isa(w,'d3d')||isa(w,'d4d')
-    ok=true;
-else
-    ok=false;
 end
 
 %------------------------------------------------------------------------------------------------------------
