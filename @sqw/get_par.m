3faae6eb638c27d48bc951da51f1ba5549ee39d3
Toby Perring <toby.perring@stfc.ac.uk>
2013-02-22 10:21:24 +0000

    Correct an error in help for functions that document detector structure
    refs #116
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@683 1cb955da-4463-431d-88dd-2af2f5550de3

--- @sqw/get_par.m
+++ @sqw/get_par.m
@@ -11,8 +11,8 @@ function det=get_par(this,filename,varargin)
 % data has following fields:
 %   det.filename    Name of file excluding path
 %   det.filepath    Path to file including terminating file separator
-%   det.x2          Secondary flightpath (m)
 %   det.group       Row vector of detector group number - assumed to be 1:ndet
+%   det.x2          Secondary flightpath (m)
 %   det.phi         Row vector of scattering angles (deg)
 %   det.azim        Row vector of azimuthal angles (deg)
 %                  (West bank=0 deg, North bank=90 deg etc.)
