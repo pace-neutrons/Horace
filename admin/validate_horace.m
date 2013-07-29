bc2d04acfe756e6c0c84717bafe9c3b6910882cd
Toby Perring <toby.perring@stfc.ac.uk>
2013-07-29 07:30:44 +0000

    Edit validate_horace to prepare for more systematic testing of Horace across Herbert and Libisis
    refs #76
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/branches/HoraceV3.0.0@721 1cb955da-4463-431d-88dd-2af2f5550de3

--- admin/validate_horace.m
+++ admin/validate_horace.m
@@ -1,46 +1,56 @@
 function  validate_horace()
-% function intended to run unit tests after successfull horace
-% installation to be sure that everything has been installed fine. 
+% Run unit tests on Horace installation
+%
+%   >> validate_horace
+%
+% This function will run the Horace unit tests with either Herbert or Libisis as the
+% underlying core utilities library.
+%
+% If an error is encountered during the test procedure, then the unit
+% test application folder will remain on the path, the Horace informational
+% output level, and the Matlab warning state will still be off. The state
+% of Herbert or Libisis, and Horace, will not be the same in
+% consequence. However, if tests are com,pleted succesfully, then the
+% unit test application will be removed and the warnings set to 'on'.
 
-herbert_enabled=true;
-her_path = fileparts(which('herbert_init'));
-if isempty(her_path)
-    herbert_enabled= false;
-end
 
-% if we can found herbert which is needed as source of unit tests utilites
-her_path=fileparts(which('herbert_init'));
-if ~isempty(her_path)
-	herbert_found  =true;
-else
-    herbert_found =false;
-end
-
-test_pack_path='';
-if herbert_found
-    if herbert_enabled
+% Look for Herbert which is needed as source of unit tests utilities
+try
+    using_herbert=is_herbert_used;
+    if using_herbert
+        rootpath = fileparts(which('herbert_init'));
         if ~get(herbert_config,'init_tests')
-            test_pack_path= fullfile(her_path,'_test/matlab_xunit/xunit');
-            addpath(test_pack_path);
+            xunit_path= fullfile(rootpath,'_test/matlab_xunit/xunit');
+            addpath(xunit_path);
+        else
+            xunit_path='';
         end
     else
-    	test_pack_path= fullfile(her_path,'_test/matlab_xunit/xunit');
-        addpath(test_pack_path);
-        
+        rootpath = fileparts(which('libisis_init'));
+        if ~get(libisis_config,'init_tests')
+            xunit_path= fullfile(rootpath,'matlab_xunit/xunit');
+            addpath(xunit_path);
+        else
+            xunit_path='';
+        end
     end
-else
-    error('VALIDATE_HORACE:wrong_call','can not identify the unit test utilites folder');
+catch
+    error('VALIDATE_HORACE:wrong_call','Cannot identify the unit test utilites folder')
 end
-% path to unit tests:
-hor_path = fileparts(which('horace_init'));
-test_path=fullfile(hor_path,'test');
 
+% Get path to unit tests:
+horace_path = fileparts(which('horace_init'));
+test_path=fullfile(horace_path,'test');
+
+% Run unit tests
 warning off all;
-info_level = get(hor_config,'horace_info_level');
-set(hor_config,'horace_info_level',0);
-% test package specific folders
-if herbert_enabled
-else
+info_level = get(hor_config,'horace_info_level');   % store current Horace informational output level
+set(hor_config,'horace_info_level',0);              % turn off Horace informational output
+ 
+%==============================================================================
+% Place call to tests here
+% -----------------------------------------------------------------------------
+if ~using_herbert   % test data loaders only if libisis
 	runtests(fullfile(test_path,'test_data_loaders'));
 end
 %runtests(fullfile(test_path,'test_admin'));
@@ -50,10 +60,12 @@ runtests(fullfile(test_path,'test_transformation'));
 % test generic horace part
 %runtests(fullfile(test_path,'test_horace'));
 
+%==============================================================================
+
 warning on all;
 set(hor_config,'horace_info_level',info_level);
 
-if ~isempty(test_pack_path)
-    rmpath(test_pack_path);
+% Remove unit test application if was not present already
+if ~isempty(xunit_path),
+    rmpath(xunit_path);
 end
-
