58b03a40dcef949f341c52b8bbfa87b9b97d596e
Alex Buts <alex.buts@stfc.ac.uk>
2013-10-04 08:27:16 +0000

    refs #24 attempt to fix svn horace hook (it has not been updated for ages)
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@732 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 45783d161b37081a0605ae82134cc33f3344a796

--- admin/tortoseHoracehook.bat
+++ admin/tortoseHoracehook.bat
@@ -1,5 +1,6 @@
 @echo off
 set SCRDIR=%CD%
 rem pause
-"%MATLABDIR%\sys\perl\win32\bin\perl.exe" "%SCRDIR%\..\Libisis_Dev\Libisis\ISIS_utilities\svn_matlab_commit_hook.pl" "%SCRDIR%\horace_version.m"
+"%MATLABDIR%64\sys\perl\win32\bin\perl.exe" "%SCRDIR%\svn_matlab_commit_hook.pl" "%SCRDIR%\horace_version.m"
+rem pause
 exit 0;
