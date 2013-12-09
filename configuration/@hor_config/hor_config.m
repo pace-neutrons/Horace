2bc53b3ca87d131deba8f52d5f5fdd043341379f
Toby Perring <toby.perring@stfc.ac.uk>
2013-12-09 12:01:57 +0000

    trivial change to commenting
    refs #131
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@814 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 2c024d2acb06fff307af85456df58d004b87c2e0

--- configuration/@hor_config/hor_config.m
+++ configuration/@hor_config/hor_config.m
@@ -8,7 +8,7 @@ function this=hor_config()
 %   >> set(hor_config,'name1',val1,'name2',val2,...)
 %
 % To fetch values:
-%   >> [val1,val2,...]=get(hor_config,'name1''name2',...)
+%   >> [val1,val2,...]=get(hor_config,'name1','name2',...)
 %
 %
 % Fields are:
