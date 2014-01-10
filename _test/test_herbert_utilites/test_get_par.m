32684b52555895b7acd5d04cf3d4a8deea5982ef
Alex Buts <alex.buts@stfc.ac.uk>
2014-01-10 22:23:09 +0000

    refs #93 removed deprecated -hor option from unit tests
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@820 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: dbde15afe05c51f71c09719b5f2214cf1c5efd11

--- _test/test_herbert_utilites/test_get_par.m
+++ _test/test_herbert_utilites/test_get_par.m
@@ -1,49 +1,52 @@
 classdef test_get_par< TestCase
-% 
-% $Revision$ ($Date$)
-%
+    %
+    % $Revision$ ($Date$)
+    %
     
-    properties 
+    properties
     end
-    methods       
-        % 
+    methods
+        %
         function this=test_get_par(name)
             this = this@TestCase(name);
         end
         % tests themself
-        function test_wrong_file_name(this)               
-            f = @()get_par('non-existent-file-name');            
-            assertExceptionThrown(f,'GET_PAR:invalid_argument');
-        end               
+        function test_wrong_file_name(this)
+            f = @()get_par('non-existent-file-name');
+            assertExceptionThrown(f,'LOADERS_FACTORY:get_loader');
+        end
         function test_get_par_from_ASCII(this)
-            par = get_par('one2one_112.par');
+            par = get_par('one2one_112.par','-array');
             assertEqual([6,69632],size(par));
-        end               
+        end
         function test_gethor_par_from_ASCII(this)
-            par = get_par('one2one_112.par','-hor');
+            par = get_par('one2one_112.par');
             assertTrue(isstruct(par));
             assertTrue(isfield(par,'x2'));
             assertTrue(isfield(par,'phi'));
-            assertTrue(isfield(par,'azim'));            
-            assertTrue(isfield(par,'width'));                        
-            assertTrue(isfield(par,'height')); 
+            assertTrue(isfield(par,'azim'));
+            assertTrue(isfield(par,'width'));
+            assertTrue(isfield(par,'height'));
             
-            parar=get_par('one2one_112.par');
+            parar=get_par('one2one_112.par','-nohor');
             assertTrue(all(parar(3,:)==par.azim));
-        end               
-        function test_wrong_data_format_ignored(this)                       
-            par = get_par('one2one_112.par');
+        end
+        function test_wrong_data_format_ignored(this)
+            par = get_par('one2one_112.par','-nohor');
+            assertTrue(~isstruct(par));
             assertEqual([6,69632],size(par));
         end
         function test_get_par_nxspe(this)
-        	parar = get_par('MAR11001_test.nxspe');
-			assertEqual([6,285],size(parar));      
+            parar = get_par('MAR11001_test.nxspe','-nohor');
+            assertTrue(~isstruct(parar));
             
-        	par = get_par('MAR11001_test.nxspe','-hor');
-            assertTrue(all(parar(3,:)== par.azim));            
+            assertEqual([6,285],size(parar));
+            
+            par = get_par('MAR11001_test.nxspe');
+            assertTrue(all(parar(3,:)== par.azim));
         end
-  
-
+        
+        
     end
 end
 
