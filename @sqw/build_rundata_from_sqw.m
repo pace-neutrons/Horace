0443fb95af089e366922cda3fcb09747d9cddc78
Alex Buts <alex.buts@stfc.ac.uk>
2014-03-13 11:42:01 +0000

    refs #24 function which generates rundata from sqw, modified demo which uses it and unit test for this function.
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@832 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: be74db7b822db90ec05cf0118c419f9193f2056d

---
+++ @sqw/build_rundata_from_sqw.m
@@ -0,0 +1,56 @@
+function rd=build_rundata_from_sqw(sqw)
+% function builds rundata object from sqw object
+%  
+%
+data = sqw.data;
+header = sqw.header;
+detpar = sqw.detpar;
+[dummy,filename] = fileparts(sqw.main_header.filename);
+%
+en     = header.en;
+ne=numel(en)-1;    % number of energy bins
+ndet0=numel(detpar.group);% number of detectors
+
+tmp=data.pix(6:9,:)';     % columns are: det number, energy bin number, signal, error
+tmp=sortrows(tmp,[1,2]);  % order by detector group number, then energy
+group=unique(tmp(:,1));   % unique detector group numbers in the data in numerical increasing order
+
+% Now check that the data is complete i.e. no missing pixels
+if size(tmp,1)~=ne*numel(group)
+    error('Data for one or more energy bins is missing in the sqw data')
+end
+
+% Get the indexing of detector group in the detector information
+[lia,ind]=ismember(group,detpar.group);
+
+signal=NaN(ne,ndet0);
+err=zeros(ne,ndet0);
+signal(:,ind)=reshape(tmp(:,3),ne,numel(group));
+err(:,ind)=sqrt(reshape(tmp(:,4),ne,numel(group)));
+
+
+lattice = oriented_lattice();
+lattice.alatt = header.alatt;
+lattice.angdeg = header.angdeg;
+lattice.u      = header.cu;
+lattice.v      = header.cv;
+lattice.psi    = header.psi*(180/pi);
+lattice.omega = header.omega*(180/pi);
+lattice.dpsi  = header.dpsi*(180/pi);
+lattice.gl    = header.gl*(180/pi);
+lattice.gs    = header.gs*(180/pi);
+
+rd = rundata();
+
+rd.lattice = lattice;
+rd.det_par = detpar;
+
+rd.emode   = header.emode;
+
+rd.efix = header.efix;
+rd.en  = en;
+rd.S   = signal;
+rd.ERR = err;
+
+
+
---
+++ _test/test_sqw/test_build_rundata_from_sqw.m
@@ -0,0 +1,64 @@
+classdef test_build_rundata_from_sqw < TestCase
+    % Series of tests to check work of mex files against Matlab files
+    
+    properties
+        out_dir=tempdir();
+        
+        en=-80:8:760;
+        par_file='map_4to1_dec09.par';
+        sqw_file_single='test_build_rundata_from_sqw.sqw';
+        efix=800;
+        emode=1;
+        alatt=[2.87,2.87,2.87];
+        angdeg=[90,90,90];
+        u=[1,0,0];
+        v=[0,1,0];
+        omega=1;dpsi=2;gl=3;gs=4;
+        
+        psi=4;
+        
+        sqw_obj;
+    end
+    
+    methods
+        function this=test_build_rundata_from_sqw (name)
+            this = this@TestCase(name);
+            root_dir = fileparts(which('horace_init.m'));
+            data_dir = fullfile(root_dir,'_test','common_data');
+            this.sqw_file_single = fullfile(this.out_dir,this.sqw_file_single);
+            this.par_file = fullfile(data_dir,this.par_file);
+            
+            fake_sqw(this.en, this.par_file, this.sqw_file_single, this.efix,...
+                this.emode, this.alatt, this.angdeg,...
+                this.u, this.v, this.psi, this.omega, this.dpsi, this.gl, this.gs);
+            
+            this.sqw_obj = read_sqw(this.sqw_file_single);
+            delete(this.sqw_file_single);
+        end
+        
+        function this=test_build_rundata(this)
+
+            
+            rd = build_rundata_from_sqw(this.sqw_obj);
+            
+            assertEqual(rd.emode, this.emode);
+            assertEqual(rd.efix, this.efix);
+            
+            lattice = rd.lattice;
+            assertElementsAlmostEqual(lattice.alatt,this.alatt,'absolute',2e-7);
+            assertElementsAlmostEqual(lattice.angdeg,this.angdeg);
+            assertEqual(lattice.u,this.u);
+            assertEqual(lattice.v,this.v);
+            assertElementsAlmostEqual(lattice.psi,this.psi,'absolute',2e-7);
+            assertElementsAlmostEqual(lattice.omega,this.omega,'absolute',2e-7);
+            assertElementsAlmostEqual(lattice.dpsi,this.dpsi,'absolute',2e-7);
+            assertElementsAlmostEqual(lattice.gl,this.gl,'absolute',2e-7);
+            assertElementsAlmostEqual(lattice.gs,this.gs,'absolute',2e-7);
+            
+            det = get_par(this.par_file);
+            det_par = rd.det_par;
+            assertEqual(det_par,det);
+            
+        end
+    end
+end
