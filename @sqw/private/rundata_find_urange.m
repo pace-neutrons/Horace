d81f2fd490fa79befebb97083d77cdc4e5fff291
Toby Perring <toby.perring@stfc.ac.uk>
2014-05-24 14:06:09 +0000

    Changes to diagnostic output of timings
    refs #24
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@862 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 31034a152983b322a52254309eb8cf5debb6454e

--- @sqw/private/rundata_find_urange.m
+++ @sqw/private/rundata_find_urange.m
@@ -12,13 +12,17 @@ function urange = rundata_find_urange(run_files)
 %   urange    	2x4 array, describing min-max values in momentum/energy
 %              transfer, in crystal Cartesian coordinates and meV. Uses bin centres.
 
+
 nfiles = numel(run_files);
-if get(hor_config,'horace_info_level')>-1
-	disp('--------------------------------------------------------------------------------')
-	disp(['Calculating limits of data from ',num2str(nfiles),' spe files...'])
+
+horace_info_level=get(hor_config,'horace_info_level');
+if horace_info_level>-1
+    disp('--------------------------------------------------------------------------------')
+    disp(['Calculating limits of data from ',num2str(nfiles),' spe files...'])
 end
 
 % Get the maximum limits along the projection axes across all spe files
+bigtic;
 data.filename='';
 data.filepath='';
 urange=[Inf, Inf, Inf, Inf;-Inf,-Inf,-Inf,-Inf];
@@ -38,4 +42,8 @@ for i=1:nfiles
     end
     [u_to_rlu, ucoords] = calc_projections (efix, emode, alatt, angdeg, u, v, psi, omega, dpsi, gl, gs, data, det);
     urange = [min(urange(1,:),min(ucoords,[],2)'); max(urange(2,:),max(ucoords,[],2)')];
+end
+
+if horace_info_level>-1
+    bigtoc('Time to compute limits:',horace_info_level);
 end
