df0c4d1b850cdb5e56614567c5c74bf52fcd3c17
Toby Perring <toby.perring@stfc.ac.uk>
2014-10-23 14:50:54 +0000

    Additional functionality to writing out integer arrays as strings
    refs pace-neutrons/Herbert#5
    
    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@402 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

--- utilities/strings/str_compress.m
+++ utilities/strings/str_compress.m
@@ -1,7 +1,20 @@
 function strout=str_compress(str,subst)
-% Trim a string and replace any continuous internal whitespace with a single instance of the substitution string
-% Default is to substitute a single space.
+% Trim a string or cellarray of strings and substitute internal whitespace
 %
+%   >> strout=str_compress(str)
+%   >> strout=str_compress(str,subst)
+%
+% Input:
+% ------
+%   str     String, or cell array of strings
+%   subst   Character string  to replace continuous internal whitespace
+%           If not given, substitute a single space
+%
+% Output:
+% -------
+%   strout  Output string or cell array of strings
+%
+% EXAMPLES
 %   >> str='   hello    mister   man ';
 %   >> strout=str_compress(str)
 %           'hello mister man'
@@ -13,13 +26,31 @@ function strout=str_compress(str,subst)
 if nargin==1
     subst=' ';
     nsub=1;
-elseif ischar(subst) && size(subst,1)==1
-    strout=subst;
+elseif ischar(subst) && size(subst,1)==1 && size(subst,2)>0
     nsub=length(subst);
 else
     error('Check substitution string is a row of characters')
 end
 
+if ischar(str)
+    if size(str,1)~=1
+        error('Input is not a character string or cell array of character strings')
+    end
+    strout=str_compress_single(str,subst,nsub);
+elseif iscellstr(str)
+    strout=cell(size(str));
+    for i=1:numel(str)
+        if size(str{i},1)~=1
+            error('Input is not a character string or cell array of character strings')
+        end
+        strout{i}=str_compress_single(str{i},subst,nsub);
+    end
+else
+    error('Check input is a string or cell array of strings')
+end
+
+%------------------------------------------------------------------------------
+function strout=str_compress_single(str,subst,nsub)
 strout=subst;
 while ~isempty(str)
     [tok,str]=strtok(str);
