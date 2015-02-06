6eefef286b421f598a231a3e2e32fb8af5381ec5
Toby Perring <toby.perring@stfc.ac.uk>
2015-02-06 18:08:59 +0000

    Further changes to functions that find bins that contribute to a cut.Verified that they will work correctly for non-orthogonal axes.
    refs #116 #24
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@963 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: b3f1bdd13748dbdf64fe3160f5546dbde8de460c

--- @sqw/private/get_nrange_rot_section.m
+++ @sqw/private/get_nrange_rot_section.m
@@ -4,65 +4,86 @@ function [nstart,nend] = get_nrange_rot_section (urange,rot,trans,nelmts,varargi
 % inside a hypercuboid volume that on the first three axes can be rotated and
 % translated w.r.t. to the hypercuboid that is split into bins.
 %
-% Output will not necessarily be strictly contiguous blocks, as the routine handles the first
-% three dimensions separately from the following ones. The blocks are contiguous within
-% the first three dimensions, however.
-%
-%   >> [nstart,nend] = get_nrange (urange,rot,trans,nelmts,,p1,p2,p3,...)
+%   >> [nstart,nend] = get_nrange (urange,rot,trans,nelmts,p1,p2,p3,...)
 % 
 % Input:
-%   urange(2,n)     Range to cover: 2 x n array of [urange_lo; urange_hi]
-%   rot             Matrix [3x3]     --|  that relate a vector expressed in the
-%   trans           Translation [3x1]--|  frame of the bin boundaries to those of urange:
-%                                             r'(i) = A(i,j)(r(j) - trans(j))
-%                                  (trans is the vector from the origin of the frame
-%                                   in which the bins are expressed to that in which
-%                                   urange is expressed)
-%   nelmts          Array of number of points in n-dimensional array of bins
-%                       e.g. 3x5x7 array such that nelmts(i,j,k) gives no. points in (i,j,k)th bin
-%   p1(:)           Bin boundaries along first axis
-%   p2(:)           Similarly axis 2
-%   p3(:)           Similarly axis 3
-%    :                      :
+% ------
+%   urange  Range to cover: 2 x n array of upper and lower limits
+%          [urange_lo; urange_hi] (in units of a coordinate frame
+%          that is rotated shifted with respect to that frame in
+%          which the bin boundaries p1,p2,p3 are expressed, see below)
+%   rot     Matrix, A     --|  that relate the two coordinate frames
+%   trans   Translation, T--|  of the bin boundary arrays and of urange:
+%              r'(i) = A(i,j)(r(j) - T(j))
+%          where r(j) are the coordinates of a vector in the frame of the
+%          bin boundaries p1,p2,p3 (below), and r'(i) are the coordinates
+%          of the frame in which urange is expressed.
+%           Note that T is given in coordinates of the axes of the bin
+%          boundaries.
+%   nelmts  Array of number of points in n-dimensional array of bins
+%          e.g. 3x5x7 array such that nelmts(i,j,k) gives no. points in (i,j,k)th bin
+%   p1(:)   Bin boundaries along first axis (column vector)
+%   p2(:)   Similarly axis 2
+%   p3(:)   Similarly axis 3
+%    :              :
 %
 % Output:
-%   nstart          Array of starting values of contiguous block in nelmts(:)
-%   nend            Array of finishing values
+% -------
+%   nstart  Column vector of starting values of contiguous blocks in
+%          the array of values with the number of elements in a bin
+%          given by nelmts(:).
+%   nend    Column vector of finishing values.
 %
-%   If the region defined by urange lies outside the bins, or there are no elements in the range (i.e. the
-%   bins that are in the range contain no elements) then nstart and nend returned as empty array [].
+%           nstart and nend have column length zero if there are no
+%          elements i.e. have the value zeros(0,1).
+%
+% Output will not necessarily be strictly contiguous blocks, as the routine
+% handles the first three dimensions separately from the following ones. The
+% blocks are contiguous within the first three dimensions, however.
+% This should be straightforward to improve, following the algorithm of
+% get_nrange
 
 
-% T.G.Perring 03/07/2007
+% Original author: T.G.Perring
+%
+% $Revision$ ($Date$)
+
 
 % Default return values
-nstart=[];
-nend=[];
+nstart=zeros(0,1);
+nend=zeros(0,1);
 
 % Check number of input arguments
-ndim=length(varargin);
-if ndim<3; error('Must have at least three dimensions'); end
-dims=zeros(1,ndim);
-for i=1:ndim
-    dims(i)=length(varargin{i})-1;
+nd=numel(varargin);
+if nd<3; error('Must have at least three dimensions'); end
+nbin=zeros(1,nd);
+for i=1:nd
+    nbin(i)=numel(varargin{i})-1;
+end
+if any(nbin<1)
+    error('Must have at least one bin along each axis')
+end
+
+% Check consistency of lengths of p arrays and size of nelmts
+dims = size(nelmts);
+ndim = numel(dims);
+if ndim<3
+    error('Array ''nelmts'' must have at least three dimensions')
+elseif ~( (ndim<nd && all(nbin(1,ndim+1:nd)==1) && all(nbin(2,ndim+1:nd)==1)) || ... % allow for outer p{i} being singleton dimensions
+      (ndim==nd && all(nbin==dims)) )
+    error('Dimensions of number array and number and/or length of bin boundary arrays inconsistent')
 end
 
 % Check dimensions of ranges and bin boundaries are consistent
 if size(urange,2)~=ndim
     error('Dimension of range and number of bin boundaries arrays are inconsistent')
-end
-
-% Check consistency of lengths of p arrays and size of nelmts
-dims_nelmts = size(nelmts);
-ndim_nelmts = length(dims_nelmts);
-if ~( (ndim_nelmts<ndim && all(dims(ndim_nelmts+1:ndim)==1)) || ... % allow for outer p{i} being singleton dimensions
-      (ndim_nelmts==ndim && all(dims==dims_nelmts)) )
-    error('Dimensions of number array and bin boundaries inconsistent')
+elseif any(urange(1,:)>urange(2,:))
+    error('Must have urange_lo <= urange_hi for all dimensions')
 end
 
 if ndim>3
     % Check first if the simple section of dimensions 4,5,... leaves any points (as fast to test)
-    [irange,inside,outside] = get_irange(urange(:,4:end),varargin{4:end});
+    [irange,inside,outside] = get_irange(urange(:,4:end),varargin{4:ndim});
     if outside; return; end
     irangecell=cell(1,ndim-2);  % one cell for the first three dimensions
     for i=4:ndim
@@ -70,19 +91,8 @@ if ndim>3
     end
     % Get index ranges for column representation for first three axes
     [istart,iend] = get_irange_rot(urange,rot,trans,varargin{1:3});
-    if isempty(iend)    % iend is empty when hdf used too. 
-        if isempty(istart), return, end; 
-        % *** works in 4D only!!! 
-        length3d = prod(dims(1:3));
-        ind4     = (irange(1):irange(2))-1;              
-        selection= numel(ind4)*numel(istart);
-        ind = zeros(selection,2);
-        ind(:,1) = reshape(repmat(istart,[1,numel(ind4)]),1,selection);
-        ind(:,2) = reshape(ind4(ones(1,numel(istart)),:),1,selection);
-        nstart   = ind(:,2)*length3d+ind(:,1);
-        clear ind;
-        return;                      % but then istart would have pixel information
-    end 
+    if isempty(istart); return; end
+    % Get contiguous blocks for the whole array
     ncum=cumsum(nelmts(:));
     ncum = reshape (ncum,[prod(dims(1:3)),dims(4:end)]);
     nelmts = reshape (nelmts,[prod(dims(1:3)),dims(4:end)]);
@@ -95,9 +105,7 @@ if ndim>3
 else
     % Get index ranges for column representation for first three axes
     [istart,iend] = get_irange_rot(urange,rot,trans,varargin{1:3});
-    if isempty(iend); nstart=istart; 
-        return; 
-    end % iend is empty when hdf used too.
+    if isempty(istart); return; end
     ncum=cumsum(nelmts(:));
     nstart = ncum(istart)-nelmts(istart)+1;
     nend   = ncum(iend);
