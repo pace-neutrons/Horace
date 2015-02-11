30ea1bfb7996a0e9698497ca16268b50cce4e195
Toby Perring <toby.perring@stfc.ac.uk>
2015-02-11 08:15:18 +0000

    Further changes to functions that find bins that contribute to a cut.
    refs #116 #24
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@965 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 53ff93815356ac4a52c2c599bb8e40382b4e643a

--- @sqw/private/get_nrange_section.m
+++ @sqw/private/get_nrange_section.m
@@ -10,30 +10,55 @@ function [nstart,nend] = get_nrange_section (urange,nelmts,varargin)
 % 
 % Input:
 % ------
-%   urange  Range to cover: 2 x nd array of [urange_lo; urange_hi]
+%   urange  Range to cover: 2 x ndim array of [urange_lo; urange_hi]
 %   nelmts  Array of number of points in n-dimensional array of bins
 %          e.g. 3x5x7 array such that nelmts(i,j,k) gives no. points in
-%          the (i,j,k)th bin.
+%          the (i,j,k)th bin. If the number of dimensions defined by urange,
+%          ndim=size(urange,2), is greater than the number of dimensions
+%          defined by nelmts, n=numel(size(nelmts)), then the excess
+%          dimensions required of nelmts are all assumed to be singleton
+%          following the usual matlab convention.
 %   p1      Bin boundaries along first axis (column vector)
 %   p2      Similarly axis 2
 %   p3      Similarly axis 3
 %    :              :
+%   pndim   Similarly axis ndim
+%           It is assumed that each array of bin boundaries has
+%          at least two values (i.e. at least one bin), and that
+%          the bin boundaries are monotonic increasing.
 %   
 % Output:
 % -------
-%   nstart      Column vector of starting values of contiguous blocks in
-%              the array of values with the number of elements in a bin
-%              given by nelmts(:).
-%   nend        Column vector of finishing values.
+%   nstart  Column vector of starting values of contiguous blocks in
+%          the array of values with the number of elements in a bin
+%          given by nelmts(:).
+%   nend    Column vector of finishing values.
 %
-%               nstart and nend have column length zero if there are no
-%              elements i.e. have the value zeros(0,1).
+%           nstart and nend have column length zero if there are no
+%          elements i.e. have the value zeros(0,1).
 
 
 % Original author: T.G.Perring
 %
 % $Revision$ ($Date$)
 
+
+% Check input arguments
+ndim=numel(varargin);
+if ndim==0
+    error('Must give bin boundary array(s)')
+elseif numel(size(urange))~=2 || size(urange,1)~=2 || size(urange,2)~=ndim
+    error('Check urange is a 2 x ndim array where ndim is the number of bin boundary arrays')
+elseif any(urange(1,:)>urange(2,:))
+    error('Must have urange_lo <= urange_hi for all dimensions')
+end
+
+sz = size(nelmts);
+if isempty(nelmts)
+    error('Number array ''nelmts'' cannot be empty')
+elseif ~((ndim==1 && sz(2)==1) || ndim>=numel(sz))
+    error('Size of number array ''nelmts'' is inconsistent with the number of bin boundary arrays')
+end
 
 % Get contiguous arrays
 [irange,inside,outside] = get_irange(urange,varargin{:});
