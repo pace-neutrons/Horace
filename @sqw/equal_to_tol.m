0eeb9a0f98ec16de727a1b45dbcb350e08f126df
Toby Perring <toby.perring@stfc.ac.uk>
2015-02-19 16:50:12 +0000

    Solve a problem with the new sqw/equal_to_tol with array input; unit tests now pass again.
    refs #24
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@973 1cb955da-4463-431d-88dd-2af2f5550de3

--- @sqw/equal_to_tol.m
+++ @sqw/equal_to_tol.m
@@ -42,69 +42,89 @@ function [ok,mess]=equal_to_tol(w1,w2,varargin)
 % $Revision: 877 $ ($Date: 2014-06-10 12:35:28 +0100 (Tue, 10 Jun 2014) $)
 
 
+if isa(w1,'sqw') && isa(w2,'sqw')
+    % Check array sizes match
+    if ~isequal(size(w1),size(w2))
+        ok=false; mess='Sizes of objects being compared are not equal'; return
+    end
+    % Check that corresponding objects in the array have the same type
+    for i=1:numel(w1)
+        if is_sqw_type(w1(i))~=is_sqw_type(w2(i))
+            elmtstr=''; if numel(w1)>1, elmtstr=['(element ',num2str(i),')']; end
+            ok=false; mess=['Objects being compared are not both sqw-type or dnd-type',elmtstr]; return
+        end
+    end
+    % Perform comparison
+    for i=1:numel(w1)
+        [ok,mess]=equal_to_tol_internal(w1(i),w2(i),varargin{:});
+        if ~ok,
+            elmtstr=''; if numel(w1)>1, elmtstr=['(element ',num2str(i),')']; end
+            mess=[mess,elmtstr];
+            return
+        end
+    end
+else
+    ok=false; mess='One of the objects to be compared is not an sqw object'; return
+end
+
+
+%----------------------------------------------------------------------------------------
+function [ok,mess]=equal_to_tol_internal(w1,w2,varargin)
+% Compare scalar sqw objects of same type (sqw-type or dnd-type)
+
 horace_info_level=get(hor_config,'horace_info_level');
 
-if isa(w1,'sqw') && isa(w2,'sqw') && (is_sqw_type(w1)==is_sqw_type(w2))
-    opt=struct('reorder',1);
-    [args,opt,present,filled,ok,mess]=parse_args_simple(varargin,opt);
-    if ~ok
-        error(mess);
-    end
-    
-    if ~present.reorder || ~is_sqw_type(w1)
-        % Test strict equality; pass structures to get to the generic equal_to_tol
-        [ok,mess]=equal_to_tol(struct(w1),struct(w2),args{:});
-        
-    else
-        % Test pixels in a fraction of non-empty bins, accounting for reordering of pixels
-        
-        % Test all except pix array
-        tmp1=struct(w1); tmp1.data.pix=0;
-        tmp2=struct(w2); tmp2.data.pix=0;
-        [ok,mess]=equal_to_tol(tmp1,tmp2,args{:});
-        if ~ok, return, end
-        
-        % Check a subset of the bins with reordering
-        npix=w1.data.npix(:);
-        nend=cumsum(npix);
-        nbeg=nend-npix+1;
-        
-        if opt.reorder>1 || opt.reorder<0
-            error('Check value of ''reorder''')
-        elseif opt.reorder==0 || all(npix==0)
-            return                  % no bins
-        else
-            pix1=w1.data.pix;
-            pix2=w2.data.pix;
-            ix=find(npix>0);
-            if opt.reorder==1
-                ibin=1:numel(npix); % all bins
-            else
-                ind=randperm(numel(ix));
-                ind=ind(1:ceil(opt.reorder*numel(ix)));
-                ibin=sort(ix(ind))';
-            end
-            if horace_info_level>=1
-                disp(['                       Number of bins = ',num2str(numel(npix))])
-                disp(['             Number of non-empty bins = ',num2str(numel(ix))])
-                disp(['Number of bins that will be reordered = ',num2str(numel(ibin))])
-                disp(' ')
-            end
-            for i=ibin
-                s1=sortrows(pix1(:,nbeg(i):nend(i))');
-                s2=sortrows(pix2(:,nbeg(i):nend(i))');
-                [ok,mess]=equal_to_tol(s1,s2,args{:});
-            end
-        end
-        
-        % Fail if the comparison failed the test and no output arguments
-        if nargout==0
-            error(mess)
-        end
-        
-    end
+% Check if reorder option is present (only relevant if sqw-type)
+opt=struct('reorder',1);
+[args,opt,present,filled,ok,mess]=parse_args_simple(varargin,opt);
+if ~ok
+    error(mess);
+end
+
+if ~present.reorder || ~is_sqw_type(w1)
+    % Test strict equality; pass structures to get to the generic equal_to_tol
+    [ok,mess]=equal_to_tol(struct(w1),struct(w2),args{:});
     
 else
-    % One of the two objects is not an sqw object
-    error('One of the objects to be compared is not an sqw object, or they are not both sqw-type or both dnd-type')
+    % Test pixels in a fraction of non-empty bins, accounting for reordering of pixels
+    
+    % Test all except pix array
+    tmp1=struct(w1); tmp1.data.pix=0;
+    tmp2=struct(w2); tmp2.data.pix=0;
+    [ok,mess]=equal_to_tol(tmp1,tmp2,args{:});
+    if ~ok, return, end
+    
+    % Check a subset of the bins with reordering
+    npix=w1.data.npix(:);
+    nend=cumsum(npix);
+    nbeg=nend-npix+1;
+    
+    if opt.reorder>1 || opt.reorder<0
+        error('Check value of ''reorder''')
+    elseif opt.reorder==0 || all(npix==0)
+        return                  % no bins
+    else
+        pix1=w1.data.pix;
+        pix2=w2.data.pix;
+        ix=find(npix>0);
+        if opt.reorder==1
+            ibin=1:numel(npix); % all bins
+        else
+            ind=randperm(numel(ix));
+            ind=ind(1:ceil(opt.reorder*numel(ix)));
+            ibin=sort(ix(ind))';
+        end
+        if horace_info_level>=1
+            disp(['                       Number of bins = ',num2str(numel(npix))])
+            disp(['             Number of non-empty bins = ',num2str(numel(ix))])
+            disp(['Number of bins that will be reordered = ',num2str(numel(ibin))])
+            disp(' ')
+        end
+        for i=ibin
+            s1=sortrows(pix1(:,nbeg(i):nend(i))');
+            s2=sortrows(pix2(:,nbeg(i):nend(i))');
+            [ok,mess]=equal_to_tol(s1,s2,args{:});
+        end
+    end
+   
 end
