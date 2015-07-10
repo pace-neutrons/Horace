ddd49670541f00683e3eb3438879279156aaf867
Alex Buts <alex.buts@stfc.ac.uk>
2015-07-10 09:49:15 +0000

    refs #76 More playing with hook 3
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1015 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 257a48d929874ca66e62fbcfcc8787012dd98dcd

--- admin/tortoseHoracehook.bat
+++ admin/tortoseHoracehook.bat
@@ -1,9 +1,12 @@
 rem @echo off
-set SCRDIR=%CD%
+rem set SCRDIR= %CD%
+
+set SCRDIR=c:\Users\wkc26243\Documents\work\Horace\admin
 set MATLABDIR=c:\programming\Matlab2012a
-pause
+copy /b "%SCRDIR%\horace_version.m"+,,
+rem pause
 rem the path to matlab assumed to be defined as "some_path"32 or "some_path"64 where number identifies appropriate matlab version (32 or 64 bit)
 rem the same convention is used in visual studio projects which build 32 or 64-bit versions of mex files. 
-"%MATLABDIR%32\sys\perl\win32\bin\perl.exe" "%SCRDIR%\svn_matlab_commit_hook.pl" "%SCRDIR%\horace_version.m"
+rem "%MATLABDIR%32\sys\perl\win32\bin\perl.exe" "%SCRDIR%\svn_matlab_commit_hook.pl" "%SCRDIR%\horace_version.m"
 pause
 exit 0;
