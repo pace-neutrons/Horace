e16e7717fea7418b0ff3e67aa5f1ccce629c9ce5
Alex Buts <alex.buts@stfc.ac.uk>
2015-07-20 09:31:07 +0000

    Re #148 started unit tests for test_proj_captions
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1031 1cb955da-4463-431d-88dd-2af2f5550de3

--- _test/test_transformation/test_proj_captions.m
+++ _test/test_transformation/test_proj_captions.m
@@ -1,4 +1,4 @@
-classdef test_spher_proj_caption<TestCase
+classdef test_proj_captions<TestCase
     % The test class to verify how projection works
     %
     properties
@@ -6,30 +6,38 @@ classdef test_spher_proj_caption<TestCase
     end
     
     methods
-        function this=test_spher_proj_caption(name)
+        function this=test_proj_captions(name)
             this=this@TestCase(name);
             %sqw/dnd data structure with fields used in caption
-            this.data= struct('filename','_a_file','filepath','_a_path',...
-                'title','_a_titile',...
-                'alatt',[3.3,3.3,3.3],...
-                'angdeg',[90,90,90], 'uoffset',[],...
-                'u_to_rlu',eye(3),'ulen',[], 'ulabel',[],...
-                'iax',[], 'iint',[], 'pax',[],'p',[],'dax',[], 's',[],'e',[],...
-                'npix',[],'urange',[],'pix',[],'axis_caption',[]);
+            this.data= data_sqw_dnd();
         end
-        function xtest_cube_caption(this)
+        function test_cube_caption(this)
             
             capt = an_axis_caption();
             assertTrue(capt.changes_aspect_ratio);
             
             [title_main, title_pax, title_iax, display_pax, display_iax, energy_axis]=...
                 capt.data_plot_titles(this.data);
-            
-            
+            assertTrue(iscell(title_main));
+            assertEqual(size(title_main),[1,2]);
+            %
+            assertTrue(iscell(title_pax));
+            assertTrue(isempty(title_pax));
+            %
+            assertTrue(iscell(title_iax));
+            assertEqual(size(title_iax),[4,1]);
+            %
+            assertTrue(iscell(display_pax));
+            assertTrue(isempty(display_pax));
+            %
+            assertTrue(iscell(display_iax));
+            assertEqual(size(display_iax),[4,1]);
+            %
+            assertEqual(energy_axis,4);
         end
         
         
-        function xtest_spher_caption(this)
+        function test_spher_caption(this)
             
             capt = spher_proj_caption();
             assertFalse(capt.changes_aspect_ratio);
