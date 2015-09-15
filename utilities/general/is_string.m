070ea52196ac09a1975886e4ec6ccbed38cb9c7a
Alex Buts <alex.buts@stfc.ac.uk>
2015-09-15 15:49:18 +0000

    Re pace-neutrons/Herbert#14 More changes to remove warnings with Matlab 2015b (and fix bug in previous changes)
    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@450 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

---
+++ utilities/general/is_string.m
@@ -0,0 +1,9 @@
+function ok=is_string(var)
+% true if variable is a character string i.e. 1xn character array (n>=0), or empty character
+%
+%   >> ok=is_string(var)
+%
+% Note: if var is empty but has size 1x0 then will return true
+%       Also, if empty, will return true
+
+ok=ischar(var) && (isrowvector(var) || isempty(var));
--- utilities/general/isstring.m
+++
@@ -1,9 +0,0 @@
-function ok=isstring(var)
-% true if variable is a character string i.e. 1xn character array (n>=0), or empty character
-%
-%   >> ok=isstring(var)
-%
-% Note: if var is empty but has size 1x0 then will return true
-%       Also, if empty, will return true
-
-ok=ischar(var) && (isrowvector(var) || isempty(var));
