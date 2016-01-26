5c16636d3a9e41149e8dbe6b7bc73d8a9258f427
Duc Le <duc.le@stfc.ac.uk>
2016-01-26 09:26:40 +0000

    Refs #155. Added spinW helper functions. Added spinW example in demo.
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1162 1cb955da-4463-431d-88dd-2af2f5550de3

--- _test/test_multifit/test_multifit_horace_spinw.m
+++ _test/test_multifit/test_multifit_horace_spinw.m
@@ -16,7 +16,6 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
         w2data;
         win;
         sbcc;        % SpinW object
-        sqw_bcc_spw; % Fit function wrapper
     end
     
     methods
@@ -56,9 +55,10 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
             sbcc.addmatrix('mat',diag([0 0 -1]),'color', [255; 0; 0],'label',{'D'}); sbcc.addaniso('D');
             sbcc.genmagstr('mode','direct','S',[0 0 1; 0 0 1]');  % Ferromagnetic
             sbcc.fileid(0);   % Silences information text from SpinW to commandline window
-            this.sqw_bcc_spw = @(qh,qk,ql,en,p) ...
-               (p(3)/S)*disp2sqw(qh,qk,ql,en,@sbcc.horace,{[p(1)/2 p(2)]/S,'mat',{'D(3,3)','J1'},'hermit',false,'fitmode',2},p(4)) ...
-               + p(5);
+            sbcc = spinw_setpar(sbcc,'mapping',{'D(3,3)','J1'},'hermit',false,'convolvfn',@spinw_gauss_sqw,'useMex',true);
+            % We want to use the same parameter numbers as disp_bcc_hfm (gap energy and J*S)
+            % so need to scale D,J and intensity by 1/S, and additionally, D by half.
+            sbcc = spinw_setpar(sbcc,'partrans',@(x)x./[2*S S S 1 1]);
                
             % Combine the two cuts into an array of sqw objects and fit
             % ---------------------------------------------------------
@@ -78,20 +78,20 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
             % ------------------------------------------------------------------------------------------------
             % Example of fitting more than one sqw object
             % -------------------------------------------------------------------------------------------------
-            if isempty(this.sqw_bcc_spw)
+            if isempty(this.sbcc)
                 return
             end
             % This will take a long time with your cross-section... first the evaluation of the initial conditions
-            wsim_1=multifit_sqw_sqw(this.win, this.sqw_bcc_spw, [5,5,0,10,0], [1,1,0,0,0],...
-                this.sqw_bcc_spw, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}}, 'evaluate' );
+            wsim_1=multifit_sqw_sqw(this.win, this.sbcc, [5,5,0,10,0], [1,1,0,0,0],...
+                this.sbcc, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}}, 'evaluate' );
             
             % acolor b r  % Set colours to blue followed by red; repeats this in succession if more than two objects in an array
             % dp(this.win)     % Draw Points
             % pl(wsim_1)  % Plot Line
             
             % and now the fit
-            [wfit_1,fitpar_1]=multifit_sqw_sqw(this.win, this.sqw_bcc_spw, [5,5,0,10,0], [1,1,0,0,0],...
-                this.sqw_bcc_spw, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}});
+            [wfit_1,fitpar_1]=multifit_sqw_sqw(this.win, this.sbcc, [5,5,0,10,0], [1,1,0,0,0],...
+                this.sbcc, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}});
             % Test against saved or store to save later
             % Test only against simulated spectra from analytical calculations, because fit parameters differ significantly due
             % to accumulation of numerical errors in diagonalisation procedure.
@@ -106,18 +106,18 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
         end
         
         function this=test_fit_averg_hkl(this)
-            if isempty(this.sqw_bcc_spw)
+            if isempty(this.sbcc)
                 return
             end
             % This will be faster, because it gets the average h,k,l,e for all data pixels in a bin
             % and evaluates only at that point. The final answer will be a little different of course -
             % the extent will be dependent on how rapidly your dispersion function varies, and how big your
             % bins are in the cut.
-            wsim_2=multifit_sqw_sqw(this.win, this.sqw_bcc_spw, [5,5,0,10,0], [1,1,0,0,0],...
-                this.sqw_bcc_spw, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}}, 'evaluate', 'ave' );
+            wsim_2=multifit_sqw_sqw(this.win, this.sbcc, [5,5,0,10,0], [1,1,0,0,0],...
+                this.sbcc, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}}, 'evaluate', 'ave' );
             
-            [wfit_2,fitpar_2]=multifit_sqw_sqw(this.win, this.sqw_bcc_spw, [5,5,0,10,0], [1,1,0,0,0],...
-                this.sqw_bcc_spw, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}}, 'ave' );
+            [wfit_2,fitpar_2]=multifit_sqw_sqw(this.win, this.sbcc, [5,5,0,10,0], [1,1,0,0,0],...
+                this.sbcc, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}}, 'ave' );
             % Test against saved or store to save later
             this=test_or_save_variables(this,wsim_2);
             tol = this.tol;
@@ -133,14 +133,14 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
         % Example of fitting single dataset or independently fitting an array of datasets
         % -------------------------------------------------------------------------------------------------
         function this=test_fit_single_or_array(this)
-            if isempty(this.sqw_bcc_spw)
+            if isempty(this.sbcc)
                 return
             end
             % Check fit_sqw correctly fits array of input
-            [wfit_single1,fitpar_single1]=fit_sqw(this.w1data, this.sqw_bcc_spw, [5,5,1,10,0], [0,1,1,1,1], @linear_bkgd, [0,0]);
+            [wfit_single1,fitpar_single1]=fit_sqw(this.w1data, this.sbcc, [5,5,1,10,0], [0,1,1,1,1], @linear_bkgd, [0,0]);
             
-            [wfit_single2,fitpar_single2]=fit_sqw(this.w2data, this.sqw_bcc_spw, [5,5,1,10,0], [0,1,1,1,1], @linear_bkgd, [0,0]);
-            [wfit_single12,fitpar_single12]=fit_sqw(this.win, this.sqw_bcc_spw, [5,5,1,10,0], [0,1,1,1,1], @linear_bkgd, [0,0]);
+            [wfit_single2,fitpar_single2]=fit_sqw(this.w2data, this.sbcc, [5,5,1,10,0], [0,1,1,1,1], @linear_bkgd, [0,0]);
+            [wfit_single12,fitpar_single12]=fit_sqw(this.win, this.sbcc, [5,5,1,10,0], [0,1,1,1,1], @linear_bkgd, [0,0]);
             
             assertTrue(equal_to_tol([wfit_single1,wfit_single2],wfit_single12),'fit_sqw workspaces not working');
             assertTrue(equal_to_tol([fitpar_single1,fitpar_single2],fitpar_single12),'fit_sqw fitting not working')
@@ -151,14 +151,14 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
         end
 
         function this=test_fit_single_or_array2(this)
-            if isempty(this.sqw_bcc_spw)
+            if isempty(this.sbcc)
                 return
             end
         
-            [wfit_single12,fitpar_single12]=fit_sqw(this.win, this.sqw_bcc_spw, [5,5,1,10,0.05], [0,1,1,1,1], @linear_bkgd, [0,0]);
+            [wfit_single12,fitpar_single12]=fit_sqw(this.win, this.sbcc, [5,5,1,10,0.05], [0,1,1,1,1], @linear_bkgd, [0,0]);
             
-            [wfit_single1,fitpar_single1]=multifit_sqw(this.w1data, this.sqw_bcc_spw, [5,5,1,10,0.05], [0,1,1,1,1], @linear_bkgd, [0,0]);
-            [wfit_single2,fitpar_single2]=multifit_sqw(this.w2data, this.sqw_bcc_spw, [5,5,1,10,0.05], [0,1,1,1,1], @linear_bkgd, [0,0]);
+            [wfit_single1,fitpar_single1]=multifit_sqw(this.w1data, this.sbcc, [5,5,1,10,0.05], [0,1,1,1,1], @linear_bkgd, [0,0]);
+            [wfit_single2,fitpar_single2]=multifit_sqw(this.w2data, this.sbcc, [5,5,1,10,0.05], [0,1,1,1,1], @linear_bkgd, [0,0]);
             
             assertTrue(equal_to_tol([wfit_single1,wfit_single2],wfit_single12),'fit_sqw not working for dataset');
             assertTrue(equal_to_tol([fitpar_single1,fitpar_single2],fitpar_single12),'fit_sqw not working for fit parameters');
@@ -174,21 +174,25 @@ classdef test_multifit_horace_spinw< TestCaseWithSave
             fitpar_single1.sig(5)=NaN;     this.old.fitpar_single1.sig(5)=NaN; 
             fitpar_single2.sig(5)=NaN;     this.old.fitpar_single2.sig(5)=NaN; 
             fitpar_single12(1).sig(5)=NaN; this.old.fitpar_single12(1).sig(5)=NaN; 
-            fitpar_single12(2).sig(5)=NaN; this.old.fitpar_single12(2).sig(5)=NaN; 
+            fitpar_single12(2).sig(5)=NaN; this.old.fitpar_single12(2).sig(5)=NaN;
+            fitpar_single1.p(5)=NaN;       this.old.fitpar_single1.p(5)=NaN;
+            fitpar_single12(1).p(5)=NaN;   this.old.fitpar_single12(1).p(5)=NaN;
+            fitpar_single1.bp(1)=NaN;      this.old.fitpar_single1.bp(1)=NaN;
+            fitpar_single12(1).bp(1)=NaN;  this.old.fitpar_single12(1).bp(1)=NaN;
             this=test_or_save_variables(this,fitpar_single1,fitpar_single2,fitpar_single12);
             this.tol=tol;
             %
             
         end
-        %}
+
         function this=test_multifit_single_or_array(this)
-            if isempty(this.sqw_bcc_spw)
+            if isempty(this.sbcc)
                 return
             end
             % Check fit_sqw_sqw behaves as is should
-            [wfit_sqw_sqw,fitpar_sqw_sqw]=fit_sqw_sqw(this.win, this.sqw_bcc_spw, [5,5,1,10,0], [0,1,1,1,0], this.sqw_bcc_spw, [5,5,0,1,0], [0,0,0,0,1]);
-            [tmp1,ftmp1]=multifit_sqw_sqw(this.w1data, this.sqw_bcc_spw, [5,5,1,10,0], [0,1,1,1,1]);
-            [tmp2,ftmp2]=multifit_sqw_sqw(this.w2data, this.sqw_bcc_spw, [5,5,1,10,0], [0,1,1,1,1]);
+            [wfit_sqw_sqw,fitpar_sqw_sqw]=fit_sqw_sqw(this.win, this.sbcc, [5,5,1,10,0], [0,1,1,1,0], this.sbcc, [5,5,0,1,0], [0,0,0,0,1]);
+            [tmp1,ftmp1]=multifit_sqw_sqw(this.w1data, this.sbcc, [5,5,1,10,0], [0,1,1,1,1]);
+            [tmp2,ftmp2]=multifit_sqw_sqw(this.w2data, this.sbcc, [5,5,1,10,0], [0,1,1,1,1]);
             assertTrue(equal_to_tol([tmp1,tmp2],wfit_sqw_sqw,-1e-8),'fit_sqw_sqw not working')
             
             % Test against saved or store to save later
