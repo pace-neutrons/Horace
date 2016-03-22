b6e10caeec1290112f1361e103314270c822c3fb
Alex Buts <alex.buts@stfc.ac.uk>
2016-03-22 20:16:43 +0000

    Re #156 Minor bugs found while working with large files
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1222 1cb955da-4463-431d-88dd-2af2f5550de3

--- sqw/algorithms/combine_cuts_list.m
+++ sqw/algorithms/combine_cuts_list.m
@@ -159,7 +159,7 @@ disp('');
 %Finally save the correct output to disk:
 an_sqw = sqw();
 write_nsqw_to_sqw (an_sqw, zone_fnames_list, outfile,...
-    'allow_equal_headers','combine_subzones_headers');
+    'allow_equal_headers','drop_subzones_headers');
 
 %if nargout>0
 %    wout = read_sqw(outfile);
