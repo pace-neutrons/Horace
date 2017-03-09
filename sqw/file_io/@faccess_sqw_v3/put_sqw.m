8f52a9a21bbd3a3075e8246d1d5fbbe419524978
Alex Buts <alex.buts@stfc.ac.uk>
2017-03-09 20:58:51 +0000

    Re #24 modifications trying to address issues on CEPH. Waiting time introduced to allow filesystem to flush data.
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1444 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 931ff1e4dd884b19742ccddee70dc16aafda4ad3

--- sqw/file_io/@faccess_sqw_v3/put_sqw.m
+++ sqw/file_io/@faccess_sqw_v3/put_sqw.m
@@ -1,8 +1,18 @@
 function  obj = put_sqw(obj,varargin)
 % Save sqw file using sqw v3 binary format
 %
+% if key -v2 is provided, force writing v2 file format
+% (for testing purposes)
+%
+%
 
-obj = put_sqw@sqw_binfile_common(obj,varargin{:});
+[ok,mess,force_v2,argi]=parse_char_options(varargin,{'-v2'});
+if ~ok
+    error('SQW_FILE_IO:invalid_artgument',...
+        ['DND_BINFILE_COMMON::put_sqw Error: ',mess]);
+end
+
+obj = put_sqw@sqw_binfile_common(obj,argi{:});
 
 pix_end = obj.eof_pix_pos_;
 fseek(obj.file_id_,0,'eof');
@@ -11,19 +21,30 @@ if uint64(pix_end) - uint64(file_end)>0
     warning('FACCESS_SQW_V3:runtime_error',...
         'file end position %d is not equal to pixel end position: %d\n%s',...
         pix_end,file_end,...
-        'waiting 30 sec to flush filesystem');
-    pause(30);
-%     fseek(obj.file_id_,0,'eof');
-%     file_end = ftell(obj.file_id_);
-%     if uint64(pix_end) - uint64(file_end)>0
-%         sqw_obj = obj.
-%         obj=obj.delete();
-%         obj1=faccess_sqw_v2(sqw_obj);
-%         obj1=obj1.init(
-%         error('FACCESS_SQW_V3:runtime_error',...
-%             ['can not move to pixel end to write auxiliary V3 information',...
-%             ' file redefined as v2 file, which may be itself corrupted']);
-%     end
+        'waiting 1 min to flush filesystem');
+    for i=1:60
+        fprintf('.')
+        pause(1);
+        fseek(obj.file_id_,0,'eof');
+        file_end = ftell(obj.file_id_);
+        if uint64(pix_end) - uint64(file_end) == 0
+            break;
+        end
+    end
+    fprintf('.\n')
+    
+    if uint64(pix_end) - uint64(file_end)~= 0
+        % make object an sqw_v2 object and exit
+        write_v2_obj_header(obj);
+        error('FACCESS_SQW_V3:runtime_error',...
+            ['can not move to pixel end to write auxiliary V3 information\n',...
+            ' File written as v2 file, which may have corrupted pixels.']);
+    end
+end
+if force_v2
+    write_v2_obj_header(obj);
+    error('FACCESS_SQW_V3:runtime_error',...
+        'forced saving sqw v2 object. v3 writer destroyed')    
 end
 
 obj = put_sample_instr_records_(obj);
@@ -32,5 +53,21 @@ obj = put_sample_instr_records_(obj);
 obj.position_info_pos_= obj.instr_sample_end_pos_;
 obj = put_sqw_footer_(obj);
 
+function obj=write_v2_obj_header(obj)
+% wirte v3 file as v2 file
+%
+head = obj.build_app_header();
+head.version = 2;
+%
+head_form = obj.app_header_form_;
+
+% write sqw header
+bytes = obj.sqw_serializer_.serialize(head,head_form);
+fseek(obj.file_id_,0,'bof');
+check_error_report_fail_(obj,'Error moving to the beginning of the file');
+fwrite(obj.file_id_,bytes,'uint8');
+check_error_report_fail_(obj,'Error writing the sqw file header');
+
+obj=obj.delete();
 
 
