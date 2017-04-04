56f739ecd4f870127301fa16961625049e93f83e
Alex Buts <alex.buts@stfc.ac.uk>
2017-04-04 12:28:55 +0000

    Re #165 Restored aProjection class deleted after folder renamed
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1464 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 41294312be9c946b7c900b4b89bf0063ac7ccc33

---
+++ sqw/coord_transform/@aProjection/private/set_data_transf_.m
@@ -0,0 +1,38 @@
+function proj = set_data_transf_(proj,data,upix_to_rlu,upix_offset)
+% Retrieve all parameters for transformation already
+% defined over sqw data and store them in projection to
+% use later with new transformation.
+%
+% $Revision: 1170 $ ($Date: 2016-02-01 17:35:02 +0000 (Mon, 01 Feb 2016) $)
+%
+
+if size(upix_to_rlu) ~= [3 3]
+    error('APROJECTION:invalid_argument','aProjection: set_data_transf: upix_to_rlu should be 3x3 matrix')
+end
+%
+if size(upix_offset) ~= [4 1]
+    error('APROJECTION:invalid_argument','aProjection: set_data_transf: upix_offset should be 4x1 vector')
+end
+
+
+proj.alatt_=data.alatt;
+proj.angdeg_=data.angdeg;
+%
+% existing transformation -- currently can be only rectangular one 
+proj.data_u_to_rlu_    = data.u_to_rlu; %(4x4)
+proj.data_uoffset_     = data.uoffset;  %(4x1)
+proj.data_lab_         = data.ulabel;
+proj.data_ulen_        = data.ulen;
+
+proj.data_iax_         = data.iax;
+proj.data_pax_         = data.pax;
+proj.data_iint_        = data.iint;
+proj.data_p_           = data.p;
+
+
+
+proj.data_upix_to_rlu_ = upix_to_rlu;
+proj.data_upix_offset_ = upix_offset;
+
+
+
