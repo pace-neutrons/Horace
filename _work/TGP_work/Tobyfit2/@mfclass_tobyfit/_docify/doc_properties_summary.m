ab0bb5fafdf23158204899abb5cb693e03c56232
Toby Perring <toby.perring@stfc.ac.uk>
2017-05-08 08:05:17 +0000

    Tobyfit2 development: documentation changes
    refs #133
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1485 1cb955da-4463-431d-88dd-2af2f5550de3

---
+++ _work/TGP_work/Tobyfit2/@mfclass_tobyfit/_docify/doc_properties_summary.m
@@ -0,0 +1,5 @@
+% In addition, specifically for Tobyfit:
+%   mc_contributions    - Defines which components contribute to the resolution
+%   mc_points           - The number of Monte Carlo points per pixel
+%   refine_crystal      - Crystal orientation refinement parameters
+%   refine_moderator    - Moderator parameter refinement parameters
