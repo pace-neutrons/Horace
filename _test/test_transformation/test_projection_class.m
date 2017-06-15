840ef568f89ea8019967003c4e78129e44c8a6b3
Alex Buts <alex.buts@stfc.ac.uk>
2017-06-15 12:21:28 +0000

    Re #24 Miscellaneous additional unit tests
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1498 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: d082ea00c569ccb8e1d4aec12a3e2d93df1c7aba

--- _test/test_transformation/test_projection_class.m
+++ _test/test_transformation/test_projection_class.m
@@ -2,11 +2,29 @@ classdef test_projection_class<TestCase
     % The test class to verify how projection works
     %
     properties
+        tests_folder
+        par_file
+        %en, par_file, sqw_file, efix, emode, alatt, angdeg, u, v, psi, omega, dpsi, gl, gs
+        fake_sqw_par = {[],'','',35.5,1,[4.4,5.5,6.6],[100,105,110],...
+            [1.02,0.99,0.02],[0.025,-0.01,1.04],...
+            90,10.5,0.2,3-1/6,2.4+1/7};                
     end
     
     methods
-        function this=test_projection_class(name)
+        function this=test_projection_class(varargin)
+            if nargin == 0
+                name = 'test_projection_class';
+            else
+                name = varargin{1};
+            end            
             this=this@TestCase(name);
+            this.tests_folder = fileparts(fileparts(mfilename('fullpath')));
+            this.par_file=fullfile( this.tests_folder,'test_sqw','gen_sqw_96dets.nxspe');
+            
+            efix = this.fake_sqw_par{4};
+            en = 0.05*efix:0.2+1/50:0.95*efix;
+            this.fake_sqw_par{1} = en;
+            this.fake_sqw_par{2} = this.par_file;            
         end
         function test_constructor(this)
             proj = projection();
@@ -46,7 +64,7 @@ classdef test_projection_class<TestCase
             data.pax=[1,2,3,4];
             data.iint=[];
             data.p={1:10;1:20;1:30;1:40};
-     
+            
             
             proj=proj.retrieve_existing_tranf(data,upix_to_rlu,upix_offset);
         end
@@ -54,5 +72,16 @@ classdef test_projection_class<TestCase
             proj = projection();
             assertTrue(proj.can_mex_cut);
         end
+        function test_cut(this)
+            hc = hor_config();
+            cur_mex = hc.use_mex;
+            hc.use_mex = 0;
+            clob = onCleanup(@()set(hor_config,'use_mex',cur_mex));
+            [w, grid_size, urange]=fake_sqw (this.fake_sqw_par{:});
+            w = dnd(w{1});
+            %w = w{1};
+            wc = cut_dnd(w,0.01,0.01,[-0.1,0.1],2);
+        end
+        
     end
 end
