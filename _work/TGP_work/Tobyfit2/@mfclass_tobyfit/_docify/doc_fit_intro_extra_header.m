6a466bf181037e5383989f5701b28bee654a0483
Toby Perring <toby.perring@stfc.ac.uk>
2017-08-27 15:58:51 +0000

    Repository maintenance - recovering from some unknown scre-up
    refs #76
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1513 1cb955da-4463-431d-88dd-2af2f5550de3

---
+++ _work/TGP_work/Tobyfit2/@mfclass_tobyfit/_docify/doc_fit_intro_extra_header.m
@@ -0,0 +1,6 @@
+% If refining crystal orientation:
+%   >> [data_out, fitdata, ok, mess, rlu_corr] = obj.fit (...)
+%
+% If refining moderator parameters:
+%   >> [data_out, fitdata, ok, mess, pulse_model, p, psig] = obj.fit (...)
+%
