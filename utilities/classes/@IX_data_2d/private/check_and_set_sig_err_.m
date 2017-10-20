f67e100325378412a0e0e4baf0ed0b79144f462d
Alex Buts <alex.buts@stfc.ac.uk>
2017-10-20 14:36:40 +0000

    Re pace-neutrons/Herbert#1 Allow ix_dataset to accept single precision inputs
    git-svn-id: https://svn.isis.rl.ac.uk/Herbert/trunk@642 6b2803e2-fb1c-43d7-acfb-f5118c1e7254

--- utilities/classes/@IX_data_2d/private/check_and_set_sig_err_.m
+++ utilities/classes/@IX_data_2d/private/check_and_set_sig_err_.m
@@ -11,8 +11,12 @@ end
 
 
 if ~isa(val,'double')|| numel(size(val))~=2
-    error('IX_dataset_2d:invalid_argument',...
-        [field_name ' values array must be a double precision two dimensional array']);
+    if (isa(val,'single') || isa(val,'int64')) && numel(size(val))==2
+        val = double(val);
+    else
+        error('IX_dataset_2d:invalid_argument',...
+            [field_name ' values array must be a double precision two dimensional array']);
+    end
 end
 
 if size(val,1)==1
