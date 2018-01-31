dd78d869267c260d2ab1a25a7d771b754b3848f4
Alex Buts <alex.buts@stfc.ac.uk>
2018-01-31 16:23:00 +0000

    Re #93 test_SQW_GENCUT_perf.m test, stripped of all general code and containing the specific relevant for sqw file generation/cuts only.
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1570 1cb955da-4463-431d-88dd-2af2f5550de3

--- _test/performance_test/test_SQW_GENCUT_perf.m
+++ _test/performance_test/test_SQW_GENCUT_perf.m
@@ -1,4 +1,4 @@
-classdef test_SQW_GENCUT_perf < TestCaseWithSave
+classdef test_SQW_GENCUT_perf < TestPerformance
     % Test checks performance achieved during sqw file generation and
     % different cuts, done over the test sqw files.
     %
@@ -33,22 +33,6 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
         %  Number of input files to use. Depending on this number the test
         %  would verify small, large or huge datasets
         n_files_to_use% = 10;
-        % time to run the test which should not be substantially increase
-        % on a given machine. The first time one runs the test on the
-        % machine, it is ignored
-        time_to_run = [];
-        % performance suite name consists of the pc name and the number of
-        % input files to run. Describes the test suite which was or will be
-        % processed on current pc with current number of test files.
-        perf_test_name;
-        % performance data to compare against or store current results
-        perf_data
-        % performance tests result file, containing the results of all
-        % tests, run using this class. For normal operations of adding/modifying
-        % the performance results, this file should come with the test
-        % class, exist in the same folder as the test class and be
-        % accessible for read/write operations.
-        perf_test_res_file
     end
     
     properties
@@ -65,18 +49,6 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
         % list of source files to process
         test_source_files_list_
         %
-        % the mat file containing the performance data for the tests run on
-        % different machines. The file is located in the test folder so should
-        % be write-enabled on unix when the test is run for the first time.
-        %
-        perf_test_res_file_ = 'SQW_GENCUT_Perf.mat'
-        % performance test suite name to run/verify
-        perf_suite_name_;
-        % performance data to compare against or store current results
-        perf_data_;
-        % performance suite name consists of the pc name and the number of
-        % input files to run
-        perf_test_name_ ='';
         %  Number of input files to use. Depending on this number the test
         %  would verify small, large or huge datasets
         n_files_to_use_ = 10;
@@ -87,106 +59,42 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
         % parameter file
         par_file = 'one2one_125.par'
         
-        % time to run the test which should not be substantially increase
-        % on a given machine. The first time one runs the test on the
-        % machine, it is ignored
-        time_to_run_ = [];
-        
     end
     methods
         %------------------------------------------------------------------
-        function tr = get.time_to_run(obj)
-            % the time to run resent test case
-            tr = obj.time_to_run_;
-        end
         function nf = get.n_files_to_use(obj)
             % number of test files, used in performance tests
             nf = obj.n_files_to_use_;
-        end
-        function pfn = get.perf_test_name(obj)
-            % current test name, combined from the host name and the number of
-            % test files, used in tests
-            pfn = obj.perf_test_name_;
-        end
-        function pfd = get.perf_data(obj)
-            % returns the structure, containing all performance data,
-            % availible for tests. Can be equivalent to loading the whole
-            % perf_test_res_file in memory
-            pfd = obj.perf_data_;
-        end
-        function pf = get.perf_test_res_file(obj)
-            % the name of binary Matlab file, containign all existing
-            % performance results.
-            pf = obj.perf_test_res_file_;
         end
         %------------------------------------------------------------------
         function obj = test_SQW_GENCUT_perf(varargin)
             % create test suite, generate source files and load existing
             % perfomance data.
             %
-            obj = obj@TestCaseWithSave(varargin{:});
+            if nargin > 0
+                argi = varargin;
+            else
+                argi = {'SQW_GENCUT_perf',...
+                    TestPerformance.default_PerfTest_fname(mfilename('fullpath'))};
+            end
+            obj = obj@TestPerformance(argi{:});
+            %
             obj.source_data_dir = pwd();
             % locate the test data folder
             stat = mkdir('test_SQWGEN_performance_rw_test');
+            clob = onCleanup(@()rmdir('test_SQWGEN_performance_rw_test','s'));
             if stat == 1
-                rmdir('test_SQWGEN_performance_rw_test','s');
+                clear clob;
                 obj.working_dir = obj.source_data_dir;
             else
                 obj.working_dir = tmpdir;
-                
             end
+            % set up the default number of files to use and prepare all
+            % dependent properties to work with them.
             obj.n_files_to_use = obj.n_files_to_use_;
+            % add target sqw files to cleanList to delete it after test is
+            % completed.
             obj.add_to_files_cleanList(obj.sqw_file);
-            %
-            tests_name = obj.perf_test_name_;
-            if exist(obj.perf_test_res_file_,'file')==2
-                ld = load(obj.perf_test_res_file_);
-                obj.perf_data_ = ld.sqw_gen_cut_perf_data;
-            else
-                obj.perf_data_ = struct(tests_name,[]);
-            end
-        end
-        %
-        function test_data = save_or_test_performance(obj,start_time,test_method_name)
-            % save performance data if the previous version for current pc
-            % does not exist or test performance against previously stored
-            % performance data
-            %
-            % start_time -- time of the test run start measured by tic
-            %               function
-            
-            run_time= toc(start_time);
-            sqw_gen_cut_perf_data = obj.perf_data_;
-            if isfield(sqw_gen_cut_perf_data,obj.perf_test_name)
-                test_data = sqw_gen_cut_perf_data.(obj.perf_test_name);
-            else
-                test_data = [];
-            end
-            if isempty(test_data)
-                test_data = struct(test_method_name,run_time);
-                fprintf('*** Method %s: Run time: %3.2e min;\n',...
-                    test_method_name,run_time/60);
-                
-            else
-                if isfield(test_data,test_method_name)
-                    old_time = test_data.(test_method_name);
-                    fprintf(...
-                        ['*** Method %s: Run time: %3.2e min; old time:',...
-                        ' %3.2e min: run is %3.2e times faster\n'],...
-                        test_method_name,run_time/60,old_time/60,...
-                        (old_time-run_time)/old_time)
-                    %assertEqualToTol(run_time,old_time,'relTol',0.1);
-                else
-                    fprintf('*** Method %s: Run time: %3.2e min;\n',...
-                        test_method_name,run_time/60);                    
-                end
-                test_data.(test_method_name) = run_time;
-            end
-            obj.time_to_run_ = run_time;
-            sqw_gen_cut_perf_data.(obj.perf_test_name) = test_data;
-            save(obj.perf_test_res_file_,'sqw_gen_cut_perf_data')
-            obj.perf_data_ = sqw_gen_cut_perf_data;
-            
         end
         %-------------------------------------------------------------
         
@@ -198,11 +106,12 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             if obj.n_files_to_use_ < 1
                 obj.n_files_to_use_ = 1;
             end
-            cn = getComputerName();
-            % change computer name on Unix into the form, acceptable as a
-            % field name
-            cn = strrep(cn,'.','_');
-            obj.perf_test_name_ = [cn,'_nf',num2str(obj.n_files_to_use_)];
+            % change performance suite name as different number of input
+            % files has different impact on performance
+            perf_test = obj.build_test_suite_name(['nf',num2str(obj.n_files_to_use_)]);
+            obj.perf_suite_name = perf_test;
+            
+            %
             filelist = source_nxspe_files_generator(obj.n_files_to_use,...
                 obj.source_data_dir,obj.working_dir,obj.template_file_);
             % delete generated files after the test completed.
@@ -262,7 +171,9 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             ts = tic();
             write_nsqw_to_sqw(tmp_files,obj.sqw_file);
             %
-            perf_val=obj.save_or_test_performance(ts,['combine_tmp_using_',combine_method]);
+            perf_val=obj.assertPerformance(ts,...
+                ['combine_tmp_using_',combine_method],...
+                'performance of the tmp-files combine procedure');
             
             % spurious check to ensure the cleanup object is not deleted
             % before the end of the test
@@ -282,7 +193,7 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             % workers to run the test routines with.
             %
             % n_workers>1 sets up parallel file combining.
-            % if n_workers==0 or absent the class does not change the 
+            % if n_workers==0 or absent the class does not change the
             % number of workers defined by current Horace configuration.
             if nargin == 1
                 n_workers = 0;
@@ -311,25 +222,30 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             
             gen_sqw (obj.test_source_files_list_,'',obj.sqw_file, efix, emode, alatt, angdeg,u, v, psi, omega, dpsi, gl, gs,'replicate');
             
-            obj.save_or_test_performance(ts,['gen_sqw_nWorkers',nwk]);
+            obj.assertPerformance(ts,['gen_sqw_nWorkers',nwk],...
+                'whole sqw file generation');
             
             % test small 1 dimensional cuts, non-axis aligned
             ts = tic();
             proj1 = struct('u',[1,0,0],'v',[0,1,1]);
             sqw1 = cut_sqw(obj.sqw_file,proj1,0.01,[-0.1,0.1],[-0.1,0.1],[-5,5]);
-            obj.save_or_test_performance(ts,['cutH1D_Small_nw',nwk]);
+            obj.assertPerformance(ts,['cutH1D_Small_nw',nwk],...
+                'small memory based 1D cut in non-axis aligned direction 1');
             
             ts = tic();
             sqw1 = cut_sqw(obj.sqw_file,proj1,[-0.1,0.1],0.01,[-0.1,0.1],[-5,5]);
-            obj.save_or_test_performance(ts,['cutK1D_Small_nw',nwk]);
+            obj.assertPerformance(ts,['cutK1D_Small_nw',nwk],...
+                'small memory based 1D cut in non-axis aligned direction 2');
             
             ts = tic();
             sqw1 = cut_sqw(obj.sqw_file,proj1,[-0.1,0.1],[-0.1,0.1],0.01,[-5,5]);
-            obj.save_or_test_performance(ts,['cutL1D_Small_nw',nwk]);
+            obj.assertPerformance(ts,['cutL1D_Small_nw',nwk],...
+                'small memory based 1D cut in non-axis aligned direction 3');
             
             ts = tic();
             sqw1 = cut_sqw(obj.sqw_file,proj1,[-0.1,0.1],[-0.1,0.1],[-0.1,0.1],0.2);
-            obj.save_or_test_performance(ts,['cutE_Small_nw',nwk]);
+            obj.assertPerformance(ts,['cutE_Small_nw',nwk],...
+                'small memory based 1D cut along energy direction (q are not axis aligned)');
             
             % check nopix performance -- read and integrate the whole file from the HDD
             hs = head_sqw(obj.sqw_file);
@@ -337,19 +253,24 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             ts = tic();
             proj1 = struct('u',[1,0,0],'v',[0,1,1]);
             sqw1=cut_sqw(obj.sqw_file,proj1,0.01,urng(2,:),urng(3,:),urng(4,:),'-nopix');
-            obj.save_or_test_performance(ts,['cutH1D_AllInt_nopix_nw',nwk]);
+            obj.assertPerformance(ts,['cutH1D_AllInt_nopix_nw',nwk],...
+                'large 1D cut direction 1 with whole dataset integration along 3 other directions. -nopix mode');
             
             ts = tic();
             sqw1=cut_sqw(obj.sqw_file,proj1,urng(1,:),0.01,urng(3,:),urng(4,:),'-nopix');
-            obj.save_or_test_performance(ts,['cutK1D_AllInt_nopix_nw',nwk]);
+            obj.assertPerformance(ts,['cutK1D_AllInt_nopix_nw',nwk],...
+                'large 1D cut direction 2 with whole dataset integration along 3 other directions. -nopix mode');
             
             ts = tic();
             sqw1=cut_sqw(obj.sqw_file,proj1,urng(1,:),urng(2,:),0.01,urng(4,:),'-nopix');
-            obj.save_or_test_performance(ts,['cutL1D_AllInt_nopix_nw',nwk]);
+            obj.assertPerformance(ts,['cutL1D_AllInt_nopix_nw',nwk],...
+                'large 1D cut direction 3 with whole dataset integration along 3 other directions. -nopix mode');
             
             ts = tic();
             sqw1=cut_sqw(obj.sqw_file,proj1,urng(1,:),urng(2,:),urng(3,:),0.2,'-nopix');
-            obj.save_or_test_performance(ts,['cutE_AllInt_nopix_nw',nwk]);
+            
+            obj.assertPerformance(ts,['cutE_AllInt_nopix_nw',nwk],...
+                'large 1D cut along energy direction with whole dataset integration along 3 other directions. -nopix mode');
             
             
             % test large 1 dimensional cuts, non-axis aligned, with whole
@@ -362,19 +283,23 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             ts = tic();
             proj1 = struct('u',[1,0,0],'v',[0,1,1]);
             cut_sqw(obj.sqw_file,proj1,0.01,urng(2,:),urng(3,:),urng(4,:),'cutH1D_AllInt.sqw');
-            obj.save_or_test_performance(ts,['cutH1D_AllInt_filebased_nw',nwk]);
+            obj.assertPerformance(ts,['cutH1D_AllInt_filebased_nw',nwk],...
+                'large file-based 1D cut. Direction 1; Whole dataset integration along 3 other directions');
             
             ts = tic();
             cut_sqw(obj.sqw_file,proj1,urng(1,:),0.01,urng(3,:),urng(4,:),'cutK1D_AllInt.sqw');
-            obj.save_or_test_performance(ts,['cutK1D_AllInt_filebased_nw',nwk]);
+            obj.assertPerformance(ts,['cutK1D_AllInt_filebased_nw',nwk],...
+                'large file-based 1D cut. Direction 2; Whole dataset integration along 3 other directions');
             
             ts = tic();
             cut_sqw(obj.sqw_file,proj1,urng(1,:),urng(2,:),0.01,urng(4,:),'cutL1D_AllInt.sqw');
-            obj.save_or_test_performance(ts,['cutL1D_AllInt_filebased_nw',nwk]);
+            obj.assertPerformance(ts,['cutL1D_AllInt_filebased_nw',nwk],...
+                'large file-based 1D cut. Direction 1; Whole dataset integration along 3 other directions');
             
             ts = tic();
             cut_sqw(obj.sqw_file,proj1,urng(1,:),urng(2,:),urng(3,:),0.2,'cutE_AllInt.sqw');
-            perf_res=obj.save_or_test_performance(ts,['cutE_AllInt_filebased_nw',nwk]);
+            perf_res=obj.assertPerformance(ts,['cutE_AllInt_filebased_nw',nwk],...
+                'large file-based 1D cut. Energy directin; Whole dataset integration along 3 other directions');
             
             % spurious check to ensure the cleanup object is not deleted
             % before the end of the test
@@ -387,13 +312,13 @@ classdef test_SQW_GENCUT_perf < TestCaseWithSave
             %
             % returns cleanup object which returns the number of temporary
             % workers to its initial value on destruction
-			%  if input n_workers == 0, current number of parallel
+            %  if input n_workers == 0, current number of parallel
             % workers remains unchanged
-			%
-			if n_workers == 0 % keep existing number of workers unchanged
+            %
+            if n_workers == 0 % keep existing number of workers unchanged
                 clob = onCleanup(@()(0));
-				return;		
-			end
+                return;
+            end
             hc = hor_config;
             as = hc.accum_in_separate_process;
             an = hc.accumulating_process_num;
