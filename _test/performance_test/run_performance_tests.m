3bc4280e7b08566e76c91c567ce4da263805ab0d
Alex Buts <alex.buts@stfc.ac.uk>
2018-06-20 17:57:47 +0000

    Re #169 Performance from Windows and changes to run selected performance tests
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1620 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 24dc545318d7dc65606bee5e61c8efac15a72feb

--- _test/performance_test/run_performance_tests.m
+++ _test/performance_test/run_performance_tests.m
@@ -2,13 +2,18 @@ function perf_res = run_performance_tests(varargin)
 % function to run performance test on a given pc or cluster
 %
 % Usage:
-% perf_results = run_performance_tests([n_workers])
+% perf_results = run_performance_tests([n_workers],[the_test])
 %
-% optional variable n_workers tries to run performance tests with number of
-% workers, specified as input.
+% optional variables:
+% n_workers --  tries to run performance tests with number of
+%               workers, specified as input.
+% the_test  -- run only the test(s) specified as input. More then one test
+%              name should be specified in a cellarray of names
+%
 % returns:
 % perf_results  -- the structure containing the performance results for
 %                  given pc
+%
 % these results are also stored in special file, defined by
 % test_SQW_GENCUT_perf  class overwriting previous values for the same
 % machine if such values were present
@@ -29,10 +34,15 @@ if nargin == 0
 else
     n_workers = varargin{1};
 end
+if nargin == 2
+    selected_tests = varargin{2};
+else
+    selected_tests = [];    
+end
 
 %--------------------------------------------------------------------------
 % run performance tests for small sqw file (default file)
-small_perf = hor_tes.test_gensqw_performance(n_workers);
+small_perf = hor_tes.test_gensqw_performance(n_workers,selected_tests);
 perf_res.small_ds_perf = small_perf;
 %--------------------------------------------------------------------------
 
@@ -47,14 +57,14 @@ clob_tmp = onCleanup(@()set(hc,hcd));
 hc.delete_tmp = 0;
 %--------------------------------------------------------------------------
 % run performance tests for medium size file
-medium_perf = hor_tes.test_gensqw_performance(n_workers);
+medium_perf = hor_tes.test_gensqw_performance(n_workers,selected_tests);
 perf_res.medium_ds_perf = medium_perf;
 %--------------------------------------------------------------------------
 %
 %--------------------------------------------------------------------------
 %run performance for combining tmp files only (depends on previous test
 %completed successfully and left tmp files to combine during this test)
-medium_perf = hor_tes.combine_performance_test(n_workers);
+medium_perf = hor_tes.combine_performance_test(n_workers,selected_tests);
 perf_res.medium_ds_perf = medium_perf;
 %--------------------------------------------------------------------------
 clear clob_tmp; % reset configuration not to keep tmp files any more
@@ -62,7 +72,7 @@ clear clob_tmp; % reset configuration not to keep tmp files any more
 %
 % prepare performance tests for large dataset
 hor_tes.n_files_to_use = 250;
-large_perf = hor_tes.test_gensqw_performance(n_workers);
+large_perf = hor_tes.test_gensqw_performance(n_workers,selected_tests);
 perf_res.large_ds_perf = large_perf;
 %
 hor_tes.save_to_csv();
