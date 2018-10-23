9000740247cf5698be8e866171060a3525667e2a
Toby Perring <toby.perring@stfc.ac.uk>
2018-10-23 09:57:29 +0000

    First implementation of resolution function plotting - not yet complete, but correct!
    refs #24 #116
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1667 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: 9140d880543ebbaf6d6b79d2994d9bee3e95340a

---
+++ Tobyfit/Tobyfit/@sqw/private/resolution_plot_private.m
@@ -0,0 +1,134 @@
+function resfun_plot (x0,C,iax,fig,newplot)
+% Plot resolution function on 2D axes
+%
+%   >> resfun_plot (C,iax)
+%
+% Input:
+% ------
+%   x0      Origin of resolution function, [x1,x2]
+%
+%   C       Covariance matrix (4x4) in qx,qy,qz,en
+%
+%   iax     Indicies of axes to plot (all unique, in range 1 to 4)
+%           If length 2, these give the axes of the plot plane into C
+%           If length 3, then the third axis is one for which an
+%          ellipse section is drawn at a positive value along that axis
+%
+%   fig     Figure name or number on which to plot or overplot
+%
+%   newplot If true, create new plot; If false overplot
+
+
+% Original author: T.G.Perring
+%
+% $Revision: 1524 $ ($Date: 2017-09-27 15:48:11 +0100 (Wed, 27 Sep 2017) $)
+
+
+% Check input arguments
+if ~(isnumeric(x0) && numel(x0)==2 && all(isfinite(x0)))
+    error('Centre must be a numeric vector length 2')
+end
+
+if ~(isnumeric(C) && isequal(size(C),[4,4]))
+    error('Covariance matrix has wrong size')
+end
+
+if ~(isnumeric(iax) && (numel(iax)==2 || numel(iax)==3) &&...
+        numel(unique(iax))==numel(iax) && all(iax>=1) && all(iax)<=4)
+    error('Check axes indicies')
+end
+
+% Plot parameters
+frac = 0.5;     % fraction of maximum at which to draw contours
+
+val = 2*log(1/frac);
+
+% Get envelope and intersection(s)
+% - Envelope
+Cplane = C(iax(1:2),iax(1:2));    % pick out the covariance elements for the plot axes
+m = inv(Cplane);
+[x1e,x2e] = ellipse (m(1,1), m(1,2), m(2,2), val);  % envelope
+
+% - Intersection with x1-x2 plane for x3=0
+M = inv(C);
+m = M(iax(1:2),iax(1:2));
+[x1c,x2c] = ellipse (m(1,1), m(1,2), m(2,2), val);  % intersection with the plane
+
+% - Intersection with x1-x2 plane for x3>0
+if numel(iax)==3
+    x3max = val*sqrt(C(iax(3),iax(3)));
+    x3 = round_mantissa(0.667*x3max);
+    % Offset of ellipse centre
+    m = M(iax,iax);
+    dx1 = x3 * (-m(2,2)*m(1,3)+m(1,2)*m(2,3)) / (m(1,1)*m(2,2)-m(1,2)*m(1,2));
+    dx2 = x3 * (-m(1,1)*m(2,3)+m(1,2)*m(1,3)) / (m(1,1)*m(2,2)-m(1,2)*m(1,2));
+    E = m(1,1)*dx1^2 + 2*m(1,2)*dx1*dx2 + m(2,2)*dx2^2 ...
+        + 2*m(1,3)*dx1*x3 + 2*m(2,3)*dx2*x3 + m(3,3)*x3^2;
+    [x1ch,x2ch] = ellipse (m(1,1), m(1,2), m(2,2), val-E);    % intersection with the plane at x3
+end
+
+% Perform plot
+% ------------
+default_fig_name = 'Resolution function';
+[fig_out,ok,mess]=genie_figure_target(fig,newplot,default_fig_name);
+if ~ok, error(mess), end
+
+% Create new graphics window if required
+if is_string(fig_out)
+    new_figure = genie_figure_create (fig_out);
+    if new_figure
+        newplot=true;   % if had to create a new figure window
+    end
+else
+    figure(fig_out); % overplotting on existing plot; make the current figure
+end
+
+% If newplot, delete any axes
+if newplot
+    delete(gca)     % not necessary if new_figure, but doesn't do any harm
+else
+    hold on;        % hold plot for overplotting
+end
+
+% Get genie line and colour characteristics, and plot
+lwidth = aline;
+lcol = acolor;
+plot(x1e+x0(1),x2e+x0(2),'Color',lcol,'LineStyle','-','LineWidth',lwidth);
+hold on
+plot(x1c+x0(1),x2c+x0(2),'Color',lcol,'LineStyle','--','LineWidth',lwidth);
+if numel(iax)==3
+    plot(x1ch+x0(1)+dx1,x2ch+x0(2)+dx2,'Color',lcol,'LineStyle','--','LineWidth',lwidth);
+end
+
+hold off
+
+%========================================================================================
+function r=round_mantissa(x)
+% Round a positive number to the nearest number with form n*10^m where n, m are integer
+xlog = log10(x);
+r = round(10^mod(xlog,1))*10^floor(xlog);
+
+
+%========================================================================================
+function [x1,x2] = ellipse (m11,m12,m22,A)
+% Get a set of points that lie on the ellipse m11*x1^2 + 2*m12*x1*x2 + m22*x2^2 = A
+
+npnt = 500;     % number of points on the ellipse
+
+% Get the orientation of the ellipsoid: angle theta to a principal axis x1'
+theta = 0.5*atan2(2*m12, m11-m22);
+c = cos(theta);
+s = sin(theta);
+
+% Get the lengths of the principal axes along x1', x2'
+a1 = sqrt(A/(m11*c^2 + 2*m12*c*s + m22*s^2));
+a2 = sqrt(A/(m11*s^2 - 2*m12*c*s + m22*c^2));
+
+% Coordinate of points in x1',x2'
+ang=linspace(0,2*pi,npnt);
+x1prime = a1*cos(ang);
+x2prime = a2*sin(ang);
+
+% Transform to input coordinate frame
+x1 = c*x1prime - s*x2prime;
+x2 = s*x1prime + c*x2prime;
---
+++ algorithms/resolution_plot.m
@@ -0,0 +1,251 @@
+function varargout = resolution_plot (en, instrument, sample, detpar, efix, emode,...
+    alatt, angdeg, u, v, psi, omega, dpsi, gl, gs, varargin)
+% Plot the instrumental resolution function
+%
+% New resolution plot:
+%   >> resolution_plot (en, instrument, sample, detpar, efix, emode,...
+%                           alatt, angdeg, u, v, psi, omega, dpsi, gl, gs)
+%   >> resolution_plot (..., proj)
+%   >> resolution_plot (..., iax)
+%   >> resolution_plot (..., proj, iax)
+%
+% On current existing resolution plot:
+%   >> resolution_plot (..., 'over')
+%
+% On current plot, or named or numbered existing plot
+%   >> resolution_plot (..., 'curr')
+%   >> resolution_plot (..., 'name', name)
+%
+%
+% Input:
+% ------
+%   en              Energy bin boundaries (must be monotonically increasing and equally spaced)
+%   instrument      Structure containing instrument description
+%   sample          IX_sample object
+%   detpar          Structure with detector parameters: fields must include:
+%                       x2      sample-detector distance (m)
+%                       phi     scattering angle (deg)
+%                       axim    azimuthal scattering angle (deg)
+%                               (West bank=0 deg, North bank=90 deg etc.)
+%                       width   detector width (m)
+%                       height  detector height (m)
+%   efix            Fixed energy (meV)                 [scalar or vector length nfile]
+%   emode           Direct geometry=1, indirect geometry=2    [scalar]
+%   alatt           Lattice parameters (Ang^-1)        [row or column vector]
+%   angdeg          Lattice angles (deg)               [row or column vector]
+%   u               First vector (1x3) defining scattering plane (r.l.u.)
+%   v               Second vector (1x3) defining scattering plane (r.l.u.)
+%   psi             Angle of u w.r.t. ki (deg)         [scalar or vector length nfile]
+%   omega           Angle of axis of small goniometer arc w.r.t. notional u (deg) [scalar or vector length nfile]
+%   dpsi            Correction to psi (deg)            [scalar or vector length nfile]
+%   gl              Large goniometer arc angle (deg)   [scalar or vector length nfile]
+%   gs              Small goniometer arc angle (deg)   [scalar or vector length nfile]
+%
+% {optional]
+%   proj            Projection structure or object. Type help projaxes for details.
+%                   Default: if not given or empty: assume to be spectrometer axes
+%                  i.e. x || ki, z vertical upwards, y perpendicular to z and y.
+%
+%   iax             Indicies of axes into the projection for purposes of plotting
+%                       [iax1, iax2]        plotting axes
+%                       [iax1, iax2, iax3]  plotting axes and intersection axis
+%
+%                   Default: if not given or empty, assume first two projection axes
+%                  and draw intersection with non-zero positive energy transfer deviation
+%
+% Plot options:
+%   'over'          If present, then overplot on an existing resolution function plot
+%                   If one doesn't already exist, create a new resolution function plot
+%
+%   'curr'          Overplot on the currently active plot
+%
+%   'name', name    Overplot on the named figure or figure number of an existing plot
+
+
+% Check parameters
+% ----------------
+% Energies and sample orientation
+nfiles_in = 1;
+[ok,mess,efix,emode,alatt,angdeg,u,v,psi,omega,dpsi,gl,gs]=gen_sqw_check_params...
+    (nfiles_in,efix,emode,alatt,angdeg,u,v,psi,omega,dpsi,gl,gs);
+if ~ok, error(mess), end
+if efix(1)==0, error('Must have emode=1 (director geometry) or =2 (indirect geometry)'), end
+
+% Optional arguments
+key = struct ('over',false,'current',false,'name',[]);
+flags = {'over','current'};
+opts.flags_noneg = true;
+opts.flags_noval = true;
+[par,key,present] = parse_arguments (varargin, key, flags, opts);
+if sum(cell2mat(struct2cell(present(2:end))))>1
+    error('Only one of the plot options ''over'', ''current'' and ''name'' can be present')
+end
+
+% - Projection and/or iax:
+proj = [];
+iax = [1,2,4];
+if numel(par)==1
+    if ~isnumeric(par{1})   % can only be a projection
+        if ~isempty(par{1}), proj = par{1}; end
+    else
+        if ~isempty(par{1}), iax = par{1}; end
+    end
+elseif numel(par)==2
+    if ~isempty(par{1}), proj = par{1}; end
+    if ~isempty(par{2}), iax = par{2}; end
+elseif numel(par)~=0
+    error('Check the number and type of optional arguments')
+end
+
+if ~(isempty(proj) || isa(proj,'projaxes'))
+    proj = projaxes(proj);
+end
+
+if ~(isnumeric(iax) && (numel(iax)==2 || numel(iax)==3) &&...
+        numel(unique(iax))==numel(iax) && all(iax>=1) && all(iax)<=4)
+    error('Check axes indicies')
+end
+
+% - Plot target
+if present.current && isempty(findobj(0,'Type','figure'))
+    error('No current figure exists - cannot overplot')
+end
+
+if present.over
+    plot_args = {'over'};
+elseif present.current
+    plot_args = {'current'};
+elseif present.name
+    plot_args = {'name',key.name};
+else
+    plot_args = {};
+end
+
+
+% Make main_header
+% ----------------
+main_header.filename = '';
+main_header.filepath = '';
+main_header.title = '';
+main_header.nfiles = nfiles_in;
+
+wres.main_header = main_header;
+
+
+% Make header
+% --------------
+header.filename = '';
+header.filepath = '';
+header.efix = efix;
+header.emode = emode;
+header.alatt = alatt;
+header.angdeg = angdeg;
+header.cu = u;
+header.cv = v;
+header.psi = psi;
+header.omega = omega;
+header.dpsi = dpsi;
+header.gl = gl;
+header.gs = gs;
+header.en = en;
+header.uoffset = [0,0,0,0]';
+header.u_to_rlu = zeros(4,4);
+[~, header.u_to_rlu(1:3,1:3),spec_to_rlu] = calc_proj_matrix (alatt, angdeg,...
+    u, v, psi, omega, dpsi, gl, gs);
+header.u_to_rlu(4,4) = 1;
+header.ulen = [1,1,1,1];
+header.ulabel = {'Q_\zeta'  'Q_\xi'  'Q_\eta'  'E'};
+header.instrument = instrument;
+header.sample = sample;
+
+if isstruct(instrument) && isscalar(instrument)
+    header.instrument = instrument;
+else
+    error('Instrument must be a scale structure')
+end
+
+if isa(sample,'IX_sample')
+    header.sample = sample;
+else
+    error('Sample must be a scalar IX_sample object')
+end
+
+wres.header = header;
+
+
+% Check detector
+% --------------
+% if ~(isstruct(detpar) && isfield(detpar,'x2') && isfield(detpar,'phi') && isfield(detpar,'azim') &&...
+%         isfield(detpar,'width') && isfield(detpar,'height'))
+%     error('Detector parameters must be a structure (see help to this function)')
+% end
+if ~isstruct(detpar)
+    error('Detector parameters must form a structure')
+end
+if numel(detpar.x2)~=1
+    error('Detector parameters can be for a single detector only')
+end
+if ~isfield(detpar,'filename'), detpar.filename = ''; end
+if ~isfield(detpar,'filepath'), detpar.filepath = ''; end
+if ~isfield(detpar,'group'), detpar.group = 1; end
+wres.detpar = detpar;
+
+
+% Make data structure
+% -------------------
+% Create an sqw object that has two plot axes
+data.filename = '';
+data.filepath = '';
+data.title = '';
+data.alatt = alatt;
+data.angdeg = angdeg;
+if ~isempty(proj)
+    data.uoffset = proj.uoffset;
+    data.u_to_rlu = zeros(4,4);
+    data.ulen = zeros(1,4);
+    [~, data.u_to_rlu(1:3,1:3), data.ulen(1:3), mess] = projaxes_to_rlu (proj, alatt, angdeg);
+    if ~isempty(mess), error(mess); end
+    data.u_to_rlu(4,4) = 1;
+    data.ulen(4) = 1;
+else
+    data.uoffset = [0,0,0,0]';
+    data.u_to_rlu = zeros(4,4);
+    data.u_to_rlu(1:3,1:3) = spec_to_rlu;
+    data.u_to_rlu(4,4) = 1;
+    data.ulen = [1,1,1,1];
+end
+data.ulabel = {'Q_\zeta'  'Q_\xi'  'Q_\eta'  'E'};
+ok=true(1,4); ok(iax(1:2))=false;
+data.iax = find(ok);
+data.iint = [-Inf,-Inf; Inf,Inf];
+data.pax = iax(1:2);
+data.p = {[-1;0;1], [-1;0;1]};
+data.dax = [1,2];
+data.s = zeros(2,2);
+data.e = zeros(2,2);
+data.npix = zeros(2,2);
+data.urange = [Inf,Inf,Inf,Inf;-Inf,-Inf,-Inf,-Inf];
+data.pix = zeros(9,0);
+
+wres.data = data;
+
+
+% Construct sqw object
+% --------------------
+wres = sqw(wres);
+
+
+% Now plot the resolution function
+% --------------------------------
+if numel(iax)==2
+    covariance_matrix = resolution_plot (wres, 'axis', 'none', plot_args{:});
+else
+    covariance_matrix = resolution_plot (wres, 'axis', iax(3), plot_args{:});
+end
+
+
+% Return covariance matrix, if requested
+% --------------------------------------
+if nargout==1
+    varargout{1} = covariance_matrix;
+end
