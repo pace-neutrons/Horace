1be4de9199b88e88264e99a6ca3b5be80ba8d1b4
Alex Buts <alex.buts@stfc.ac.uk>
2018-11-17 23:35:34 +0000

    Re #24 final version of matlab caching script
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1676 1cb955da-4463-431d-88dd-2af2f5550de3

--- admin/cache.m
+++ admin/cache.m
@@ -1,19 +1,23 @@
 function cache(filename)
 % function loads a sqw file in a cache of a parallel file system
-% Input: 
+% Input:
 % filename -- the name of the sqw file to cache
-% 
+%
 % if appropriate file system is present (e.g. ISIS CEPS filesystem on isiscompute),
-% the function substantially (almost tenfold) improves access time to 
+% the function substantially (almost tenfold) improves access time to
 % the sqw file.
 %
 % function provides wrapper around python program with the same name, which
 % is started in a separate process.
-% 
+%
+%
+%
+% $Revision: 347 $ ($Date: 2018-10-30 17:52:52 +0000 (Tue, 30 Oct 2018) $)
+%
 
 if ~isunix
     warning('CACHE:invalid_os',...
-        'This function should be only used on appropriate Unix system. Exiting')
+        'This function should be only used on an appropriate Unix system. Exiting')
     return;
 end
 if ~exist(filename,'file')
@@ -21,10 +25,13 @@ if ~exist(filename,'file')
         ' File %s does not exist',filename);
 end
 script_location = fileparts(mfilename('fullpath'));
-disp('****************************************************************')
-disp(['*** Starting caching script "cache.py" locating in the folder: ',script_location]);
-disp('*** To see the acceleratiom, wait for 2-5 minutes until the caching finishes before starting cutting it.');
-disp('*** Its better to execute caching program in a separate terminal as it reports progress while running there');
-disp('****************************************************************')
+disp('*****************************************************************************************');
+disp(['*** Launching caching script "cache.py" located in the folder: ',script_location]);
+disp('*** To benifit from the acceleratiom, wait for 1-5 minutes until the caching finishes.***');
+disp('*** Run top command in a terminal to observe python processes, caching the file.      ***');
+disp('*** Wait for these processes to exit to know about the caching process completion.    ***');
+disp('*** Its recommended to execute the "cache.py" script directly in a terminal,          ***');
+disp('*** to observe progress of caching.                                                   ***');
+disp('*****************************************************************************************');
 
 system(sprintf('python %s %s &',fullfile(script_location,'cache.py'),filename),'-echo')
