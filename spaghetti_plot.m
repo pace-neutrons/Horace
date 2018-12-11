4b5f3ab73c66661dfc681ebbcde238245c003aae
Alex Buts <alex.buts@stfc.ac.uk>
2018-12-11 14:22:14 +0000

    Re #24 The option to provide width of the q-subplots
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1691 1cb955da-4463-431d-88dd-2af2f5550de3

--- spaghetti_plot.m
+++ spaghetti_plot.m
@@ -26,10 +26,11 @@ function varargout=spaghetti_plot(varargin)
 %                     (character string or cellarray with one character string)
 %
 %   wdisp           Array of d2d objects containing the cuts - e.g. previous generated
+%                   spaghetti plot.
 %
 % Keyword options (can be abbreviated to single letter):
 %
-%   'labels'        Tick labels to place at the positions of the Q points in 
+%   'labels'        Tick labels to place at the positions of the Q points in
 %                     argument rlp.
 %                     e.g. {'G','X','M','R'}
 %                   By default the labels are character representations of rlp
@@ -56,12 +57,12 @@ function varargout=spaghetti_plot(varargin)
 %   'qbin'          Size of momentum transfer bins in 1/Ang (default = 0.05)
 %
 %   'qwidth'        Integration width for q-directions perpendicular to the desired q
-%                     in 1/Ang. This may either be a scalar which will be applied to 
+%                     in 1/Ang. This may either be a scalar which will be applied to
 %                     both perpendicular directions, or a 2-vector [dqv dqw] with dqv
 %                     being the width in v, and dqw the width in w (see below)
 %                     (default = 0.1)
 %
-%   'ebin'          The energy bin parameters in meV as a 3-vector [min, step, max] 
+%   'ebin'          The energy bin parameters in meV as a 3-vector [min, step, max]
 %                     (default: use energy bins in data_source)
 %
 %   'logscale'      Plots the data in a logarithmic (base 10) scale if true
@@ -69,6 +70,23 @@ function varargout=spaghetti_plot(varargin)
 %   'clim'          A 2-vector giving the signal (colour) value limits. This is needed
 %                     if using 'logscale' as this breaks the interactive slider.
 %                     (default: [NaN NaN] - use limits of data)
+%
+%   'cuts_plot_size' under normal operation, the width of every part of
+%                   spagetti plot is proportional to the physical distance (A^-1)
+%                   between various points in recipocal space (the rlp
+%                   parameter)
+%                   This parameter should be followed by the list of the
+%                   relative plot widths, every pannel of the spaghetti
+%                   plot would occupy. E.g. If your rlp are
+%                   [0,0,0;1,0,0;1/2,1/2,0], the X-sizes of the plot
+%                   [0,0,0]->[1,0,0] and [1,0,0]->[1/2,1/2,0] would be
+%                   related as 1 to 1/sqrt(2) (in Cubic lattice). If you provide
+%                   cuts_plot_size array equal [1,1], the length of X-axis
+%                   on the each q-pannel plot would be equal.
+%
+%                   TODO: big changes in the width of the sublots will
+%                   cause big difference in the subplot resolution. The
+%                   changes in the plot width should cause changes in qbin too.
 %
 % Output:
 % -------
@@ -93,7 +111,8 @@ function varargout=spaghetti_plot(varargin)
 % Set defaults
 % ------------
 arglist = struct('qbin',0.05,'qwidth',0.1,'ebin',[],'labels','','noplot',false,...
-                 'smooth',0,'smooth_shape','hat','logscale',false,'clim',[NaN NaN]);
+    'smooth',0,'smooth_shape','hat','logscale',false,'clim',[NaN NaN],...
+    'cuts_plot_size',[]);
 flags = {'noplot','logscale'};
 
 
@@ -103,16 +122,34 @@ flags = {'noplot','logscale'};
 [args,opt,present] = parse_arguments(varargin,arglist,flags);
 
 if numel(args)~=2
-    if length(args{1})>1 && strcmp(class(args{1}(1)),'d2d')
+    if length(args{1})>1 && isa((args{1}(1)),'d2d')
         plot_dispersion(args{1},opt);
         return
     else
-        error('Check number of arguments')
+        error('SPAGHETTI_PLOT:invalid_arguments',...
+            'Invalid number of arguments')
     end
 end
 
 if size(args{1},2)~=3 || size(args{1},1)<2
-    error('Check argument giving list of reciprocal lattice points')
+    error('SPAGHETTI_PLOT:invalid_arguments',...
+        'It should be at least 2 rlp arranged in array [Nx3] (N>=2) but size of the rlp array is: [%d,%d];',...
+        size(args{1}))
+end
+if present.cuts_plot_size
+    if size(args{1},1)-1 ~= numel(opt.cuts_plot_size)
+        error('SPAGHETTI_PLOT:invalid_arguments',...
+            [' the size of the cut_plot_dist array should be one less than the number of rlp points.'...
+            ' In fact the number of rlp is %d and the number of distances is %d'],...
+            size(args{1},1),numel(opt.cuts_plot_size));
+    else
+        if any(opt.cuts_plot_size<=0)
+            invalid = opt.cuts_plot_size<=0;
+            error('SPAGHETTI_PLOT:invalid_arguments',...            
+                'the plot sizes shoule be positive numbers but some of them are: %g; %g; %g; %g; %g',...
+                opt.cuts_plot_size(invalid));
+        end
+    end
 end
 
 try
@@ -129,7 +166,8 @@ elseif length(args{2})>1 && isa(args{2}(1),'d2d')
     plot_dispersion(args{2},opt);
     return;
 else
-    error('Check argument giving data source. Must be an sqw object or sqw file')
+    error('SPAGHETTI_PLOT:invalid_arguments',...
+        'Check argument giving data source. Must be an sqw object or sqw file')
 end
 
 qbin = opt.qbin;
@@ -190,7 +228,7 @@ else
     u2crt0 = [];
 end
 if ~isempty(u2crt0)
-    disp(sprintf('spaghetti_plot: rlp found to lie in the plane perpendicular to (%g %g %g)',inv(b)*u2crt0'));
+    fprintf('spaghetti_plot: rlp found to lie in the plane perpendicular to (%g %g %g)',inv(b)*u2crt0');
 end
 
 
@@ -199,7 +237,7 @@ end
 xrlp = 0;
 wdisp = repmat(d2d,1,nseg);
 for i=1:nseg
-    % Choose u1 along the user desired q-direction 
+    % Choose u1 along the user desired q-direction
     u1rlp = rlp(i+1,:)-rlp(i,:);
     u1crt = (b*u1rlp')';
     u1crt = u1crt./norm(u1crt);
@@ -210,7 +248,7 @@ for i=1:nseg
         if sum(abs(u2crt))<min(opt.qbin)/100
             u2crt = cross(u1crt,b*[1;0;0]./norm(b*[1;0;0]));
         end
-    else 
+    else
         u2crt = u2crt0./norm(u2crt0);
     end
     u2crt = u2crt ./ norm(u2crt);
@@ -241,10 +279,10 @@ for i=1:nseg
         for j=1:numel(u1v)-1
             varargout{2}{i}(j) = cut(wdisp(i),u1v(j:j+1),[]);
         end
-    end 
+    end
     if present.labels
         titlestr = sprintf('Segment from "%s" (%f %f %f) to "%s" (%f %f %f)',...
-                    opt.labels{i},rlp(i,:),opt.labels{i+1},rlp(i+1,:));
+            opt.labels{i},rlp(i,:),opt.labels{i+1},rlp(i+1,:));
     else
         titlestr = sprintf('Segment from (%f %f %f) to (%f %f %f)',rlp(i,:),rlp(i+1,:));
     end
@@ -254,10 +292,10 @@ for i=1:nseg
         binstr = sprintf(['q bin size approximately %f ',char(197),'^{-1}'],opt.qbin);
         if numel(opt.qwidth)==1
             wdisp(i).title=sprintf(['integrated over %f ',char(197),...
-                    '^{-1} in perpendicular q-directions\n%s\n%s'],opt.qwidth(1),binstr,titlestr);
+                '^{-1} in perpendicular q-directions\n%s\n%s'],opt.qwidth(1),binstr,titlestr);
         else
             wdisp(i).title=sprintf(['integrated over %f ',char(197),...
-                    '^{-1} in qv and %f ',char(197),'^{-1} in qw\n%s\n%s'],opt.qwidth,binstr,titlestr);
+                '^{-1} in qv and %f ',char(197),'^{-1} in qw\n%s\n%s'],opt.qwidth,binstr,titlestr);
         end
     end
 end
@@ -278,156 +316,166 @@ end
 %========================================================================================================
 function plot_dispersion(wdisp_in,opt)
 % Plots the dispersion in the structure wdisp
-    persistent old_matlab;
-    if isempty(old_matlab)
-        old_matlab = verLessThan('Matlab','8.6');
+persistent old_matlab;
+if isempty(old_matlab)
+    old_matlab = verLessThan('Matlab','8.6');
+end
+if isempty(opt.cuts_plot_size)
+    scale_x_axis = false;
+else
+    scale_x_axis = true;
+end
+
+
+qinc = 0;
+title = wdisp_in(1).title;
+lnbrk = strfind(title,sprintf('\n'));
+if ~isempty(lnbrk)
+    lnbrk = lnbrk(end);
+end
+wdisp_in(1).title = title(lnbrk+1:end);
+%
+wdisp = repmat(IX_dataset_2d,1,length(wdisp_in));
+for i=1:length(wdisp_in)
+    ulen = wdisp_in(i).ulen;
+    % Internally use IX_dataset_2d to manipulate the x-axis (flip and adjust bin boundaries)
+    if opt.smooth>0
+        wdisp(i) = IX_dataset_2d(smooth(wdisp_in(i),opt.smooth,opt.smooth_shape));
+    else
+        wdisp(i) = IX_dataset_2d(wdisp_in(i));
     end
-
-
-
-    qinc = 0;
-    title = wdisp_in(1).title;
-    lnbrk = strfind(title,sprintf('\n'));
-    if ~isempty(lnbrk)
-        lnbrk = lnbrk(end);
-    end
-    wdisp_in(1).title = title(lnbrk+1:end);
-    %
-    wdisp = repmat(IX_dataset_2d,1,length(wdisp_in));
-    for i=1:length(wdisp_in)
-        u1bin = mode(diff(wdisp_in(i).p{1}));
-        ulen = wdisp_in(i).ulen;
-        % Internally use IX_dataset_2d to manipulate the x-axis (flip and adjust bin boundaries)
-        if opt.smooth>0
-            wdisp(i) = IX_dataset_2d(smooth(wdisp_in(i),opt.smooth,opt.smooth_shape));
-        else
-            wdisp(i) = IX_dataset_2d(wdisp_in(i));
-        end
-        % Modifies the first and last bins of each segment so there is no overlap between segments
-        wdisp(i).x(1)   = wdisp(i).x(1)  +u1bin/2;
-        wdisp(i).x(end) = wdisp(i).x(end)-u1bin/2;
+    % For plotting, change bin edges to bin centers
+    bin_centers = 0.5*(wdisp(i).x(1:end-1)+wdisp(i).x(2:end));
+    if scale_x_axis
+        % scale plot axis according to the scales provided
+        min_bc = min(bin_centers);
+        size  = max(bin_centers)-min_bc;
+        scale = opt.cuts_plot_size(i);
+        wdisp(i).x = qinc + (bin_centers-min_bc)*(scale/size);        
+    else
         % Converts the x-axis from r.l.u. along the segment q-direction to incremental |q| in 1/Ang
-        wdisp(i).x = qinc + (wdisp(i).x-wdisp(i).x(1)).*ulen(1);
-        qinc = wdisp(i).x(end);
-        % Update current segment length for labelling position.
-        wdisp(i).x_axis = IX_axis('Momentum',[char(197),'^{-1}']);
-        wdisp(i).y_axis = IX_axis('Energy','meV');
-        % Finds labels in segment title
-        brk = strfind(wdisp_in(i).title,sprintf('\n'));
-        if ~isempty(brk)
-            brk = brk(end);
-            wdisp_in(i).title = title(brk+1:end);
-        end
-        bra = strfind(wdisp_in(i).title,'(');
-        ket = strfind(wdisp_in(i).title,')');
-        hkls = [sscanf(wdisp_in(i).title(bra(1):ket(1)),'(%f %f %f)');
-                sscanf(wdisp_in(i).title(bra(2):ket(2)),'(%f %f %f)')];
-        quotes = strfind(wdisp_in(i).title,'"');
-        if i>1 && abs(sum(hkls(1:3)-hkl0(1:3)))>0.01
-            warning('(hkl) points for segments %d and %d do not match',i-1,i);
-            if isempty(quotes)
-                labels{i} = sprintf('[%s]/[%s]',str_compress(num2str(hkl0(4:6)')),str_compress(num2str(hkls(1:3)')));
-            else
-                labels{i} = sprintf('%s/%s',labels{i},wdisp_in(i).title(quotes(1)+1:quotes(2)-1));
-            end
-        else
-            if isempty(quotes)
-                labels{i} = ['[',str_compress(num2str(hkls(1:3)'),','),']'];
-            else
-                labels{i} = wdisp_in(i).title(quotes(1)+1:quotes(2)-1);
-            end
-        end
+        wdisp(i).x = qinc + (bin_centers-bin_centers(1))*ulen(1);
+    end
+   qinc = wdisp(i).x(end);    
+    % Update current segment length for labelling position.
+    wdisp(i).x_axis = IX_axis('Momentum',[char(197),'^{-1}']);
+    wdisp(i).y_axis = IX_axis('Energy','meV');
+    % Finds labels in segment title
+    brk = strfind(wdisp_in(i).title,sprintf('\n'));
+    if ~isempty(brk)
+        brk = brk(end);
+        wdisp_in(i).title = title(brk+1:end);
+    end
+    bra = strfind(wdisp_in(i).title,'(');
+    ket = strfind(wdisp_in(i).title,')');
+    hkls = [sscanf(wdisp_in(i).title(bra(1):ket(1)),'(%f %f %f)');
+        sscanf(wdisp_in(i).title(bra(2):ket(2)),'(%f %f %f)')];
+    quotes = strfind(wdisp_in(i).title,'"');
+    if i>1 && abs(sum(hkls(1:3)-hkl0(1:3)))>0.01
+        warning('(hkl) points for segments %d and %d do not match',i-1,i);
         if isempty(quotes)
-            labels{i+1} = ['[',str_compress(num2str(hkls(4:6)'),','),']'];
+            labels{i} = sprintf('[%s]/[%s]',str_compress(num2str(hkl0(4:6)')),str_compress(num2str(hkls(1:3)')));
         else
-            labels{i+1} = wdisp_in(i).title(quotes(3)+1:quotes(4)-1);
+            labels{i} = sprintf('%s/%s',labels{i},wdisp_in(i).title(quotes(1)+1:quotes(2)-1));
         end
-        hkl0 = hkls;
-    end
-    % This might not be the best way to do this but the 2D dataset should(!) hopefully not
-    %   take up too much memory to make a copy of wdisp...
-    if opt.logscale
-        for i=1:length(wdisp)
-            wdisp(i).signal = log10(wdisp(i).signal);
+    else
+        if isempty(quotes)
+            labels{i} = ['[',str_compress(num2str(hkls(1:3)'),','),']'];
+        else
+            labels{i} = wdisp_in(i).title(quotes(1)+1:quotes(2)-1);
         end
     end
-    wdisp(1).title = title(1:lnbrk);
-    plot(wdisp);
-    hold on;
-    xrlp = zeros(length(wdisp)+1,1);
+    if isempty(quotes)
+        labels{i+1} = ['[',str_compress(num2str(hkls(4:6)'),','),']'];
+    else
+        labels{i+1} = wdisp_in(i).title(quotes(3)+1:quotes(4)-1);
+    end
+    hkl0 = hkls;
+end
+% This might not be the best way to do this but the 2D dataset should(!) hopefully not
+%   take up too much memory to make a copy of wdisp...
+if opt.logscale
     for i=1:length(wdisp)
-        xrlp(i+1) = wdisp(i).x(end);
-        plot([1 1]*xrlp(i+1),get(gca,'YLim'),'-k');
+        wdisp(i).signal = log10(wdisp(i).signal);
     end
-    hold off;
-    if ~isempty(opt.labels)
-        if iscellstr(opt.labels) && numel(opt.labels)==length(wdisp)+1
-            labels = opt.labels;
-        else
-            warning(['Not using user-supplied labels. They are either not a cell array of' ...
-             'strings or not enough for all segments']);
-        end
+end
+wdisp(1).title = title(1:lnbrk);
+plot(wdisp);
+hold on;
+xrlp = zeros(length(wdisp)+1,1);
+for i=1:length(wdisp)
+    xrlp(i+1) = wdisp(i).x(end);
+    plot([1 1]*xrlp(i+1),get(gca,'YLim'),'-k');
+end
+hold off;
+if ~isempty(opt.labels)
+    if iscellstr(opt.labels) && numel(opt.labels)==length(wdisp)+1
+        labels = opt.labels;
+    else
+        warning(['Not using user-supplied labels. They are either not a cell array of' ...
+            'strings or not enough for all segments']);
     end
-    plot_labels(labels,xrlp);
-    if sum(isnan(opt.clim))~=2
-        if opt.logscale
-            clim = log10(opt.clim); 
-        else
-            clim = opt.clim; 
-        end
-        caxis(clim);
-        chld = get(gcf,'Children');
-        set(findobj(chld,'tag','color_slider_min_value'),'String',num2str(opt.clim(1)));
-        set(findobj(chld,'tag','color_slider_max_value'),'String',num2str(opt.clim(2)));
-    end
+end
+plot_labels(labels,xrlp);
+if sum(isnan(opt.clim))~=2
     if opt.logscale
-        if sum(isnan(opt.clim))~=2
-            clim = opt.clim;
-        else
-            clim = 10.^get(gca,'CLim')
-            chld = get(gcf,'Children');
-            set(findobj(chld,'tag','color_slider_min_value'),'String',num2str(clim(1)));
-            set(findobj(chld,'tag','color_slider_max_value'),'String',num2str(clim(2)));
-        end
-        hc=colorbar; 
-        % Old style workaround - does not work on Matlab 2015 onwards
-        if old_matlab
-            yd=10.^get(get(hc,'Children'),'YData');
-            set(hc,'YScale','log');
-            set(hc,'YLim',yd);
-            set(get(hc,'Children'),'YData',yd);
-        % New style workaround - only works on Matlab 2015 onwards!
-        else
-            set(hc,'TicksMode','manual');
-            majorticks = ceil(log10(clim(1))):floor(log10(clim(2)));
-            ntick=(numel(majorticks)-1)*9;
-            unitbefore = 10^majorticks(1)/10;
-            tickstart = ceil(clim(1)*unitbefore)/unitbefore;
-            ticksbefore = tickstart:unitbefore:unitbefore*9;
-            unitafter = 10^majorticks(end);
-            tickend = floor(clim(2)*unitafter)/unitafter;
-            ticksafter = unitafter:unitafter:tickend;
-            ntick = ntick + numel(ticksbefore) + numel(ticksafter);
-            minorticks = zeros(1,ntick);
-            ticklabels = cell(1,ntick);
-            offset = numel(ticksbefore);
-            minorticks(1:offset) = ticksbefore;
-            for i=0:numel(majorticks)-2
-                unit = 10^majorticks(i+1);
-                minorticks(i*9+1+offset:(i+1)*9+offset) = [unit:unit:9*unit];
-                ticklabels{i*9+1+offset} = num2str(unit);
-            end
-            if numel(majorticks)<2
-                i = offset+1;
-            else
-                i = (i+1)*9+1+offset;
-            end
-            minorticks(i:end) = ticksafter;
-            ticklabels{i} = num2str(10^majorticks(end));
-            set(hc,'Ticks',log10(minorticks));
-            set(hc,'TickLabels',ticklabels);
-        end
+        clim = log10(opt.clim);
+    else
+        clim = opt.clim;
     end
+    caxis(clim);
+    chld = get(gcf,'Children');
+    set(findobj(chld,'tag','color_slider_min_value'),'String',num2str(opt.clim(1)));
+    set(findobj(chld,'tag','color_slider_max_value'),'String',num2str(opt.clim(2)));
+end
+if opt.logscale
+    if sum(isnan(opt.clim))~=2
+        clim = opt.clim;
+    else
+        clim = 10.^get(gca,'CLim');
+        chld = get(gcf,'Children');
+        set(findobj(chld,'tag','color_slider_min_value'),'String',num2str(clim(1)));
+        set(findobj(chld,'tag','color_slider_max_value'),'String',num2str(clim(2)));
+    end
+    hc=colorbar;
+    % Old style workaround - does not work on Matlab 2015 onwards
+    if old_matlab
+        yd=10.^get(get(hc,'Children'),'YData');
+        set(hc,'YScale','log');
+        set(hc,'YLim',yd);
+        set(get(hc,'Children'),'YData',yd);
+        % New style workaround - only works on Matlab 2015 onwards!
+    else
+        set(hc,'TicksMode','manual');
+        majorticks = ceil(log10(clim(1))):floor(log10(clim(2)));
+        ntick=(numel(majorticks)-1)*9;
+        unitbefore = 10^majorticks(1)/10;
+        tickstart = ceil(clim(1)*unitbefore)/unitbefore;
+        ticksbefore = tickstart:unitbefore:unitbefore*9;
+        unitafter = 10^majorticks(end);
+        tickend = floor(clim(2)*unitafter)/unitafter;
+        ticksafter = unitafter:unitafter:tickend;
+        ntick = ntick + numel(ticksbefore) + numel(ticksafter);
+        minorticks = zeros(1,ntick);
+        ticklabels = cell(1,ntick);
+        offset = numel(ticksbefore);
+        minorticks(1:offset) = ticksbefore;
+        for i=0:numel(majorticks)-2
+            unit = 10^majorticks(i+1);
+            minorticks(i*9+1+offset:(i+1)*9+offset) = [unit:unit:9*unit];
+            ticklabels{i*9+1+offset} = num2str(unit);
+        end
+        if numel(majorticks)<2
+            i = offset+1;
+        else
+            i = (i+1)*9+1+offset;
+        end
+        minorticks(i:end) = ticksafter;
+        ticklabels{i} = num2str(10^majorticks(end));
+        set(hc,'Ticks',log10(minorticks));
+        set(hc,'TickLabels',ticklabels);
+    end
+end
 
 %========================================================================================================
 function plot_labels(labels,xvals)
