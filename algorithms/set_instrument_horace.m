89d5fb57fb253be767b8bdc445fd90df4d2c0676
Toby Perring <toby.perring@stfc.ac.uk>
2019-01-23 14:45:49 +0000

    Comments to set_instrument_horace
    Refs #131
    
    
    git-svn-id: https://svn.isis.rl.ac.uk/Horace/trunk@1714 1cb955da-4463-431d-88dd-2af2f5550de3
    
    Former-commit-id: a2a5773ca5280c0deaaed1d3e91187dfcc1d3877

--- algorithms/set_instrument_horace.m
+++ algorithms/set_instrument_horace.m
@@ -15,6 +15,31 @@ function varargout=set_instrument_horace(filename,instrument,varargin)
 %              runs contributing to the sqw object(s).
 %               If the instrument is any empty object, then the instrument
 %              is set to the default empty structure.
+%
+% *OR*
+%   inst_func       Function handle to generate instrument object or structure
+%                  Must be of the form
+%                       inst = my_func (p1, p2, ...)
+%                  where p1,p2, ... are parameters to be passed to the
+%                  instrument definition function, in this case called my_func,
+%                  which in this example will be passed as @my_func.
+%
+%   arg1, arg2,...  Arguments to be provided to the instrument function.
+%                  The arguments must be:
+%                   - scalars, row vectors (which can be numerical, logical,
+%                     structure, cell array or object), or character strings.
+%                   - Multiple arguments can be passed, one for each run that
+%                     constitutes the sqw object, by having one row per run
+%                     i.e
+%                       scalar      ---->   column vector (nrun elements)
+%                       row vector  ---->   2D array (nrun rows)
+%                       string      ---->   cell array of strings
+%
+%                  Certain arguments win the sqw object can be referred to by
+%                  special strings;
+%                       '-efix'     ---->   use value of fixed energy in the
+%                                           header block of the sqw object
+
 
 % Original author: T.G.Perring
 %
