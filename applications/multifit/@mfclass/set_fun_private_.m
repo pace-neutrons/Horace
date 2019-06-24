0afa12768855de2b60b27c61b35faf3cfd21466f
Alex Buts <Alex.Buts@stfc.ac.uk>
2019-06-24 13:44:59 +0100

    Re pace-neutrons/Herbert#1 Just in case, changed set_fun_private_.m to better reflect the previous behaviour (though both instances satisfy the unit tests)

--- applications/multifit/@mfclass/set_fun_private_.m
+++ applications/multifit/@mfclass/set_fun_private_.m
@@ -27,20 +27,30 @@ end
 
 
 arg1 = args{1};
-if iscell(arg1)
-    nfun = numel(arg1);
-elseif isa(arg1,'function_handle')
-    nfun = 1;
+if isfore
+    %fun_type = 'fore';
+    nfun = numel(obj.fun_);
 else
-    error('SET_FUN:invalid_argument',...
-        'first parameter of the set_fun or set_bfun method should be function handle or cellarray of function handles');
+    %fun_type = 'back';
+    nfun = numel(obj.bfun_);
 end
-if isfore   %fun_type = 'fore';   
+
+if nfun == 0
+    if iscell(arg1)
+        nfun = numel(arg1);
+    elseif isa(arg1,'function_handle')
+        nfun = 1;
+    else
+        error('SET_FUN:invalid_argument',...
+            'first parameter of the set_fun or set_bfun method should be function handle or cellarray of function handles');
+    end
+end
+if isfore   %fun_type = 'fore';
     if nfun>1
         obj.foreground_is_local_ = true;
     end
-% else        %fun_type = 'back';
-%      nfun = numel(obj.bfun_);
+    % else        %fun_type = 'back';
+    %      nfun = numel(obj.bfun_);
 end
 
 
