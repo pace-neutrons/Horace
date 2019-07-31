037dadfd1bd042c2c3a028cb23124d9cd8cb0486
abuts <Alex.Buts@stfc.ac.uk>
2019-07-31 12:50:50 +0100

    Re#9 modified test_performance not to change contents of the test folders.

--- _test/test_admin/test_Class_TestPerformance.m
+++ _test/test_admin/test_Class_TestPerformance.m
@@ -27,14 +27,22 @@ classdef test_Class_TestPerformance< TestCase
             tc.assertPerformance(time,'test_constructor_operations',...
                 'this is not a performance test itself but the test of performance test operational');
         end
-        function test_inheritance(this)
-            tc = TestPerformanceChildTestHelper();
+        function test_inheritance_with_load_results(this)
+            test_res_name = 'TestPerformanceChildTestHelper_PerfRez.xml';
+            this_folder = fileparts(mfilename('fullpath'));
+            perf_test_source  = fullfile(this_folder,test_res_name);
+            per_test_working = fullfile(tempdir,test_res_name);
+            copyfile(perf_test_source,per_test_working,'f');
+            clob = onCleanup(@()delete(per_test_working));
+            tc = TestPerformanceChildTestHelper('TestPerformanceChildTestHelper',per_test_working);
             
-            if exist(tc.test_results_file,'file') == 2
-                delete(tc.test_results_file);
-            end
+            assertEqual(tc.test_results_file,per_test_working)
+            assertEqual(exist(tc.test_results_file,'file'),2);
+            
+            delete(tc.test_results_file);
             
             tc.fake_test_method()
+            
             assertEqual(exist(tc.test_results_file,'file'),2);
         end
         
