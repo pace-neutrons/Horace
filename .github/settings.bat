faf9ce5e24a7032fdd321f16a3c594132e6c8bde
abuts <Alex.Buts@stfc.ac.uk>
2019-08-01 13:24:38 +0100

    Re#9 changes to work on linux

--- .github/settings.bat
+++ .github/settings.bat
@@ -1,5 +1,9 @@
-#!/usr/bin/bash 
+#!/bin/bash
+#
+#echo "one "
 git config --global core.autocrlf true
+#echo "two"
 git config --global core.eof native
+#echo "three "
 git config --global -l
 pause
