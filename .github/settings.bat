0a76492d7046b0ab463b8c6bccdb4b9c9a88335d
abuts <Alex.Buts@stfc.ac.uk>
2019-08-01 13:38:53 +0100

    Re#9 minor changes from Unix

--- .github/settings.bat
+++ .github/settings.bat
@@ -4,16 +4,16 @@
 #user.name=abuts
 #user.email=Alex.Buts@stfc.ac.uk
 #
-#echo "one "
 git config --global core.autocrlf true
-#echo "two"
 git config --global core.eof native
-#echo "three "
-git config --global -l
 #
-echo "set up:"
+echo "****  Current github global configuration: "
+git config --global -l
+echo "*****************************************"
+#
+echo "**** set up:"
 echo ">>git config --global user.name [you github user name]"
 echo " and "
 echo ">>git config --global user.email [your github email]"
-echo "variables if they have not been already set up"
+echo "**** variables if they have not been already set up"
 pause
