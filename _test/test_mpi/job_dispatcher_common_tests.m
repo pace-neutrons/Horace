f25999805eca19c6d08860225e38d74d8db0a865
Alex Buts <Alex.Buts@stfc.ac.uk>
2019-10-24 18:52:35 +0100

    Re pace-neutrons/Herbert#11 minor changes to mpi tests to accept an external worker

--- _test/test_mpi/job_dispatcher_common_tests.m
+++ _test/test_mpi/job_dispatcher_common_tests.m
@@ -5,12 +5,29 @@ classdef job_dispatcher_common_tests< MPI_Test_Common
     %
     
     properties
+        worker='worker_4tests'
+        current_worker_cache_=[];
     end
     methods
         %
         function this=job_dispatcher_common_tests(test_name,framework_name)
             this = this@MPI_Test_Common(test_name,framework_name);
         end
+        function setUp(obj)
+            parc = parallel_config;
+            parc.saveable= false;
+            obj.current_worker_cache_ = parc.worker;
+            parc.worker = obj.worker;
+            
+        end
+        function tearDown(obj)
+           parc = parallel_config;
+           parc.worker = obj.current_worker_cache_;
+           parc.saveable = true;
+            
+            
+        end
+
         function test_job_fail_restart(obj,varargin)
             if obj.ignore_test
                 return;
