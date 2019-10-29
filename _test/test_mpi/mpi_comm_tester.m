0392337514bef74787728ef3d41d0f382e5fa827
abuts <Alex.Buts@stfc.ac.uk>
2019-10-29 11:09:39 +0000

    Re pace-neutrons/Herbert#6 working linux version of the communicator and the script to compile it under linux

--- _test/test_mpi/mpi_comm_tester.m
+++ _test/test_mpi/mpi_comm_tester.m
@@ -8,12 +8,15 @@ if isempty(which('herbert_init.m'))
 end
 %[comm,labInd,numLabs]=cpp_communicator('init');
 me =MessagesCppMPI('test_MPI_job');
+
+labInd = me.labIndex;
+numLabs = me.numLabs;
 %clob0 = onCleanup(@()finalize_all(me));
 fname = sprintf('Log_file_for_labN%d#of%d.txt',labInd,numLabs);
 fh = fopen(fname,'w');
 clob = onCleanup(@()fclose(fh));
 fprintf(fh,'Lab N %d out of %d received control string: %s',...
-    me.labIndex,me.numLabs,worker_controls_string);
+    labInd,numLabs,worker_controls_string);
 disp('file written, entering pause')
 pause(10)
 %comm=cpp_communicator('finalize',comm);
---
+++ admin/mex_cpp_communicator.m
@@ -0,0 +1,25 @@
+function [ok,mess] = mex_cpp_communicator(varargin)
+
+input_files = {'cpp_communicator.cpp', 'input_parser.cpp', 'MPI_wrapper.cpp'};
+mpich_folder = '/usr/local/mpich/';
+
+ok = true;
+mess = [];
+
+
+her_folder = fileparts(which('herbert_init'));
+code_folder = fullfile(her_folder,'_LowLevelCode','CPP','cpp_communicator');
+input_files = cellfun(@(fn)fullfile(code_folder,fn),input_files,'UniformOutput',false);
+
+add_include = ['-I',mpich_folder,'include'];
+add_lib = ['-L',mpich_folder,'lib'];
+
+outdir = fullfile(her_folder,'DLL','_GLNXA64','_R2015a');
+%mex('-lut','CXXFLAGS=$CFLAGS -fopenmp -std=c++11','LDFLAGS= -pthread -Wl,--no-undefined  -fopenmp',add_files{:}, '-outdir', outdir);
+try 
+    mex(input_files{:},add_include,add_lib,'-v','-l:libmpi.so.12 -lmpich -lmpicxx',...
+        '-outdir',outdir);
+catch Err
+    ok = false; 
+    mess = Err.message;
+end
