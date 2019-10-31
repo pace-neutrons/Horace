09aaae94b8de5fd16865c6bd5f2d4bfbe46a7914
Alex Buts <Alex.Buts@stfc.ac.uk>
2019-10-31 17:00:23 +0000

    Re pace-neutrons/Herbert#6 undocumented unlock improvement.

--- classes/MPIFramework/@MessagesFilebased/private/unlock_.m
+++ classes/MPIFramework/@MessagesFilebased/private/unlock_.m
@@ -10,35 +10,43 @@ tried = 0;
 if isempty(filename) % file is already closed
     return
 end
+%s = warning('error', 'MATLAB:DELETE:Permission');
 
-ws=warning('off','MATLAB:DELETE:Permission');
+
+ws=warning('error','MATLAB:DELETE:Permission');
 permission_denied = false;
 while exist(filename,'file')==2 || permission_denied
-    delete(filename);
-    [~,warn_id] = lastwarn;
-    if strcmpi(warn_id,'MATLAB:DELETE:Permission')
-        permission_denied=true;
-        lastwarn('');
-        pause(0.1);
-        tried = tried+1;
-        if tried > n_attempts_allowed
-            warning('UNLOCK:runtime_error',...
-                ' Can not remove lock %s. It looks like threads got dead-locked. Progressing after Leaving lock removal job in background',...
-                filename);
-            wlock_obj  = @()lock_bg_deleter(filename,ws);
-            return;
+    try
+        delete(filename);
+    catch ME
+        %     [~,warn_id] = lastwarn;
+        warn_id = ME.identifier;
+        if strcmpi(warn_id,'MATLAB:DELETE:Permission')
+            permission_denied=true;
+            %lastwarn('');
+            pause(0.1);
+            tried = tried+1;
+            if tried > n_attempts_allowed
+                warning('UNLOCK:runtime_error',...
+                    ' Can not remove lock %s. It looks like threads got dead-locked. Progressing after Leaving lock removal job in background',...
+                    filename);
+                wlock_obj  = @()lock_bg_deleter(filename,ws);
+                return;
+            end
+        else
+            permission_denied=false;
         end
-    else
-        permission_denied=false;
     end
-    
 end
 warning(ws);
 
 function lock_bg_deleter(filename,ws)
 
 while exist(filename,'file')==2
-    pause(0.1);
-    delete(filename);
+    pause(1);
+    try
+        delete(filename);
+    catch ME
+    end
 end
 warning(ws);
