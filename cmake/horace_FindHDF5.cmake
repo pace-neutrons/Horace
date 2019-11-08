01e0a07f895c9080a8455e4ce9f35f1390c715d4
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-11-08 10:29:26 +0000

    Add CMakeLists files for hdf_mex_reader
    Former-commit-id: 1fdac62016022000e1c5e56ee08fb20d2981812c

---
+++ cmake/horace_FindHDF5.cmake
@@ -0,0 +1,19 @@
+#[=======================================================================[.rst:
+horace_FindHDF5
+-----------------
+
+Find the HDF5 library that is contained within this repository.
+
+Variables defined by the module
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+See the FindHDF5.cmake documentation for other variables defined by this
+module. You'll find the file bundled with your CMake isntallation.
+
+#]=======================================================================]
+if(UNIX)
+    set(HDF5_ROOT "${CMAKE_SOURCE_DIR}/_LowLevelCode/build_all/HDF5_1.8.12_unix/")
+elseif(WIN32)
+    set(HDF5_ROOT "${CMAKE_SOURCE_DIR}/_LowLevelCode/build_all/HDF5_1.8.12_win/")
+endif()
+find_package(HDF5)
