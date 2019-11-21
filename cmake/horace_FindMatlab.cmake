a7b4c250f64f1976df967d659a52a790b7df4796
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-11-21 17:55:43 +0000

    Update horace_FindMatlab to export path to dlls
    The script now exports the variable `Matlab_DLL_DIR` which points to the
    directory containing dlls that are required for running code that uses
    Matlab's C++ mex libraries. This directory is required for our tests.

--- cmake/horace_FindMatlab.cmake
+++ cmake/horace_FindMatlab.cmake
@@ -11,15 +11,32 @@ Variables defined by the module
 ``Matlab_UT_LIBRARY``
 the path to the Matlab UT library
 
-See the FindMatlab.cmake documentation for other variables defined by this 
+``Matlab_DLL_DIR`` (Windows only)
+the path to the directory containing Matlab's dynamically linked libraries
+
+See the FindMatlab.cmake documentation for other variables defined by this
 module. You'll find the file bundled with your CMake isntallation.
 
 #]=======================================================================]
-
 find_package(Matlab REQUIRED COMPONENTS MAIN_PROGRAM)
+
+# Find the libut library
 get_filename_component(Matlab_LIBRARY_DIR "${Matlab_MEX_LIBRARY}" DIRECTORY)
 find_library(Matlab_UT_LIBRARY
     NAMES "ut" "libut"
     HINTS "${Matlab_LIBRARY_DIR}"
     NO_DEFAULT_PATH
 )
+mark_as_advanced(FORCE Matlab_UT_LIBRARY)
+
+# Get the directory containing Matlab's dlls
+get_filename_component(_Matlab_BIN_DIR_ "${Matlab_MAIN_PROGRAM}" DIRECTORY)
+set(Matlab_BIN_DIR "${_Matlab_BIN_DIR_}" CACHE PATH "Path to Matlab's bin directory" FORCE)
+mark_as_advanced(FORCE Matlab_BIN_DIR)
+if(WIN32)
+    if("CMAKE_SIZEOF_VOID_P" EQUAL 4)
+        set(Matlab_DLL_DIR "${Matlab_BIN_DIR}/win32")
+    else()
+        set(Matlab_DLL_DIR "${Matlab_BIN_DIR}/win64")
+    endif()
+endif()
