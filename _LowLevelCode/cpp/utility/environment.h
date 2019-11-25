f6a4dade87810bd085f96c2cc46b8e2c6f827aff
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-11-25 15:32:01 +0000

    Give get_env_variable an optional default argument
    You can now call `get_env_variable` specfying a string to return if the
    environment variable is not found.
    
    
    Former-commit-id: 8b0951fea252944474c50353d311ac8ee13a7556

--- _LowLevelCode/cpp/utility/environment.h
+++ _LowLevelCode/cpp/utility/environment.h
@@ -3,11 +3,12 @@
 namespace Horace {
 namespace Utility {
 namespace Environment {
+// The HORACE_ROOT environment variable key
+const std::string HORACE_ROOT{"HORACE_ROOT"};
 // Get an environment variable
-std::string get_env_variable(const std::string &key);
-// The HORACE_ROOT environment variable
-const std::string HORACE_ROOT{get_env_variable("HORACE_ROOT")};
+std::string get_env_variable(const std::string &key,
+                             const std::string &default_value = "");
 
-}  // namespace Environment
-}  // namespace Utility
-}  // namespace Horace
+} // namespace Environment
+} // namespace Utility
+} // namespace Horace
