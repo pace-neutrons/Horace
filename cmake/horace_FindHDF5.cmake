c0335c3f3a8cd95e1ff7d61ac6f3021bce0e15ee
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-11-26 10:40:46 +0000

    Make FindHDF5 find Matlab's HDF5 version
    Matlab comes shipped with HDF5 .so files. Using these guarantees
    (hopefully) compatibility between the C++ and Matlab versions.

--- cmake/horace_FindHDF5.cmake
+++ cmake/horace_FindHDF5.cmake
@@ -13,6 +13,9 @@ module. You'll find the file bundled with your CMake isntallation.
 #]=======================================================================]
 if(UNIX)
     set(HDF5_ROOT "${CMAKE_SOURCE_DIR}/_LowLevelCode/external/HDF5_1.8.12_unix/")
+    # On Linux, link to Matlab's version of HDF5
+    set(HDF5_hdf5_LIBRARY_RELEASE "${Matlab_LIBRARY_DIR}/libhdf5.so.8"
+        CACHE PATH "" FORCE)
 elseif(WIN32)
     set(HDF5_ROOT "${CMAKE_SOURCE_DIR}/_LowLevelCode/external/HDF5_1.8.12_win/")
 endif()
