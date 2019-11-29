7d9add5978567c231dd01ce253cae54e17db36da
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-11-29 16:38:44 +0000

    C++ tests look for data relative to HERBERT_ROOT
    Add utility function in the C++ code to retrieve an environment
    variable.
    
    This was primarily for enabling tests to read the HERBERT_ROOT
    environment variable, so test data files can be found relative to this
    path for all tests.

--- _LowLevelCode/CPP/test/get_ascii_file.tests/IIget_ascii_file.test.cpp
+++ _LowLevelCode/CPP/test/get_ascii_file.tests/IIget_ascii_file.test.cpp
@@ -1,12 +1,17 @@
 #include "get_ascii_file/get_ascii_file.h"
+#include "utility/environment.h"
 
 #include <gtest/gtest.h>
 
 #include <iostream>
 #include <string>
 
+using namespace Herbert::Utility;
+
 TEST(TestGetAsciiFile, get_ASCII_header_identifies_spe_header_type) {
-  std::string spe_file{"_test/common_data/MAP10001.spe"};
+  const std::string herbert_root{
+      Environment::get_env_variable(Environment::HERBERT_ROOT, ".")};
+  std::string spe_file{herbert_root + "/_test/common_data/MAP10001.spe"};
   std::ifstream data_stream;
   auto file_descriptor{get_ASCII_header(spe_file, data_stream)};
   ASSERT_EQ(file_descriptor.Type, fileTypes::iSPE_type);
