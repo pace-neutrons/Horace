8e92e16c478478584e5e5ee171a6559facf1ef4f
Harry Saunders <harry.saunders@stfc.ac.uk>
2019-12-03 16:55:22 +0000

    Add a bash build script to be run on build server
    A very basic first iteration build script to be run on a Linux CI
    server. There is currently a fair bit of logging to help with debugging
    which may be removed later.
    
    
    Former-commit-id: 200a67f43e19fbf890b7527c4db34eadd1977421

---
+++ tools/build_config/build_script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+function echo_and_run {
+    echo "cmd: $1"
+    eval "$1"
+}
+
+this_dir=$(dirname "$0")
+horace_src="$this_dir/../.."
+build_dir="$horace_src/DLL"
+
+echo_and_run "cmake --version"
+echo_and_run "cd $build_dir"
+
+echo ""
+echo "Running CMake configure step:"
+matlab_root="$(dirname $(readlink -f $(which matlab)))/.."
+echo "Found matlab root: $matlab_root"
+cmake_conf_cmd="cmake .. -G \"Unix Makefiles\" -DBUILD_TESTS=ON -DMatlab_ROOT_DIR=$matlab_root"
+echo_and_run "$cmake_conf_cmd"
+
+echo ""
+echo "Running build step:"
+build_cmd="cmake --build ."
+echo_and_run "$build_cmd"
+
+echo ""
+echo "Running test step: "
+test_cmd="ctest"
+echo_and_run "$test_cmd"
+
+# # cd -
