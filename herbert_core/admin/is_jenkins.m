5655ed692f38f70e69554cb4a78b195b0652ce07
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-01-15 09:18:02 +0000

    Use cell array to hold list of Jenkins env vars

--- herbert_core/admin/is_jenkins.m
+++ herbert_core/admin/is_jenkins.m
@@ -3,25 +3,10 @@ function [is_jenkins_pc] = is_jenkins()
 %   This is detected through environment variables that are always set by
 %   Jenkins.
 
-jenkins_env_vars = ['JENKINS_URL', 'JOB_URL', 'JENKINS_HOME'];
-
-[~, out] = system('env');
-vars = regexp(strtrim(out), '^(.*)=(.*)$', ...
-                'tokens', 'lineanchors', 'dotexceptnewline');
-vars = vertcat(vars{:});
-keys = vars(:,1);
-vals = vars(:,2);
-[keys,ord] = sort(keys);
-vals = vals(ord);
-for i = 1:length(vals)
-    key = keys(i);
-    val = vals(i);
-    fprintf('%s:\n    %s\n', key{1}, val{1});
-end
+jenkins_env_vars = {'JENKINS_URL', 'JOB_URL', 'JENKINS_HOME'};
 
 for i = 1:length(jenkins_env_vars)
-    env_var = getenv(jenkins_env_vars(i))
-    if isempty(getenv(jenkins_env_vars(i)))
+    if isempty(getenv(jenkins_env_vars{i}))
         is_jenkins_pc = false;
         return;
     end
