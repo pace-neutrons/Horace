e43eebf9a3fd0b34bde8d2a759a5b22076aa1acd
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-01-20 09:49:01 +0000

    Add version and platform to CPack package name
    Add a `herebrt_Version.cmake` script which determines the
    platform/version/revision of what is being built.
    
    Nightly builds will have format
    Herbert-<major.minor.patch>-yyyymmdd-<platform>-<git-revision>.<extension>
    Pull request builds will have format
    Herbert-<major.minor.patch>-<platform>-<git-revision>.<extension>
    Release builds will have format
    Herbert-<major.minor.patch>-<platform>.<extension>

---
+++ cmake/herbert_Version.cmake
@@ -0,0 +1,49 @@
+set(Herbert_FULL_VERSION "${PROJECT_VERSION}")
+
+if(HERBERT_RELEASE_TYPE STREQUAL "nightly")
+    string(TIMESTAMP _date "%Y%m%d")
+    set(Herbert_FULL_VERSION "${Herbert_FULL_VERSION}-${_date}")
+endif()
+
+macro(get_release_value)
+    set(_release_key ${ARGV0})
+    execute_process(
+        COMMAND cat "/etc/os-release"
+        COMMAND grep "-e" "^${_release_key}="
+        RESULT_VARIABLE _res
+        OUTPUT_VARIABLE ${ARGV1}
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    string(REGEX REPLACE
+        "${_release_key}=\\\"?\(.+\)\\\"?"
+        "\\1"
+        ${ARGV1}
+        ${${ARGV1}}
+    )
+    string(REPLACE " " "." ${ARGV1} ${${ARGV1}})
+    string(REPLACE "\"" "" ${ARGV1} ${${ARGV1}})
+endmacro()
+
+if(UNIX)
+    # Get distribution name from /etc/os-release
+    get_release_value("ID" _id)
+    get_release_value("VERSION_ID" _version_id)
+    set(Herbert_PLATFORM "${_id}.${_version_id}")
+elseif(WIN32)
+    set(Herbert_PLATFORM "win64")
+endif()
+set(Herbert_FULL_VERSION "${Herbert_FULL_VERSION}-${Herbert_PLATFORM}")
+
+if(NOT "${HERBERT_RELEASE_TYPE}" STREQUAL "release")
+    find_package(Git QUIET)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        RESULT_VARIABLE _res
+        OUTPUT_VARIABLE GIT_REVISION_SHA
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(Herbert_FULL_VERSION "${Herbert_FULL_VERSION}-${GIT_REVISION_SHA}")
+endif()
+
+message(STATUS "Herbert_FULL_VERSION: ${Herbert_FULL_VERSION}")
