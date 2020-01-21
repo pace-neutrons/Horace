575e8b2c4ca49d2e247e0848282582f1abe8400d
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-01-21 16:31:50 +0000

    Replace platform-specific Linux name with `linux` in version strings

--- cmake/herbert_Version.cmake
+++ cmake/herbert_Version.cmake
@@ -33,30 +33,8 @@ if(Herbert_RELEASE_TYPE STREQUAL "nightly")
     set(Herbert_FULL_VERSION "${Herbert_FULL_VERSION}-${_date}")
 endif()
 
-macro(get_release_value)
-    set(_release_key ${ARGV0})
-    execute_process(
-        COMMAND cat "/etc/os-release"
-        COMMAND grep "-e" "^${_release_key}="
-        RESULT_VARIABLE _res
-        OUTPUT_VARIABLE ${ARGV1}
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    string(REGEX REPLACE
-        "${_release_key}=\\\"?\(.+\)\\\"?"
-        "\\1"
-        ${ARGV1}
-        ${${ARGV1}}
-    )
-    string(REPLACE " " "." ${ARGV1} ${${ARGV1}})
-    string(REPLACE "\"" "" ${ARGV1} ${${ARGV1}})
-endmacro()
-
 if(UNIX)
-    # Get distribution name from /etc/os-release
-    get_release_value("ID" _id)
-    get_release_value("VERSION_ID" _version_id)
-    set(Herbert_PLATFORM "${_id}.${_version_id}")
+    set(Herbert_PLATFORM "linux")
 elseif(WIN32)
     set(Herbert_PLATFORM "win64")
 endif()
