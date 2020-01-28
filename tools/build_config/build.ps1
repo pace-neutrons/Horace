0ccaf1d01e64d05bacf8f50bd339409b2946dc0e
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-01-28 17:21:58 +0000

    Refactor build.ps1 into smaller functions

--- tools/build_config/build.ps1
+++ tools/build_config/build.ps1
@@ -11,6 +11,7 @@ param (
   [switch]$package,
   [switch]$print_versions
 )
+
 # Mapping from year to Visual Studio version
 $VS_VERSION_MAP = @{
   2015 = 'Visual Studio 14 2015';
@@ -25,7 +26,43 @@ function Write-And-Invoke([string]$command) {
   Invoke-Expression "$command"
 }
 
-function Write-Versions() {
+function Invoke-In-Dir {
+  param([string]$directory, [string]$command)
+  Push-Location -Path $directory
+  try {
+    Write-And-Invoke "$command"
+  }
+  finally {
+    Pop-Location
+  }
+}
+
+function New-Build-Directory {
+  param([string]$build_dir)
+  try {
+    Write-Output "Creating build directory: $build_dir"
+    $mkdir_cmd = "New-Item -Path $build_dir -ItemType Directory -ErrorAction Stop | Out-Null"
+    Write-And-Invoke "$mkdir_cmd"
+  }
+  catch [System.IO.IOException] {
+    Write-Warning $_.Exception.Message
+    Write-Warning "This may not be a clean build."
+  }
+}
+
+function New-CMake-Generator-Command {
+  param([int]$vs_version)
+  $cmake_generator = "$($VS_VERSION_MAP[$vs_version])"
+  if ($vs_version -eq '2019') {
+    $generator_cmd += "-G ""$cmake_generator"" -A x64"
+  }
+  else {
+    $generator_cmd += "-G ""$cmake_generator Win64"""
+  }
+  return $generator_cmd
+}
+
+function Write-Versions {
   Write-Output "$(cmake --version)"
   Write-Output "Matlab: $($(Get-Command matlab.exe).Source)"
   Write-Output "Visual Studio version: $($VS_VERSION_MAP[$vs_version])"
@@ -41,21 +78,14 @@ function Invoke-Configure {
     [string]$cmake_flags
   )
   Write-Output "`nRunning CMake configure step..."
-  $cmake_generator = "$($VS_VERSION_MAP[$vs_version])"
-  if ($vs_version -eq '2019') {
-    $generator_cmd += "-G ""$cmake_generator"" -A x64"
-  }
-  else {
-    $generator_cmd += "-G ""$cmake_generator Win64"""
-  }
   $cmake_cmd = "cmake $HERBERT_ROOT"
-  $cmake_cmd += " $generator_cmd"
+  $cmake_cmd += " $(New-CMake-Generator-Command -vs_version $vs_version)"
   $cmake_cmd += " -DCMAKE_BUILD_TYPE=$build_config"
   $cmake_cmd += " -DBUILD_TESTS=$build_tests"
   $cmake_cmd += " -DBUILD_FORTRAN=$build_fortran"
   $cmake_cmd += " $cmake_flags"
 
-  Write-And-Invoke "$cmake_cmd"
+  Invoke-In-Dir -directory $build_dir -command $cmake_cmd
   if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
   }
@@ -74,25 +104,20 @@ function Invoke-Test {
   param([string]$build_dir, [string]$build_config)
   Write-Output "`nRunning test step..."
   Push-Location -Path $build_dir
-  try {
-    Write-And-Invoke "ctest -C $build_config -T Test"
-  }
-  finally {
-    Pop-Location
-  }
+  Invoke-In-Dir -directory $build_dir -command "ctest -C $build_config -T Test"
   if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
   }
 }
 
-function Invoke-Package() {
+function Invoke-Package {
+  param([string]$build_dir)
   Write-Output "`nRunning package step..."
-  Write-And-Invoke "cpack -G ZIP"
+  Invoke-In-Dir -directory $build_dir -command "cpack -G ZIP"
   if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
   }
 }
-
 
 # Resolve/set default parameters
 if ($build_dir -eq "") {
@@ -104,29 +129,14 @@ if ($print_versions -eq $true) {
 }
 
 if ($build -eq $true) {
-  try {
-    Write-Output "Creating build directory: $build_dir"
-    $mkdir_cmd = "New-Item -Path $build_dir -ItemType Directory -ErrorAction Stop | Out-Null"
-    Write-And-Invoke "$mkdir_cmd"
-  }
-  catch [System.IO.IOException] {
-    Write-Warning $_.Exception.Message
-    Write-Warning "This may not be a clean build."
-  }
-  Push-Location -Path $build_dir
-  try {
-    Invoke-Configure `
-      -vs_version $vs_version `
-      -build_dir $build_dir `
-      -build_config $build_config `
-      -build_tests $build_tests `
-      -build_fortran $build_fortran `
-      -cmake_flags $cmake_flags
-  }
-  finally {
-    Pop-Location
-  }
-
+  New-Build-Directory -build_dir $build_dir
+  Invoke-Configure `
+    -vs_version $vs_version `
+    -build_dir $build_dir `
+    -build_config $build_config `
+    -build_tests $build_tests `
+    -build_fortran $build_fortran `
+    -cmake_flags $cmake_flags
   Invoke-Build -build_dir $build_dir -build_config $build_config
 }
 
@@ -135,11 +145,5 @@ if ($test -eq $true) {
 }
 
 if ($package -eq $true) {
-  Push-Location -Path $build_dir
-  try {
-    Invoke-Package
-  }
-  finally {
-    Pop-Location
-  }
+  Invoke-Package -build_dir $build_dir
 }
