8da90676795f903ba6553475082de43d798dd420
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-03 15:36:55 +0000

    Make sure test_gen_runfiles executes serially
    The test is not intended to run in parallel, the
    test_gen_sqw_accumulate_<framework> tests will test gen_sqw in parallel.
    
    
    Former-commit-id: d09ec1f0fd06e87ef5e1fcfb0aea9d126c504c5e

--- _test/test_sqw/test_gen_runfiles.m
+++ _test/test_sqw/test_gen_runfiles.m
@@ -1,6 +1,6 @@
 classdef test_gen_runfiles< TestCase
     % Test that a bug discovered in gen_runfiles on 31/10/2013 has been resolved
-    
+
     %banner_to_screen(mfilename)
     properties
         common_data_dir
@@ -11,17 +11,23 @@ classdef test_gen_runfiles< TestCase
 
             % -----------------------------------------------------------------------------
             % Add common functions folder to path, and get location of common data
-            hor_root = horace_root(); 
+            hor_root = horace_root();
             addpath(fullfile(hor_root,'_test','common_functions'))
             obj.common_data_dir=fullfile(hor_root,'_test','common_data');
             % -----------------------------------------------------------------------------
         end
-        function test_gen_sqw(obj)
-            
+        function test_gen_sqw_serial(obj)
+
+            % This test should always run serially
+            % test_gen_sqw_accumulate_sqw_<framework> tests in parallel
+            original_hpc_conf = hpc_config().get_data_to_store();
+            set(hpc_config, 'build_sqw_in_parallel', false)
+            cleanup = onCleanup(@() set(hpc_config, original_hpc_conf));
+
             outdir=tmp_dir;     % directory of spe and tmp files
-            
+
             nfiles_max=2;
-            
+
             % =====================================================================================================================
             % Make spe files
             % =====================================================================================================================
@@ -32,7 +38,7 @@ classdef test_gen_runfiles< TestCase
             end
             sqw_file_12=fullfile(outdir,'test_gen_runfiles_sqw_12.sqw');
             cleanup_obj=onCleanup(@()delete(spe_file{:},sqw_file_12));
-            
+
             en=cell(1,nfiles_max);
             efix=zeros(1,nfiles_max);
             psi=zeros(1,nfiles_max);
@@ -50,36 +56,36 @@ classdef test_gen_runfiles< TestCase
                 gs(i)=2.4+i/7;
             end
             psi=90:-1:90-nfiles_max+1;
-            
+
             emode=1;
             alatt=[4.4,5.5,6.6];
             angdeg=[100,105,110];
             u=[1.02,0.99,0.02];
             v=[0.025,-0.01,1.04];
-            
+
             pars=[1000,8,2,4,0];  % [Seff,SJ,gap,gamma,bkconst]
             scale=0.3;
             for i=1:nfiles_max
                 simulate_spe_testfunc (en{i}, par_file, spe_file{i}, @sqw_sc_hfm_testfunc, pars, scale,...
                     efix(i), emode, alatt, angdeg, u, v, psi(i), omega(i), dpsi(i), gl(i), gs(i));
             end
-            
+
             % =====================================================================================================================
             % The line that failed before: the problem was efix the same for two files, but number of energy bins different
             % =====================================================================================================================
-            
+
             try
                 gen_sqw (spe_file, par_file, sqw_file_12, efix(1), emode, alatt, angdeg, u, v, psi, omega, dpsi, gl, gs, 'replicate');
-            catch Err               
+            catch Err
                 rethrow(Err);
             end
-            
+
             assertEqual(exist(spe_file{1},'file'),2)
             assertEqual(exist(spe_file{2},'file'),2)
-            
-            assertEqual(exist(sqw_file_12,'file'),2)            
+
+            assertEqual(exist(sqw_file_12,'file'),2)
             %banner_to_screen([mfilename,': Test(s) passed'],'bot')
         end
-        
+
     end
 end
