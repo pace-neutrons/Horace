9c1154a33819a4f0b85cb265be168f90ca5a982e
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-03 13:42:57 +0000

    Hit validate_horace with MBeautifier

--- admin/validate_horace.m
+++ admin/validate_horace.m
@@ -21,7 +21,7 @@ function err = validate_horace(varargin)
 %                                       % default otherwise for Horace to revert to
 %                                       % using matlab code.
 %   >> validate_horace (...'-exit_on_completion') % Exit Matlab when test suite ends.
-                                                  % Exits with non-zero error code if any tests failed
+% Exits with non-zero error code if any tests failed
 
 % $Revision:: 1758 ($Date:: 2019-12-16 18:18:50 +0000 (Mon, 16 Dec 2019) $)
 
@@ -32,35 +32,35 @@ end
 
 % Parse arguments
 % ---------------
-options = {'-parallel','-talkative','-nomex','-forcemex','-exit_on_completion'};
-[ok,mess,parallel,talkative,nomex,forcemex, exit_on_completion, test_folders] = ...
-        parse_char_options(varargin,options);
+options = {'-parallel', '-talkative', '-nomex', '-forcemex', '-exit_on_completion'};
+[ok, mess, parallel, talkative, nomex, forcemex, exit_on_completion, test_folders] = ...
+    parse_char_options(varargin, options);
 if ~ok
-    error('VALIDATE_HORACE:invalid_argument',mess)
+    error('VALIDATE_HORACE:invalid_argument', mess)
 end
 
 %==============================================================================
 % Place list of test folders here (relative to the master _test folder)
 % -----------------------------------------------------------------------------
-if isempty(test_folders)  % no tests specified on command line - run them all
-    test_folders={...
-        'test_ascii_column_data',...
-        'test_change_crystal',...
-        'test_combine',...
-        'test_dnd',...
-        'test_gen_sqw_for_powders',...
-        'test_herbert_utilites',...
-        'test_mslice_utilities',...
-        'test_multifit',...
-        'test_rebin',...
-        'test_sqw',...
-        'test_sqw_file',...
-        'test_symmetrisation',...
-        'test_tobyfit',...
-        'test_transformation'...
-        'test_utilities',...
-        'test_sym_op'...
-        % 'test_spinw_integration',...
+if isempty(test_folders) % no tests specified on command line - run them all
+    % 'test_spinw_integration',...
+    test_folders = { ...
+        'test_ascii_column_data', ...
+        'test_change_crystal', ...
+        'test_combine', ...
+        'test_dnd', ...
+        'test_gen_sqw_for_powders', ...
+        'test_herbert_utilites', ...
+        'test_mslice_utilities', ...
+        'test_multifit', ...
+        'test_rebin', ...
+        'test_sqw', ...
+        'test_sqw_file', ...
+        'test_symmetrisation', ...
+        'test_tobyfit', ...
+        'test_transformation', ...
+        'test_utilities', ...
+        'test_sym_op', ...
         };
 end
 
@@ -68,14 +68,14 @@ end
 % Check mex files if mex functions are available
 % ----------------------------------------------
 
-[mess,n_errors]=check_horace_mex();
-if n_errors==0
-    test_folders{end+1}='test_mex_nomex';
+[~, n_errors] = check_horace_mex();
+if n_errors == 0
+    test_folders{end+1} = 'test_mex_nomex';
 else
     nomex = true;
-    warning('VALIDATE_HORACE:mex','mex files are not all working, and will not be tested');
+    warning('VALIDATE_HORACE:mex', 'mex files are not all working, and will not be tested');
     if forcemex
-        error('VALIDATE_HORACE:mex','cannot force mex if mex files are not working');
+        error('VALIDATE_HORACE:mex', 'cannot force mex if mex files are not working');
     end
 end
 
@@ -83,8 +83,8 @@ end
 % Generate full test paths to unit tests
 % --------------------------------------
 horace_path = horace_root();
-test_path=fullfile(horace_path,'_test');
-test_folders_full = cellfun(@(x)fullfile(test_path,x),test_folders,'UniformOutput',false);
+test_path = fullfile(horace_path, '_test');
+test_folders_full = cellfun(@(x)fullfile(test_path, x), test_folders, 'UniformOutput', false);
 
 
 hec = herbert_config();
@@ -96,19 +96,18 @@ clear config_store;
 % (Validation must always return Horace and Herbert to their initial states, regardless
 %  of any changes made in the test routines)
 
-
 % On exit always revert to initial Horace and Herbert configurations
 % ------------------------------------------------------------------
-cur_herbert_conf=hec.get_data_to_store();
-cur_horace_config=hoc.get_data_to_store();   % only get the public i.e. not sealed, fields
+cur_herbert_conf = hec.get_data_to_store();
+cur_horace_config = hoc.get_data_to_store(); % only get the public i.e. not sealed, fields
 cur_hpc_config = hpc.get_data_to_store();
 hec.saveable = false;
 hoc.saveable = false;
 hpc.saveable = false;
 
 % Create cleanup object (*** MUST BE DONE BEFORE ANY CHANGES TO CONFIGURATIONS)
-cleanup_obj=onCleanup(@()...
-    validate_horace_cleanup(cur_herbert_conf,cur_horace_config,cur_hpc_config,{}));
+cleanup_obj = onCleanup(@() ...
+    validate_horace_cleanup(cur_herbert_conf, cur_horace_config, cur_hpc_config, {}));
 
 
 % Run unit tests
@@ -117,30 +116,30 @@ cleanup_obj=onCleanup(@()...
 % (The validation should be done starting with the defaults, otherwise an error
 %  may be due to a poor choice by the user of configuration parameters)
 
-set(hec,'defaults');
-set(hoc,'defaults');
+set(hec, 'defaults');
+set(hoc, 'defaults');
 
 % Special unit tests settings.
-hec.init_tests=true;       % initialise unit tests
+hec.init_tests = true; % initialise unit tests
 hoc.use_mex = ~nomex;
-hoc.force_mex_if_use_mex=forcemex;
+hoc.force_mex_if_use_mex = forcemex;
 if ~talkative
-    set(hec,'log_level',-1);    % turn off informational output
+    set(hec, 'log_level', -1); % turn off informational output
 end
 
 
-if parallel && license('checkout','Distrib_Computing_Toolbox')
+if parallel && license('checkout', 'Distrib_Computing_Toolbox')
     cores = feature('numCores');
-    if verLessThan('matlab','8.4')
-        if matlabpool('SIZE')==0
-            if cores>12
+    if verLessThan('matlab', '8.4')
+        if matlabpool('SIZE') == 0
+            if cores > 12
                 cores = 12;
             end
             matlabpool(cores);
         end
     else
         if isempty(gcp('nocreate'))
-            if cores>12
+            if cores > 12
                 cores = 12;
             end
             parpool(cores);
@@ -148,16 +147,16 @@ if parallel && license('checkout','Distrib_Computing_Toolbox')
         end
     end
     test_ok = false(1, numel(test_folders_full));
-    time=bigtic();
-    parfor i=1:numel(test_folders_full)
+    time = bigtic();
+    parfor i = 1:numel(test_folders_full)
         test_ok(i) = runtests(test_folders_full{i})
     end
-    bigtoc(time,'===COMPLETED UNIT TESTS IN PARALLEL');
+    bigtoc(time, '===COMPLETED UNIT TESTS IN PARALLEL');
     tests_ok = all(test_ok);
 else
-    time=bigtic();
+    time = bigtic();
     tests_ok = runtests(test_folders_full{:});
-    bigtoc(time,'===COMPLETED UNIT TESTS RUN ');
+    bigtoc(time, '===COMPLETED UNIT TESTS RUN ');
 
 end
 close all
@@ -171,16 +170,15 @@ if exit_on_completion
 end
 
 %=================================================================================================================
-function validate_horace_cleanup(cur_herbert_config,cur_horace_config,cur_hpc_config,test_folders)
-% Reset the configurations, and remove unit test folders from the path
-set(herbert_config,cur_herbert_config);
-set(hor_config,cur_horace_config);
-set(hpc_config,cur_hpc_config);
+function validate_horace_cleanup(cur_herbert_config, cur_horace_config, cur_hpc_config, test_folders)
+    % Reset the configurations, and remove unit test folders from the path
+    set(herbert_config, cur_herbert_config);
+    set(hor_config, cur_horace_config);
+    set(hpc_config, cur_hpc_config);
 
-% Clear up the test folders, previously placed on the path
-warn = warning('off','all'); % avoid warning on deleting non-existent path
-for i=1:numel(test_folders)
-    rmpath(test_folders{i});
-end
-warning(warn);
-
+    % Clear up the test folders, previously placed on the path
+    warn = warning('off', 'all'); % avoid warning on deleting non-existent path
+    for i = 1:numel(test_folders)
+        rmpath(test_folders{i});
+    end
+    warning(warn);
