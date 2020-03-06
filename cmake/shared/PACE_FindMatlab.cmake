b93b03161a32629cf826c9d12db2ec3d474b0f39
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-06 10:53:18 +0000

    Move general PACE CMake files into cmake/shared
    This gives some logical separation between CMake files that are intended
    for use by Herbert only and CMake files that are intended to be used
    across other PACE projects.

--- cmake/shared/PACE_FindMatlab.cmake
+++ cmake/shared/PACE_FindMatlab.cmake
@@ -41,7 +41,7 @@ See the FindMatlab.cmake documentation for other variables defined by this
 module. You'll find the FindMatlab.cmake script bundled with this repo.
 
 #]=======================================================================]
-include(MatlabHelpers)
+include(PACE_MatlabHelpers)
 
 # Call `find_package(Matlab)` using passed in arguments `Matlab_ROOT_DIR` and/or
 # `Matlab_RELEASE` to find the desired version.
