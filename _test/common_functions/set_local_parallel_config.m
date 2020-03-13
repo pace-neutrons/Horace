b2a04e629cc46046de673f125c2a3199ec5d99eb
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-13 11:00:28 +0000

    Hit some test files with MBeautifier

--- _test/common_functions/set_local_parallel_config.m
+++ _test/common_functions/set_local_parallel_config.m
@@ -3,8 +3,8 @@ function [new_config, old_config] = set_local_parallel_config()
     %
     % The config directory is moved to "tempdir/config" and the shared directories
     % for parallel communications are set to "tempdir/local" and "tempdir/remote".
-    % 
-    % This function is useful for if you're running the same tests in parallel on 
+    %
+    % This function is useful for if you're running the same tests in parallel on
     % the same machine (e.g. on a build server), it avoids IO errors when separate
     % processes attempt to read/write the same config/temporary files.
     %
@@ -17,12 +17,11 @@ function [new_config, old_config] = set_local_parallel_config()
     CONFIG_FOLDER = fullfile(tempdir(), 'config');
     SHARED_LOCAL_DIR = fullfile(tempdir(), 'local');
     SHARED_REMOTE_DIR = fullfile(tempdir(), 'remote');
-     
+
     old_config = parallel_config().get_data_to_store();
-    
+
     config_store.set_config_folder(CONFIG_FOLDER);
-    
+
     new_config = parallel_config;
     new_config.shared_folder_on_local = SHARED_LOCAL_DIR;
     new_config.shared_folder_on_remote = SHARED_REMOTE_DIR;
-    
