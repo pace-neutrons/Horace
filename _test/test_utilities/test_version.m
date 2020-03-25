fddabec40671bae02da49d48409c6407e02fbc2f
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-25 12:46:33 +0000

    Add Matlab tests for herbert_version.m

---
+++ _test/test_utilities/test_version.m
@@ -0,0 +1,24 @@
+classdef test_version < TestCase
+
+properties
+    VERSION_REGEX = '[0-9]\.[0-9]\.[0-9](\.[0-9a-z]+|)'
+end
+
+methods 
+    function obj = test_full_version_string_returned_if_nargout_is_1(obj)
+        version_string = herbert_version();
+        match_start = regexp(version_string, obj.VERSION_REGEX, 'ONCE');
+        assertFalse(isempty(match_start));
+    end
+    
+    function obj = test_major_minor_and_patch_returned_if_nargout_is_3(obj)
+        version_string = herbert_version();
+        numbers = split(version_string, '.');
+        [major, minor, vpatch] = herbert_version();
+        assertEqual(major, numbers{1});
+        assertEqual(minor, numbers{2});
+        assertEqual(vpatch, numbers{3});
+    end
+end
+
+end
