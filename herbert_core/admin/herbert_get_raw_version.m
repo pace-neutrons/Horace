5901477974e015d3398cd53380896f93d81ea36a
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-25 17:51:40 +0000

    Add herbert_get_raw_version.m function and template
    This will now be where CMake formats Herbert's version into. This
    function is called by herbert_version() to give an API to the function.
    This keeps the amount of (duplicated) code in the template and default
    version of herbert_get_raw_version to a minimum.
    
    The default version number (i.e. for a clean checkout of the repo that
    has not been built) is 0.0.0.dev.

---
+++ herbert_core/admin/herbert_get_raw_version.m
@@ -0,0 +1,8 @@
+% This file will be overwritten at CMake configure time, any edits will be lost
+% This file is se to be assumed unchanged by Git - this is so that when it is
+% overwritten by CMake it does not show up as modfied. If you need to make 
+% changes to this file  you can run:
+%       git update-index --no-assume-unchanged <file>
+%
+function out = herbert_get_raw_version()
+out = '0.0.0.dev';
