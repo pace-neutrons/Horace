d596b0b19d85d06f4249ab56e8521d3909cc22e7
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-03-25 11:36:08 +0000

    Re pace-neutrons/Herbert#101 disabling test_job_fail_restart on Unix

--- _test/test_mpi/test_job_dispatcher_mpiexec.m
+++ _test/test_mpi/test_job_dispatcher_mpiexec.m
@@ -14,10 +14,15 @@ classdef test_job_dispatcher_mpiexec < job_dispatcher_common_tests
             this = this@job_dispatcher_common_tests(name,'mpiexec_mpi');
         end
         %     
-%         function test_job_fail_restart(obj, varargin)
-%         end
+        function test_job_fail_restart(obj, varargin)
+            if isunix
+                return
+            else
+                test_job_fail_restart@job_dispatcher_common_tests(obj, varargin{:})                                
+            end
+        end
         function test_job_with_logs_3workers(obj, varargin)
-            % disabled for windows and unixx
+            % disabled for windows and unix
         end
         
     end
