4845f4e48a923551402772d77af9ff8f3abf8006
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-03-27 10:08:13 +0000

    Re pace-neutrons/Herbert#101 minor comments and code improvement,

--- herbert_core/classes/data_loaders/saveNxspe.m
+++ herbert_core/classes/data_loaders/saveNxspe.m
@@ -1,4 +1,4 @@
-function  saveNXSPE(nxspe_filename,data2save,varargin)
+function  saveNxspe(nxspe_filename,data2save,varargin)
 % Save data obtained from various data sources as nxspe file.
 %
 % Inputs:
@@ -21,12 +21,12 @@ if ~ischar(nxspe_filename)
     if iscell(nxspe_filename)
         for i=1:numel(nxspe_filename)
             if ~ischar(nxspe_filename)
-                error('saveNXSPE:invalid_argument',...
+                error('saveNxspe:invalid_argument',...
                 ' If first input is a cellarray, each element of cellarray must me a filename.')
             end
         end
     else
-        error('saveNXSPE:invalid_argument',...
+        error('saveNxspe:invalid_argument',...
             'first input should be a filename to save nxspe to');
     end
 end
@@ -35,23 +35,23 @@ if isa(data2save,'rundata') % just use existing
     return
 end
 if ~isstruct(data2save)
-    error('saveNXSPE:invalid_argument',...
+    error('saveNxspe:invalid_argument',...
         'second input should be a structure, containign dat to save as nxspe');
 end
 if isfield(data2save,'efixed') && isfield(data2save,'det_theta') ...
         && isfield(data2save,'emode')
     % that's "fromwindow" info from mslice. Process it
-    rd = parce_fromwindow(data2save,nxspe_filename);
+    rd = parce_fromwindow(data2save);
     rd.saveNXSPE(nxspe_filename,'w');
     return
 end
 
-error('saveNXSPE:not_implemented',...
-    ' Advanced options on saveNXSPE are not yet implemented');
+error('saveNxspe:not_implemented',...
+    ' Advanced options on saveNxspe are not yet implemented');
 
 
 
-function rd = parce_fromwindow(data2save,nxspe_filename)
+function rd = parce_fromwindow(data2save)
 % the routine takes fromwindow mslice structure and converts it into
 % rundata to save in nxspe file
 tf = struct();
@@ -61,8 +61,7 @@ tf.en = data2save.en;
 tf.det_par = [ones(numel(data2save.det_theta),1),data2save.det_theta*(180/pi),...
     data2save.det_psi*(180/pi),data2save.det_dtheta,...
     data2save.det_dpsi,data2save.det_group]';
-%[~,fn] = fileparts(nxspe_filename);
-%tf.save(fn);
+
 lat = oriented_lattice();
 if isfield(data2save,'psi_samp')
     lat.psi = data2save.psi_samp*180/pi;
