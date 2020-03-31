ef92e2cbd907ac1e3e0cc4b2d7a9796508a77c89
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-03-31 10:44:28 +0100

    Strip trailing whitespaces

--- _test/test_instrument_classes/test_IX_sample.m
+++ _test/test_instrument_classes/test_IX_sample.m
@@ -9,7 +9,7 @@ classdef test_IX_sample < TestCaseWithSave
         s3
         slookup
     end
-    
+
     methods
         %--------------------------------------------------------------------------
         function self = test_IX_sample (name)
@@ -17,21 +17,21 @@ classdef test_IX_sample < TestCaseWithSave
                 name = 'test_IX_sample';
             end
             self@TestCaseWithSave(name);
-            
+
             % Make some samples and sample arrays
             self.sam1 = IX_sample ([1,0,0],[0,1,0],'cuboid',[2,3,4]);
             self.sam2 = IX_sample ([0,1,0],[0,0,1],'cuboid',[12,13,34]);
             self.sam3 = IX_sample ([1,1,0],[0,0,1],'cuboid',[22,23,24]);
-            
+
             self.s1 = [self.sam1, self.sam1, self.sam2, self.sam2, self.sam2];
             self.s2 = [self.sam3, self.sam1, self.sam2, self.sam3, self.sam1];
             self.s3 = [self.sam2, self.sam3, self.sam1, self.sam2, self.sam3];
-            
+
             self.slookup = object_lookup({self.s1, self.s2, self.s3});
-            
+
             self.save()
         end
-        
+
         %--------------------------------------------------------------------------
         function test_IX_sample_constructor_error_if_required_args_missing(name)
             f = @()IX_sample([1,0,0],[0,1,0],'cuboid');
@@ -132,16 +132,15 @@ classdef test_IX_sample < TestCaseWithSave
             samp = rand_ind(self.slookup,2,ind);
             samp2 = samp(:,ind==2);
             samp3 = samp(:,ind==3);
-            
-            
+
             mean2 = mean(samp2,2);
             mean3 = mean(samp3,2);
             std2 = std(samp2,1,2);
             std3 = std(samp3,1,2);
-            
+
             assertEqualToTol(mean2, [0;0;0], 'tol', 0.003);
             assertEqualToTol(mean3, [0;0;0], 'tol', 0.02);
-            
+
             assertEqualToTol(std2, self.sam1.ps'/sqrt(12), 'reltol', 0.001);
             assertEqualToTol(std3, self.sam2.ps'/sqrt(12), 'tol', 0.01);
         end
