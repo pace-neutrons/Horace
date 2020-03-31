376d0a63de1fcf0f1866d5448445d71f50cd9109
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-03-31 12:14:53 +0100

    Re-enable test_job_fail_restart test

--- _test/disabled_tests.md
+++ _test/disabled_tests.md
@@ -3,21 +3,8 @@
 - test_herbert_on.m
     - test_herWrongEmpty (no ticket)
 
-- MPI_test
-   # disabled on Jenkins:
-   - test_job_dispatcher_herbert:test_job_fail_restart
-   - test_job_dispatcher_parpool:test_job_fail_restart
-   - test_job_dispatcher_mpiexec:test_job_fail_restart
-    https://github.com/pace-neutrons/Herbert/issues/92
-
-  # disabled on Windows:
-   - test_job_dispatcher_mpiexec:test_job_fail_restart
-     works on some windows machines and fails on other. Needs investigations
-
-
 - test_ParpoolMPI_Framework.m
    - test_labprobe_nonmpi (no ticket)
-
 
 - test_job_dispatcher_parpool.m
   - test_job_submittion (no ticket)
--- _test/test_mpi/test_job_dispatcher_parpool.m
+++ _test/test_mpi/test_job_dispatcher_parpool.m
@@ -99,18 +99,11 @@ classdef test_job_dispatcher_parpool< job_dispatcher_common_tests
             delete(cjob)
         end
         function test_job_fail_restart(obj, varargin)
-            if is_jenkins || ispc
-                warning('test_job_fail_restart disabled')
-                return
-            else
-                test_job_fail_restart@job_dispatcher_common_tests(obj, varargin{:})
-            end
+            test_job_fail_restart@job_dispatcher_common_tests(obj, varargin{:})
         end
         function test_job_with_logs_3workers(obj, varargin)
             test_job_with_logs_3workers@job_dispatcher_common_tests(obj, varargin{:})
         end
-        
-        
     end
 end
 
