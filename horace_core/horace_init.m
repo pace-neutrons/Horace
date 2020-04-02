6a400393da9bba338ead2aff0cda441e861dd207
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-02 09:24:02 +0100

    Update horace_init to print new version string
    Former-commit-id: 0e90589729ade0f98d7a30807574e02d94a31422

--- horace_core/horace_init.m
+++ horace_core/horace_init.m
@@ -61,8 +61,6 @@ horace_plot.name_contour = 'Horace contour plot';
 horace_plot.name_sliceomatic = 'Sliceomatic';
 set_global_var('horace_plot',horace_plot);
 
-[~,Matlab_code,mexMinVer,mexMaxVer,date] = horace_version();
-mc = [Matlab_code(1:48),'$)'];
 %
 hc = hor_config;
 check_mex = false;
@@ -90,8 +88,6 @@ if check_mex
     end
 end
 
-
-
 hec = herbert_config;
 if hec.init_tests
     % add path to folders, which responsible for administrative operations
@@ -105,34 +101,7 @@ end
 % an sqw file and is automatically converted into MAPS
 warning('off','SQW_FILE:old_version')
 
-
-disp('!==================================================================!')
-disp('!                      HORACE                                      !')
-disp('!------------------------------------------------------------------!')
-disp('!  Visualisation of multi-dimensional neutron spectroscopy data    !')
-disp('!                                                                  !')
-disp('!  R.A. Ewings, A. Buts, M.D. Le, J van Duijn,                     !')
-disp('!  I. Bustinduy, and T.G. Perring                                  !')
-disp('!                                                                  !')
-disp('!  Nucl. Inst. Meth. A 834, 132-142 (2016)                         !')
-disp('!                                                                  !')
-disp('!  http://dx.doi.org/10.1016/j.nima.2016.07.036                    !')
-disp('!------------------------------------------------------------------!')
-disp(['! Matlab  code: ',mc,' !']);
-if isempty(mexMaxVer)
-    disp('! Mex code:    Disabled  or not supported on this platform         !')
-else
-    if mexMinVer==mexMaxVer
-        mess=sprintf('! Mex files   : $Revision:: %4d (%s  $) !',mexMaxVer,date(1:28));
-    else
-        mess=sprintf(...
-            '! Mex files   :$Revisions::%4d-%3d(%s$)!',mexMinVer,mexMaxVer,date(1:28));
-    end
-    disp(mess)
-    
-end
-disp('!------------------------------------------------------------------!')
-
+print_banner();
 
 %--------------------------------------------------------------------------
 function addpath_message (type,varargin)
@@ -184,3 +153,23 @@ if ~ok
         ' Can not write script file: %s used as local worker in gen_sqw tests; Error: %s',...
         target,mess)
 end
+
+function print_banner()
+    width = 66;
+    lines = {
+        ['Horace ', horace_version()], ...
+        repmat('-', 1, width), ...
+        'Visualisation of multi-dimensional neutron spectroscopy data', ...
+        '', ...
+        'R.A. Ewings, A. Buts, M.D. Le, J van Duijn,', ...
+        'I. Bustinduy, and T.G. Perring', ...
+        '', ...
+        'Nucl. Inst. Meth. A 834, 132-142 (2016)', ...
+        '', ...
+        'http://dx.doi.org/10.1016/j.nima.2016.07.036'
+    };            
+    fprintf('!%s!\n', repmat('=', 1, width));                
+    for i = 1:numel(lines)
+        fprintf('!%s!\n', center_and_pad_string(lines{i}, ' ', width));
+    end
+    fprintf('!%s!\n', repmat('-', 1, width));
