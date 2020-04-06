91e7dd012d2e5998ba5dfd31bda680d60e53333b
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-06 14:42:42 +0100

    Stop cpp_communicator segfault if called with nlhs=0

--- _LowLevelCode/CPP/cpp_communicator/cpp_communicator.cpp
+++ _LowLevelCode/CPP/cpp_communicator/cpp_communicator.cpp
@@ -93,7 +93,7 @@ Outputs:
 
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
 {
-    if (nrhs == 0 && nlhs == 1) {
+    if (nrhs == 0 && (nlhs == 0 || nlhs == 1)) {
         plhs[0] = mxCreateString(Herbert::VERSION);
         return;
     }
