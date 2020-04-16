f26fb4b059aec237803cc436bd0fbb7236d40c4d
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-04-16 19:53:12 +0100

    Re #182 Disabled gen_sqw_1456

--- _test/test_sqw_gen_workflow/test_gen_sqw_accumulate_sqw_parpool.m
+++ _test/test_sqw_gen_workflow/test_gen_sqw_accumulate_sqw_parpool.m
@@ -88,15 +88,14 @@ classdef test_gen_sqw_accumulate_sqw_parpool <  ...
         %             end
         %
         %         end
-        %         function test_accumulate_sqw1456(obj,varargin)
-        %             if is_jenkins && ispc
-        %                 warning('test_accumulate_sqw1456 disabled')
-        %                 return
-        %             else
-        %                 test_accumulate_sqw1456@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
-        %             end
-        %
-        %         end
+        function test_accumulate_sqw1456(obj,varargin)
+            if is_jenkins && ispc
+                warning('test_accumulate_sqw1456 disabled')
+            else
+                test_accumulate_sqw1456@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
+            end
+            
+        end
         function test_accumulate_sqw11456(obj,varargin)
             if is_jenkins && ispc
                 warning('test_accumulate_sqw11456 disabled')
