f9ad43aede38c01da155fd1b4acef06f160b0fa7
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-04-16 15:15:29 +0100

    Re #182 one more dodgy parpool test on Windows
    Former-commit-id: d2e5798c7a99ffeff1f0a810306aef9d3dc07b6f

--- _test/test_sqw_gen_workflow/test_gen_sqw_accumulate_sqw_parpool.m
+++ _test/test_sqw_gen_workflow/test_gen_sqw_accumulate_sqw_parpool.m
@@ -64,14 +64,13 @@ classdef test_gen_sqw_accumulate_sqw_parpool <  ...
         
         %         %------------------------------------------------------------------
         %         %         % Block of code to disable some tests for debugging Jenkins jobs
-        %         function test_gen_sqw(obj,varargin)
-        %             if is_jenkins && ispc
-        %                 warning('test_gen_sqw disabled')
-        %                 return
-        %             else
-        %                 test_gen_sqw@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
-        %             end
-        %         end
+        function test_gen_sqw(obj,varargin)
+            if is_jenkins && ispc
+                warning('test_gen_sqw disabled')
+            else
+                test_gen_sqw@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
+            end
+        end
         function test_accumulate_sqw14(obj,varargin)
             if is_jenkins && ispc
                 warning('test_accumulate_sqw14 disabled')
