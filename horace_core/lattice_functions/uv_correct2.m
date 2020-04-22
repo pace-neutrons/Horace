b57f49dc301381ef59a52a73ab690d91a0357dc6
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-22 12:20:10 +0100

    Hit edited files with MBeautifier
    Files edited as part of #198 hit with code formatter
    
    
    Former-commit-id: 9ac8a2d13ec4791ec16c2c22afc1fe00211ffea9

--- horace_core/lattice_functions/uv_correct2.m
+++ horace_core/lattice_functions/uv_correct2.m
@@ -3,12 +3,12 @@ function [u_true, v_true, alatt_true, angdeg_true] = uv_correct2 (u, v, alatt, a
 %
 %   >> [u_true, v_true] = uv_correct2 (u, v, rlu_corr, alatt, angdeg)
 %
-% This function can be used to correct the scattering plane vectors to the true wectors once the 
+% This function can be used to correct the scattering plane vectors to the true wectors once the
 % misorientation angles and true lattice parameters have been determined.
 %
 % In particular, this can be used to get the true u and v after determining the misorientation parameters
 % e.g. with Tobyfit. All that is required is to change u and v (and the lattice parameters to
-% alatt_true and angdeg_true if these are different from alatt and angdeg). The value of psi does 
+% alatt_true and angdeg_true if these are different from alatt and angdeg). The value of psi does
 % not need to be changed, as that is accounted for in the output vectors u_true and v_true.
 %
 % Input:
@@ -16,7 +16,7 @@ function [u_true, v_true, alatt_true, angdeg_true] = uv_correct2 (u, v, alatt, a
 %   u,v                 Vectors (in rlu) used to define the scattering plane, as expressed in the notional lattice
 %   alatt,              Lattice parameters of notional lattice [a,b,c] (Ang)
 %   angdeg              Lattice angles of notional lattice [alf,bet,gam] (deg)
-%   rlu_corr            Matrix to convert from coords in notional reciprocal lattice to true reciprocal lattice, 
+%   rlu_corr            Matrix to convert from coords in notional reciprocal lattice to true reciprocal lattice,
 %                      accounting for the misorientation of the lattice and the true lattice parameters. This
 %                      matrix will have been obtained using the function refine_crystal (type
 %                      >> help refine_crystal   for more information)
@@ -29,11 +29,11 @@ function [u_true, v_true, alatt_true, angdeg_true] = uv_correct2 (u, v, alatt, a
 %   alatt_true          True values for lattice parmeters [a,b,c] (Ang)
 %   angdeg_true         True calues of lattice angles [alf,bet,gam] (deg)
 
-u_true=rlu_corr*u(:);   % make u a column vector
-v_true=rlu_corr*v(:);   % make v a column vector
-[alatt_true,angdeg_true,rotmat,ok,mess]=rlu_corr_to_lattice(rlu_corr,alatt,angdeg);
+u_true = rlu_corr * u(:); % make u a column vector
+v_true = rlu_corr * v(:); % make v a column vector
+[alatt_true, angdeg_true, rotmat, ok, mess] = rlu_corr_to_lattice(rlu_corr, alatt, angdeg);
 if ~ok, error(mess), end
 
 % Reshape output
-u_true=reshape(u_true,size(u));
-v_true=reshape(v_true,size(v));
+u_true = reshape(u_true, size(u));
+v_true = reshape(v_true, size(v));
