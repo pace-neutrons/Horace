c48ee37e531332f3d3985b3ec585b2051fc52720
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-23 15:49:32 +0100

    Remove rebin_3d_z_hist_mex binaries
    This removes the Fortran mex binaries and updates usages to use the
    Matlab equivalent

--- herbert_core/utilities/classes/@IX_data_3d/rebin_hist.m
+++ herbert_core/utilities/classes/@IX_data_3d/rebin_hist.m
@@ -1,4 +1,4 @@
-function [wout_s, wout_e] = rebin_hist(iax,x, s, e, xout, use_mex, force_mex)
+function [wout_s, wout_e] = rebin_hist(iax,x, s, e, xout)
 % Rebins histogram data along specific axis.
 %
 % Overcomplicated but follows minimal change rules
@@ -8,7 +8,7 @@ switch iax
     case(2)
         [wout_s,wout_e] = rebin_3d_y_hist (x, s, e, xout);
     case(3)
-        [wout_s,wout_e] = rebin_3d_z_hist (x, s, e, xout, use_mex, force_mex);
+        [wout_s,wout_e] = rebin_3d_z_hist (x, s, e, xout);
     otherwise
         error('IX_data_3d:invalid_argument',...
             'rebin axis number=%d but for 3D dataset it can be ony 1,2 or 3',iax);
