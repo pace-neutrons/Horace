4b133bb82f2ac752e2aca6dd7811018e2b999dff
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-28 11:01:19 +0100

    Delete Herbert directory if exits in Jenkins
    The correct branch will then be checked out
    
    
    Former-commit-id: 78f34ef71918a1e89a2c5140d4c3a078c23b7cac

--- tools/build_config/clone_herbert_branch.sh
+++ tools/build_config/clone_herbert_branch.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-# Clone and build the given Herbert branch. If no branch is given, this
-# defaults to cloning master
+# Clone and build the given Herbert branch. If no branch is given, default to
+# master
 
+readonly DEFAULT_BRANCH="master"
 readonly HERBERT_URL="https://github.com/pace-neutrons/Herbert.git"
+
 readonly bash_helpers="$(realpath "$(dirname "$0")")/bash_helpers.sh"
 
 . "${bash_helpers}"  # imports echo_and_run
 
 herbert_branch=${1-master}
 if [ "${herbert_branch}" = "" ]; then
-    herbert_branch="master"
+    herbert_branch="${DEFAULT_BRANCH}"
 fi
 
-echo "Cloning Herbert branch '${herbert_branch}'..."
+echo "Cloning and building Herbert branch '${herbert_branch}'..."
+if [[ -d "Herbert" ]]; then
+    echo_and_run "rm -r Herbert"
+fi
 echo_and_run "git clone ${HERBERT_URL} --depth 1 --branch ${herbert_branch}" &&
 echo_and_run "cd Herbert" &&
 echo_and_run "./tools/build_config/build.sh --build"
