7ee409d2dcd8112aa277ab1779f4d81025d09725
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-28 12:22:23 +0100

    Add clone_herbert_branch.ps1 script
    This will be called by the Jenkinsfile during PR builds to checkout
    Herbert source for GitHub

---
+++ tools/build_config/clone_herbert_branch.ps1
@@ -0,0 +1,26 @@
+# Clone and build the given Herbert branch. If no branch is given, default to
+# master
+# Pass a specific branch using the -branch parameter, any other arguments are
+# passed verbatim to the Herbert build script
+
+param(
+  [string][Alias("B")]$branch = "master"
+)
+
+. $PSScriptRoot/powershell_helpers.ps1 <# Imports:
+  Write-And-Invoke
+  Invoke-In-Dir
+#>
+
+$HERBERT_URL = "https://github.com/pace-neutrons/Herbert.git"
+
+Write-Output "Cloning and building Herbert branch $branch..."
+if (Test-Path -Path "./Herbert") {
+  Invoke-In-Dir -directory "Herbert" -command "git fetch origin"
+  Invoke-In-Dir -directory "Herbert" -command "git checkout origin/$branch"
+
+} else {
+  Write-And-Invoke "git clone $HERBERT_URL --depth 1 --branch $branch"
+}
+$build_cmd = "./tools/build_config/build.ps1 -build $args"
+Invoke-In-Dir -directory "Herbert" -command "$build_cmd"
