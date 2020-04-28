d9df46b8e736082b460a51acd9aa08163e2ecfa7
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-28 16:05:33 +0100

    Correct Jenkinsfile syntax for getting Herbert

--- tools/build_config/clone_herbert_branch.sh
+++ tools/build_config/clone_herbert_branch.sh
@@ -10,20 +10,25 @@ readonly bash_helpers="$(realpath "$(dirname "$0")")/bash_helpers.sh"
 # shellcheck source=./bash_helpers.sh
 . "${bash_helpers}"  # imports echo_and_run
 
-branch=${1-master}
+while [[ $# -gt 0 ]]; do
+key="$1"
+case $key in
+    --branch) herbert_branch="$2"; shift; shift ;;
+    *) build_args="${@}"; break ;;
+esac
+done
+
 if [ "${branch}" = "" ]; then
     branch="${DEFAULT_BRANCH}"
 fi
 
-build_args="${*:2}"
-
-echo "Cloning and building Herbert branch '${branch}'..."
+echo "Cloning and building Herbert branch '${herbert_branch}'..."
 if [[ -d "Herbert" ]]; then
     echo_and_run "cd Herbert" &&
     echo_and_run "git fetch origin" &&
-    echo_and_run "git checkout origin/${branch}"
+    echo_and_run "git checkout origin/${herbert_branch}"
 else
-    echo_and_run "git clone ${HERBERT_URL} --depth 1 --branch ${branch}" &&
+    echo_and_run "git clone ${HERBERT_URL} --depth 1 --branch ${herbert_branch}" &&
     echo_and_run "cd Herbert"
 fi
 
