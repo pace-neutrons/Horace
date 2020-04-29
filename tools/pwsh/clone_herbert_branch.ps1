da730284e67404a85896fb79fe400dd166b343a2
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-29 13:52:06 +0100

    Refactor repeated str into variable in pwsh script
    Former-commit-id: b2eba82b7c30cbbcba4e799765d38a78aa9a77ee

--- tools/pwsh/clone_herbert_branch.ps1
+++ tools/pwsh/clone_herbert_branch.ps1
@@ -14,13 +14,14 @@ param(
 . $PSScriptRoot/powershell_helpers.ps1
 
 $HERBERT_URL = "https://github.com/pace-neutrons/Herbert.git"
+$HERBERT_DIR = "Herbert"
 
 Write-Output "Building Herbert branch $branch..."
-if (Test-Path -Path "./Herbert") {
-  Invoke-In-Dir -directory "Herbert" -command "git fetch origin"
-  Invoke-In-Dir -directory "Herbert" -command "git checkout origin/$branch"
+if (Test-Path -Path "$HERBERT_DIR") {
+  Invoke-In-Dir -directory "$HERBERT_DIR" -command "git fetch origin"
+  Invoke-In-Dir -directory "$HERBERT_DIR" -command "git checkout origin/$branch"
 } else {
-  Write-And-Invoke "git clone $HERBERT_URL --depth 1 --branch $branch"
+  Write-And-Invoke "git clone $HERBERT_URL --depth 1 --branch $branch $HERBERT_DIR"
 }
 $build_cmd = "./tools/build_config/build.ps1 -build -build_tests OFF $build_args"
-Invoke-In-Dir -directory "Herbert" -command "$build_cmd"
+Invoke-In-Dir -directory "$HERBERT_DIR" -command "$build_cmd"
