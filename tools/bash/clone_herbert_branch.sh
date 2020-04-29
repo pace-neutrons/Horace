472e970baef4fe77cab100a7113a6b97721b61dc
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-29 14:20:56 +0100

    Refactor repeated str in bash Herbert build script
    Former-commit-id: abe31d31a98bdde9ba93a913bca65f22a3d337cf

--- tools/bash/clone_herbert_branch.sh
+++ tools/bash/clone_herbert_branch.sh
@@ -3,6 +3,7 @@
 # master
 
 readonly DEFAULT_BRANCH="master"
+readonly HERBERT_DIR="Herbert"
 readonly HERBERT_URL="https://github.com/pace-neutrons/Herbert.git"
 
 readonly bash_helpers="$(realpath "$(dirname "$0")")/bash_helpers.sh"
@@ -24,13 +25,13 @@ if [ "${herbert_branch}" = "" ]; then
 fi
 
 echo "Building Herbert branch '${herbert_branch}'..."
-if [[ -d "Herbert" ]]; then
-    echo_and_run "cd Herbert" &&
+if [[ -d "${HERBERT_DIR}" ]]; then
+    echo_and_run "cd ${HERBERT_DIR}" &&
     echo_and_run "git fetch origin" &&
     echo_and_run "git checkout origin/${herbert_branch}"
 else
-    echo_and_run "git clone ${HERBERT_URL} --depth 1 --branch ${herbert_branch}" &&
-    echo_and_run "cd Herbert"
+    echo_and_run "git clone ${HERBERT_URL} --depth 1 --branch ${herbert_branch} ${HERBERT_DIR}" &&
+    echo_and_run "cd ${HERBERT_DIR}"
 fi
 
 echo_and_run "./tools/build_config/build.sh --build --build_tests OFF ${build_args}"
