53c96899e468b095df86292e7c5c1fce77c4f6b8
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-04-30 13:45:19 +0100

    Create Herbert install in Horace Jenkins pipeline
    We were previously pulling the source. Now, from this source, we
    generate an install package and Horace uses this to build/run against

--- tools/pwsh/clone_herbert_branch.ps1
+++ tools/pwsh/clone_herbert_branch.ps1
@@ -14,7 +14,9 @@ param(
 . $PSScriptRoot/powershell_helpers.ps1
 
 $HERBERT_URL = "https://github.com/pace-neutrons/Herbert.git"
-$HERBERT_DIR = "Herbert"
+$HERBERT_DIR = "Herbert-download"
+$HERBERT_BUILD_DIR = "$HERBERT_DIR/build"
+$HERBERT_INSTALL_DIR = "$($(Get-Location).Path)/Herbert"
 
 Write-Output "Building Herbert branch $branch..."
 if (Test-Path -Path "$HERBERT_DIR") {
@@ -23,5 +25,16 @@ if (Test-Path -Path "$HERBERT_DIR") {
 } else {
   Write-And-Invoke "git clone $HERBERT_URL --depth 1 --branch $branch $HERBERT_DIR"
 }
-$build_cmd = "./tools/build_config/build.ps1 -build -build_tests OFF $build_args"
+$build_cmd = "./tools/build_config/build.ps1 -build"
+$build_cmd += " -build_dir $HERBERT_BUILD_DIR"
+$build_cmd += " -build_tests OFF $build_args"
 Invoke-In-Dir -directory "$HERBERT_DIR" -command "$build_cmd"
+
+# Set Herbert's CMake install directory
+$set_install_dir_cmd = "cmake -B $HERBERT_BUILD_DIR -S $HERBERT_DIR"
+$set_install_dir_cmd += " -DCMAKE_INSTALL_PREFIX=$HERBERT_INSTALL_DIR"
+Write-And-Invoke "$set_install_dir_cmd"
+
+# Run the "install" build target - this copies a package into the install dir
+$install_cmd = "cmake --build $HERBERT_BUILD_DIR --target INSTALL"
+Write-And-Invoke "$install_cmd"
