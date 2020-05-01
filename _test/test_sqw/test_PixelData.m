6d4a4f0b3abad0603b7c89c6988da031e55a1fbe
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-05-01 16:54:38 +0100

    Create PixelData class and create getters for data
    The PixelData class has dependent attributes that retrieve the desired
    data from the 9xn pixel data array.
    
    
    Former-commit-id: d81bbc6d249e807bcfbe1466c23c0b605033c49b

---
+++ _test/test_sqw/test_PixelData.m
@@ -0,0 +1,62 @@
+classdef test_PixelData < TestCase
+
+properties
+    raw_pix_data = rand(9, 10);
+    pixel_data_obj;
+end
+
+methods
+
+    function obj = test_PixelData(~)
+        obj = obj@TestCase('test_PixelData');
+
+        obj.pixel_data_obj = PixelData(obj.raw_pix_data);
+    end
+
+    function test_default_construction_sets_empty_pixel_data(obj)
+        pix_data = PixelData();
+        num_cols = 9;
+        assertEqual(pix_data.data, zeros(num_cols, 0));
+    end
+
+    function test_coordinates_returns_empty_array_if_pixel_data_empty(obj)
+        pix_data = PixelData();
+        assertTrue(isempty(pix_data.coordinates));
+    end
+
+    function test_pixel_data_is_set_to_input_data_on_construction(obj)
+        assertEqual(obj.pixel_data_obj.data, obj.raw_pix_data);
+    end
+
+    function test_get_coordinates_returns_coordinate_data(obj)
+        coord_data = obj.raw_pix_data(1:4, :);
+        assertEqual(obj.pixel_data_obj.coordinates, coord_data);
+    end
+
+    function test_irun_returns_run_index_data(obj)
+        run_indices = obj.raw_pix_data(5, :);
+        assertEqual(obj.pixel_data_obj.irun, run_indices)
+    end
+
+    function test_idet_returns_detector_index_data(obj)
+        detector_indices = obj.raw_pix_data(6, :);
+        assertEqual(obj.pixel_data_obj.idet, detector_indices)
+    end
+
+    function test_ienergy_returns_energy_bin_number_data(obj)
+        energy_bin_nums = obj.raw_pix_data(7, :);
+        assertEqual(obj.pixel_data_obj.ienergy, energy_bin_nums)
+    end
+
+    function test_signals_returns_signal_array(obj)
+        signal_array = obj.raw_pix_data(8, :);
+        assertEqual(obj.pixel_data_obj.signals, signal_array)
+    end
+
+    function test_errors_returns_error_array(obj)
+        error_array = obj.raw_pix_data(9, :);
+        assertEqual(obj.pixel_data_obj.errors, error_array)
+    end
+end
+
+end
---
+++ horace_core/sqw/@PixelData/PixelData.m
@@ -0,0 +1,74 @@
+classdef PixelData
+% PixelData Provides an interface for access to pixel data
+%
+
+properties (Access=private)
+    data_ = zeros(9, 0);
+end
+properties (Dependent)
+    % Returns the full raw pixel data block (9 x n array)
+    data;
+
+    % Returns the coordinates of the pixels in the projection axes, i.e.: u1,
+    % u2, u3 and dE (4 x n array)
+    coordinates;
+
+    % The run index the pixel originated from (1 x n array)
+    irun;
+
+    % The detector group number in the detector listing for the pixels (1 x n array)
+    idet;
+
+    % The energy bin numbers (1 x n array)
+    ienergy;
+
+    % The signal array (1 x n array)
+    signals;
+
+    % The errors on the signal array (variance i.e. error bar squared) (1 x n array)
+    errors;
+end
+
+methods
+
+    function obj = PixelData(data)
+        if nargin == 1
+            obj.data = data;
+        end
+    end
+
+    function pixel_data = get.data(obj)
+        pixel_data = obj.data_;
+    end
+
+    function obj = set.data(obj, pixel_data)
+        obj.data_ = pixel_data;
+    end
+
+    function coord_data = get.coordinates(obj)
+        coord_data = obj.data(1:4, :);
+    end
+
+    function run_index = get.irun(obj)
+        run_index = obj.data(5, :);
+    end
+
+    function detector_index = get.idet(obj)
+       detector_index = obj.data(6, :);
+    end
+
+    function detector_index = get.ienergy(obj)
+       detector_index = obj.data(7, :);
+    end
+
+    function signals = get.signals(obj)
+       signals = obj.data(8, :);
+    end
+
+    function errors = get.errors(obj)
+       errors = obj.data(9, :);
+    end
+
+end
+
+end
