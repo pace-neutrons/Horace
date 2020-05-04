99905396b9c31031b8ca55adb2c6fc48c9eddf00
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-05-04 17:00:47 +0100

    Revert "Define subsref operators for PixelData class"
    This reverts commit dcaa10b464a766de713325c7636504d7fd95bf7b.

--- _test/test_sqw/test_PixelData.m
+++ _test/test_sqw/test_PixelData.m
@@ -122,18 +122,6 @@ methods
         assertExceptionThrown(f, 'MATLAB:subsassigndimmismatch')
     end
 
-    function test_PixelData_objects_can_be_indexed_to_retrieve_data(obj)
-        pix_data_obj = obj.get_random_pix_data_(10);
-        assertEqual(pix_data_obj(1:5, 2:6), pix_data_obj.data(1:5, 2:6));
-    end
-
-    function test_PixelData_objects_can_be_indexed_to_set_data(obj)
-        pix_data_obj = obj.get_random_pix_data_(10);
-        new_data = ones(9, 4);
-        pix_data_obj(:, 2:5) = new_data;
-        assertEqual(pix_data_obj.data(:, 2:5), new_data);
-    end
-
     function test_size_of_PixelData_object_returns_underlying_data_size(obj)
         % This may no longer be true if we start adding additional columns that
         % are not part of the underlying pixel block
