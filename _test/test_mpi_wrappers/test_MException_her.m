02e40dd3be9ec12b8871784b016e4c2bb1939818
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-06-04 12:43:29 +0100

    Re pace-neutrons/Herbert#161 Trying to understand what the hell is occurring on Jenkins Linux

--- _test/test_mpi_wrappers/test_MException_her.m
+++ _test/test_mpi_wrappers/test_MException_her.m
@@ -94,9 +94,16 @@ classdef test_MException_her < TestCase
             rep1 = strsplit(rep1);
             rep2 = strsplit(rep2);
             rep3 = strsplit(rep3);
-            %assertEqual(rep1(18:end)',rep2(23:end)');            
+            %assertEqual(rep1(18:end)',rep2(23:end)');
             %assertEqual(rep1(17:end-16)',rep3(22:end-5)')
-            assertEqual(rep1(12:19)',rep3(1:8)')            
+            disp('**************   rep1:')
+            disp(rep1(12:19)')
+            disp('**************   rep3:')
+            disp(rep3(1:8)')
+            disp('**************   rep2:')
+            disp(rep2(1:8)')
+            
+            assertEqual(rep1(12:19)',rep3(1:8)')
             assertEqual(rep2(1:8)',rep3(1:8)')
         end
         
