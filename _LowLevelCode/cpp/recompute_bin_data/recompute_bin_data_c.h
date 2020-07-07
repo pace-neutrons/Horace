6da2beca8532c8beda73983dcd37a0f8fc77a864
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-07-07 17:18:48 +0100

    Further recompute_bin_data_c refactor

--- _LowLevelCode/cpp/recompute_bin_data/recompute_bin_data_c.h
+++ _LowLevelCode/cpp/recompute_bin_data/recompute_bin_data_c.h
@@ -9,3 +9,9 @@ void validate_inputs(const int &nlhs, mxArray *plhs[], const int &nrhs,
                      const mxArray *prhs[]);
 const double *const get_npix_array(const mxArray *prhs[]);
 const double *const get_pixel_array(const mxArray *prhs[]);
+int get_num_threads(const mxArray *prhs[]);
+
+double *get_output_signal_ptr(mwSize &num_dims, const mwSize *dims,
+                              mxArray *plhs[]);
+double *get_output_error_ptr(mwSize &num_dims, const mwSize *dims,
+                             mxArray *plhs[]);
