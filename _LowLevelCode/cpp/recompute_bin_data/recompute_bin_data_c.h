f4fab2488dfeabed63e3183d86c6d64057afb415
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-07-07 15:59:02 +0100

    Remove old versioning from recompute_bin_data_c.h
    Former-commit-id: eb087b2bdb45f9e179b66aebf96d673f3a55a025

--- _LowLevelCode/cpp/recompute_bin_data/recompute_bin_data_c.h
+++ _LowLevelCode/cpp/recompute_bin_data/recompute_bin_data_c.h
@@ -1,15 +1,9 @@
-#ifndef H_RECOMPUTE_BIN_DATA
-#define H_RECOMPUTE_BIN_DATA
+#pragma once
 
-#include "../CommonCode.h"
+#include "CommonCode.h"
 
-#include <sstream>
 #include <memory>
-//
-// $Revision:: 1758 ($Date:: 2019-12-16 18:18:50 +0000 (Mon, 16 Dec 2019) $)"
-//
+#include <sstream>
 
 void validate_inputs(const int &nlhs, mxArray *plhs[], const int &nrhs,
                      const mxArray *prhs[]);
-
-#endif
