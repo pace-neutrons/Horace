c8c35b1e61ff3aa215e9fe2523c8f86ef735a820
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-07-08 13:32:33 +0100

    Add PixelData regression tests for compute_bin_data
    Former-commit-id: 253547e95888938a27d44b09cd76b1ed590eb711

--- _test/test_sqw/test_PixelData_operations.m
+++ _test/test_sqw/test_PixelData_operations.m
@@ -1,9 +1,76 @@
 classdef test_PixelData_operations < TestCase
 
 properties
+    test_sqw_file_path = '../test_sqw_file/sqw_1d_1.sqw';
+    ref_npix_data = [];
+    ref_s_data = [];
+    ref_e_data = [];
 
+    config;
+    old_config;
 end
 
 methods
+
+    function obj = test_PixelData_operations(~)
+        obj = obj@TestCase('test_PixelData_operations');
+
+        sqw_test_obj = sqw(obj.test_sqw_file_path);
+        obj.ref_npix_data = sqw_test_obj.data.npix;
+        obj.ref_s_data = sqw_test_obj.data.s;
+        obj.ref_e_data = sqw_test_obj.data.e;
+    end
+
+    function setUp(obj)
+        obj.config = hor_config;
+        obj.old_config = obj.config.get_data_to_store();
+    end
+
+    function tearDown(obj)
+        set(hor_config, obj.old_config);
+    end
+
+    function test_compute_bin_data_correct_output_all_data_in_memory_mex_on(obj)
+        obj.config.use_mex = true;
+
+        pix = PixelData(obj.test_sqw_file_path);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+    function test_compute_bin_data_correct_output_all_data_in_memory_mex_off(obj)
+        obj.config.use_mex = false;
+
+        pix = PixelData(obj.test_sqw_file_path);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+
+    function test_compute_bin_data_correct_output_file_backed_mex_on(obj)
+        obj.config.use_mex = true;
+
+        pix = PixelData(obj.test_sqw_file_path, 1e3);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+    function test_compute_bin_data_correct_output_file_backed_mex_off(obj)
+        obj.config.use_mex = false;
+
+        pix = PixelData(obj.test_sqw_file_path, 1e3);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+end
 
 end
