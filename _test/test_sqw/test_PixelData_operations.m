3a13dbdbea83b937e58a53ff707c6fd87d4438c4
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-07-13 15:43:15 +0100

    Adapt compute_bin_data_mex for file-backed pix data

--- _test/test_sqw/test_PixelData_operations.m
+++ _test/test_sqw/test_PixelData_operations.m
@@ -6,6 +6,8 @@ properties
     ref_s_data = [];
     ref_e_data = [];
 
+    page_size;
+
     config;
     old_config;
 end
@@ -19,6 +21,8 @@ methods
         obj.ref_npix_data = sqw_test_obj.data.npix;
         obj.ref_s_data = sqw_test_obj.data.s;
         obj.ref_e_data = sqw_test_obj.data.e;
+
+        obj.page_size = dir(obj.test_sqw_file_path).bytes/6;  % Gives us 6 pages
     end
 
     function setUp(obj)
@@ -30,8 +34,20 @@ methods
         set(hor_config, obj.old_config);
     end
 
-    function test_compute_bin_data_correct_output_all_data_in_memory_mex_on(obj)
+    function test_compute_bin_data_correct_output_in_memory_mex_1_thread(obj)
         obj.config.use_mex = true;
+        obj.config.threads = 1;
+
+        pix = PixelData(obj.test_sqw_file_path);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+    function test_compute_bin_data_correct_output_in_memory_mex_4_threads(obj)
+        obj.config.use_mex = true;
+        obj.config.threads = 4;
 
         pix = PixelData(obj.test_sqw_file_path);
         [s, e] = pix.compute_bin_data(obj.ref_npix_data);
@@ -50,11 +66,34 @@ methods
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
     end
 
-
-    function test_compute_bin_data_correct_output_file_backed_mex_on(obj)
+    function test_compute_bin_data_correct_output_file_backed_mex_1_thread(obj)
         obj.config.use_mex = true;
+        obj.config.threads = 1;
 
-        pix = PixelData(obj.test_sqw_file_path, 1e3);
+        pix = PixelData(obj.test_sqw_file_path, obj.page_size);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+    function test_compute_bin_data_correct_output_5_pages_mex_1_thread(obj)
+        obj.config.use_mex = true;
+        obj.config.threads = 1;
+
+        pg_size = dir(obj.test_sqw_file_path).bytes/5;
+        pix = PixelData(obj.test_sqw_file_path, pg_size);
+        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+
+        assertEqual(s, obj.ref_s_data, '', 4.75e-4);
+        assertEqual(e, obj.ref_e_data, '', 4.75e-4);
+    end
+
+    function test_compute_bin_data_correct_output_file_backed_mex_4_threads(obj)
+        obj.config.use_mex = true;
+        obj.config.threads = 4;
+
+        pix = PixelData(obj.test_sqw_file_path, obj.page_size);
         [s, e] = pix.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
@@ -64,7 +103,7 @@ methods
     function test_compute_bin_data_correct_output_file_backed_mex_off(obj)
         obj.config.use_mex = false;
 
-        pix = PixelData(obj.test_sqw_file_path, 1e3);
+        pix = PixelData(obj.test_sqw_file_path, obj.page_size);
         [s, e] = pix.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
--- horace_core/sqw/PixelData/@PixelData/private/compute_bin_data_mex_.m
+++ horace_core/sqw/PixelData/@PixelData/private/compute_bin_data_mex_.m
@@ -12,6 +12,41 @@ if nargin < 3
     n_threads = config_store.instance().get_value('hor_config','threads');
 end
 
-[mean_signal, mean_variance] = recompute_bin_data_c(npix, obj.data, n_threads);
-mean_signal = mean_signal./npix(:);
-mean_variance = mean_variance./npix(:).^2;
+obj.move_to_first_page();
+
+pg_size = min(obj.max_page_size_, obj.num_pixels);
+npix_cum_sum = cumsum(npix);
+signal_sum = zeros(size(npix));
+variance_sum = zeros(size(npix));
+
+while true
+    start_idx = find(npix_cum_sum > 0, 1);
+    leftover_begin = npix_cum_sum(start_idx);
+
+    npix_cum_sum = npix_cum_sum - pg_size;
+    end_idx = find(npix_cum_sum > 0, 1);
+    if isempty(end_idx)
+        end_idx = numel(npix);
+    end
+
+    if start_idx == end_idx
+        npix_chunk = min(pg_size, npix(start_idx) - leftover_end);
+    else
+        leftover_end = pg_size - (leftover_begin + sum(npix(start_idx + 1:end_idx - 1)));
+        leftover_end = min(leftover_end, npix(end_idx));
+        npix_chunk = [leftover_begin, npix(start_idx + 1:end_idx - 1)', leftover_end];
+    end
+
+    [sig, variance] = recompute_bin_data_c(npix_chunk, obj.data, n_threads);
+    signal_sum(start_idx:end_idx) = signal_sum(start_idx:end_idx) + sig';
+    variance_sum(start_idx:end_idx) = variance_sum(start_idx:end_idx) + variance';
+
+    if obj.has_more()
+        obj.advance();
+    else
+        break
+    end
+end
+
+mean_signal = signal_sum./npix(:);
+mean_variance = variance_sum./npix(:).^2;
