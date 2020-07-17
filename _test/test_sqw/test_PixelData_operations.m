f0ccb6a4da27f73f94283ab78cef8cb0f295fe52
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-07-17 14:11:48 +0100

    Reduce test_PixelData_operations run time
    Load files at the beginning and re-use it, rather than loading the same
    file for every test

--- _test/test_sqw/test_PixelData_operations.m
+++ _test/test_sqw/test_PixelData_operations.m
@@ -6,6 +6,8 @@ properties
     ref_s_data = [];
     ref_e_data = [];
 
+    pix_in_memory;
+    pix_with_pages;
     page_size;
 
     config;
@@ -24,6 +26,9 @@ methods
 
         file_info = dir(obj.test_sqw_file_path);
         obj.page_size = file_info.bytes/6;  % Gives us 6 pages
+
+        obj.pix_in_memory = sqw_test_obj.data.pix;
+        obj.pix_with_pages = PixelData(obj.test_sqw_file_path, obj.page_size);
     end
 
     function setUp(obj)
@@ -39,8 +44,7 @@ methods
         obj.config.use_mex = true;
         obj.config.threads = 1;
 
-        pix = PixelData(obj.test_sqw_file_path);
-        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+        [s, e] = obj.pix_in_memory.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
@@ -50,8 +54,7 @@ methods
         obj.config.use_mex = true;
         obj.config.threads = 4;
 
-        pix = PixelData(obj.test_sqw_file_path);
-        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+        [s, e] = obj.pix_in_memory.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
@@ -60,8 +63,7 @@ methods
     function test_compute_bin_data_correct_output_all_data_in_memory_mex_off(obj)
         obj.config.use_mex = false;
 
-        pix = PixelData(obj.test_sqw_file_path);
-        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+        [s, e] = obj.pix_in_memory.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
@@ -71,8 +73,7 @@ methods
         obj.config.use_mex = true;
         obj.config.threads = 1;
 
-        pix = PixelData(obj.test_sqw_file_path, obj.page_size);
-        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+        [s, e] = obj.pix_with_pages.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
@@ -95,8 +96,7 @@ methods
         obj.config.use_mex = true;
         obj.config.threads = 4;
 
-        pix = PixelData(obj.test_sqw_file_path, obj.page_size);
-        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+        [s, e] = obj.pix_with_pages.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
@@ -105,8 +105,7 @@ methods
     function test_compute_bin_data_correct_output_file_backed_mex_off(obj)
         obj.config.use_mex = false;
 
-        pix = PixelData(obj.test_sqw_file_path, obj.page_size);
-        [s, e] = pix.compute_bin_data(obj.ref_npix_data);
+        [s, e] = obj.pix_with_pages.compute_bin_data(obj.ref_npix_data);
 
         assertEqual(s, obj.ref_s_data, '', 4.75e-4);
         assertEqual(e, obj.ref_e_data, '', 4.75e-4);
