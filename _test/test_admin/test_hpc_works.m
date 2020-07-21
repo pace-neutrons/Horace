1536ca90c4e200b882ddd1f3642ac43883fa509f
Alex Buts <Alex.Buts@stfc.ac.uk>
2020-07-21 18:41:57 +0100

    Re #298 fixing unit test

--- _test/test_admin/test_hpc_works.m
+++ _test/test_admin/test_hpc_works.m
@@ -18,16 +18,18 @@ classdef test_hpc_works< TestCase
             data_2restore = pc.get_data_to_store();
             clOb = onCleanup(@()set(pc,data_2restore));
             pc.saveable = false;
-
-            [old_config,new_hpc_config]=hpc();            
+            
+            [old_config,new_hpc_config]=hpc();
             old_dte = old_config.get_data_to_store();
             assertEqual(old_dte,data_2restore);
             
             pc.build_sqw_in_parallel = false;
             
-            hpc('off');                       
+            hpc('off');
             assertEqual(pc.build_sqw_in_parallel,false);
-
+            
+            new_hpc_config.build_sqw_in_parallel = false; % ensure this property is set to false
+            % on any machine, as we just set it to false for this config
             new_config = pc.get_data_to_store();
             assertEqual(new_hpc_config,new_config);
         end
