b3e25498806128c906147689736018a4715d7459
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-07-29 10:21:23 +0100

    Define template for PixelData.do_binary_op
    Define templates/placeholder functions for the different types of
    objects that can be used in the binary operations (double, dnd, sqw)

--- horace_core/sqw/PixelData/@PixelData/do_binary_op.m
+++ horace_core/sqw/PixelData/@PixelData/do_binary_op.m
@@ -4,7 +4,8 @@ function pix_out = do_binary_op(obj, operand, binary_op, flip)
 %
 % Input
 % -----
-% operand    The second operand to use in the binary operation
+% operand    The second operand to use in the binary operation.
+%
 % binary_op  Function handle pointing to the desired binary operation. The
 %            function should take 2 objects with '.s' and '.e' attributes, e.g.
 %            a sigvar object
@@ -16,12 +17,26 @@ if ~exist('flip', 'var')
 end
 pix_out = copy(obj);
 
+if isscalar(operand)
+    do_op_func = @do_binary_op_scalar_;
+elseif is_sqw_type(operand)
+    do_op_func = @do_binary_op_sqw_;
+elseif isprop(operand, 's') && isprop(operand, 'e')
+    do_op_func = @do_binary_op_dnd_;
+elseif isa(operand, 'double')
+    do_op_func = @do_binary_op_array_;
+else
+    error('PIXELDATA:do_binary_op', ...
+          ['Cannot perform binary operation between PixelData and ''%s'' ' ...
+           'object.'], class(operand));
+end
+
 pix_out = pix_out.move_to_first_page();
 while true
 
     pix_sigvar = sigvar(pix_out.signal, pix_out.variance);
-    [pix_out.signal, pix_out.variance] = ...
-            do_binary_op_scalar_(pix_sigvar, operand, binary_op, flip);
+    [pix_out.signal, pix_out.variance] = do_op_func(pix_sigvar, operand, ...
+                                                    binary_op, flip);
 
     if pix_out.has_more()
         pix_out = pix_out.advance();
@@ -44,4 +59,19 @@ function [signal, variance] = do_binary_op_scalar_(pix_sigvar, scalar_value, ...
     end
     signal = result.s;
     variance = result.e;
+end
+
+function [signal, variance] = do_binary_op_array_(pix_sigvar, double_array, ...
+                                                  binary_op, flip)
+
+end
+
+function [signal, variance] = do_binary_op_sqw_(pix_sigvar, other_sqw, ...
+                                                binary_op, flip)
+
+end
+
+function [signal, variance] = do_binary_op_dnd_(pix_sigvar, dnd_obj, ...
+    binary_op, flip)
+
 end
