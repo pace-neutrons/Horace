7aeacfd04ee42b95888d6c820c713e60563ab1e7
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-08-20 11:22:06 +0100

    Copy PixelData if required in PixelData.append
    If an output argument is provided to PixelData.append, return a copy and
    make no changes to the original object. If no output arguments are
    provided then make changes to the calling instance.
    
    
    Former-commit-id: dee65827d1ed1c4695c77aa0732afb765d64c369

--- horace_core/sqw/PixelData/@PixelData/append.m
+++ horace_core/sqw/PixelData/@PixelData/append.m
@@ -1,4 +1,4 @@
-function obj = append(obj, pix)
+function pix_out = append(obj, pix)
 % Join the pixels in the given PixelData object to the end of this
 % object.
 %
@@ -10,6 +10,12 @@ function obj = append(obj, pix)
 % -----
 % pix    A PixelData object containing the pixels to append
 %
+if nargout == 1
+    pix_out = copy(obj);
+else
+    pix_out = obj;
+end
+
 if ~isa(pix, 'PixelData')
     error('PIXELDATA:append', ...
             'Input object must be of class ''PixelData''. Found ''%s''', ...
@@ -18,49 +24,49 @@ end
 if isempty(pix)
     return;
 end
-if pix.num_pixels > obj.max_page_size_
+if pix.num_pixels > pix_out.max_page_size_
     error('PIXELDATA:append', ...
           ['Cannot append more pixels than allowed in a single page.\n ' ...
            'Found ''%i'' pixels, ''%i'' allowed.'], pix.num_pixels, ...
-           obj.max_page_size_);
+           pix_out.max_page_size_);
 elseif pix.get_num_pages_() > 1
     error('PIXELDATA:append', ...
           ['Cannot append pixels from a PixelData object with more than one page.\n ' ...
            'Found ''%i'' pages.'], pix.get_num_pages_());
 end
 
-if ~(obj.get_num_pages_() == obj.page_number_)
-    if obj.page_is_dirty_(obj.page_number_) && obj.dirty_page_edited_
-        obj.write_dirty_page_();
+if ~(pix_out.get_num_pages_() == pix_out.page_number_)
+    if pix_out.page_is_dirty_(pix_out.page_number_) && pix_out.dirty_page_edited_
+        pix_out.write_dirty_page_();
     end
-    obj.load_page_(obj.get_num_pages_());
+    pix_out.load_page_(pix_out.get_num_pages_());
 end
 
-if isempty(obj)
-    obj.data_ = pix.data;
-    obj.set_page_dirty_(true);
+if isempty(pix_out)
+    pix_out.data_ = pix.data;
+    pix_out.set_page_dirty_(true);
 else
-    if obj.page_size < obj.max_page_size_
-        num_to_allocate_to_pg = obj.max_page_size_ - obj.page_size;
-        obj.data_ = horzcat(obj.data, pix.data(:, 1:num_to_allocate_to_pg));
-        obj.set_page_dirty_(true);
+    if pix_out.page_size < pix_out.max_page_size_
+        num_to_allocate_to_pg = pix_out.max_page_size_ - pix_out.page_size;
+        pix_out.data_ = horzcat(pix_out.data, pix.data(:, 1:num_to_allocate_to_pg));
+        pix_out.set_page_dirty_(true);
 
         if num_to_allocate_to_pg ~= pix.num_pixels
-            obj.write_dirty_page_();
+            pix_out.write_dirty_page_();
 
-            obj.data_ = pix.data(:, (num_to_allocate_to_pg + 1):end);
-            obj.page_number_ = obj.page_number_ + 1;
+            pix_out.data_ = pix.data(:, (num_to_allocate_to_pg + 1):end);
+            pix_out.page_number_ = pix_out.page_number_ + 1;
         end
 
-    elseif obj.page_size == obj.max_page_size_
-        obj.set_page_dirty_(true);
-        if obj.dirty_page_edited_
-            obj.write_dirty_page_();
+    elseif pix_out.page_size == pix_out.max_page_size_
+        pix_out.set_page_dirty_(true);
+        if pix_out.dirty_page_edited_
+            pix_out.write_dirty_page_();
         end
-        obj.data_ = pix.data;
-        obj.page_number_ = obj.page_number_ + 1;
+        pix_out.data_ = pix.data;
+        pix_out.page_number_ = pix_out.page_number_ + 1;
     end
 end
 
-obj.num_pixels_ = obj.num_pixels_ + pix.num_pixels;
-obj.set_page_dirty_(true);
+pix_out.num_pixels_ = pix_out.num_pixels_ + pix.num_pixels;
+pix_out.set_page_dirty_(true);
