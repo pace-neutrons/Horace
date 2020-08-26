9bb4c0172b7df3cfdbb1493b3c3d33ea82a580a5
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-08-26 15:59:06 +0100

    Update sqw.recompute_urange for file-backed pixels

--- horace_core/sqw/@sqw/private/recompute_urange.m
+++ horace_core/sqw/@sqw/private/recompute_urange.m
@@ -34,10 +34,30 @@ pix_to_rlu=h_ave.u_to_rlu(1:3,1:3); % pix to rlu
 pix0 = h_ave.uoffset;               % pix offset
 u_to_rlu=w.data.u_to_rlu(1:3,1:3);  % proj to rlu
 u0 = w.data.uoffset;                % proj offset
-u_q=(u_to_rlu\pix_to_rlu)*(w.data.pix.q_coordinates) + u_to_rlu\repmat((pix0(1:3)-u0(1:3)),1,npixtot);
-urange=zeros(2,4);
-urange(1,1:3)=min(u_q,[],2)';
-urange(2,1:3)=max(u_q,[],2)';
-urange(1,4)=min(w.data.pix.dE) + (pix0(4)-u0(4));
-urange(2,4)=max(w.data.pix.dE) + (pix0(4)-u0(4));
 
+min_uq = realmax*ones(size(w.data.pix.q_coordinates));
+max_uq = -realmax*ones(size(w.data.pix.q_coordinates));
+min_dE = realmax;
+max_dE = -realmax;
+while true  % loop through pages of pixel data
+    u_q = (u_to_rlu\pix_to_rlu)*(w.data.pix.q_coordinates) ...
+            + u_to_rlu\(pix0(1:3) - u0(1:3));
+
+    min_uq = min(min(u_q, min_uq), [], 2);
+    max_uq = max(max(u_q, max_uq), [], 2);
+
+    min_dE = min(min_dE, min(w.data.pix.dE));
+    max_dE = max(max_dE, max(w.data.pix.dE));
+
+    if w.data.pix.has_more()
+        w.data.pix.advance();
+    else
+        break;
+    end
+end
+
+urange=zeros(2, 4);
+urange(1, 1:3) = min_uq';
+urange(2, 1:3) = max_uq';
+urange(1, 4) = min_dE + (pix0(4)-u0(4));
+urange(2, 4) = max_dE + (pix0(4)-u0(4));
