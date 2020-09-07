6c7607b2f4ffc9fd08594d920b1c137b0a6e16ac
Toby Perring <toby.perring@stfc.ac.uk>
2020-09-07 11:10:22 +0100

    Deleted legacy tobyfit and associated functions and updated tests accordingly
    Former-commit-id: 03f15b0b7edb4cba38a8312f4004e0e541530a24

--- _test/test_multifit/test_multifit_horace_2.m
+++ _test/test_multifit/test_multifit_horace_2.m
@@ -1,65 +1,63 @@
-classdef test_multifit_horace_2< TestCaseWithSave
-    % Performs some tests of fitting to Horace objects using multifit_sqw and other functions.
+classdef test_multifit_horace_2 < TestCaseWithSave
+    % Performs some tests of fitting to Horace objects using multifit_sqw_sqw
     % Optionally writes results to output file
     %
-    %   >>runtests test_multifit_horace_2            % Compares with previously saved results in test_multifit_horace_1_output.mat
-    %                                        % in the same folder as this function
-    %   >>save(test_multifit_horace_2())    % Save to test_multifit_horace_1_output.mat
+    %   >> runtests test_multifit_horace_2  % Compares with previously saved results in
+    %                                       % test_multifit_horace_2_output.mat
+    %                                       % in the same folder as this function
     %
-    %   >>test_name(test_multifit_horace_2()) % run particular test from this
+    %   >> test_multifit_horace_2('-save)   % Save to test_multifit_horace_2_output.mat
+    %                                       % in the Matlab temporary folder (copy to
+    %                                       % the same folder as this function after)
+    %
+    %   >> runtests test_multifit_horace_2:<test_func> % run a particular test from this
+    %                                       % this test suite
     %
     % Reads previously created test data sets.
+    
     properties
-        test_data_path;
-        sd;  % source data for fitting
-        w1data;
-        w2data;
-        win;
+        w1data
+        w2data
+        win
     end
     
     methods
-        function this=test_multifit_horace_2(varargin)
-            % constructor
-            if nargin > 0
-                name = varargin{1};
-            else
-                name= mfilename('class');
-            end
-            this = this@TestCaseWithSave(name,fullfile(fileparts(mfilename('fullpath')),'test_multifit_horace_2_output.mat'));
-            
-            this.comparison_par={ 'min_denominator', 0.01, 'ignore_str', 1};
-            %this.tol = 1.e-8;
-            
-            demo_dir=fileparts(mfilename('fullpath'));
+        function this=test_multifit_horace_2(name)
+            % Construct object
+            output_file = 'test_multifit_horace_2_output.mat';
+            this = this@TestCaseWithSave(name, output_file);
             
             % Read in data
-            % ------------
-            this.w1data=read_sqw(fullfile(demo_dir,'w1data.sqw'));
-            this.w2data=read_sqw(fullfile(demo_dir,'w2data.sqw'));
+            data_dir = fileparts(mfilename('fullpath'));
             
+            this.w1data = read_sqw(fullfile(data_dir,'w1data.sqw'));
+            this.w2data = read_sqw(fullfile(data_dir,'w2data.sqw'));
+            this.win=[this.w1data,this.w2data];     % combine the two cuts into an array of sqw objects and fit
             
-            % Combine the two cuts into an array of sqw objects and fit
-            % ---------------------------------------------------------
-            % The data were created using the cross-section model that is fitted shortly,
-            % with parameters [5,5,1,20,0], random noise added and then a background of
-            % 0.01 and 0.02 to the first and second data sets. That is, when the fit is
-            % performed, we expect the results [5,5,1,20,0.01] and [5,5,1,20,0.02]
-            
-            % Perform a fit that constrains the first two parameters (gap and J) to be
-            % the same in both data sets, but allow the intensity and gamma to vary
-            % independently. A constant background can also vary independently.
-            
-            this.win=[this.w1data,this.w2data];
-            
+            % Save output, if requrested
+            this.save();
         end
+        
+        % ------------------------------------------------------------------------------------------------
         function this=test_fit(this)
-            % fit
-            [wfit_1,fitpar_1]=multifit_sqw_sqw(this.win, @sqw_bcc_hfm, [5,5,0,10,0], [1,1,0,0,0],...
-                @sqw_bcc_hfm, {[5,5,1.2,10,0],[5,5,1.4,15,0]}, [1,1,1,1,1], {{{1,1,0},{2,2,0}}});
+            % Test the reversal of foreground and background models in multifit_sqw_sqw
             
-            % fit using new local foreground model
-            [wfit_1_locfore,fitpar_1_locfore]=multifit_sqw(this.win, @sqw_bcc_hfm_no_bkgd, {[5,5,1.2,10],[5,5,1.4,15]}, [], {{},{{1,1,-1},{2,2,-1}}},...
-                @linear_bkgd, [0,0], [1,0], 'local_fore');
+            % Fit with global foreground
+            mss = multifit_sqw_sqw(this.win);
+            mss = mss.set_fun(@sqw_bcc_hfm,  [5,5,0,10,0]);  % set foreground function(s)
+            mss = mss.set_free([1,1,0,0,0]); % set which parameters are floating
+            mss = mss.set_bfun(@sqw_bcc_hfm, {[5,5,1.2,10,0],[5,5,1.4,15,0]}); % set background function(s)
+            mss = mss.set_bfree([1,1,1,1,1]);    % set which parameters are floating
+            mss = mss.set_bbind({1,[1,-1],1},{2,[2,-1],1});
+            [wfit_1,fitpar_1] = mss.fit();
+            
+            % Fit using local foreground model
+            mss = multifit_sqw_sqw(this.win);
+            mss = mss.set_local_foreground;
+            mss = mss.set_fun(@sqw_bcc_hfm_no_bkgd, {[5,5,1.2,10],[5,5,1.4,15]});  % set foreground function(s)
+            mss = mss.set_bind({1,[1,1],1},{2,[2,1],1});
+            mss = mss.set_bfun(@sqw_constant, 0); % set background function(s)
+            [wfit_1_locfore,fitpar_1_locfore] = mss.fit();            
             
             % Check fit parameter values
             p1=[fitpar_1.bp{1},fitpar_1.bp{2}];
@@ -67,12 +65,17 @@ classdef test_multifit_horace_2< TestCaseWithSave
             p1_alt=[fitpar_1_locfore.p{1},fitpar_1_locfore.bp{1}(1),fitpar_1_locfore.p{2},fitpar_1_locfore.bp{2}(1)];
             p1sig_alt=[fitpar_1_locfore.sig{1},fitpar_1_locfore.bsig{1}(1),fitpar_1_locfore.sig{2},fitpar_1_locfore.bsig{2}(1)];
             
-            tol=-1e-8;
-            assertTrue(equal_to_tol(p1, p1_alt, tol, 'min_denominator', 0.01),'local background and local foreground equivalent fitting give different answers');
-            assertTrue(equal_to_tol(p1sig, p1sig_alt, tol, 'min_denominator', 0.01),'local background and local foreground equivalent fitting give different answers')
-            % Test against saved or store to save later
-            this=save_or_test_variables(this,wfit_1,wfit_1,wfit_1_locfore,fitpar_1_locfore);           
+            tol = [1e-10,1e-8];
+            assertTrue(equal_to_tol(p1, p1_alt, tol),...
+                'local background and local foreground equivalent fitting give different answers')
+            assertTrue(equal_to_tol(p1sig, p1sig_alt, tol),...
+                'local background and local foreground equivalent fitting give different answers')
 
+            % Test against saved output or store to save later
+            assertEqualToTolWithSave (this, wfit_1, 'tol', tol)
+            assertEqualToTolWithSave (this, fitpar_1, 'tol', tol)
+            assertEqualToTolWithSave (this, wfit_1_locfore, 'tol', tol)
+            assertEqualToTolWithSave (this, fitpar_1_locfore, 'tol', tol)
         end        
         
     end
