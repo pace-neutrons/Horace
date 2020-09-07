b703e39c03eebc0e04b0f6a58e52c8c196b150da
Toby Perring <toby.perring@stfc.ac.uk>
2020-09-07 14:20:41 +0100

    Removed legacy Tobyfit

---
+++ horace_core/Tobyfit/Tobyfit/tobyfit_mc_contributions.m
@@ -0,0 +1,11 @@
+function [mc_contrib,ok,mess]=tobyfit_mc_contributions(varargin)
+% *** Obsolete function ***
+%
+% This function was used with the obsolete Tobyfit syntax
+% Please migrate to the new Tobyfit syntax supported since January 2018'
+% For details of the new syntax:  <a href="matlab:doc(''sqw/tobyfit'');">Click here</a>'
+
+mess = ['\nThis function was used with the obsolete Tobyfit syntax.',...
+    '\nPlease migrate to the new Tobyfit syntax supported since January 2018',...
+    '\nFor details of the new syntax:  <a href="matlab:doc(''sqw/tobyfit'');">Click here</a>'];
+error('Tobyfit:legacySyntax',mess)
