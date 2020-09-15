d70e3a08c4aa88a54ee9d911f27a5d34c437bd80
Chris Marooney <christopher.marooney@stfc.ac.uk>
2020-09-15 14:58:42 +0100

    declare method

--- horace_core/sqw/PixelData/@PixelData/PixelData.m
+++ horace_core/sqw/PixelData/@PixelData/PixelData.m
@@ -222,6 +222,7 @@ methods
     pix_out = do_unary_op(obj, unary_op);
     pix_out = append(obj, pix);
     pix_out = mask(obj, mask_array, npix);
+    pix_out = do_sigvar_pair_va_op(obj, op, varargin);
 
     function obj = PixelData(arg, mem_alloc)
         % Construct a PixelData object from the given data. Default
