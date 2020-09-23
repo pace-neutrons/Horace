aba7e0620467ed55c913c0d030fdafa00e76be3d
Chris Marooney <christopher.marooney@stfc.ac.uk>
2020-09-23 16:20:26 +0100

    fix noisify bugs pace-neutrons/Herbert#234

--- herbert_core/utilities/maths/noisify.m
+++ herbert_core/utilities/maths/noisify.m
@@ -1,6 +1,7 @@
 function [yout,eout] = noisify (y,e,varargin)
 % Adds noise to y values and associated error bars. The arrays y, e must
-% have same size.
+% have same size. y is the signal and e is its variance (not standard
+% deviation)
 %
 % Syntax:
 %   >> yout = noisify (y)
@@ -17,6 +18,14 @@ function [yout,eout] = noisify (y,e,varargin)
 %           Add noise with Poisson distribution, where the mean value at
 %           a point is the value y.
 %
+%   >> [yout, eout] = noisify(y,e,factor,maxval)
+%           Add noise with Gaussian distribution, calculating the standard
+%           deviation by an externally provided maximum y value.
+%           Typically this will be the overall maximum value if the
+%           data is processed page by page and the maximum must be
+%           extracted before this processing.
+%           Setting factor<0.0 will use the default value 0.1.
+%
 %   If no input errors, e, just set e=[]
 
 if nargin==3 && ischar(varargin{1})
@@ -25,32 +34,44 @@ if nargin==3 && ischar(varargin{1})
         for i=1:numel(y)
             yout(i)=randpoisson(abs(y(i)));
         end
-        eout=abs(y);  % the inpuy y is the mean and variance of the Poisson distribution
+        eout=abs(y);  % the input y is the mean and variance of the Poisson distribution
     else
         error('Unrecognised random sampling distribution')
     end
-else
-    if nargin==3
+else % 3rd arg did not exist or was not a distribution name
+    default_fac = 0.1;
+    if nargin>=3 % 3 args for fac or 4 args for fac and maxval
         if isnumeric(varargin{1}) && isscalar(varargin{1})
             fac = varargin{1};
+            if fac < 0.0 % default option if 4th arg is needed
+                fac = default_fac;
+            end
         else
             error('Noise as fraction of peak signal must be real scalar')
         end
     else
-        fac = 0.1;
+        fac = default_fac;
     end
-    ymax = max(abs(y(:)));          % find maximum magnitude of y for arbitrary dimensions
+    if nargin==4 % means the maxval arg has been used
+        ymax = varargin{2};             % use max value previously extracted
+    else
+        ymax = max(abs(y(:)));          % find maximum magnitude of y for arbitrary dimensions
+    end
     dy=(fac*ymax)*randn(size(y));   % st. dev. of randn is sigma=1
     yout=y+dy;
     eout=ones(size(y))*(fac*ymax)^2;
 end
 
-if exist('e','var') && isempty(e)
+% this code now outputs a variance to be consistent between the use of eout
+% in the Poisson distribution section, the input definition for e and this
+% output.
+%
+% this code now adds e (an input *variance* rather than std deviation) to
+% eout if it exists rather than if it does not.
+if exist('e','var') && ~isempty(e)
     if isequal(size(e),size(eout))
-        eout=sqrt(eout+e.^2);
+        eout=eout+e;
     else
         error('Input array of error bars must have same size as input y array')
     end
-else
-    eout=sqrt(eout);
 end
