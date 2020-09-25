f71e3163e24fd315fcab03914924f27fd3e0f1f0
Chris Marooney <christopher.marooney@stfc.ac.uk>
2020-09-25 12:39:43 +0100

    use input parser

--- herbert_core/utilities/maths/noisify.m
+++ herbert_core/utilities/maths/noisify.m
@@ -17,70 +17,63 @@ function [yout,eout] = noisify (y,e,varargin)
 %           Add noise with Poisson distribution, where the mean value at
 %           a point is the value y.
 %
-%   >> [yout, eout] = noisify(y,e,[factor,]'maxval',maxval)
+%   >> [yout, eout] = noisify(y,e,[factor,]'maximum_value',maxval)
 %           Add noise with Gaussian distribution, calculating the standard
 %           deviation by an externally provided maximum y value.
-%           The max value is preceded by a keyword string 'maxval'.
+%           The max value is preceded by a keyword string 'maximum_value'.
 %           Typically this value will be the overall maximum value if the
 %           data is processed page by page and the maximum must be
 %           extracted before this processing.
 %           Omitting factor will use the default value 0.1.
 %
-%   If no input errors, e, just set e=[]
+%   If no input errors, e, just set e=[]. Note that eout will be created
+%   regardless of whether e is present, so the first overload output is
+%   probably incomplete.
+
+% deal with optional arguments
+is_poisson= false;
+fac = 0.1;
+if nargin>=2
+    p = inputParser;
+    addRequired(p,'y');   % y compulsory
+    addRequired(p,'e');   % e compulsory if any of remaining optional/parameter arguments present
+    numeric_or_string = @(x) isnumeric(x) || ischar(x)
+    addOptional(p,'dist_or_factor',fac, numeric_or_string);  % fac (numeric) or distribution ('poisson') optional, default to fac=0.1
+    addParameter(p,'maximum_value',-999,@isnumeric);  % ymax, as parameter 'maximum_value'. Default to highly visible negative value.
+    parse(p,y,e,varargin{:});
+
+    % pick up signal max value as either default or input
+    ymax = p.Results.maximum_value;
+
+    % vary if 'poisson' or fac present
+    if isnumeric(p.Results.dist_or_factor)
+        fac = p.Results.dist_or_factor;
+    elseif p.Results.dist_or_factor == 'poisson'
+        is_poisson = true;
+    else
+        error('3rd argument cannot be interpreted as a Gaussian factor or legal probability distribution')
+    end
+end
 
 % Use Poisson distribution and ignore other arguments
-if nargin==3 && ischar(varargin{1}) && is_stringmatchi(varargin{1},'poisson')
-        yout=zeros(size(y));
-        for i=1:numel(y)
-            yout(i)=randpoisson(abs(y(i)));
-        end
-        eout=abs(y);  % the input y is the mean and variance of the Poisson distribution
-        return % RETURN here as the poisson route is independent of the other argument options
-end
-
-% If not return, use normal distribution with or without an input maximum signal:
-default_fac = 0.1;
-ymax = [];
-fac = default_fac;
-if nargin>=3
-    % Check for maxval keyword and set ymax, else calc ymax locally below
-    pos = find(strcmp(varargin,'maxval')==1);
-    if ~isempty(pos) && pos<size(varargin,2)
-        ymax = varargin{pos+1};
-    else
-        error('Could not find maxval value arg with maxval specified')
+if is_poisson
+    yout=zeros(size(y));
+    for i=1:numel(y)
+        yout(i)=randpoisson(abs(y(i)));
     end
-    % Check for input of fac as 3rd arg, set to default otherwise
-    if isempty(pos) || pos ~= 1
-        if isnumeric(varargin{1}) && isscalar(varargin{1})
-            fac = varargin{1};
-        else
-            error('Noise as fraction of peak signal must be real scalar')
-        end
-    else
-        fac = default_fac;
+    eout=abs(y);  % the input y is the mean and variance of the Poisson distribution
+else
+    % if ymax was not set by an argument, set from max of |y|
+    if ymax < 0.0
+        ymax = max(abs(y(:)));
     end
+
+    % make noise dy and add to y for output; make error bar for noise    
+    dy=(fac*ymax)*randn(size(y));   % st. dev. of randn is sigma=1
+    yout=y+dy;
+    eout=ones(size(y))*(fac*ymax)^2;
 end
 
-% Check for any other distribution in arg #3
-if nargin>=3 && ischar(varargin{1}) && ~is_stringmatchi(varargin{1},'maxval')
-    error('Unrecognised random sampling distribution')
-end
-
-% if ymax was not set by an argument, set from max of |y|
-if isempty(ymax)
-    ymax = max(abs(y(:)));
-end
-    
-% make noise dy and add to y for output; make error bar for noise    
-dy=(fac*ymax)*randn(size(y));   % st. dev. of randn is sigma=1
-yout=y+dy;
-eout=ones(size(y))*(fac*ymax)^2;
-
-% this code now outputs a variance to be consistent between the use of eout
-% in the Poisson distribution section, the input definition for e and this
-% output.
-%
 % adds e (the input variance) to eout if it exists 
 % (it may not,see @sqw/nosify)
 if exist('e','var') && ~isempty(e)
