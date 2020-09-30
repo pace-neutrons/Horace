94a79a93a949ae8fb87734052333a9454764724c
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-09-30 17:47:23 +0100

    Implement PixelData - sivar-like binary operations
    Former-commit-id: 499ddeb91f4a533eaedab4071e995bbc84413a0a

--- horace_core/sqw/PixelData/@PixelData/private/binary_op_sigvar_.m
+++ horace_core/sqw/PixelData/@PixelData/private/binary_op_sigvar_.m
@@ -2,7 +2,7 @@ function pix_out = binary_op_sigvar_(obj, dnd_obj, binary_op, flip, npix)
 %% BINARY_OP_SIGVAR_ perform a binary operation between this and a sigvar or
 % sigvar-like object (e.g. dnd)
 %
-validate_inputs(obj, dnd_obj, npix);
+npix_cum_sum = validate_inputs(obj, dnd_obj, npix);
 
 if nargout == 0
     pix_out = obj;
@@ -10,26 +10,55 @@ else
     pix_out = copy(obj);
 end
 
-sigvar_pix = sigvar(obj.signal, obj.variance);
+pix_out.move_to_first_page();
 
-if ~isequal(size(npix), [1, 1])
-    sigvar_dnd = sigvar(repelem(dnd_obj.s(:), npix(:))', ...
-                        repelem(dnd_obj.e(:), npix(:))');
+end_idx = 1;
+leftover_end = 0;
+pg_size = pix_out.page_size;
+while true
+
+    start_idx = (end_idx - 1) + find(npix_cum_sum(end_idx:end) > 0, 1);
+    leftover_begin = npix_cum_sum(start_idx);
+    npix_cum_sum = npix_cum_sum - pg_size;
+
+    end_idx = (start_idx - 1) + find(npix_cum_sum(start_idx:end) > 0, 1);
+    if isempty(end_idx)
+        end_idx = numel(npix);
+    end
+
+    if start_idx == end_idx
+        npix_chunk = min(pix_out.page_size, npix(start_idx) - leftover_end);
+    else
+        npix_chunk = [leftover_begin, ...
+                      reshape(npix(start_idx + 1:end_idx - 1), 1, []), ...
+                      0];
+        pix_in_chunk = sum(npix_chunk);
+        leftover_end = pix_out.page_size - pix_in_chunk;
+        npix_chunk(end) = leftover_end;
+    end
+
+    sigvar_pix = sigvar(pix_out.signal, pix_out.variance);
+    if ~isequal(size(npix), [1, 1])
+        sigvar_dnd = sigvar(...
+            replicate_array(dnd_obj.s(start_idx:end_idx), npix_chunk(:))', ...
+            replicate_array(dnd_obj.e(start_idx:end_idx), npix_chunk(:))' ...
+        );
+    end
+
+    [pix_out.signal, pix_out.variance] = ...
+            sigvar_binary_op_(sigvar_pix, sigvar_dnd, binary_op, flip);
+
+    if pix_out.has_more()
+        pix_out.advance();
+    else
+        break;
+    end
 end
 
-if flip
-    res = binary_op(sigvar_dnd, sigvar_pix);
-else
-    res = binary_op(sigvar_pix, sigvar_dnd);
-end
-
-pix_out.signal = res.s;
-pix_out.variance = res.e;
-
-end
+end % function
 
 % -----------------------------------------------------------------------------
-function validate_inputs(pix, dnd_obj, npix)
+function npix_cum_sum = validate_inputs(pix, dnd_obj, npix)
     dnd_size = sigvar_size(dnd_obj);
     if ~isequal(dnd_size, [1, 1]) && ~isequal(dnd_size, size(npix))
         error('PIXELDATA:do_binary_op', ...
@@ -38,12 +67,12 @@ function validate_inputs(pix, dnd_obj, npix)
             iarray_to_matstr(dnd_size), iarray_to_matstr(size(npix)));
     end
 
-    npix_sum = sum(npix(:));
-    if npix_sum ~= pix.num_pixels
+    npix_cum_sum = cumsum(npix(:));
+    if npix_cum_sum(end) ~= pix.num_pixels
         error('PIXELDATA:binary_op_sigvar_', ...
             ['Cannot perform binary operation. Sum of ''npix'' must be ' ...
             'equal to the number of pixels in the PixelData object.\n' ...
             'Found ''%i'' pixels in npix but ''%i'' in PixelData.'], ...
-            npix_sum, pix.num_pixels);
+            npix_cum_sum(end), pix.num_pixels);
     end
 end
