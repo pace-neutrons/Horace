487d7ff99b18cb78e9d20ed2137f53d2b5eb49c1
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-10-08 18:16:17 +0100

    Update pix.equal_to_tol for non-paged & paged data
    Allows comparison of paged pixels and all-in-memory pixels

--- horace_core/sqw/PixelData/@PixelData/equal_to_tol.m
+++ horace_core/sqw/PixelData/@PixelData/equal_to_tol.m
@@ -19,9 +19,40 @@ end
 obj = obj.move_to_first_page();
 other_pix = other_pix.move_to_first_page();
 
-[ok, mess] = equal_to_tol(obj.data, other_pix.data, varargin{:});
-while ok && obj.has_more()
-    obj = obj.advance();
-    other_pix = other_pix.advance();
+if obj.page_size == other_pix.page_size
     [ok, mess] = equal_to_tol(obj.data, other_pix.data, varargin{:});
+    while ok && obj.has_more()
+        obj = obj.advance();
+        other_pix = other_pix.advance();
+        [ok, mess] = equal_to_tol(obj.data, other_pix.data, varargin{:});
+    end
+else
+    if obj.page_size == obj.num_pixels
+        [ok, mess] = pix_paged_and_in_mem_equal_to_tol(other_pix, obj, ...
+                                                       varargin{:});
+    elseif other_pix.page_size == other_pix.num_pixels
+        [ok, mess] = pix_paged_and_in_mem_equal_to_tol(obj, other_pix, ...
+                                                       varargin{:});
+    else
+        error('Some error')
+    end
+end
+
+end
+
+
+% -----------------------------------------------------------------------------
+function [ok, mess] = pix_paged_and_in_mem_equal_to_tol(...
+        paged_pix, in_mem_pix, varargin)
+    start_idx = 1;
+    end_idx = paged_pix.page_size;
+    [ok, mess] = equal_to_tol(in_mem_pix.data(:, start_idx:end_idx), ...
+                              paged_pix.data, varargin{:});
+    while ok && paged_pix.has_more()
+        paged_pix.advance();
+        start_idx = end_idx + 1;
+        end_idx = end_idx + paged_pix.page_size;
+        [ok, mess] = equal_to_tol(in_mem_pix.data(:, start_idx:end_idx), ...
+                                  paged_pix.data, varargin{:});
+    end
 end
