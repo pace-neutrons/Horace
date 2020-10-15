65d53152054bf025360f1c5fc35a4235a828ad7b
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-10-15 13:33:14 +0100

    Refactor get_abs_pix_range to take an index vector
    The function used to take a range as an argument, it not takes an index
    array. This mirrors the behaviour you would see if you were to index
    into a Matlab array with another Matlab array.

--- horace_core/sqw/PixelData/@PixelData/get_abs_pix_range.m
+++ horace_core/sqw/PixelData/@PixelData/get_abs_pix_range.m
@@ -1,58 +1,48 @@
-function pix_out = get_abs_pix_range(obj, start_idx, end_idx)
-%% GET_ABS_PIX_RANGE get the given range of pixels by absolute index into sqw
-% file backing the given PixelData object
+function pix_out = get_abs_pix_range(obj, pix_indices)
+%% GET_ABS_PIX_RANGE get the given range of pixels by absolute index into the
+% .sqw file backing the given PixelData object
 %
 % This function may be useful if you want to extract data for a particular
 % image bin.
 %
-[start_idx, end_idx] = parse_args(obj, start_idx, end_idx);
+pix_indices = parse_args(pix_indices);
 
-start_page_num = ceil(start_idx/obj.max_page_size_);
-obj.move_to_page(start_page_num);
+if obj.is_file_backed_()
+    obj.move_to_first_page();
 
-num_pix_in_range = end_idx - start_idx;
-pg_start_idx = start_idx - (obj.page_number_ - 1)*obj.max_page_size_;
-pg_end_idx = min(obj.max_page_size_, pg_start_idx + num_pix_in_range);
+    pix_out = PixelData(numel(pix_indices));
 
-leftover = (pg_start_idx + num_pix_in_range) - obj.max_page_size_;
-pix_out = obj.get_pixels(pg_start_idx:pg_end_idx);
-while leftover > 0
-    obj = obj.advance();
-
-    pg_end_idx = min(obj.max_page_size_, leftover);
-    leftover = leftover - obj.max_page_size_;
-    pix_out.append(obj.get_pixels(1:pg_end_idx));
+    [pg_idxs, global_idxs] = get_idxs_in_current_pg(obj, pix_indices);
+    pix_out.data(:, global_idxs) = obj.data(:, pg_idxs);
+    while obj.has_more()
+        obj.advance();
+        [pg_idxs, global_idxs] = get_idxs_in_current_pg(obj, pix_indices);
+        pix_out.data(:, global_idxs) = obj.data(:, pg_idxs);
+    end
+else
+    pix_out = obj.data(:, pix_indices);
 end
 
 end  % function
 
 
 % -----------------------------------------------------------------------------
-function [start_idx, end_idx] = parse_args(obj, varargin)
+function pix_indices = parse_args(varargin)
     parser = inputParser();
-    parser.addRequired('start_idx', @is_scalar_int_greater_than_zero);
-    parser.addRequired('end_idx', @is_scalar_int_greater_than_zero);
+    parser.addRequired('pix_indices', @isvector);
     parser.parse(varargin{:});
 
-    start_idx = parser.Results.start_idx;
-    end_idx = parser.Results.end_idx;
-
-    if start_idx > end_idx
-        error('PIXELDATA:get_abs_pix_range', ...
-              ['Argument ''end_idx'' cannot be larger than ''start_idx''\n' ...
-               'Found start_idx = %i and end_idx = %i.'], start_idx, end_idx);
-    end
-
-    if end_idx > obj.num_pixels
-        error('PIXELDATA:get_abs_pix_range', ...
-              ['Given end_idx exceeds the number of pixels.\nFound ' ...
-               'end_idx = %i, %i pixels in object.'], end_idx, obj.num_pixels);
-    end
+    pix_indices = parser.Results.pix_indices;
 end
 
+function [idx_in_pg, global_idx] = get_idxs_in_current_pg(obj, abs_indices)
+    % Extract the indices from abs_indices that lie within the bounds of the
+    % currently cached page of data.
+    % Get the corresponding absolute indices as well.
+    %
+    pg_start_idx = (obj.page_number_ - 1)*obj.max_page_size_ + 1;
+    pg_end_idx = pg_start_idx + obj.max_page_size_ - 1;
 
-function is = is_scalar_int_greater_than_zero(int_candidate)
-    is = isscalar(int_candidate) ...
-        && (floor(int_candidate) == int_candidate) ...
-        && int_candidate > 0;
+    global_idx = find((abs_indices >= pg_start_idx) & (abs_indices <= pg_end_idx));
+    idx_in_pg = abs_indices(global_idx) - (obj.page_number_ - 1)*obj.max_page_size_;
 end
