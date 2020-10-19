eb5ec1ffbd6c2d51991cdad76b74642c4b859d8e
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-10-19 12:39:38 +0100

    Improve argument validation in PixelData.get_data

--- horace_core/sqw/PixelData/@PixelData/get_data.m
+++ horace_core/sqw/PixelData/@PixelData/get_data.m
@@ -1,4 +1,4 @@
-function data = get_data(obj, pix_fields, abs_pix_indices)
+function data = get_data(obj, pix_fields, varargin)
 % Retrive data for a field, or fields, for the given pixel indices in
 % the current page. If no pixel indices are given, all pixels in the
 % current page are returned.
@@ -19,23 +19,11 @@ function data = get_data(obj, pix_fields, abs_pix_indices)
 %   pix_fields       The name of a field, or a cell array of field names
 %   abs_pix_indices  The pixel indices to retrieve, if not given, get full range
 %
-if ~isa(pix_fields, 'cell')
-    pix_fields = {pix_fields};
-end
-obj = obj.load_current_page_if_data_empty_();
-try
-    field_indices = cell2mat(obj.FIELD_INDEX_MAP_.values(pix_fields));
-catch ME
-    switch ME.identifier
-    case 'MATLAB:Containers:Map:NoKey'
-        error('PIXELDATA:get_data', ...
-                'Invalid field requested in PixelData.get_data().')
-    otherwise
-        rethrow(ME)
-    end
-end
+[pix_fields, abs_pix_indices] = parse_args(obj, pix_fields, varargin{:});
 
-if nargin < 3
+field_indices = cell2mat(obj.FIELD_INDEX_MAP_.values(pix_fields));
+
+if abs_pix_indices == -1
     % No pixel indices given, return them all
     data = obj.data(field_indices, :);
 else
@@ -43,3 +31,40 @@ else
 end
 
 end  % function
+
+
+% -----------------------------------------------------------------------------
+function [pix_fields, abs_pix_indices] = parse_args(obj, varargin)
+    parser = inputParser();
+    parser.addRequired('pix_fields', @(x) ischar(x) || iscell(x));
+    parser.addOptional('abs_pix_indices', -1, @is_positive_int_vector_or_logical_vector);
+    parser.parse(varargin{:});
+
+    pix_fields = parser.Results.pix_fields;
+    abs_pix_indices = parser.Results.abs_pix_indices;
+
+    pix_fields = validate_pix_fields(obj, pix_fields);
+end
+
+
+function pix_fields = validate_pix_fields(obj, pix_fields)
+    if ~isa(pix_fields, 'cell')
+        pix_fields = {pix_fields};
+    end
+
+    for i = 1:numel(pix_fields)
+        field = pix_fields{i};
+        if ~obj.FIELD_INDEX_MAP_.isKey({field})
+            valid_fields = obj.FIELD_INDEX_MAP_.keys();
+            error('PIXELDATA:get_data', ...
+                  ['Given field ''%s'' is not a valid pixel field.\n' ...
+                   'Valid fields are: [''%s'']'], ...
+                  strip(evalc('disp(field)')), strjoin(valid_fields, ''', '''));
+        end
+    end
+end
+
+
+function is = is_positive_int_vector_or_logical_vector(vec)
+    is = isvector(vec) && (islogical(vec) || (all(vec > 0 & all(floor(vec) == vec))));
+end
