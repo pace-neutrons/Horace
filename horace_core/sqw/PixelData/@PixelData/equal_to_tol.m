c7fe5bc8f5d97dfc87f8c017a977c1a321f142c7
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-10-20 12:02:01 +0100

    Add nan_equal option to PixelData.equal_to_tol

--- horace_core/sqw/PixelData/@PixelData/equal_to_tol.m
+++ horace_core/sqw/PixelData/@PixelData/equal_to_tol.m
@@ -1,18 +1,10 @@
 function [ok, mess] = equal_to_tol(obj, other_pix, varargin)
 %% EQUAL_TO_TOL Check if two PixelData objects are equal to a given tolerance
 %
-if ~(isa(other_pix, 'PixelData'))
-    ok = false;
-    mess = sprintf('Objects of class ''%s'' and ''%s'' cannot be equal.', ...
-                   class(obj), class(other_pix));
-    return
-end
+[tol, nan_equal] = parse_args(varargin{:});
 
-if ~all(size(obj) == size(other_pix))
-    ok = false;
-    mess = sprintf(['PixelData objects are not equal. ' ...
-                    'Argument 1 has size [%s] , argument 2 has size [%s]'], ...
-                   num2str(size(obj)), num2str(size(other_pix)));
+[ok, mess] = validate_other_pix(obj, other_pix);
+if ~ok
     return
 end
 
@@ -20,18 +12,20 @@ obj = obj.move_to_first_page();
 other_pix = other_pix.move_to_first_page();
 
 if obj.page_size == other_pix.page_size
-    [ok, mess] = equal_to_tol(obj.data, other_pix.data, varargin{:});
+    [ok, mess] = equal_to_tol(...
+            obj.data, other_pix.data, tol, 'nan_equal', nan_equal);
     while ok && obj.has_more()
         obj = obj.advance();
         other_pix = other_pix.advance();
-        [ok, mess] = equal_to_tol(obj.data, other_pix.data, varargin{:});
+        [ok, mess] = equal_to_tol(...
+                obj.data, other_pix.data, tol, 'nan_equal', nan_equal);
     end
 elseif obj.page_size == obj.num_pixels
-    [ok, mess] = pix_paged_and_in_mem_equal_to_tol(other_pix, obj, ...
-                                                   varargin{:});
+    [ok, mess] = pix_paged_and_in_mem_equal_to_tol(...
+            other_pix, obj, tol, 'nan_equal', nan_equal);
 elseif other_pix.page_size == other_pix.num_pixels
-    [ok, mess] = pix_paged_and_in_mem_equal_to_tol(obj, other_pix, ...
-                                                   varargin{:});
+    [ok, mess] = pix_paged_and_in_mem_equal_to_tol(...
+            obj, other_pix, tol, 'nan_equal', nan_equal);
 else
     error('PIXELDATA:equal_to_tol', ...
           ['Cannot compare PixelData objects that have different page ' ...
@@ -56,4 +50,36 @@ function [ok, mess] = pix_paged_and_in_mem_equal_to_tol(...
         [ok, mess] = equal_to_tol(in_mem_pix.data(:, start_idx:end_idx), ...
                                   paged_pix.data, varargin{:});
     end
+end
+
+
+function [ok, mess] = validate_other_pix(obj, other_pix)
+    ok = true;
+    mess = '';
+
+    if ~(isa(other_pix, 'PixelData'))
+        ok = false;
+        mess = sprintf('Objects of class ''%s'' and ''%s'' cannot be equal.', ...
+                       class(obj), class(other_pix));
+        return
+    end
+
+    if ~all(size(obj) == size(other_pix))
+        ok = false;
+        mess = sprintf(['PixelData objects are not equal. ' ...
+                        'Argument 1 has size [%s] , argument 2 has size [%s]'], ...
+                       num2str(size(obj)), num2str(size(other_pix)));
+        return
+    end
+end
+
+
+function [tol, nan_equal] = parse_args(varargin)
+    parser = inputParser();
+    parser.addOptional('tol', [0, 0], @(x) (numel(x) <= 2) && all(x >= 0));
+    parser.addParameter('nan_equal', true, @(x) isscalar(x) && islogical(x));
+    parser.parse(varargin{:});
+
+    tol = parser.Results.tol;
+    nan_equal = parser.Results.nan_equal;
 end
