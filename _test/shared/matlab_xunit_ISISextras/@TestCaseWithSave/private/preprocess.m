624024054fcfd3a90e1dde186954e4d0dbb9c93d
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-11-05 12:48:24 +0000

    Add preprocess shim to manage conversion of sqw to sqw_old in test snapshot files

---
+++ _test/shared/matlab_xunit_ISISextras/@TestCaseWithSave/private/preprocess.m
@@ -0,0 +1,23 @@
+function processed = preprocess(data)
+% Perform pre-processing on data loaded from .mat files as part of the
+% TestCaseWithSave workflow.
+%
+% This is a temporary function to manage reengineering of the `sqw` object
+% during which the old class is renamed as `sqw_old`, and will load as
+% a struct rather than class instance.
+
+    if isfield(data, 'main_header') && isfield(data, 'header') && isfield(data, 'detpar')
+        processed = sqw_old(data);
+    elseif isstruct(data)
+        field_names = fields(data);
+        for idx = 1:length(field_names)
+            field_name = field_names{idx};
+            for inner_idx = 1:numel(data.(field_name))
+                processed.(field_name)(inner_idx) = preprocess(data.(field_name)(inner_idx));
+            end
+            processed.(field_name) = processed.(field_name)';
+        end
+    else
+        processed = data;
+    end
+end
