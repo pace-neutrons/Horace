61a8454d08627ad3fadcdba06b3a737d103bbd9a
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-11-06 08:39:02 +0000

    Remove unnecessary indent

--- _test/shared/matlab_xunit_ISISextras/manage_legacy_sqw_class_rename.m
+++ _test/shared/matlab_xunit_ISISextras/manage_legacy_sqw_class_rename.m
@@ -6,18 +6,18 @@ function processed = manage_legacy_sqw_class_rename(data)
 % during which the old class is renamed as `sqw_old`, and will load as
 % a struct rather than class instance.
 
-    if isfield(data, 'main_header') && isfield(data, 'header') && isfield(data, 'detpar')
-        processed = sqw_old(data);
-    elseif isstruct(data)
-        field_names = fields(data);
-        for idx = 1:length(field_names)
-            field_name = field_names{idx};
-            for inner_idx = 1:numel(data.(field_name))
-                processed.(field_name)(inner_idx) = manage_legacy_sqw_class_rename(data.(field_name)(inner_idx));
-            end
-            processed.(field_name) = processed.(field_name)';
+if isfield(data, 'main_header') && isfield(data, 'header') && isfield(data, 'detpar')
+    processed = sqw_old(data);
+elseif isstruct(data)
+    field_names = fields(data);
+    for idx = 1:length(field_names)
+        field_name = field_names{idx};
+        for inner_idx = 1:numel(data.(field_name))
+            processed.(field_name)(inner_idx) = manage_legacy_sqw_class_rename(data.(field_name)(inner_idx));
         end
-    else
-        processed = data;
+        processed.(field_name) = processed.(field_name)';
     end
+else
+    processed = data;
+end
 end
