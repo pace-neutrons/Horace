0fa24bf7a3534c66c76e4b9af3018ecb82a82f5a
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-11-13 11:24:02 +0000

    Remove base-clase example abstract methods and props
    Examples to be removed when real functions added

--- horace_core/sqw/@SQWDnDBase/SQWDnDBase.m
+++ horace_core/sqw/@SQWDnDBase/SQWDnDBase.m
@@ -1,38 +1,37 @@
 classdef (Abstract) SQWDnDBase
     %SQWDnDBase Abstract SQW/DnD object base class
-    %   Abstract class defining common API and atrributes of the SQW and 
+    %   Abstract class defining common API and atrributes of the SQW and
     %   DnD objects
-    
+
     properties (Abstract) % Public
-       AbsProp
+      %  abstract_prop
     end
-    
+
     properties (Access = ?SQWDnDBase)  % Protected members
-        Property1
+        % base_property
     end
-    
+
     methods (Abstract)
-       abstMethod(obj)
+       % x = abstract_method(obj)
     end
-    
+
     methods  % Public
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+        % function outputArg = method1(obj,inputArg)
+        %     %METHOD1 Summary of this method goes here
+        %     %   Detailed explanation goes here
+        %     outputArg = obj.Property1 + inputArg;
+        % end
     end
-    
+
     methods (Access = ?SQWDnDBase)  % Protected members
-        w = binary_op_manager (w1, w2, binary_op)
-        w = unary_op_manager (w1, unary_op)
-        
-        function obj = untitled(inputArg1,inputArg2)
-            %UNTITLED Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-    end    
+        % w = binary_op_manager (w1, w2, binary_op)
+        % w = unary_op_manager (w1, unary_op)
+
+        % function obj = untitled(inputArg1,inputArg2)
+        % % UNTITLED Construct an instance of this class
+        % %   Detailed explanation goes here
+        %   obj.Property1 = inputArg1 + inputArg2;
+        % end
+    end
 end
 
