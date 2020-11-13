d3afed513a5bc4ca12555c7b50871a11fba0acd3
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-11-13 15:02:47 +0000

    Add missing function header
    Former-commit-id: 0d7aa0e997f590c9d729050593b5eeaaedeaeaeb

--- horace_core/sqw/@sqw/private/make_sqw.m
+++ horace_core/sqw/@sqw/private/make_sqw.m
@@ -1,7 +1,12 @@
 function d = make_sqw(ndims)
-    % Create a structure for an sqw object
-    d.main_header=make_sqw_main_header;
-    d.header=make_sqw_header;
-    d.detpar=make_sqw_detpar;
-    d.data=data_sqw_dnd(ndims);
+% Create a valid structure for an sqw object
+%
+% Input:
+% -------
+% ndim            Number of dimensions
+%
+    d.main_header = make_sqw_main_header;
+    d.header = make_sqw_header;
+    d.detpar = make_sqw_detpar;
+    d.data = data_sqw_dnd(ndims);
 end
