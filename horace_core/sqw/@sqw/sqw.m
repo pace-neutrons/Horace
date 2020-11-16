5884a138f36e20ef0ee3148e0611859db137df53
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-11-16 17:55:33 +0000

    Prettify code
    Former-commit-id: 78ec6279e4f94f16779901c6a4d59286e83c1f03

--- horace_core/sqw/@sqw/sqw.m
+++ horace_core/sqw/@sqw/sqw.m
@@ -6,14 +6,14 @@ classdef sqw < SQWDnDBase
     %   >> w = sqw (sqw_object)     % Create a new SQW object from a existing one
     %   >> w = sqw (struct)         % Create from a structure with valid fields (internal use)
     %   >> w = sqw ()               % Create a default, zero-dimensional SQW object
-
+    
     properties
         main_header
         header
         detpar
         data
     end
-
+    
     methods
         function obj = sqw(varargin)
             % Constructors
@@ -21,52 +21,52 @@ classdef sqw < SQWDnDBase
             % ii) filename
             % iii) copy
             obj = obj@SQWDnDBase();
-
+            
             [args] = obj.parse_args(varargin{:});
-
+            
             % i) copy
             if ~isempty(args.sqw_obj)
-               obj = copy(args.sqw_obj);
-
-            % ii) filename
+                obj = copy(args.sqw_obj);
+                
+                % ii) filename
             elseif ~isempty(args.filename)
-               obj = obj.init_from_file(args.filename);
-
-            % iii) struct
+                obj = obj.init_from_file(args.filename);
+                
+                % iii) struct
             elseif ~isempty(args.data_struct)
                 obj = obj.init_from_loader_struct(args.data_struct);
             end
         end
     end
-
-    methods (Static)
-%TODO: disabled until full functionality is implemeneted in new class;
-% The addition of this method causes sqw_old tests to incorrectly load data from .mat files
-% as new-SQW class objects
-%        function obj = loadobj(S)
-%            % Load a sqw object from a .mat file
-%            %
-%            %   >> obj = loadobj(S)
-%            %
-%            % Input:
-%            % ------
-%            %   S       An instance of this object or struct
-%            %
-%            % Output:
-%            % -------
-%            %   obj     An instance of this object
-%            %
-%               obj = sqw(S);
-%        end
+    
+    methods(Static)
+        %TODO: disabled until full functionality is implemeneted in new class;
+        % The addition of this method causes sqw_old tests to incorrectly load data from .mat files
+        % as new-SQW class objects
+        %        function obj = loadobj(S)
+        %            % Load a sqw object from a .mat file
+        %            %
+        %            %   >> obj = loadobj(S)
+        %            %
+        %            % Input:
+        %            % ------
+        %            %   S       An instance of this object or struct
+        %            %
+        %            % Output:
+        %            % -------
+        %            %   obj     An instance of this object
+        %            %
+        %               obj = sqw(S);
+        %        end
     end
-
-    methods (Static, Access = 'private')
+    
+    methods(Static, Access = 'private')
         % Signatures of private functions declared in files
         sqw_struct = make_sqw(ndims);
         detpar_struct = make_sqw_detpar();
         header = make_sqw_header();
         main_header = make_sqw_main_header();
-
+        
         function args = parse_args(varargin)
             % Parse a single argument passed to the SQW constructor
             %
@@ -78,10 +78,10 @@ classdef sqw < SQWDnDBase
             parser.addOptional('input', [], @(x) (isa(x, 'SQWDnDBase') || is_string(x) || isstruct(x)));
             parser.KeepUnmatched = true;
             parser.parse(varargin{:});
-
+            
             input = parser.Results.input;
             args = struct('sqw_obj', [], 'filename', [], 'data_struct', []);
-
+            
             if isa(input, 'SQWDnDBase')
                 if isa(input, 'DnDBase')
                     error('SQW:sqw', 'SQW cannot be constructed from a DnD object');
@@ -97,22 +97,22 @@ classdef sqw < SQWDnDBase
             end
         end
     end
-
-    methods (Access = 'private')
+    
+    methods(Access = 'private')
         function obj = init_from_file(obj, in_filename)
             % Parse SQW from file
             %
             % An error is raised if the data file is identified not a SQW object
             ldr = sqw_formats_factory.instance().get_loader(in_filename);
-            if ~strcmpi(ldr.data_type,'a')   % not a valid sqw-type structure
+            if ~strcmpi(ldr.data_type, 'a') % not a valid sqw-type structure
                 error('SQW:sqw', 'Data file does not contain valid sqw-type object');
             end
-
-            w=struct();
+            
+            w = struct();
             [w.main_header, w.header, w.detpar, w.data] = ldr.get_sqw('-legacy');
             obj = obj.init_from_loader_struct(w);
         end
-
+        
         function obj = init_from_loader_struct(obj, data_struct)
             obj.main_header = data_struct.main_header;
             obj.header = data_struct.header;
@@ -121,4 +121,3 @@ classdef sqw < SQWDnDBase
         end
     end
 end
-
