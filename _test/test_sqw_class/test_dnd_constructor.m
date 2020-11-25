c5a485d67b23c70be3c0e25689ae79436914c535
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-11-25 14:42:15 +0000

    Initial dnd-sqw struct constructor and tests

--- _test/test_sqw_class/test_dnd_constructor.m
+++ _test/test_sqw_class/test_dnd_constructor.m
@@ -21,8 +21,39 @@ methods
         dnd_obj = d2d();
 
         assertTrue(isa(dnd_obj, 'd2d'));
-        % assertTrue( ... properties ...);
+        assertEqual(numel(dnd_obj.pax), 2);
     end
 
+    function test_d2d_contains_expected_properties(obj)
+        expected_props = { ...
+            'filename', 'filepath', 'title', 'alatt', 'angdeg', ...
+            'uoffset', 'u_to_rlu', 'ulen', 'ulabel', 'iax', ...
+             'iint', 'pax', 'p', 'dax', 's', 'e', 'npix'};
+
+        dnd_obj = d2d();
+        actual_props = fieldnames(dnd_obj);
+
+        assertEqual(numel(actual_props), numel(expected_props));
+        for idx = 1:numel(actual_props)
+            assertTrue( ...
+                any(strcmp(expected_props, actual_props(idx))), ...
+                sprintf('Unrecognised DnD property "%s"', actual_props{idx}));
+        end
+    end
+
+    function test_d2d_get_returns_set_properties(obj)
+        dnd_obj = d2d();
+        class_props = fieldnames(dnd_obj);
+
+        % properties are mapped to an internal data structure; verify the getters and
+        % setters are correctly wired
+        for idx = 1:numel(class_props)
+            test_value = rand(10);
+            prop_name = class_props{idx};
+            dnd_obj.(prop_name) = test_value;
+            assertEqual(dnd_obj.(prop_name), test_value, ...
+                sprintf('Value set to "%s" not returned', prop_name));
+        end
+    end
 end
 end
--- horace_core/sqw/@d2d/d2d.m
+++ horace_core/sqw/@d2d/d2d.m
@@ -2,13 +2,20 @@ classdef d2d < DnDBase
     %D2D Create an 2-dimensional DnD object
     %
     % Syntax:
-    %   >> w = d2d ()               % Create a default, zero-dimensional SQW object
-    %   >> w = d2d (struct)         % Create from a structure with valid fields (internal use)
+    %   >> w = d2d()               % Create a default, zero-dimensional SQW object
+    %   >> w = d2d(struct)         % Create from a structure with valid fields (internal use)
+
 
     methods
         function obj = d2d(varargin)
             obj = obj@DnDBase(varargin{:});
             [args] = obj.parse_args(varargin{:});
+
+
+            % ii) struct
+            if ~isempty(args.data_struct)
+                obj = obj.init_from_loader_struct(args.data_struct);
+            end
         end
     end
 
@@ -18,8 +25,41 @@ classdef d2d < DnDBase
     end
 
     methods(Static, Access = private)
+
         function args = parse_args(varargin)
-            args = [];
+            % Parse a single argument passed to the SQW constructor
+            %
+            % Return struct with the data set to the appropriate element:
+            % args.filename  % string, presumed to be filename
+            % args.sqw_obj   % D2D class instance
+            % args.data_struct % generic struct, presumed to represent D2D
+            parser = inputParser();
+            parser.addOptional('input', [], @(x) (isa(x, 'SQWDnDBase') || is_string(x) || isstruct(x)));
+            parser.KeepUnmatched = true;
+            parser.parse(varargin{:});
+
+            input = parser.Results.input;
+            args = struct('dnd_obj', [], 'filename', [], 'data_struct', []);
+
+            if isa(input, 'SQWDnDBase')
+                if ~isa(input, 'd2d') % relax this in future updates
+                    error('D2D:d2d', 'D2D cannot be constructed from a non-D2D object');
+                end
+                args.dnd_obj = input;
+            elseif is_string(parser.Results.input)
+                args.filename = input;
+            elseif isstruct(input) && ~isempty(input)
+                args.data_struct = input;
+            else
+                % create struct holding default instance
+                args.data_struct = data_sqw_dnd(2);
+            end
+        end
+    end
+
+    methods(Access = 'private')
+        function obj = init_from_loader_struct(obj, data_struct)
+            obj.data = data_struct;
         end
     end
 end
