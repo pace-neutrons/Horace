f2f0437609c37dd4580c60c9eb0b7e289455ba57
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2020-11-26 10:30:07 +0000

    Add horage image to docs
    Former-commit-id: 6cf70f9cb030da6b7f0f59137b0c46e12a7ce1f2

--- documentation/user_docs/docs/conf.py
+++ documentation/user_docs/docs/conf.py
@@ -68,3 +68,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_last_updated_fmt = "%c"
+html_logo = "../../150px-Quintus_Horatius_Flaccus.jpg"
