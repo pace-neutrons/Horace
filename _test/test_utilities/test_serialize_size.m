ab1aeff69a8a8962e71392cfedaaf48fbeb78811
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2020-11-30 14:49:31 +0000

    Add tests, further setup cpp, fix a few minor bugs

--- _test/test_utilities/test_serialize_size.m
+++ _test/test_utilities/test_serialize_size.m
@@ -229,7 +229,8 @@ classdef test_serialize_size< TestCase
         end
 
         %------------------------------------------------------------------
-        function DISABLED_test_ser_size_struct_array(this)
+        function test_ser_size_struct_array(this)
+            skipTest('Old serialiser cannot handle struct arrays')
             test_struct = struct('HonkyTonk', {1, 2, 3});
             ser =  hlp_serialize(test_struct);
             ser_siz = hlp_serial_size(test_struct);
