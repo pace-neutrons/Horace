c53948dcfe07b43c08665a67564b558a88ae4441
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-12-07 16:23:55 +0000

    Improve error reporting in sigvar_set

--- horace_core/sqw/@DnDBase/sigvar_set.m
+++ horace_core/sqw/@DnDBase/sigvar_set.m
@@ -1,17 +1,18 @@
-function w = sigvar_set(win, sigvarobj)
+function w = sigvar_set(win, sigvar_obj)
 % Set output object signal and variance fields from input sigvar object
 %
 %   >> w = sigvar_set(win, sigvarobj)
 
-if ~isequal(size(win.s),size(sigvarobj.s))
+if ~isequal(size(win.s),size(sigvar_obj.s))
     error([upper(class(win)) ':sigvar_set'], ...
-        [class(win) ' object and sigvar object have inconsistent sizes']);
+        [class(win) ' object and sigvar object have inconsistent sizes: [%s] and [%s]'], ...
+        num2str(size(win.s)), num2str(size(sigvar_obj.s)));
 end
 
 w = win;
 
-w.s = sigvarobj.s;
-w.e = sigvarobj.e;
+w.s = sigvar_obj.s;
+w.e = sigvar_obj.e;
 
 % If no pixels, then our convention is that signal and error set to zero
 nopix = (w.npix==0);
