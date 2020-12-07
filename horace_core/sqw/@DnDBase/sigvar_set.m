b8ea40f6d85d54fb0ee7bf67bb2148d2d7f22a49
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-12-07 17:30:14 +0000

    Add unit tests for sigvar_set

--- horace_core/sqw/@DnDBase/sigvar_set.m
+++ horace_core/sqw/@DnDBase/sigvar_set.m
@@ -3,10 +3,16 @@ function w = sigvar_set(win, sigvar_obj)
 %
 %   >> w = sigvar_set(win, sigvarobj)
 
-if ~isequal(size(win.s),size(sigvar_obj.s))
+if ~isequal(size(win.s), size(sigvar_obj.s))
     error([upper(class(win)) ':sigvar_set'], ...
-        [class(win) ' object and sigvar object have inconsistent sizes: [%s] and [%s]'], ...
+        [class(win) ' object and sigvar object signal have inconsistent sizes: [%s] and [%s]'], ...
         num2str(size(win.s)), num2str(size(sigvar_obj.s)));
+end
+
+if ~isequal(size(win.e), size(sigvar_obj.e))
+    error([upper(class(win)) ':sigvar_set'], ...
+        [class(win) ' object and sigvar object variance have inconsistent sizes: [%s] and [%s]'], ...
+        num2str(size(win.e)), num2str(size(sigvar_obj.e)));
 end
 
 w = win;
