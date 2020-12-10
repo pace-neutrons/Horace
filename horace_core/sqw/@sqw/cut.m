441a3eb2fc19938915dbd3661c55d49664a72f15
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-12-10 15:53:43 +0000

    Replace old cut implementation with new
    Function help and further refactoring will follow
    
    
    Former-commit-id: 8763f1b301d3d83ab5a9a42ad971682badd0ddc5

--- horace_core/sqw/@sqw/cut.m
+++ horace_core/sqw/@sqw/cut.m
@@ -1,200 +1,236 @@
-function varargout = cut (varargin)
-% Take a cut from an sqw object by integrating over one or more axes.
+function sqw_obj = cut(obj, varargin)
+%%CUT
 %
-% Cut using existing projection axes:
-%   >> w = cut (data_source, p1_bin, p2_bin...)  %(as many binning arguments
-%                                                % as there are plot axes)
-%
-% Cut with new projection axes:
-%   >> w = cut (data_source, proj, p1_bin, p2_bin, p3_bin, p4_bin)
-%
-%   >> w = cut (..., '-nopix')      % output cut is dnd structure (i.e. no
-%                                   % pixel information is retained)
-%
-%   >> w = cut (..., '-save')       % save cut to file (prompts for file)
-%   >> w = cut (...,  filename)     % save cut to named file
-%
-% Write directly to file without creating an output object (useful if the
-% output is a large dataset in order to avoid out-of-memory errors)
-%
-%   >> cut (...)
-%
-% Input:
-% ------
-%   data_source     Data source: sqw file name or sqw-type object
-%                  Can also be a cell array of file names or an array of
-%                  sqw objects.
-%
-%   proj            Data structure containing details of projection axes,
-%                  with fields described below. Alternatively, a projaxes
-%                  object created from those fields (type >> help projaxes
-%                  for details).
-%     ---------------------------------------------------------------------
-%     Required fields:
-%       u           [1x3] Vector of first axis (r.l.u.) defining projection axes
-%       v           [1x3] Vector of second axis (r.l.u.) defining projection axes
-%
-%     Optional fields:
-%       w           [1x3] Vector of third axis (r.l.u.) - only needed if the third
-%                   character of argument 'type' is 'p'. Will otherwise be ignored.
-%
-%       nonorthogonal Indicate if non-orthogonal axes are permitted
-%                   If false (default): construct orthogonal axes u1,u2,u3 from u,v
-%                   by defining: u1 || u; u2 in plane of u and v but perpendicular
-%                   to u with positive component along v; u3 || u x v
-%
-%                   If true: use u,v (and w, if given) as non-orthogonal projection
-%                   axes: u1 || u, u2 || v, u3 || w if given, or u3 || u x v if not.
-%
-%       type        [1x3] Character string defining normalization. Each character
-%                   indicates how u1, u2, u3 are normalized, as follows:
-%                   - if 'a': projection axis unit length is one inverse Angstrom
-%                   - if 'r': then if ui=(h,k,l) in r.l.u., is normalized so
-%                             max(abs(h,k,l))=1
-%                   - if 'p': if orthogonal projection axes:
-%                                   |u1|=|u|, (u x u2)=(u x v), (u x u3)=(u x w)
-%                               i.e. the projections of u,v,w along u1,u2,u3 match
-%                               the lengths of u1,u2,u3
-%
-%                             if non-orthogonal axes:
-%                                   u1=u;  u2=v;  u3=w
-%                   Default:
-%                         'ppr'  if w not given
-%                         'ppp'  if w is given
-%
-%         uoffset   Row or column vector of offset of origin of projection axes (rlu)
-%
-%       lab         Short labels for u1,u2,u3,u4 as cell array
-%                   e.g. {'Q_h', 'Q_k', 'Q_l', 'En'})
-%                       *OR*
-%       lab1        Short label for u1 axis (e.g. 'Q_h' or 'Q_{kk}')
-%       lab2        Short label for u2 axis
-%       lab3        Short label for u3 axis
-%       lab4        Short label for u4 axis (e.g. 'E' or 'En')
-%     ---------------------------------------------------------------------
-%
-%   p1_bin          Binning along first Q axis
-%   p2_bin          Binning along second Q axis
-%   p3_bin          Binning along third Q axis
-%           - [] or ''          Plot axis: use bin boundaries of input data
-%           - [pstep]           Plot axis: sets step size; plot limits
-%                              taken from the extent of the data
-%           - [plo, phi]        Integration axis: range of integration
-%           - [plo, pstep, phi] Plot axis: minimum and maximum bin centers
-%                              and step size
-%
-%   p4_bin          Binning along the energy axis:
-%           - [] or ''          Plot axis: use bin boundaries of input data
-%           - [pstep]           Plot axis: sets step size; plot limits
-%                              taken from the extent of the data.
-%                               If pstep=0 then use bin size of the first
-%                              spe file and synchronize the output bin
-%                              boundaries with those boundaries. The overall
-%                              range is chosen to ensure that the energy
-%                              range of the input data is contained within
-%                              the bin boundaries.
-%           - [plo, phi]        Integration axis: range of integration
-%           - [plo, pstep, phi] Plot axis: minimum and maximum bin centers
-%                              and step size.
-%                               If pstep=0 then use bin size of the first
-%                              spe file and synchronize the output bin
-%                              boundaries with the reference boundaries.
-%                              The overall range is chosen to ensure that
-%                              the energy range plo to phi is contained
-%                              within the bin boundaries.
-%
-%
-% Output:
-% -------
-%   w               Output data object:
-%                     - sqw-type object with full pixel information
-%                     - dnd-type object if option '-nopix' given
-%                   If an array of input data sources was given, then w
-%                  is an array
+sqw_obj = copy(obj);
 
+ndims_source = numel(sqw_obj.data.pax);
+return_cut = true;
 
-% Original author: T.G.Perring
-%
-% $Revision:: 1759 ($Date:: 2020-02-10 16:06:00 +0000 (Mon, 10 Feb 2020) $)
-
-
-% Parse input
-% -----------
-[w, args, mess] = horace_function_parse_input (nargout,varargin{:});
-if ~isempty(mess), error(mess); end
-
-% Perform operations
-% ------------------
-nout=w.nargout_req;
-nw=numel(w.data);
-
-if all(w.sqw_type(:))
-    % sqw type data
-    if numel(args)>=1 && ~isstruct(args{1}) % proj structure not given, so all sqw objects must have same dimensionality
-        if ~all(w.ndims==w.ndims(1))
-            error('All sqw objects must have same dimensionality if not using new projection axes')
-        end
-    end
-    for i=1:nw
-        if nout>0
-            if i==1
-                wout=cut_sqw_main(w.data(i),w.ndims(i),args{:});
-                if nw>1
-                    cut_array = (numel(wout)>1);
-                    if ~cut_array
-                        wout=repmat(wout,size(w.data));     % make array
-                    else
-                        wout=repmat({wout},size(w.data));   % make cell array
-                    end
-                end
-            else
-                if ~cut_array
-                    wout(i)=cut_sqw_main(w.data(i),w.ndims(i),args{:});
-                else
-                    wout{i}=cut_sqw_main(w.data(i),w.ndims(i),args{:});
-                end
-            end
-        else
-            cut_sqw_main(w.data(i),w.ndims(i),args{:});
-        end
-    end
-elseif ~any(w.sqw_type(:)) && all(w.ndims==w.ndims(1))
-    % dnd type data
-    for i=1:nw
-        if nout>0
-            if i==1
-                wout=cut_dnd_main(w.data(i),w.ndims(i),args{:});
-                if nw>1
-                    cut_array = (numel(wout)>1);
-                    if ~cut_array
-                        wout=repmat(wout,size(w.data));     % make array
-                    else
-                        wout=repmat({wout},size(w.data));   % make cell array
-                    end
-                end
-            else
-                if ~cut_array
-                    wout(i)=cut_dnd_main(w.data(i),w.ndims(i),args{:});
-                else
-                    wout{i}=cut_dnd_main(w.data(i),w.ndims(i),args{:});
-                end
-            end
-        else
-            cut_dnd_main(w.data(i),w.ndims(i),args{:});
-        end
-    end
-else
-    error('Data files must all be sqw type, or all dnd type with same dimensionality')
+[ok, mess, ~, proj, pbin, args, opt] = cut_sqw_check_input_args(obj, ndims_source, return_cut, varargin{:});
+if ~ok
+    error ('CUT_SQW:invalid_arguments', mess)
 end
 
-if nout>0
-    argout{1}=wout;
-else
-    argout={};
+% Ensure there are no excess input arguments
+if numel(args)~=0
+    error ('CUT_SQW:invalid_arguments', 'Check the number and type of input arguments')
 end
 
-% Package output arguments
-% ------------------------
-[varargout,mess]=horace_function_pack_output(w,argout{:});
-if ~isempty(mess), error(mess), end
+% Process projection
+[proj, pbin, ~, pin, en] = update_projection_bins(proj, sqw_obj, pbin);
 
+% TODO: add loop here when cutting more than one sqw
+%   get size of array of cuts
+%   get pbins for cuts
+%   allocate array of sqw objects to output
+
+
+%% Start: cut_sqw_main_single -------------------------------------------------
+
+bounds = get_bin_boundaries(proj, sqw_obj.data.urange, pbin, pin, en);
+proj = proj.set_proj_binning( ...
+    bounds.urange, ...
+    bounds.plot_ax_idx, ...
+    bounds.integration_axis_idx, ...
+    bounds.plot_ax_bounds ...
+);
+[bin_starts, bin_ends] = proj.get_nbin_range(sqw_obj.data.npix);
+
+targ_pax = proj.target_pax;
+targ_nbin = proj.target_nbin;
+
+
+%% Start: cut_data_from_array -------------------------------------------------
+
+% Pre-allocate image data
+nbin_as_size = get_nbin_as_size(targ_nbin);
+s = zeros(nbin_as_size);
+e = zeros(nbin_as_size);
+npix = zeros(nbin_as_size);
+urange_step_pix = [Inf(1, 4); -Inf(1, 4)];
+
+% Get pixels that will likely contribute to the cut
+pix_indices = get_values_in_ranges(bin_starts, bin_ends);
+
+iter = 1;
+block_size = sqw_obj.data.pix.base_page_size;
+pix_retained = cell(1, ceil(numel(pix_indices)/block_size));
+pix_ix_retained = cell(1, ceil(numel(pix_indices)/block_size));
+for block_start = 1:block_size:numel(pix_indices)
+    block_end = min(block_start + block_size - 1, numel(pix_indices));
+    candidate_pix = sqw_obj.data.pix.get_pixels(pix_indices(block_start:block_end));
+
+    if get(hor_config, 'log_level') >= 0
+        fprintf(['Step %3d of %3d; Have read data for %d pixels -- ' ...
+                 'now processing data...'], iter, numel(pix_retained), ...
+                candidate_pix.num_pixels);
+    end
+
+    [ ...
+        s, ...
+        e, ...
+        npix, ...
+        urange_step_pix, ...
+        del_npix_retain, ...
+        ok, ...
+        ix ...
+    ] = cut_data_from_file_job.accumulate_cut( ...
+            s, ...
+            e, ...
+            npix, ...
+            urange_step_pix, ...
+            opt.keep_pix, ...
+            candidate_pix, ...
+            proj, ...
+            targ_pax ...
+    );
+
+    if get(hor_config, 'log_level') >= 0
+        fprintf(' ----->  retained  %d pixels\n',del_npix_retain);
+    end
+
+    %% Continue: cut_data_from_array ----------------------------------------------
+
+    if opt.keep_pix
+        pix = candidate_pix.get_pixels(ok);
+        pix_retained{iter} = pix;
+        pix_ix_retained{iter} = ix;
+    end
+
+    iter = iter + 1;
+
+end  % loop over pixel blocks
+
+if opt.keep_pix
+    pix_out = sort_pix(pix_retained, pix_ix_retained, npix);
+end
+
+%% End: cut_data_from_array ---------------------------------------------------
+
+
+%% Continue: cut_sqw_main_single ----------------------------------------------
+
+% Convert range from steps to actual range with respect to output uoffset
+urange_pix = urange_step_pix.*repmat(proj.usteps, [2, 1]) + repmat(proj.urange_offset, [2, 1]);
+
+ppax = bounds.plot_ax_bounds(1:length(bounds.plot_ax_idx));
+if isempty(ppax)
+    nbin_as_size = [1, 1];
+elseif length(ppax) == 1
+    nbin_as_size = [length(ppax{1}) - 1, 1];
+else
+    nbin_as_size = cellfun(@(nd) length(nd) - 1, ppax);
+end
+
+% Prepare output data
+data_out = sqw_obj.data;
+s = reshape(s, nbin_as_size);
+e = reshape(e, nbin_as_size);
+npix = reshape(npix, nbin_as_size);
+
+[ ...
+    data_out.uoffset, ...
+    data_out.ulabel, ...
+    data_out.dax, ...
+    data_out.u_to_rlu, ...
+    data_out.ulen, ...
+    axis_caption ...
+] = proj.get_proj_param(sqw_obj.data, bounds.plot_ax_idx);
+
+data_out.axis_caption = axis_caption;
+
+data_out.iax = bounds.integration_axis_idx;
+data_out.iint = bounds.integration_range;
+data_out.pax = bounds.plot_ax_idx;
+data_out.p = bounds.plot_ax_bounds;
+
+data_out.s = s./npix;
+data_out.e = e./(npix.^2);
+data_out.npix = npix;
+no_pix = (npix == 0);  % true where no pixels contribute to given bin
+data_out.s(no_pix) = 0;
+data_out.e(no_pix) = 0;
+
+if opt.keep_pix
+    data_out.urange = urange_pix;
+    data_out.pix = pix_out;
+
+    sqw_obj.data = data_out;
+else
+    switch numel(data_out.pax)
+    case 0
+        sqw_obj = d0d(data_out);
+    case 1
+        sqw_obj = d1d(data_out);
+    case 2
+        sqw_obj = d2d(data_out);
+    case 3
+        sqw_obj = d3d(data_out);
+    case 4
+        sqw_obj = d4d(data_out);
+    end
+end
+
+
+end  % function
+
+
+% -----------------------------------------------------------------------------
+function sqw_obj = get_sqw_obj(file_path)
+    pixel_page_size = get(hor_config, 'pixel_page_size');
+    sqw_obj = sqw(file_path, 'pixel_page_size', pixel_page_size);
+end
+
+function [proj, pbin, num_dims, pin, en] = update_projection_bins( ...
+        proj, sqw_obj, pbin)
+    % header_av = header_average(sqw_obj.header);
+    header_av = testgateway(sqw, 'header_average', sqw_obj.header);
+    [proj, pbin, num_dims, pin, en] = proj.update_pbins( ...
+            header_av, sqw_obj.data, pbin);
+end
+
+
+function bounds = get_bin_boundaries(proj, urange, pbin, pin, en)
+    [iax, iint, pax, p, urange] = proj.calc_ubins(urange, pbin, pin, en);
+    bounds.integration_axis_idx = iax;
+    bounds.integration_range = iint;
+    bounds.plot_ax_bounds = p;
+    bounds.plot_ax_idx = pax;
+    bounds.urange = urange;
+end
+
+
+function nbin_as_size = get_nbin_as_size(nbin)
+    % Output arrays for accumulated data
+    % Note: Matlab silliness when one dimensional: MUST add an outer dimension
+    % of unity. For 2D and higher, outer dimensions can always be assumed.
+    % The problem with 1D is that e.g. zeros([5]) is not the same as
+    % zeros([5,1]) whereas zeros([5,3]) is the same as zeros([5,3,1]).
+    if isempty(nbin)
+        nbin_as_size = [1, 1];
+    elseif length(nbin) == 1
+        nbin_as_size = [nbin, 1];
+    else
+        nbin_as_size = nbin;
+    end
+end
+
+
+function out = get_values_in_ranges(range_starts, range_ends)
+    % Get an array containing the values between the given ranges
+    % e.g.
+    %   >> range_starts = [1, 15, 12]
+    %   >> range_ends = [4, 17, 14]
+    %   >> get_values_in_range(range_starts, range_ends)
+    %       ans = [1, 2, 3, 4, 15, 16, 17, 12, 13, 14]
+
+    % Find the indexes of the boundaries of each range
+    range_bounds_idxs = cumsum([1; range_ends(:) - range_starts(:) + 1]);
+    z = ones(range_bounds_idxs(end) - 1, 1);
+    % Insert size of the difference between boundaries in each boundary index
+    z(range_bounds_idxs(1:end - 1)) = [ ...
+        range_starts(1); range_starts(2:end) - range_ends(1:end - 1) ...
+    ];
+    % Take a cumulative sum
+    out = cumsum(z);
+end
