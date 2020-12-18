ba8701519915e564d97253f1fd5c67bad1fca402
Harry Saunders <harry.saunders@stfc.ac.uk>
2020-12-18 13:19:04 +0000

    Merge ranges before fread in get_pix_in_ranges
    Merging adjacent ranges reduces the number of times you need to call
    fread and significantly improves read times
    
    
    Former-commit-id: a32a53cab8f2ff4bd5159e23d8ff7e57aa5da5e9

--- horace_core/sqw/file_io/@sqw_binfile_common/get_pix_in_ranges.m
+++ horace_core/sqw/file_io/@sqw_binfile_common/get_pix_in_ranges.m
@@ -5,6 +5,9 @@ function pix = get_pix_in_ranges(obj, pix_starts, pix_ends)
 NUM_BYTES_IN_FLOAT = 4;
 PIXEL_SIZE = NUM_BYTES_IN_FLOAT*PixelData.DEFAULT_NUM_PIX_FIELDS;  % bytes
 
+% This decreases no. of calls needed to read data - big speed increase
+[pix_starts, pix_ends] = merge_adjacent_ranges(pix_starts, pix_ends);
+
 % Position file reader at start of pixel array
 first_seek_pos = obj.pix_pos_ + (pix_starts(1) - 1)*PIXEL_SIZE;
 do_fseek(obj.file_id_, first_seek_pos, 'bof');
@@ -13,7 +16,7 @@ blocks = cell(1, numel(pix_starts));
 for i = 1:numel(pix_starts)
     num_pix_to_read = pix_ends(i) - pix_starts(i) + 1;
     read_size = [PixelData.DEFAULT_NUM_PIX_FIELDS, num_pix_to_read];
-    blocks{i} = fread(obj.file_id_, read_size, 'float32');
+    blocks{i}= fread_catch(obj.file_id_, read_size, '*float32');
 
     try
         seek_size = (pix_starts(i + 1) - pix_ends(i) - 1)*PIXEL_SIZE;
@@ -25,3 +28,28 @@ for i = 1:numel(pix_starts)
     do_fseek(obj.file_id_, seek_size, 'cof');
 end
 pix = [blocks{:}];
+
+end  % function
+
+
+% -----------------------------------------------------------------------------
+function [starts, ends] = merge_adjacent_ranges(starts, ends)
+    %%MERGE_ADJACENT_RANGES merge ranges starting in 'starts' and ending in
+    % 'ends' that are adjacent
+    % e.g.
+    %    >> starts = [1, 10, 45, 79, 86]
+    %    >> ends   = [5, 44, 67, 85, 90]
+    %    >> merge_adjacent_ranges(starts, ends)
+    %       ans =
+    %         [1, 10, 79]
+    %         [5, 67, 90]
+
+    % TODO: does this work for multiple adjacent ranges!?
+
+    % Find the ranges for start and end differ by one
+    offsets = starts(2:end) - ends(1:(end - 1));
+    idxs_to_del = find(offsets == 1);
+    % Delete the indices such that those ranges are merged
+    starts(idxs_to_del + 1) = [];
+    ends(idxs_to_del) = [];
+end
