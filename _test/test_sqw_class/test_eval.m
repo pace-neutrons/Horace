480e5620532eca19e7bdb20837acaa25e0015bd3
Nick Battam <nicholas.battam@stfc.ac.uk>
2020-12-22 13:16:06 +0000

    Move SQW API into new SQW function with tests

---
+++ _test/test_sqw_class/test_eval.m
@@ -0,0 +1,107 @@
+classdef test_eval < TestCase
+    % Series of tests to check the call to user functions always obey the
+    % same interface
+
+    properties
+        data_dir
+        sqw_obj
+    end
+
+    methods
+        function obj=test_eval(varargin)
+            obj = obj@TestCase('test_eval');
+            obj.data_dir = fullfile(fileparts(fileparts(mfilename('fullpath'))),'test_sqw_file');
+            test_sqw_file = fullfile(obj.data_dir, 'sqw_2d_2.sqw');
+            obj.sqw_obj = sqw(test_sqw_file);
+        end
+
+        function test_func_eval_sqw(obj)
+            %
+            err_message = '';
+            try
+                ds = func_eval(obj.sqw_obj, ...
+                    @test_eval.funceval_tester2D, [], 'all');
+                failed = false;
+            catch ME
+                failed = true;
+                err_message = ME.message;
+            end
+            assertFalse(failed, err_message);
+
+            sig = ds.data.s;
+            assertEqual(sig(1), numel(sig));
+            assertEqual(sig(2), 1);
+
+            pix = ds.data.pix;
+            assertEqual(pix.signal(1), numel(sig));
+            assertEqual(pix.signal(2), numel(sig));
+
+        end
+
+        function test_sqw_eval_aver(obj)
+            %
+            err_message = '';
+            try
+                ds = sqw_eval(obj.sqw_obj, ...
+                    @test_eval.sqw_eval_tester, [], 'av');
+                failed = false;
+            catch ME
+                failed = true;
+                err_message = ME.message;
+            end
+            assertFalse(failed, err_message);
+
+            sig = ds.data.s;
+            pix = ds.data.pix;
+
+            assertEqual(sig(1), numel(sig));
+            assertEqual(sig(1), pix.signal(1));
+            assertEqual(pix.signal(2), sig(1));
+        end
+
+        function test_sqw_eval(obj)
+            %
+            err_message = '';
+            try
+                ds = sqw_eval(obj.sqw_obj, ...
+                    @test_eval.sqw_eval_tester, []);
+                failed = false;
+            catch ME
+                failed = true;
+                err_message = ME.message;
+            end
+            assertFalse(failed,err_message);
+
+            pix = ds.data.pix;
+            assertEqual(pix.signal(2), 1);
+            assertEqual(size(pix,2), pix.signal(1));
+        end
+    end
+    methods(Static)
+        function dis = sqw_eval_tester(h, k, l, en, par)
+            sz = size(h);
+            if any(sz ~= size(k)) || any(sz ~=size(l)) || any(sz ~= size(en))
+                error('SQW_EVAL:runtime_error','incorrect shape of input arrays');
+            end
+            if size(h,2) ~=1
+                error('SQW_EVAL:runtime_error','incorrect shape of input arrays');
+            else
+                dis = ones(size(h));
+            end
+            dis(1) = numel(h);
+        end
+
+        function dis = funceval_tester2D(x, en, par)
+            sz = size(x);
+            if any(sz ~= size(en))
+                error('FUNC_EVAL:runtime_error','incorrect shape of input arrays');
+            end
+            if size(x,2) ~=1
+                error('FUNC_EVAL:runtime_error','incorrect shape of input arrays');
+            else
+                dis = ones(size(x));
+            end
+            dis(1) = numel(x);
+        end
+    end
+end
