0572b8a9b0dfe250f6476cb69ea535ab4ec34410
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-01-11 15:35:49 +0000

    Improve cut test for if outfile cannot be created

--- _test/test_algorithms/test_cut.m
+++ _test/test_algorithms/test_cut.m
@@ -240,13 +240,17 @@ methods
         assertEqual(res(3).e, zeros(9, 9));
     end
 
-    function test_cut_errors_early_if_outfile_cannot_be_created(obj)
+    function test_cut_errors_before_cut_taken_if_outfile_cannot_be_created(obj)
+        % If the outfile cannot be created, we want to know before we carry out
+        % the potentially expensive cut.
+        % We check that the error  is raised early by checking the error's ID,
+        % which is different if writing fails after the cut is complete.
         proj = projaxes([1, -1 ,0], [1, 1, 0], 'uoffset', [1, 1, 0], 'type', 'paa');
 
         u_axis_lims = [-0.1, 0.025, 0.1];
         v_axis_lims = [-0.1, 0.025, 0.1];
         w_axis_lims = [-0.1, 0.1];
-        en_axis_lims = [106, 4, 114, 4];
+        en_axis_lims = [106, 1, 114];
 
         outfile = fullfile('P:', 'not', 'a_valid', 'path.sqw');
 
@@ -254,7 +258,7 @@ methods
             obj.sqw_file, proj, u_axis_lims, v_axis_lims, w_axis_lims, ...
             en_axis_lims, outfile ...
         );
-        assertExceptionThrown(f, 'CUT_SQW:runtime_error');
+        assertExceptionThrown(f, 'SQW:cut_sqw_check_input_args:outfile_creation_error');
     end
 
     function test_error_raised_if_cut_called_with_multiple_files(obj)
