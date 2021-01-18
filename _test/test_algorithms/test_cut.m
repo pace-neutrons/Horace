635b045dcd0edda588de80cde903f49e20d0b3c9
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-01-18 15:03:20 +0000

    Restore ability to perform out-of-memory cuts
    This slots the old implementation of out-of-memory cuts into the newer
    implementation of cut. A pix_combine_info object is used to manage and
    combine temporary files holding pixels.

--- _test/test_algorithms/test_cut.m
+++ _test/test_algorithms/test_cut.m
@@ -274,6 +274,31 @@ methods
         assertExceptionThrown(f, 'HORACE:cut');
     end
 
+    function test_you_can_take_an_out_of_memory_cut_with_tmp_files(obj)
+        conf = hor_config();
+        old_conf = conf.get_data_to_store();
+        conf.pixel_page_size = 5e5;
+        cleanup = onCleanup(@() set(hor_config, old_conf));
+
+        proj = projaxes([1, -1 ,0], [1, 1, 0], 'uoffset', [1, 1, 0], 'type', 'paa');
+        u_axis_lims = [-0.1, 0.025, 0.1];
+        v_axis_lims = [-0.1, 0.025, 0.1];
+        w_axis_lims = [-0.1, 0.1];
+        en_axis_lims = [105, 1, 114];
+
+        outfile = fullfile(tmp_dir, 'tmp_outfile.sqw');
+        cut( ...
+            obj.sqw_file, proj, u_axis_lims, v_axis_lims, w_axis_lims, ...
+            en_axis_lims, outfile ...
+        );
+        cleanup2 = onCleanup(@() cleanup_file(outfile));
+
+        ref_sqw = sqw('test_cut_ref_sqw.sqw');
+        output_sqw = sqw(outfile);
+
+        assertEqualToTol(output_sqw, ref_sqw, 1e-5, 'ignore_str', true);
+    end
+
 end
 
 end
