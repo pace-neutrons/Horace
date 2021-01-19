2252f52629bbe7b4eb9c470da2ca01147da36abe
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-01-19 13:58:21 +0000

    Add test for testing skipping tests

---
+++ _test/test_utilities/test_skip.m
@@ -0,0 +1,24 @@
+classdef test_skip < TestCase
+    properties
+    end
+    methods
+        function this=test_skip(varargin)
+            if nargin>0
+                name = varargin{1};
+            else
+                name  = 'test_skip';
+            end
+            this = this@TestCase(name);
+
+        end
+
+        %------------------------------------------------------------------
+        function test_skip_with_message(this)
+            skipTest('This test is skipped because of custom reasons.');
+        end
+
+        function test_skip_without_message(this)
+            skipTest();
+        end
+    end
+end
