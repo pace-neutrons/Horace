f73ce0475cdc0bc3fe66c3776877b3b3f8f8b2ab
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-01-25 21:03:40 +0000

    Re #464 fixing tests and some merge errors

--- horace_core/sqw/@sqw/private/cut_single.m
+++ horace_core/sqw/@sqw/private/cut_single.m
@@ -34,7 +34,7 @@ wout = copy(w, 'exclude_pix', true);
     ubins.plot_ax_idx, ...
     ubins.plot_ax_bounds, ...
     ubins.urange ...
-] = proj.calc_ubins(w.data.urange, pbin, pin, en);
+    ] = proj.calc_transf_img_bins(w.data.img_range, pbin, pin, en);
 
 % Update projection with binning
 proj = proj.set_proj_binning( ...
@@ -42,14 +42,14 @@ proj = proj.set_proj_binning( ...
     ubins.plot_ax_idx, ...
     ubins.integration_axis_idx, ...
     ubins.plot_ax_bounds ...
-);
+    );
 
 % Accumulate image and pixel data for cut
-[s, e, npix, pix_out, urange_pix] = cut_accumulate_data_(w, proj, keep_pix, log_level);
+[s, e, npix, pix_out, img_range] = cut_accumulate_data_(w, proj, keep_pix, log_level);
 
 % Compile the accumulated cut and projection data into a data_sqw_dnd object
-data_out = compile_sqw_data(w.data, proj, s, e, npix, pix_out, urange_pix, ...
-                            ubins, keep_pix);
+data_out = compile_sqw_data(w.data, proj, s, e, npix, pix_out, img_range, ...
+    ubins, keep_pix);
 
 % Assign the new data_sqw_dnd object to the output SQW object, or create a new
 % dnd.
@@ -69,8 +69,8 @@ if exist('outfile', 'var') && ~isempty(outfile)
         save_sqw(wout, outfile);
     catch ME
         warning('CUT_SQW:io_error', ...
-                'Error writing to file ''%s''.\n%s: %s', ...
-                outfile, ME.identifier, ME.message);
+            'Error writing to file ''%s''.\n%s: %s', ...
+            outfile, ME.identifier, ME.message);
     end
 end
 
@@ -79,45 +79,46 @@ end  % function
 
 % -----------------------------------------------------------------------------
 function save_sqw(sqw_obj, file_path)
-    loader = sqw_formats_factory.instance().get_pref_access();
-    loader = loader.init(sqw_obj, file_path);
-    loader.put_sqw();
-    loader.delete();
+loader = sqw_formats_factory.instance().get_pref_access();
+loader = loader.init(sqw_obj, file_path);
+loader.put_sqw();
+loader.delete();
 end
 
 
 function data_out = compile_sqw_data(data, proj, s, e, npix, pix_out, ...
-                                     urange_pix, ubins, keep_pix)
-    ppax = ubins.plot_ax_bounds(1:length(ubins.plot_ax_idx));
-    if isempty(ppax)
-        nbin_as_size = [1, 1];
-    elseif length(ppax) == 1
-        nbin_as_size = [length(ppax{1}) - 1, 1];
-    else
-        nbin_as_size = cellfun(@(nd) length(nd) - 1, ppax);
-    end
+    img_range, ubins, keep_pix)
+ppax = ubins.plot_ax_bounds(1:length(ubins.plot_ax_idx));
+if isempty(ppax)
+    nbin_as_size = [1, 1];
+elseif length(ppax) == 1
+    nbin_as_size = [length(ppax{1}) - 1, 1];
+else
+    nbin_as_size = cellfun(@(nd) length(nd) - 1, ppax);
+end
 
-    data_out = data;
-    data_out.s = reshape(s, nbin_as_size);
-    data_out.e = reshape(e, nbin_as_size);
-    data_out.npix = reshape(npix, nbin_as_size);
 
-    [ ...
-        data_out.uoffset, ...
-        data_out.ulabel, ...
-        data_out.dax, ...
-        data_out.u_to_rlu, ...
-        data_out.ulen, ...
-        data_out.axis_caption ...
+data_out = data;
+data_out.s = reshape(s, nbin_as_size);
+data_out.e = reshape(e, nbin_as_size);
+data_out.npix = reshape(npix, nbin_as_size);
+data_out.img_range = img_range;
+
+[ ...
+    data_out.uoffset, ...
+    data_out.ulabel, ...
+    data_out.dax, ...
+    data_out.u_to_rlu, ...
+    data_out.ulen, ...
+    data_out.axis_caption ...
     ] = proj.get_proj_param(data, ubins.plot_ax_idx);
 
-    data_out.iax = ubins.integration_axis_idx;
-    data_out.iint = ubins.integration_range;
-    data_out.pax = ubins.plot_ax_idx;
-    data_out.p = ubins.plot_ax_bounds;
+data_out.iax = ubins.integration_axis_idx;
+data_out.iint = ubins.integration_range;
+data_out.pax = ubins.plot_ax_idx;
+data_out.p = ubins.plot_ax_bounds;
 
-    if keep_pix
-        data_out.urange = urange_pix;
-        data_out.pix = pix_out;
-    end
+if keep_pix
+    data_out.pix = pix_out;
+end
 end
