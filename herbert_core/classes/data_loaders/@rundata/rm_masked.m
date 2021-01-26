86efc1328c50f02a663745b24d13783bb23394f7
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-01-26 10:50:39 +0000

    Re pace-neutrons/Herbert#269 removed unnecessary brackets and extra comments

--- herbert_core/classes/data_loaders/@rundata/rm_masked.m
+++ herbert_core/classes/data_loaders/@rundata/rm_masked.m
@@ -1,6 +1,14 @@
 function [S_m,Err_m,det_m]=rm_masked(obj,ignore_nan,ignore_inf)
-% method removes failed (NaN or Inf) data from the data array and deletes
+% Method removes failed (NaN or Inf) data from the data array and deletes
 % detectors, which provided such signal
+%
+% Input:
+% obj -- initialized rundata object to check for failied detectors
+% Optional:
+% ignore_nan -- mask detectors with Nan signal. If absent, assumed true
+% ignore_inf -- mask detectors with Inf signal. If absent, assumed true
+%
+% providing both ignore_nan and ignore_inf equal to false, disables masking
 %
 if isempty(obj.S)||isempty(obj.ERR)||isempty(obj.det_par)
     error('RUNDATA:rm_masked',' signal, error and detectors arrays have to be defined\n');
@@ -17,26 +25,28 @@ end
 
 
 if ignore_nan && ignore_inf
-    index_masked = (isnan(obj.S)|(isinf(obj.S))); % masked pixels
+    index_masked = isnan(obj.S)| isinf(obj.S); % masked pixels
 elseif ignore_nan
-    index_masked = (isnan(obj.S));
+    index_masked = isnan(obj.S);
 elseif ignore_inf
-    index_masked = (isinf(obj.S));
+    index_masked = isinf(obj.S);
 else
     S_m= obj.S;
     Err_m = obj.ERR;
     det_m = obj.det_par;
     return
 end
-line_notmasked= ~any(index_masked,1);           % masked detectors (for any energy)
+line_notmasked= ~any(index_masked,1);   % masked detectors (for any energy)
 
 if get(herbert_config,'log_level')> 1
     [ne,ndet]=size(obj.S);
     nnotmasked = sum(line_notmasked);
     if nnotmasked<ndet
         ndet_mask = ndet-nnotmasked;
-        disp(['Masked additional ',num2str(ndet_mask),' detectors out of toal ',num2str(ndet), ' detectors'])
-        disp(['This removes      ',num2str(ndet_mask*ne),' pixels out of total ',num2str(ne*ndet), ' pixels'])
+        disp(['Masked additional ',num2str(ndet_mask),...
+            ' detectors out of toal ',num2str(ndet), ' detectors'])
+        disp(['This removes      ',num2str(ndet_mask*ne),...
+            ' pixels out of total ',num2str(ne*ndet), ' pixels'])
     end
 end
 
