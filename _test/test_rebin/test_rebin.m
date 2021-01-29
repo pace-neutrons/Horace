744f2cae7c67e04230d8891e8bc24f26bd2ec59e
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-01-29 09:09:32 +0000

    Re #464 Fixing tests failing due to the algorithm changes
    Former-commit-id: 5307167cb0935b8eb43e0342324b26c11f9e4752

--- _test/test_rebin/test_rebin.m
+++ _test/test_rebin/test_rebin.m
@@ -110,7 +110,7 @@ classdef test_rebin < TestCase
             w2d_qq_sqw_reb_check.data.e=w2d_qq_sqw_reb.data.e;
             w2d_qq_sqw_reb_check.data.pix.variance=w2d_qq_sqw_reb.data.pix.variance;
 
-            [ok,mess]=equal_to_tol(w2d_qq_sqw_reb_check,w2d_qq_sqw_reb,-1e-6,'ignore_str', 1);
+            [ok,mess]=equal_to_tol(w2d_qq_sqw_reb_check,w2d_qq_sqw_reb,-4e-6,'ignore_str', 1);
             assertTrue(ok,['rebin sqw using template object fails: ',mess])
         end
 
