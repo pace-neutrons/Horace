ac4dda1d6b3a9daa972be7446a8cdb78aa992187
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-02-03 12:14:52 +0000

    Re #380 Added check for run_id uniqueness and some comments

--- horace_core/algorithms/helper_functions/update_duplicated_rf_id.m
+++ horace_core/algorithms/helper_functions/update_duplicated_rf_id.m
@@ -1,7 +1,21 @@
 function run_files = update_duplicated_rf_id(run_files)
 % Processes the list of runfiles and if some files have duplicated
 % run-id-s, modify these id-s to be unique.
+%
+% Input: 
+% run_files  - cellarray of rundata class instances with some run_id
+%            may be duplicated or some cells are empty.
+% Output:
+% run_files  - cellarray of the same rundata class instances, modified to
+%              ensure all run_ids are different. The first duplicated
+%              run_id will be replaced by: 
+%              max(existing_run_id) + number_of_empty_cells_in_runfiles + 1
+%              the following duplicated run_id(s) numbers are the previous
+%              number incremeted by one.
+%
+%
 
+% extract exisitng run_id(s) assigning NaN to empty cells
 run_ids_all = cellfun(@get_run_id,run_files,'UniformOutput',true);
 [run_ids_sorted,sid]= sort(run_ids_all);
 
