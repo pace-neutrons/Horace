e9b8471411039ddfcc0737c877f9bc12ea7682d1
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-05 15:43:33 +0000

    Add linting

--- tools/build_config/build.sh
+++ tools/build_config/build.sh
@@ -67,16 +67,8 @@ function run_tests() {
 function run_analysis() {
   local output_dir=$1
 
-  if [ -f "$(which cppcheck)" ]; then
-    echo -e "\nRunning analysis step..."
-
-    analysis_cmd="cppcheck --enable=all --inconclusive"
-    analysis_cmd+=" --xml --xml-version=2"
-    analysis_cmd+=" -I ${HORACE_ROOT}/_LowLevelCode/cpp"
-    analysis_cmd+=" ${HORACE_ROOT}/_LowLevelCode/"
-    analysis_cmd+=" 2> ${output_dir}/cppcheck.xml"
-    echo_and_run "${analysis_cmd}"
-  fi
+  echo_and_run "cd ${build_dir}"
+  echo_and_run "make analyse"
 }
 
 function run_package() {
