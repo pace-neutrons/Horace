e0fd179eed6eb8447b73a0a65a337b625fcea3ca
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-02-12 17:52:12 +0000

    Re pace-neutrons/Herbert#298 Account for force_mex option

--- herbert_core/utilities/misc/serialise.m
+++ herbert_core/utilities/misc/serialise.m
@@ -1,13 +1,18 @@
 function ser = serialise(a)
 %Wrapper to handle mex/nomex
-use_mex = config_store.instance().get_value('herbert_config','use_mex');
+[use_mex,fm] = config_store.instance().get_value('herbert_config',...
+    'use_mex','force_mex_if_use_mex');
 if use_mex
     try
         ser = c_serialise(a);
         return
     catch ME
-        warning(ME.identifier,'%s',ME.message);
-        use_mex = false;
+        if fm
+            rethrow(fm);
+        else
+            warning(ME.identifier,'%s',ME.message);
+            use_mex = false;
+        end
     end
 end
 if ~use_mex
