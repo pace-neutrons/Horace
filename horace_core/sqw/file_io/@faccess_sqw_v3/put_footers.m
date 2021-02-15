dea15c96582d4826c81836080d210038948850f4
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-02-15 14:11:11 +0000

    Refactor sqwbinfilecommon to take -nopix argument
    This allows the user of the class to perform all aspects of saving an
    sqw up to saving the pixels. They can then manually save pixels and
    write footers. This means the user can write pixels without having the
    full pixel array in memory at any one time.

---
+++ horace_core/sqw/file_io/@faccess_sqw_v3/put_footers.m
@@ -0,0 +1,6 @@
+function obj = put_footers(obj)
+obj = put_sample_instr_records_(obj);
+% should not be necessary, as init calculated it correctly, but to be on a
+% safe side...
+obj.position_info_pos_= obj.instr_sample_end_pos_;
+obj = obj.put_sqw_footer();
