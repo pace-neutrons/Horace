640f58b997db413c4f97d055de7cc4fc5ddecaba
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-02-15 14:00:53 +0000

    Add nosave option to PixelData.advance
    This allows the user of the class to advance to the next page of data
    whilst discarding any cached changes. This can be useful when you want
    to make a change to a PixelData object, but preserve the original state
    of the object; with the 'nosave' option you can avoid copying the
    potentially very large object.
    
    
    Former-commit-id: 2bf6cdab4774ce4895a1b11243d40dbfa2547a22

--- horace_core/sqw/PixelData/@PixelData/move_to_page.m
+++ horace_core/sqw/PixelData/@PixelData/move_to_page.m
@@ -1,19 +1,18 @@
-function [page_number,total_num_pages] = move_to_page(obj, page_number)
+function [page_number,total_num_pages] = move_to_page(obj, page_number, varargin)
 % Set the object to point at the given page number
 %   This function does nothing if the object is not file-backed or is
 %   already on the given page
 %
 % Inputs:
 % page_number -- page number to move to
-% 
+%
 % Returns:
 % page_number -- the page this routine moved to
-% total_num_pages -- total number of pages, presend in the file
+% total_num_pages -- total number of pages, present in the file
 %
-[page_number,total_num_pages] = parse_args(obj, page_number);
+[page_number, total_num_pages, nosave] = parse_args(obj, page_number, varargin{:});
 if obj.is_file_backed_() && obj.page_number_ ~= page_number
-    
-    if obj.page_is_dirty_(obj.page_number_) && obj.dirty_page_edited_
+    if ~nosave && obj.page_is_dirty_(obj.page_number_) && obj.dirty_page_edited_
         obj.write_dirty_page_();
     end
     obj.page_number_ = page_number;
@@ -25,13 +24,15 @@ end
 
 
 % -----------------------------------------------------------------------------
-function [page_number,total_num_pages] = parse_args(obj, varargin)
+function [page_number,total_num_pages, nosave] = parse_args(obj, varargin)
 parser = inputParser();
 parser.addRequired('page_number', @is_scalar_positive_int);
+parser.addParameter('nosave', false, @islognumscalar);
 parser.parse(varargin{:});
 
 page_number = parser.Results.page_number;
 total_num_pages = obj.get_num_pages_();
+nosave = parser.Results.nosave;
 
 if page_number > total_num_pages
     error('PIXELDATA:move_to_page', ...
