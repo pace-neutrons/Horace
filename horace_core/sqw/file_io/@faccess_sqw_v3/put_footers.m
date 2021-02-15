fc7a0c2ab46397ad7522782739486d55197ffce7
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-02-15 14:41:31 +0000

    Add function help to new sqw loader methods

--- horace_core/sqw/file_io/@faccess_sqw_v3/put_footers.m
+++ horace_core/sqw/file_io/@faccess_sqw_v3/put_footers.m
@@ -1,4 +1,6 @@
 function obj = put_footers(obj)
+%PUT_FOOTERS Write SQW footers (including instrument records) to file.
+%
 obj = put_sample_instr_records_(obj);
 % should not be necessary, as init calculated it correctly, but to be on a
 % safe side...
