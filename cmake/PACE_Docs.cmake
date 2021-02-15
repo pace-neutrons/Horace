4eb9ee086f48669a7f7a35ff301bcab3472957ef
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-02-15 14:05:39 +0000

    Update to add control of docs placement

--- cmake/PACE_Docs.cmake
+++ cmake/PACE_Docs.cmake
@@ -1,19 +1,24 @@
 # Build documentation
 
-set(DOCS_DIR "${Horace_ROOT}/documentation/user_docs" CACHE STRING "Directory containing user documentation")
+set(DOCS_ROOT_DIR "${Horace_ROOT}/documentation/user_docs" CACHE FILEPATH "Directory containing user documentation")
+set(DOCS_SOURCE_DIR "${DOCS_ROOT_DIR}/docs" CACHE FILEPATH "Directory containing docs sources")
+set(DOCS_WORK_DIR "${DOCS_ROOT_DIR}/build" CACHE FILEPATH "Directory to put in-progress docs")
+set(DOCS_OUTPUT_DIR "${DOCS_WORK_DIR}/html" CACHE FILEPATH "Directory containing built HTML documentation")
+set(MANUAL_WORK_DIR "${DOCS_WORK_DIR}/latex" CACHE FILEPATH "Directory to build LaTeX sources")
+set(MANUAL_OUTPUT_DIR "${DOCS_WORK_DIR}/latex" CACHE FILEPATH "Directory to put compiled LaTeX manual")
 
 find_program(sphinx-build NAMES sphinx-build)
 find_program(pdflatex NAMES pdflatex)
 find_program(latexmk NAMES latexmk)
 if (sphinx-build)
-  if (UNIX)
+  if (UNIX) # Potential danger on mac trying to use PWSH
     add_custom_target(denullify-docs
-      COMMAND sed -i -r "/\[NULL\]/d" "${DOCS_DIR}/build/html/*html"
+      COMMAND sed -i -r "/\[NULL\]/d" "${DOCS_OUTPUT_DIR}/*html"
       DEPENDS build-docs
       )
   else()
     add_custom_target(denullify-docs
-      COMMAND powershell -ExecutionPolicy Bypass -command "Foreach($f in Get-ChildItem -Path '${DOCS_DIR}/build/html' -Filter *.html) {(Get-Content $f.FullName) | Where-Object {$_ -notmatch '\\[NULL\\]'} | Set-Content $F.FullName}"
+      COMMAND powershell -ExecutionPolicy Bypass -command "Foreach($f in Get-ChildItem -Path '${DOCS_OUTPUT_DIR}' -Filter *.html) {(Get-Content $f.FullName) | Where-Object {$_ -notmatch '\\[NULL\\]'} | Set-Content $f.FullName}"
       DEPENDS build-docs
       VERBATIM
       )
@@ -21,8 +26,8 @@ if (sphinx-build)
 
   add_custom_target(build-docs
     COMMENT "Building user documentation"
-    BYPRODUCTS "${DOCS_DIR}/html/*"
-    COMMAND ${sphinx-build} -b html "${DOCS_DIR}/docs" "${DOCS_DIR}/build/html" ${SPHINX_OPTS} -D "release=${${PROJECT_NAME}_SHORT_VERSION}" -D "version=${${PROJECT_NAME}_SHORT_VERSION}"
+    BYPRODUCTS "${DOCS_OUTPUT_DIR}/*"
+    COMMAND ${sphinx-build} -b html "${DOCS_SOURCE_DIR}" "${DOCS_OUTPUT_DIR}" ${SPHINX_OPTS} -D "release=${${PROJECT_NAME}_SHORT_VERSION}" -D "version=${${PROJECT_NAME}_SHORT_VERSION}"
     )
 
   add_custom_target(docs
@@ -32,22 +37,24 @@ if (sphinx-build)
 
   if (pdflatex AND latexmk)
     add_custom_command(OUTPUT horace.tex
-      COMMAND ${sphinx-build} -b latex "${DOCS_DIR}/docs" "${DOCS_DIR}/build/latex" ${SPHINX_OPTS} -D "release=${${PROJECT_NAME}_SHORT_VERSION}" -D "version=${${PROJECT_NAME}_SHORT_VERSION}"
-      WORKING_DIRECTORY "${DOCS_DIR}"
+      COMMAND ${sphinx-build} -b latex "${DOCS_SOURCE_DIR}" "${DOCS_WORK_DIR}" ${SPHINX_OPTS} -D "release=${${PROJECT_NAME}_SHORT_VERSION}" -D "version=${${PROJECT_NAME}_SHORT_VERSION}"
+      WORKING_DIRECTORY "${DOCS_ROOT_DIR}"
       )
 
     add_custom_command(OUTPUT horace.pdf
-      # Necessary to ignore return code
-      COMMAND latexmk -pdf -ps- -dvi- -silent -f -r "${DOCS_DIR}/build/latex/latexmkrc" "${DOCS_DIR}/build/latex/horace.tex"
+      COMMAND latexmk -pdf -ps- -dvi- -silent -f -r "${MANUAL_WORK_DIR}/latexmkrc" "${MANUAL_WORK_DIR}/horace.tex"
+      # Copy finished manual to output dir
+      COMMAND cmake -E rename "${MANUAL_WORK_DIR}/horace.pdf" "${MANUAL_OUTPUT_DIR}/horace.pdf"
       DEPENDS horace.tex
-      BYPRODUCTS "${DOCS_DIR}/build/latex"
-      WORKING_DIRECTORY "${DOCS_DIR}/build/latex"
+      BYPRODUCTS "${MANUAL_OUTPUT_DIR}/horace.pdf"
+      WORKING_DIRECTORY "${MANUAL_WORK_DIR}"
       )
 
     add_custom_target(manual
       COMMENT "Building user manual - Will produce error 12 until docs fixed"
       DEPENDS horace.pdf
       )
+
   else()
     add_custom_target(manual
       COMMENT "Manual requires latexmk and pdflatex to build"
