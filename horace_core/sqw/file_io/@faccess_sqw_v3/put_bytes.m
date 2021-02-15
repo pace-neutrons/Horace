d7fe24d9c44daf436f89fb2ce2fbee845bd51410
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-02-15 14:45:28 +0000

    Update error messages in faccess_sqw_v3.put_bytes
    Former-commit-id: 7fa1ff1656d3f9cc759cb83d3ab12c982b74184f

--- horace_core/sqw/file_io/@faccess_sqw_v3/put_bytes.m
+++ horace_core/sqw/file_io/@faccess_sqw_v3/put_bytes.m
@@ -2,23 +2,24 @@ function obj = put_bytes(obj, to_write)
 %PUT_BYTES Write the given array to the sqw file as single
 %
 BYTES_IN_SINGLE = 4;
+DTYPE = 'float32';
 
 block_size = get(hor_config, 'mem_chunk_size');
 nbytes_to_write = BYTES_IN_SINGLE*numel(to_write);
 
 if nbytes_to_write <= block_size
-    fwrite(obj.file_id_,to_write,'float32');
-    check_error_report_fail_(obj,'Error writing pixels array');
+    fwrite(obj.file_id_,to_write,DTYPE);
+    check_error_report_fail_(obj,'Error writing array data.');
 else
     for chunk_num = 1:block_size:nbytes_to_write
         istart = chunk_num;
         iend = min(chunk_num + block_size - 1, nbytes_to_write);
-        fwrite(obj.file_id_, to_write(istart:iend), 'float32');
+        fwrite(obj.file_id_, to_write(istart:iend), DTYPE);
         check_error_report_fail_( ...
             obj, ...
             sprintf( ...
-                ['Error writing pixels array, npix from: %d to: %d in the ' ...
-                 'rage from: %d to: %d'], istart, iend, 1, npix_to_write ...
+                ['Error writing array data, bytes from: %d to: %d in the ' ...
+                 'range from: %d to: %d.'], istart, iend, 1, nbytes_to_write ...
             ) ...
         );
     end
