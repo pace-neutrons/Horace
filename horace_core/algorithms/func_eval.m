a1cc102b2116892487f29edb0495f295d07ae730
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-02-16 17:28:16 +0000

    Wrap file reading in try-catch in func_eval
    Former-commit-id: 55f8e3a6c7a1ec5d863bf76ae435168b50a2ec9c

--- horace_core/algorithms/func_eval.m
+++ horace_core/algorithms/func_eval.m
@@ -69,17 +69,21 @@ function sqw_dnd_obj = get_data_source(source)
         % Get a loader instance that can tell us what kind of file this is
         % We expect either a .sqw or .dnd file, throw an error otherwise.
         ldr = sqw_formats_factory.instance().get_loader(source);
-        if ldr.sqw_type
-            % Load the .sqw file using the sqw constructor so that we can pass the
-            % pixel_page_size argument to get an sqw with file-backed pixels.
-            pixel_page_size = get(hor_config, 'pixel_page_size');
-            sqw_dnd_obj = sqw(source, 'pixel_page_size', pixel_page_size);
-        else
-            % In contrast to the above case, we can use the loader to get the dnd
-            % as no extra constructor arguments are required.
-            sqw_dnd_obj = ldr.get_dnd(source);
+        try
+            if ldr.sqw_type
+                % Load the .sqw file using the sqw constructor so that we can pass the
+                % pixel_page_size argument to get an sqw with file-backed pixels.
+                pixel_page_size = get(hor_config, 'pixel_page_size');
+                sqw_dnd_obj = sqw(source, 'pixel_page_size', pixel_page_size);
+            else
+                % In contrast to the above case, we can use the loader to get the dnd
+                % as no extra constructor arguments are required.
+                sqw_dnd_obj = ldr.get_dnd(source);
+            end
+        catch ME
+            ldr.delete();
+            rethrow(ME);
         end
-        ldr.delete();
     elseif isa(source, 'sqw') || ismember(class(source), DND_CLASSES)
         sqw_dnd_obj = source;
     else
