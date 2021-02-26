d2b98c70e992995643ff86ee365ac0b18fc53317
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-02-26 12:50:52 +0000

    Re #591 minor changes according to review.

--- horace_core/sqw/coord_transform/@aProjection/private/check_angdeg_return_standard_val_.m
+++ horace_core/sqw/coord_transform/@aProjection/private/check_angdeg_return_standard_val_.m
@@ -1,5 +1,14 @@
-function angdeg = check_angdeg_(val)
-% verify if input lattice angles belong to acceptable range
+function angdeg = check_angdeg_return_standard_val_(val)
+% verify if input lattice angles belong to acceptable range (+-180 deg,
+% negative angles treated as positive) and have
+% acceptable form (either 3-vector of angles or single value, defining 3 equal
+% lattice parameters)
+% 
+% return acceptable lattice angles in standard form,i.e. 
+% [1x3] vector of lattice parameters.
+% 
+% Throws if lattice angles can not be transformed into standard form
+%
 %
 if numel(val) == 1
     val = abs(val);
