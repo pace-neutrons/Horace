7364ec434e34bf34c17b4db538dcc9de80ae34b5
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-02 14:35:15 +0000

    Replace pixel looping logic with split_npix call
    This refactors lots of repeated logic out and replaces it with a
    function

--- horace_core/sqw/PixelData/@PixelData/private/binary_op_sigvar_.m
+++ horace_core/sqw/PixelData/@PixelData/private/binary_op_sigvar_.m
@@ -5,38 +5,17 @@ function obj = binary_op_sigvar_(obj, operand, binary_op, flip, npix)
 npix_cum_sum = validate_inputs(obj, operand, npix);
 
 obj.move_to_first_page();
-
-end_idx = 1;
-leftover_end = 0;
-pg_size = obj.page_size;
+[npix_chunks, idxs] = obj.split_npix(npix);
+page_number = 1;
 while true
-
-    start_idx = (end_idx - 1) + find(npix_cum_sum(end_idx:end) > 0, 1);
-    leftover_begin = npix_cum_sum(start_idx);
-    npix_cum_sum = npix_cum_sum - pg_size;
-
-    end_idx = (start_idx - 1) + find(npix_cum_sum(start_idx:end) > 0, 1);
-    if isempty(end_idx)
-        end_idx = numel(npix);
-    end
-
-    if start_idx == end_idx
-        npix_chunk = min(obj.page_size, npix(start_idx) - leftover_end);
-    else
-        npix_chunk = [ ...
-            leftover_begin, ...
-            reshape(npix(start_idx + 1:end_idx - 1), 1, []), ...
-            0 ...
-        ];
-        leftover_end = obj.page_size - sum(npix_chunk);
-        npix_chunk(end) = leftover_end;
-    end
+    npix_for_page = npix_chunks{page_number};
+    idx = idxs{page_number};
 
     sigvar_pix = sigvar(obj.signal, obj.variance);
     if ~isequal(size(npix), [1, 1])
         sigvar_dnd = sigvar(...
-            replicate_array(operand.s(start_idx:end_idx), npix_chunk(:))', ...
-            replicate_array(operand.e(start_idx:end_idx), npix_chunk(:))' ...
+            replicate_array(operand.s(idx(1):idx(2)), npix_for_page(:))', ...
+            replicate_array(operand.e(idx(1):idx(2)), npix_for_page(:))' ...
         );
     end
 
