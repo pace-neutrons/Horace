ec2593e4cc4eb5553478bda6c6d297fae33bac03
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-05 10:14:14 +0000

    Rename split_counts -> split_vector
    Former-commit-id: 9cec553b63188ae50b2b6dbbc9bb6a2b1c3f476c

--- _test/test_utilities/test_split_vector.m
+++ _test/test_utilities/test_split_vector.m
@@ -1,12 +1,12 @@
-classdef test_split_counts < TestCase
+classdef test_split_vector < TestCase
 
 methods
-    function obj = test_split_counts(~)
-        obj@TestCase('test_split_counts');
+    function obj = test_split_vector(~)
+        obj@TestCase('test_split_vector');
     end
 
     function test_outputs_are_empty_if_inputs_are_empty(obj)
-        [chunks, idxs] = split_counts([], []);
+        [chunks, idxs] = split_vector([], []);
         assertTrue(isa(chunks, 'cell'))
         assertTrue(isempty(chunks));
         assertTrue(isa(idxs, 'double'))
@@ -14,47 +14,47 @@ methods
     end
 
     function test_outputs_have_length_1_if_max_counts_gt_total_counts(~)
-        counts = ones(1, 10);
-        max_counts = 11;
-        [chunks, idxs] = split_counts(counts, max_counts);
+        vector = ones(1, 10);
+        max_sum = 11;
+        [chunks, idxs] = split_vector(vector, max_sum);
         assertEqual(numel(chunks), 1);
-        assertEqual(chunks{1}, counts);
+        assertEqual(chunks{1}, vector);
         assertEqual(size(idxs), [2, 1]);
-        assertEqual(idxs, [1; numel(counts)]);
+        assertEqual(idxs, [1; numel(vector)]);
     end
 
     function test_outputs_have_length_1_if_max_counts_eq_total_counts(~)
-        counts = ones(1, 10);
-        max_counts = 11;
-        [chunks, idxs] = split_counts(counts, max_counts);
+        vector = ones(1, 10);
+        max_sum = 11;
+        [chunks, idxs] = split_vector(vector, max_sum);
         assertEqual(numel(chunks), 1);
-        assertEqual(chunks{1}, counts);
+        assertEqual(chunks{1}, vector);
         assertEqual(size(idxs), [2, 1]);
-        assertEqual(idxs, [1; numel(counts)]);
+        assertEqual(idxs, [1; numel(vector)]);
     end
 
     function test_error_if_counts_is_not_a_vector(~)
-        counts = ones(2, 10);
-        f = @() split_counts(counts, 100);
+        vector = ones(2, 10);
+        f = @() split_vector(vector, 100);
         assertExceptionThrown(f, 'MATLAB:expectedVector');
     end
 
     function test_error_if_max_counts_is_not_a_scalar(~)
-        counts = ones(1, 10);
-        f = @() split_counts(counts, [1, 1]);
+        vector = ones(1, 10);
+        f = @() split_vector(vector, [1, 1]);
         assertExceptionThrown(f, 'MATLAB:expectedScalar');
     end
 
     function test_error_if_max_counts_is_zero(~)
-        counts = ones(1, 10);
-        f = @() split_counts(counts, 0);
+        vector = ones(1, 10);
+        f = @() split_vector(vector, 0);
         assertExceptionThrown(f, 'MATLAB:expectedPositive');
     end
 
     function test_chunking_correct_if_a_count_gt_max_counts(~)
-        counts = [3, 2, 0, 6, 0, 5, 3, 1, 1, 24, 4, 2, 3, 0];
-        max_counts = 11;
-        [chunks, idxs] = split_counts(counts, max_counts);
+        vector = [3, 2, 0, 6, 0, 5, 3, 1, 1, 24, 4, 2, 3, 0];
+        max_sum = 11;
+        [chunks, idxs] = split_vector(vector, max_sum);
         expected_chunks = { ...
             [3, 2, 0, 6, 0], ...
             [5, 3, 1, 1], ...
@@ -68,9 +68,9 @@ methods
     end
 
     function test_chunking_correct_for_a_sample_counts_array(~)
-        counts = [3, 2, 0, 6, 0, 5, 3, 1, 1, 4, 2, 3, 0];
-        max_counts = 11;
-        [chunks, idxs] = split_counts(counts, max_counts);
+        vector = [3, 2, 0, 6, 0, 5, 3, 1, 1, 4, 2, 3, 0];
+        max_sum = 11;
+        [chunks, idxs] = split_vector(vector, max_sum);
         expected_chunks = { ...
             [3, 2, 0, 6, 0], ...
             [5, 3, 1, 1], ...
@@ -83,9 +83,9 @@ methods
     end
 
     function test_chunking_correct_for_counts_array_with_doubles(~)
-        counts = [3.5, 2.1, 0, 5.5, 0, 5.1, 3.4, 1, 1, 4.1, 2, 3, 0.2];
-        max_counts = 11;
-        [chunks, idxs] = split_counts(counts, max_counts);
+        vector = [3.5, 2.1, 0, 5.5, 0, 5.1, 3.4, 1, 1, 4.1, 2, 3, 0.2];
+        max_sum = 11;
+        [chunks, idxs] = split_vector(vector, max_sum);
         expected_chunks = { ...
             [3.5, 2.1, 0], ...
             [5.5, 0, 5.1], ...
