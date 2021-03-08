8a9555004a28b326f656c175053fa0d5117b96cc
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-03-08 13:57:18 +0000

    Use inputParser
    Former-commit-id: 5c9b37824960a941d28fca252a016efc8017f71a

--- _test/common_functions/is_cut_equal.m
+++ _test/common_functions/is_cut_equal.m
@@ -1,4 +1,4 @@
-function [ok,mess,w1tot,w2tot]=is_cut_equal(f1,f2,varargin)
+function [ok,mess,w1tot,w2tot]=is_cut_equal(varargin)
 % Make cut from an array of files or sqw objects,
 % add together, and compare with same for another array of files
 %
@@ -12,48 +12,81 @@ function [ok,mess,w1tot,w2tot]=is_cut_equal(f1,f2,varargin)
 %   >> proj.u=[1,1,0]; proj.v=[0,0,1];
 %   >> w1_2=cut_sqw(f1,f2,proj,[-1.5,0.05,-0.5],[-0.6,-0.44],[-0.5,0.5],[5,10]);
 
-if ischar(f1), f1={f1}; end
-if ischar(f2), f2={f2}; end
-if isa(f1,'sqw'), f1={f1}; end
-if isa(f2,'sqw'), f2={f2}; end
 
-w1=repmat(sqw,1,numel(f1));
-w2=repmat(sqw,1,numel(f2));
-for i=1:numel(f1)
-    w1(i)=cut_sqw(f1{i},varargin{:});
-end
-for i=1:numel(f2)
-    w2(i)=cut_sqw(f2{i},varargin{:});
-end
+    [f1, f2, proj, pN, extraArgs] = parse_args(varargin)
 
-w1tot=combine_cuts(w1);
-w2tot=combine_cuts(w2);
+    w1=repmat(sqw,1,numel(f1));
+    w2=repmat(sqw,1,numel(f2));
+    for i=1:numel(f1)
+        w1(i)=cut_sqw(f1{i},pN{:},extraArgs{:});
+    end
+    for i=1:numel(f2)
+        w2(i)=cut_sqw(f2{i},pN{:},extraArgs{:});
+    end
+
+    w1tot=combine_cuts(w1);
+    w2tot=combine_cuts(w2);
 
 % To check equality, see if npix, s, e arrays are the same
-if equal_to_tol(w1tot.data.npix,w2tot.data.npix) &&...
-        equal_to_tol(w1tot.data.s,w2tot.data.s) &&...
-        equal_to_tol(w1tot.data.e,w2tot.data.e) &&...
-        equal_to_tol(w1tot.data.img_range,w2tot.data.img_range)
-    if isempty(w1tot.data.pix)
-        ok=true;
-        mess='';
-    else
-        if equal_to_tol(w1tot.data.pix.pix_range,w2tot.data.pix.pix_range) && ...
-                equal_to_tol(w1tot.data.pix.num_pixels,w2tot.data.pix.num_pixels)
+    if equal_to_tol(w1tot.data.npix,w2tot.data.npix) &&...
+            equal_to_tol(w1tot.data.s,w2tot.data.s) &&...
+            equal_to_tol(w1tot.data.e,w2tot.data.e) &&...
+            equal_to_tol(w1tot.data.img_range,w2tot.data.img_range)
+        if isempty(w1tot.data.pix)
             ok=true;
             mess='';
         else
-            ok = false;
-            mess=sprintf(['Pixels parameters of two cuts are different:\n',...
-                '   npix1=%d and npix2 = %d\n',...
-                'pix_range1 = [%f  %f %f %f; pix_range2=[%f  %f %f  %f;\n',...
-                '              %f  %f %f %f]             %f  %f %f  %f]'],...
-                w1tot.data.pix.num_pixels,w2tot.data.pix.pix_range,...
-                w1tot.data.pix.pix_range(1,:),w2tot.data.pix.pix_range(1,:),...
-                w1tot.data.pix.pix_range(2,:),w2tot.data.pix.pix_range(2,:));
+            if equal_to_tol(w1tot.data.pix.pix_range,w2tot.data.pix.pix_range) && ...
+                    equal_to_tol(w1tot.data.pix.num_pixels,w2tot.data.pix.num_pixels)
+                ok=true;
+                mess='';
+            else
+                ok = false;
+                mess=sprintf(['Pixels parameters of two cuts are different:\n',...
+                              '   npix1=%d and npix2 = %d\n',...
+                              'pix_range1 = [%f  %f %f %f; pix_range2=[%f  %f %f  %f;\n',...
+                              '              %f  %f %f %f]             %f  %f %f  %f]'],...
+                             w1tot.data.pix.num_pixels,w2tot.data.pix.pix_range,...
+                             w1tot.data.pix.pix_range(1,:),w2tot.data.pix.pix_range(1,:),...
+                             w1tot.data.pix.pix_range(2,:),w2tot.data.pix.pix_range(2,:));
+            end
         end
+    else
+        ok=false;
+        mess='One or more of npix, s, e are not the same';
     end
-else
-    ok=false;
-    mess='One or more of npix, s, e are not the same';
+end
+
+function [f1, f2, proj, pN, extraArgs] = parse_args(varargin)
+    p = inputParser();
+    addRequired(p, 'f1', @validate_sqw);
+    addRequired(p, 'f2', @validate_sqw);
+    addRequired(p, 'proj', @validate_proj);
+    addRequired(p, 'p1',     @(x)(validateattributes(x,{'numeric'},{'nonempty','vector'})));
+    addRequired(p, 'p2',     @(x)(validateattributes(x,{'numeric'},{'nonempty','vector'})));
+    addOptional(p, 'p3', [], @(x)(validateattributes(x,{'numeric'},{'nonempty','vector'})));
+    addOptional(p, 'p4', [], @(x)(validateattributes(x,{'numeric'},{'nonempty','vector'})));
+    addParameter(p, 'cutArgs', {}, @iscellstr)
+    p = parse(p, varargin{:});
+
+    f1 = p.Results.f1;
+    f2 = p.Results.f2;
+    if ~iscell(f1), f1={f1}; end
+    if ~iscell(f2), f2={f2}; end
+
+    proj = p.Results.proj;
+    pN = {p.Results.p1, p.Results.p2, p.Results.p3, p.Results.p4};
+    pN = pN(~empty(pN));
+    extraArgs = p.Results.cutArgs;
+end
+
+function ok = validate_sqw(inp)
+    check = @(x)(isa(inp, 'sqw') || ischar(inp));
+    ok = check(inp) || iscell(inp) && all(cellfun(check, inp));
+end
+
+function ok = validate_proj(inp)
+    ok = hasfield(inp, 'u') && hasfield(inp, 'v');
+    ok = ok && validateattributes(inp.u, {'numeric'}, {'nonempty','vector','numel',3});
+    ok = ok && validateattributes(inp.v, {'numeric'}, {'nonempty','vector','numel',3});
 end
