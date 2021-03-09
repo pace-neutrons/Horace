de3afe52a25bb00a027b7ddfa1160dd1253fd343
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-09 13:44:44 +0000

    Add detail to PixelData.logical_to_normal_index_ help

--- horace_core/sqw/PixelData/@PixelData/private/logical_to_normal_index_.m
+++ horace_core/sqw/PixelData/@PixelData/private/logical_to_normal_index_.m
@@ -1,15 +1,35 @@
-function abs_pix_indices = logical_to_normal_index_(obj, logical_indices)
-%ABS_PIX_INDICES Convert the given logical indices to normal indices
+function normal_indices = logical_to_normal_index_(obj, logical_indices)
+%LOGICAL_TO_NORMAL_INDEX_ Convert the given logical indices to normal indices
+%
+% If there are any 'true' values outside the range of pixels
+% (e.g. logical_indices(obj.num_pixels + 1) == true), an error with ID
+% 'HORACE:PIXELDATA:badsubscript' will be thrown. 'false' values outside the
+% range of pixels are ignored.
+%
+% Input:
+% ------
+% logical_indices   An array of logicals
+%
+% Outputs:
+% --------
+% normal_indices    A vector of "normal" indices i.e. positive integers.
+%
+% Examples:
+% ---------
+%
+%   >> logical_to_normal_index_(pix, [0, 1, 1, 0, 0, 1, 0, 1])
+%        ans =
+%            [2, 3, 6, 8]
 %
 if numel(logical_indices) > obj.num_pixels
     if any(logical_indices(obj.num_pixels + 1:end))
         error( ...
             'HORACE:PIXELDATA:badsubscript', ...
-            ['The logical indices contain a true value ' ...
-                'outside of the array bounds.'] ...
+            ['The logical indices contain a true value outside of the ' ...
+             'array bounds.'] ...
         );
     else
         logical_indices = logical_indices(1:obj.num_pixels);
     end
 end
-abs_pix_indices = find(logical_indices);
+normal_indices = find(logical_indices);
