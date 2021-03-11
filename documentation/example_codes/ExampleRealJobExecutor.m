f5357244a9cdb7302cf5bc6976db4274308e06e3
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-03-11 10:39:41 +0000

    Update

--- documentation/example_codes/ExampleRealJobExecutor.m
+++ documentation/example_codes/ExampleRealJobExecutor.m
@@ -27,25 +27,36 @@ classdef ExampleRealJobExecutor < JobExecutor
         function obj = do_job(obj)
 
             % Perform a loop
-            for i = 1:numel(obj.n_steps)
+            for i = 1:obj.n_steps
                 obj.my_int = obj.my_int + i;
             end
 
             % Send my ID to my +1 neighbour (assuming 4 neighbours)
             my_message = DataMessage(obj.labIndex);
-            [ok, err_mess] = obj.send_message(mod(obj.labIndex + 1,4), my_message);
-            if ~ok
-                error('HORACE:exampleRealJobExecutor:send_error', err_mess)
+            try
+                [ok, err_mess] = obj.mess_framework.send_message(mod(obj.labIndex + 1,4)+1, my_message);
+                if ~ok
+                    error('HORACE:exampleRealJobExecutor:send_error', err_mess)
+                end
+            catch ME
+                a = fopen(['/home/jacob/tmp',num2str(obj.labIndex),'.txt']);
+                fprintf(a, "%s", ME.message);
             end
 
+
             % Receive the data
-            [ok, err_mess, data] = obj.recieve_message(mod(obj.labIndex + 3,4), my_message);
-            if ~ok
-                error('HORACE:exampleRealJobExecutor:receive_error', err_mess)
+            try
+                [ok, err_mess, data] = obj.mess_framework.recieve_message(mod(obj.labIndex + 3,4)+1, 'any');
+                if ~ok
+                    error('HORACE:exampleRealJobExecutor:receive_error', err_mess)
+                end
+            catch ME
+                a = fopen(['/home/jacob/tmp',num2str(obj.labIndex),'.txt']);
+                fprintf(a, "%s", ME.message);
             end
 
             % Output some data
-            obj.task_output = {obj.common_data_, obj.my_int, data};
+            obj.task_outputs = {obj.common_data_, obj.my_int, data};
 
         end
     end
--- documentation/example_codes/exampleParallelCall.m
+++ documentation/example_codes/exampleParallelCall.m
@@ -4,7 +4,7 @@ jd = JobDispatcher('JobName');              % Create job dispatcher
  n_failed,...                               % Count of failed workers
  task_ids,...                               % IDs of each worker (message folder?)
  jd       ...                               % Object returns itself when done
- ] = jd.start_job('exampleJobExecutor', ... % Name of class to run
+ ] = jd.start_job('ExampleJobExecutor', ... % Name of class to run
                   [1,2], ...                % Data passed to all classes via common_data
                   10, ...                   % Number of "iterations" as int or cellarr of
                   ...                       % structures of data to pass to each worker
@@ -15,30 +15,30 @@ disp(outputs);
 disp(n_failed);
 disp(task_ids);
 disp(jd);
-jd = JobDispatcher('JobName');              % Create job dispatcher
+jd = JobDispatcher('RunWithInt');              % Create job dispatcher
 
 % Run with int loop count
-[outputs, n_failed, task_ids, jd] = jd.start_job('exampleRealJobExecutor', [1,2], 10, true, 4);
+[outputs, n_failed, task_ids, jd] = jd.start_job('ExampleRealJobExecutor', [1,2], 10, true, 4);
 
 disp(outputs);
 disp(n_failed);
 disp(task_ids);
 disp(jd);
-jd = JobDispatcher('JobName');              % Create job dispatcher
+jd = JobDispatcher('RunWithCellStruct');              % Create job dispatcher
 
 % Run with struct to split
 data = {struct('a',1), struct('a',2), struct('a',3), struct('a',4)};
-[outputs, n_failed, task_ids, jd] = jd.start_job('exampleRealJobExecutor', [1,2], data, true, 4);
+[outputs, n_failed, task_ids, jd] = jd.start_job('ExampleRealJobExecutor', [1,2], data, true, 4);
 
 disp(outputs);
 disp(n_failed);
 disp(task_ids);
 disp(jd);
-jd = JobDispatcher('JobName');              % Create job dispatcher
+jd = JobDispatcher('RunWithStruct');              % Create job dispatcher
 
 % Run with struct whose first field denotes job count(?)
 data = struct('nJobs',10);
-[outputs, n_failed, task_ids, jd] = jd.start_job('exampleRealJobExecutor', [1,2], data, true, 4);
+[outputs, n_failed, task_ids, jd] = jd.start_job('ExampleRealJobExecutor', [1,2], data, true, 4);
 
 disp(outputs);
 disp(n_failed);
