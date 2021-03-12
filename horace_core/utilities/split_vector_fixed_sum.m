4967048545f7bc12862c25c253f1d7be0ab6442a
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-12 15:56:37 +0000

    Be more brief in split_vector_fixed_sum comments

--- horace_core/utilities/split_vector_fixed_sum.m
+++ horace_core/utilities/split_vector_fixed_sum.m
@@ -60,11 +60,9 @@ for chunk_num = 1:num_chunks
     remaining_sum = vector_sum - allocated;
     chunk_sum = min(chunk_sum, remaining_sum);
 
-    % Find the index of the first value in the input vector that will
-    % contribute to the current chunk.
-    % This index may contribute its full value, or only part of its value
     if chunk_num == 1
         % For the first iteration, we need >= to catch leading zeros
+        % This index may contribute its full value, or only part of its value
         start_idx = (end_idx - 1) + find(cumulative_sum(end_idx:end) >= 0, 1);
     else
         % We ignore leading zeros on subsequent iterations as they will have
@@ -72,17 +70,12 @@ for chunk_num = 1:num_chunks
         start_idx = (end_idx - 1) + find(cumulative_sum(end_idx:end) > 0, 1);
     end
 
-    % The start value of the current chunk. This will be non-zero if we didn't
-    % assign the end index's full value on the last iteration
+    % First value in the current chunk
     leftover_begin = cumulative_sum(start_idx);
 
-    % Subtract the sum of the values we've allocated this iteration from the
-    % cumulative sum. This means the next 'find' call will retrieve the index
-    % such that the chunk
+    % Offset cumulative sum so next positive value corresponds to index of
+    % the final value of current chunk
     cumulative_sum = cumulative_sum - chunk_sum;
-
-    % Now the cumulative_sum has been decremented, a similar 'find' call to
-    % above finds the final index for the this iteration's chunk
     end_idx = (start_idx - 1) + find(cumulative_sum(start_idx:end) > 0, 1);
 
     if isempty(end_idx)
@@ -97,11 +90,9 @@ for chunk_num = 1:num_chunks
         end
         chunks{chunk_num} = min(chunk_sum, numeric_vector(start_idx) - leftover_end);
     else
-        % Build the chunk, leftover begin is the remainder of the end index
-        % value, that wasn't assigned in the previous iteration.
-        % Add an empty element to the end, which will either be used to hold a
-        % part of the end index's value. If there's no remainder on the end
-        % index's value, the empty element will be removed
+        % Add an empty element to the end, which will be used to hold a part of
+        % the end index's value. If there's no remainder on the end index's
+        % value, the empty element will be removed
         chunk = [ ...
             leftover_begin, ...
             reshape(numeric_vector(start_idx + 1:end_idx - 1), 1, []), ...
@@ -110,7 +101,7 @@ for chunk_num = 1:num_chunks
 
         leftover_end = chunk_sum - sum(chunk);
         if leftover_end == 0 && chunk_num ~= num_chunks
-            chunk = chunk(1:end - 1);
+            chunk(end) = [];
             end_idx = end_idx - 1;
         else
             chunk(end) = leftover_end;
@@ -120,94 +111,3 @@ for chunk_num = 1:num_chunks
     idxs(:, chunk_num) = [start_idx; end_idx];
     allocated = allocated + sum(chunks{chunk_num});
 end
-
-
-%% Algorithm run through:
-%
-% numeric_vector = [3, 2, 3, 6]
-% chunk_sum = 6
-%
-% cumulative_sum = [3, 5, 8, 14]
-% end_idx = 1
-%
-% chunk_num == 1:
-%
-% 	start_idx = (end_idx - 1) + find(cumulative_sum(end_idx:end) > 0, 1)
-% 		= 1
-%
-% 	leftover_begin = cumulative_sum(start_idx)
-% 		= 3
-%
-% 	cumulative_sum = cumulative_sum - chunk_sum
-% 		= [-3, -1, 2, 8]
-%
-% 	end_idx = (start_idx - 1) + find(cumulative_sum(start_idx:end) > 0, 1)
-% 		= 3
-%
-% 	chunk = [ ...
-%             leftover_begin, ...
-%             reshape(numeric_vector(start_idx + 1:end_idx - 1), 1, []), ...
-%             0 ...
-%         ];
-% 		= [3, 0]
-%
-% 	leftover_end = chunk_sum - sum(chunk)
-% 		= 3
-%
-% 	chunk(end) = 3
-% 	chunk
-% 		= [3, 3]
-%
-%
-% chunk_num == 2:
-%
-% 	start_idx = (end_idx - 1) + find(cumulative_sum(end_idx:end) > 0, 1)
-% 		= 3
-%
-% 	leftover_begin = cumulative_sum(start_idx)
-% 		= 2
-%
-% 	cumulative_sum = cumulative_sum - chunk_sum
-% 		= [-9, -7, -4, 2]
-%
-% 	end_idx = (start_idx - 1) + find(cumulative_sum(start_idx:end) > 0, 1)
-% 		= 4
-%
-% 	chunk = [ ...
-% 		leftover_begin, ...
-% 		reshape(numeric_vector(start_idx + 1:end_idx - 1), 1, []), ...
-% 		0 ...
-%     ];
-% 		= [2, 0]
-%
-% 	leftover_end = chunk_sum - sum(chunk)
-% 		= 4
-%
-% 	chunk(end) = 4
-% 	chunk
-% 		= [2, 4]
-%
-%
-% chunk_num == 3:  (final chunk)
-%
-% 	start_idx = (end_idx - 1) + find(cumulative_sum(end_idx:end) > 0, 1)
-% 		= 4
-%
-% 	leftover_begin = cumulative_sum(start_idx)
-% 		= 2
-%
-% 	cumulative_sum = cumulative_sum - chunk_sum
-% 		= [-15, -13, -10, -4]
-%
-% 	end_idx = (start_idx - 1) + find(cumulative_sum(start_idx:end) > 0, 1)
-% 		= []
-%
-% 	if isempty(end_idx)
-%         end_idx = numel(numeric_vector);
-% 			= 4
-%     end
-%
-% 	if start_idx == end_idx
-% 		chunk = min(chunk_sum, numeric_vector(start_idx) - leftover_end);
-% 			= 2
-% 	end
