18c67ee870974b73c5c7bf603b9b07d0d8991c51
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-16 10:33:37 +0000

    Disable all job_dispatcher_common_tests tests

--- _test/test_mpi/test_job_dispatcher_mpiexec.m
+++ _test/test_mpi/test_job_dispatcher_mpiexec.m
@@ -15,37 +15,6 @@ classdef test_job_dispatcher_mpiexec < job_dispatcher_common_tests
             assertTrue(isfile(mpiexec_path));
         end
 
-        function test_job_fail_restart(obj, varargin)
-            if ispc() && is_jenkins()
-                skipTest('Consistent failures on Windows');
-            else
-                test_job_fail_restart@job_dispatcher_common_tests(obj, varargin{:});
-            end
-        end
-
-        function test_job_with_logs_3workers(obj, varargin)
-            if ispc() && is_jenkins()
-                skipTest('Consistent failures on Windows');
-            else
-                test_job_with_logs_3workers@job_dispatcher_common_tests(obj, varargin{:});
-            end
-        end
-
-        function test_job_with_logs_2workers(obj, varargin)
-            if ispc() && is_jenkins()
-                skipTest('Consistent failures on Windows');
-            else
-                test_job_with_logs_2workers@job_dispatcher_common_tests(obj, varargin{:});
-            end
-        end
-
-        function test_job_with_logs_worker(obj, varargin)
-            if ispc() && is_jenkins()
-                skipTest('Consistent failures on Windows');
-            else
-                test_job_with_logs_worker@job_dispatcher_common_tests(obj, varargin{:});
-            end
-        end
    end
 
 end
