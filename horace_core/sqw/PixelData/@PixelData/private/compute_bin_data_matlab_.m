0ac3f7344d92752ecc522ec3fcafe65c75285a3b
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-17 16:44:37 +0000

    Rewrite no-mex compute_bin_data
    The function was failing some tests, so has been re-written and
    (hopefully) simplified.

--- horace_core/sqw/PixelData/@PixelData/private/compute_bin_data_matlab_.m
+++ horace_core/sqw/PixelData/@PixelData/private/compute_bin_data_matlab_.m
@@ -3,79 +3,39 @@ function [mean_signal, mean_variance] = compute_bin_data_matlab_(obj, npix, log_
 %
 % See compute_bin_data for algorithm details
 %
-nbin = numel(npix);
+obj.move_to_first_page();
 npix_shape = size(npix);
 
-try
-    bin_indices = int64(1:nbin);
-    npix = reshape(npix, numel(npix), 1);
-    allocatable = npix(bin_indices) ~= 0;
-    % List of indices of bins for which pixels can be allocated
-    bin_indices = bin_indices(allocatable);
+img_signal_sum = zeros(1, numel(npix));
+img_variance_sum = zeros(1, numel(npix));
+[npix_chunks, idxs] = split_vector_fixed_sum(npix(:), obj.base_page_size);
+for i = 1:numel(npix_chunks)
+    npix_chunk = npix_chunks{i};
+    idx = idxs(:, i);
 
-    if isempty(bin_indices)
-        mean_signal = [];
-        mean_variance = [];
-        return;
-    end
+    % First argument in accumarray must be a column vector, so make input
+    % to 'repelem' a column so we get a column out
+    accum_indices = repelem(make_column(1:numel(npix_chunk)), npix_chunk);
 
-    ti = arrayfun(@(ind) {int64(ones(npix(ind), 1)) * ind}, bin_indices);
-    ind = cat(1, ti{:});
-    clear bin_indices ti allocatable;
-
-catch ME
-    switch ME.identifier
-        case 'MATLAB:nomem'
-            clear bin_indices ti allocatable;
-
-            nend = cumsum(npix(:));
-            npixtot = nend(end);
-            nbeg = nend - npix(:) + 1;
-            ind = zeros(npixtot, 1, 'int32');
-
-            if log_level > 0
-                warning('SQW:recompute_bin_data', ...
-                    'Not enough memory to define bin indexes, running slow loop')
-            end
-
-            for i = 1:nbin
-                ind(nbeg(i):nend(i)) = i;
-            end
-
-            if log_level > 0
-                warning('SQW:recompute_bin_data', ' slow loop completed')
-            end
-
-        otherwise
-            rethrow(ME);
-    end
-end
-
-obj.move_to_first_page();  % make sure we're at the first page of data
-
-signal_sum = zeros(size(npix));
-variance_sum = zeros(size(npix));
-end_idx = 1;
-while true
-    start_idx = end_idx;
-    end_idx = start_idx + obj.page_size - 1;
-    signal_sum = signal_sum ...
-        + accumarray(ind(start_idx:end_idx), obj.signal, [nbin, 1]);
-    variance_sum = variance_sum ...
-        + accumarray(ind(start_idx:end_idx), obj.variance, [nbin, 1]);
+    % Pixel data is always a row vector, make sure the increment is also
+    sig_increment = make_row(accumarray(accum_indices, obj.signal));
+    img_signal_sum(idx(1):idx(2)) = img_signal_sum(idx(1):idx(2)) + sig_increment;
+    var_increment = make_row(accumarray(accum_indices, obj.variance));
+    img_variance_sum(idx(1):idx(2)) = img_variance_sum(idx(1):idx(2)) + var_increment;
 
     if obj.has_more()
         obj.advance();
     else
-        break;
+        break
     end
 end
-mean_signal = signal_sum ./ npix;
-mean_signal = reshape(mean_signal, npix_shape);
-mean_variance = variance_sum ./ (npix.^2);
-mean_variance = reshape(mean_variance, npix_shape);
+img_signal_sum = reshape(img_signal_sum, npix_shape);
+img_variance_sum = reshape(img_variance_sum, npix_shape);
+
+mean_signal = img_signal_sum./npix;
+mean_variance = img_variance_sum./(npix.^2);
 
 % Convert NaNs to zeros (bins where we divide by zero have no pixel contributions)
-nopix = (npix(:) == 0);
+nopix = (npix == 0);
 mean_signal(nopix) = 0;
 mean_variance(nopix) = 0;
