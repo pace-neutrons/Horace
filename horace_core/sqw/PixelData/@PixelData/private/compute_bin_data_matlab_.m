8ffddd350b0a34912d0ee9febfee9c79e919d02a
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-18 10:26:18 +0000

    Fix trailing zeros bug in nomex compute_bin_data
    A "Matrix dimensions must agree" error was being thrown if the npix
    array had trailing zeros. This was because the accumarray call was not
    factoring in trailing zeros when calculating the increment.
    
    Setting the output array size on the accumarray calls fixes this.
    
    
    Former-commit-id: e37325744da72f2e0fdb1b5f32b34f7b89460c81

--- horace_core/sqw/PixelData/@PixelData/private/compute_bin_data_matlab_.m
+++ horace_core/sqw/PixelData/@PixelData/private/compute_bin_data_matlab_.m
@@ -19,14 +19,19 @@ for i = 1:numel(npix_chunks)
     npix_chunk = npix_chunks{i};
     idx = idxs(:, i);
 
-    % First argument in accumarray must be a column vector
+    % First argument in accumarray must be a column vector, because pixel signal
+    % (and variance) is always a row vector
     accum_indices = make_column(repelem(1:numel(npix_chunk), npix_chunk));
+    increment_size = [idx(2) - idx(1) + 1, 1];
 
-    % Pixel data is always a row vector, make sure the increment is also
-    sig_increment = make_row(accumarray(accum_indices, obj.signal));
-    img_signal_sum(idx(1):idx(2)) = img_signal_sum(idx(1):idx(2)) + sig_increment;
-    var_increment = make_row(accumarray(accum_indices, obj.variance));
-    img_variance_sum(idx(1):idx(2)) = img_variance_sum(idx(1):idx(2)) + var_increment;
+    % We need to set the increment size in the accumarray call or it will
+    % ignore trailing zeros on the npix chunk. Meaning the increment will be a
+    % different length to the chunk of the image we're updating
+    sig_increment = accumarray(accum_indices, obj.signal, increment_size);
+    img_signal_sum(idx(1):idx(2)) = img_signal_sum(idx(1):idx(2)) + sig_increment';
+
+    var_increment = accumarray(accum_indices, obj.variance, increment_size);
+    img_variance_sum(idx(1):idx(2)) = img_variance_sum(idx(1):idx(2)) + var_increment';
 
     if obj.has_more()
         obj.advance();
