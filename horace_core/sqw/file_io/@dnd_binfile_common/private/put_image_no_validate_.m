339cfbce4100da0e43440b0fc913ec7bbb39cda9
Harry Saunders <harry.saunders@stfc.ac.uk>
2021-03-19 11:50:21 +0000

    Add function help to put_image* methods
    Former-commit-id: ad4fed63cfcf498191a4b1a15c44f33e66d1c9c7

--- horace_core/sqw/file_io/@dnd_binfile_common/private/put_image_no_validate_.m
+++ horace_core/sqw/file_io/@dnd_binfile_common/private/put_image_no_validate_.m
@@ -1,8 +1,20 @@
 function obj = put_image_no_validate_(obj, s, e, s_pos)
-%PUT_IMAGE_NO_VALIDATE Write the image signal & error
+%PUT_IMAGE_NO_VALIDATE Write the image signal and error to file
 %
 % This function performs no checks that the loader has been initialized
-% correctly and the loader's file is open. The calling function should
+% correctly and that the loader's file is open. It also does not verify that
+% the input image and error have the same number of elements.
+% The calling function should perform these checks to avoid corrupted files.
+%
+% Input:
+% ------
+% s      The image signal. Must be a numeric array.
+% e      The image error. This must be a numeric array and have the same number
+%        of elements as s.
+% s_pos  The position in the file of the start of the signal array (which is
+%        the start of the image data). This value may be different from
+%        `obj.s_pos_` if the file format is being upgraded. This argument is
+%        optional, `obj.s_pos_` is the default.
 %
 if nargin < 4
     s_pos = obj.s_pos_;
