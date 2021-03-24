c213f1007969f60f18aa28d7e28eaf412f91a4aa
cmarooney-stfc <christopher.marooney@stfc.ac.uk>
2021-03-24 12:03:43 +0000

    updates for d2d multifit

--- horace_core/sqw/@SQWDnDBase/mask.m
+++ horace_core/sqw/@SQWDnDBase/mask.m
@@ -10,18 +10,22 @@ function wout = mask (win, mask_array)
 %   mask_array          Array of 1 or 0 (or true or false) that indicate
 %                      which points to retain (true to retain, false to ignore)
 %                       Numeric or logical array of same number of elements
-%                      as the data.
+%                      as the data_.
 %                       Note: mask will be applied to the stored data array
 %                      according as the projection axes, not the display axes.
 %                      Thus permuting the display axes does not alter the
-%                      effect of masking the data.
+%                      effect of masking the data_.
 %
 % Output:
 % -------
 %   wout                Output dataset.
 
 % Initialise output argument
-wout = copy(win, 'exclude_pix', true);  % pixels will be assigned later
+if win.has_pixels()
+    wout = copy(win, 'exclude_pix', true);  % pixels will be assigned later
+else
+    wout = copy(win);
+end
 
 % Trivial case of empty or no mask arguments
 if nargin==1 || isempty(mask_array)
@@ -29,22 +33,22 @@ if nargin==1 || isempty(mask_array)
 end
 
 % Check mask is OK
-if ~(isnumeric(mask_array) || islogical(mask_array)) || numel(mask_array) ~= numel(win.data.s)
+if ~(isnumeric(mask_array) || islogical(mask_array)) || numel(mask_array) ~= numel(win.data_.s)
     error('SQW:mask', ['Mask must provide a numeric or logical array with ' ...
-                       'same number of elements as the image data.']);
+                       'same number of elements as the image data_.']);
 end
 if ~islogical(mask_array)
     mask_array=logical(mask_array);
 end
 
 % Mask signal, variance and npix arrays
-wout.data.s(~mask_array) = 0;
-wout.data.e(~mask_array) = 0;
-wout.data.npix(~mask_array) = 0;
+wout.data_.s(~mask_array) = 0;
+wout.data_.e(~mask_array) = 0;
+wout.data_.npix(~mask_array) = 0;
 
 % Section the pix array, if non empty, and update pix_range
 if has_pixels(win)
-    wout.data.pix = win.data.pix.mask(mask_array, win.data.npix);
+    wout.data_.pix = win.data_.pix.mask(mask_array, win.data_.npix);
     %TODO: is this a suitable algorithm?
-    wout.data.img_range = recompute_img_range(wout);
+    wout.data_.img_range = recompute_img_range(wout);
 end
---
+++ horace_core/sqw/@d2d/multifit_sqw.m
@@ -0,0 +1,79 @@
+function varargout = multifit_sqw (varargin)
+% Simultaneously fit function(s) of S(Q,w)to one or more d2d objects
+%
+%   >> myobj = multifit_sqw (w1, w2, ...)      % w1, w2 objects or arrays of objects
+%
+% This creates a fitting object of class mfclass_Horace_sqw with the provided data,
+% which can then be manipulated to add further data, set the fitting
+% functions, initial parameter values etc. and fit or simulate the data.
+% For details about how to do this  <a href="matlab:help('mfclass_Horace_sqw');">Click here</a>
+%
+% For example:
+%
+%   >> myobj = multifit_sqw (w1, w2, ...); % set the data
+%       :
+%   >> myobj = myobj.set_fun (@function_name, pars);  % set forgraound function(s)
+%   >> myobj = myobj.set_bfun (@function_name, pars); % set background function(s)
+%       :
+%   >> myobj = myobj.set_free (pfree);      % set which parameters are floating
+%   >> myobj = myobj.set_bfree (bpfree);    % set which parameters are floating
+%   >> [wfit,fitpars] = myobj.fit;          % perform fit
+%
+% This method fits model(s) for S(Q,w) as the foreground function(s), and
+% function(s) of the plot axes for the background function(s)
+%
+% For the format of foreground fit functions:
+% <a href="matlab:edit('example_sqw_spin_waves');">Damped spin waves</a>
+% <a href="matlab:edit('example_sqw_flat_mode');">Dispersionless excitations</a>
+%
+% The format of the background fit functions depends on the number of plot
+% axes for each sqw object. For examples see:
+% <a href="matlab:edit('example_1d_function');">example_1d_function</a>
+% <a href="matlab:edit('example_2d_function');">example_2d_function</a>
+% <a href="matlab:edit('example_3d_function');">example_3d_function</a>
+%
+% See also multifit multifit_sqw_sqw
+%
+%
+%
+%[Help for legacy use (2017 and earlier):
+%   If you are still using the legacy version then it is strongly recommended
+%   that you change to the new operation. Help for the legacy operation can
+%   be <a href="matlab:help('d2d/multifit_legacy_sqw');">found here</a>]
+
+%-------------------------------------------------------------------------------
+% <#doc_def:>
+%   class_name = 'd2d'
+%   dim = '2'
+%   method_name = 'multifit_sqw'
+%   method_name_legacy = 'multifit_legacy_sqw'
+%   mfclass_name = 'mfclass_Horace_sqw'
+%   function_tag = 'of S(Q,w) '
+%
+%
+%   multifit_doc = fullfile(fileparts(which('multifit')),'_docify')
+%   sqw_doc = fullfile(fileparts(which('mfclass_Horace')),'_docify')
+%
+%   doc_multifit_header = fullfile(multifit_doc,'doc_multifit_header.m')
+%   doc_fit_functions = fullfile(sqw_doc,'doc_multifit_sqw_fit_functions_for_dnd.m')
+%   doc_multifit_legacy_footnote = fullfile(multifit_doc,'doc_multifit_legacy_footnote.m')
+%
+%-------------------------------------------------------------------------------
+% <#doc_beg:> multifit
+%   <#file:>  <doc_multifit_header>  <class_name>  <method_name>  <mfclass_name>  <function_tag>
+%
+%   <#file:>  <doc_fit_functions>  <dim>
+%
+% See also multifit multifit_sqw_sqw
+%
+%   <#file:>  <doc_multifit_legacy_footnote>  <class_name>/<method_name_legacy>
+% <#doc_end:>
+%-------------------------------------------------------------------------------
+
+
+if ~mfclass.legacy(varargin{:})
+    mf_init = mfclass_wrapfun (@sqw_eval, [], @func_eval, []);
+    varargout{1} = mfclass_Horace_sqw (varargin{:}, 'd1d', mf_init);
+else
+    [varargout{1:nargout}] = mfclass.legacy_call (@multifit_legacy_sqw, varargin{:});
+end
