c363c21e452d61c8e374cf41010a0356cb5f3365
Nick Battam <nicholas.battam@stfc.ac.uk>
2021-03-30 16:16:13 +0100

    Remove references to _old classes
    Some missing dnd methods identified and added
    
    
    Former-commit-id: 156cc4ba5bb0503565b8029b1ab44c0097c56108

--- horace_core/sqw/@d1d/d1d.m
+++ horace_core/sqw/@d1d/d1d.m
@@ -1,5 +1,5 @@
 classdef d1d < DnDBase
-    %D1D Create an 2-dimensional DnD object
+    %D1D Create an 1-dimensional DnD object
     %
     % Syntax:
     %   >> w = d1d()               % Create a default, empty, D1D object
@@ -9,6 +9,24 @@ classdef d1d < DnDBase
 
     properties (Constant, Access = protected)
        NUM_DIMS = 1;
+    end
+
+    methods
+        wout = combine_horace_1d(w1,w2,varargin);
+        wout = rebin_horace_1d(win, varargin);
+        wout = symmetrise_horace_1d(win, varargin);
+    end
+
+    methods(Access = private)
+        [ok, same_axes, mess] = check_rebinning_axes_1d(w1, w2);
+    end
+
+    methods(Static, Access = private)
+        [xout, yout, eout, nout] = combine_1d(x1, y1, e1, n1, x2, y2, e2, n2, tol);
+        [xout, yout, eout, nout] = symmetrise_1d(xin, yin, ein, nin, midpoint);
+        [cumulsum, esum, nsum] = rebin_1d_finegrid(snew, enew, nnew, xinnew, lonew, hinew, eps, xlo, xhi, i);
+        [cumulsum, esum, nout] = rebin_1d_multibins(ind, binfrac, snew, enew, nnew, xinnew, eps, xlo, xhi, i);
+        [sout, eout, nout] = rebin_1d_general(xin, xout, sin, ein, nin);
     end
 
     methods(Static)
--- horace_core/sqw/@d2d/symmetrise_horace_2d.m
+++ horace_core/sqw/@d2d/symmetrise_horace_2d.m
@@ -1,6 +1,6 @@
 function wout=symmetrise_horace_2d(win,varargin)
 %
-% Symmetrise d2d (or sqw of d2d-type) data.
+% Symmetrise d2d (or sqw of d2d-type) data_.
 %
 % NOTE THAT THIS ROUTINE IS SLOOOOOOOOOOOOOOOOOW, BECAUSE IT USES A RATHER
 % COMPLICATED "SHOELACE" ALGORITHM TO ENSURE THAT REBINNING OF THE
@@ -14,7 +14,7 @@ function wout=symmetrise_horace_2d(win,varargin)
 %       e.g. wout = symmetrise_horace_2d(win,[0,NaN]) reflects data about
 %       x=0. No y-reflection performed.
 %
-%    2) wout = symmetrise_horace_2d(win,v1,v2,v3) 
+%    2) wout = symmetrise_horace_2d(win,v1,v2,v3)
 %    OR wout = symmetrise_horace-2d(win,v1,v2)   - reflect data about a
 %    plane specified by the vectors v1,v2,v3. If no v3 specified then v3 is
 %    set to be [0,0,0].
@@ -28,17 +28,15 @@ function wout=symmetrise_horace_2d(win,varargin)
 %
 
 
-[ndims,sz]=dimensions(win);
+[ndims, ~]=dimensions(win);
 
 if ndims~=2
     error('Horace error: symmetrise_horace_2d only works for 2-dimensional data');
 end
 
-if isa(win,'sqw') && is_sqw_type(win)
+if isa(win,'sqw') && has_pixels(win)
     error('Horace error: symmetrise_horace_2d method is for d2d-type data only');
 end
-
-win=sqw(win);
 
 %First determine what kind of symmetrisation we are doing:
 if nargin==2
@@ -72,14 +70,15 @@ switch route
     case 1
         %this is the case where we have specified one or two midpoints.
         midpoint=varargin{1};
-        [xin,yin]=ndgrid(win.data.p{1},win.data.p{2});
-        [xout,yout,sout,eout,nout]=symmetrise_2d(xin,yin,win.data.s,...
-            win.data.e,win.data.npix,midpoint);
-        wout=d2d_old(win);
-        getout=get(wout);
-        getout.p{1}=xout(:,1); getout.p{2}=yout(1,:)';
-        getout.s=sout; getout.e=eout; getout.npix=nout;
-        wout=d2d_old(getout);
+        [xin,yin] = ndgrid(win.data_.p{1}, win.data_.p{2});
+        [xout,yout,sout,eout,nout] = symmetrise_2d(xin, yin,...
+            win.data_.s, win.data_.e, win.data_.npix, midpoint);
+        wout = d2d(win);
+        wout.data_.p{1} = xout(:,1);
+        wout.data_.p{2} = yout(1,:)';
+        wout.data_.s = sout;
+        wout.data_.e = eout;
+        wout.data_.npix = nout;
     case 2
         %We need to check here whether the symmetrisation plane specified
         %actually correpsonds to the above case. If it does then we can
@@ -87,14 +86,16 @@ switch route
         [speedup,midpoint]=compare_sym_axes(win,varargin{1},varargin{2},varargin{3});
         if speedup
             %can use the routine from case 1
-            [xin,yin]=ndgrid(win.data.p{1},win.data.p{2});
-            [xout,yout,sout,eout,nout]=symmetrise_2d(xin,yin,win.data.s,...
-                win.data.e,win.data.npix,midpoint);
-            wout=d2d_old(win);
-            getout=get(wout);
-            getout.p{1}=xout(:,1); getout.p{2}=yout(1,:)';
-            getout.s=sout; getout.e=eout; getout.npix=nout;
-            wout=d2d_old(getout);
+            [xin,yin]=ndgrid(win.data_.p{1},win.data_.p{2});
+            [xout, yout, sout, eout, nout] = symmetrise_2d(xin, yin, ...
+                win.data_.s, win.data_.e, win.data_.npix, midpoint);
+            wout = d2d(win);
+            wout.data_.p{1} = xout(:,1);
+            wout.data_.p{2} = yout(1,:)';
+            wout.data_.s = sout;
+            wout.data_.e = eout;
+            wout.data_.npix = nout;
+
         else
             %realise that we have to restrict ourselves to symmetrisation
             %planes that are pendicular to the data plane, so that the
@@ -107,24 +108,24 @@ switch route
             %cannot cope with such situations very well. There should
             %therefore be an extra case where we check if the symm plane is
             %a diagonal, and if so we can deal with it differently.
-            [ok,mess]=test_symmetrisation_plane(d2d_old(win),varargin{1},varargin{2},varargin{3});
-            [diag,type]=test_symmetrisation_plane_digaonal(d2d_old(win),varargin{1},varargin{2},varargin{3});
+            [ok, mess] = test_symmetrisation_plane(win, varargin{1}, varargin{2}, varargin{3});
+            [diag, type] = test_symmetrisation_plane_diagonal(win, varargin{1}, varargin{2}, varargin{3});
             if ok
                 if diag
                     %the symmetrisation plane was a diagonal. This means
                     %that the reflected data will have the same axes as the
                     %original data, so we can use the linear rebining code
                     %to combine them.
-                    [xin,yin]=ndgrid(win.data.p{1},win.data.p{2});
+                    [xin,yin]=ndgrid(win.data_.p{1},win.data_.p{2});
                     v1=varargin{1}; v2=varargin{2}; v3=varargin{3};
-                    [xout,yout,sout,eout,nout]=symmetrise_2d_diag(xin,yin,win.data.s,...
-                        win.data.e,win.data.npix,v1,v2,v3,type,win);
-                    wout=win;
-                    getout=get(wout);
-                    getout.data.s=sout; getout.data.e=eout; getout.data.npix=nout;
-                    getout.data.p{1}=xout(:,1);
-                    getout.data.p{2}=yout(1,:)';
-                    wout=d2d_old(sqw_old(getout));
+                    [xout, yout, sout, eout, nout] = symmetrise_2d_diag(xin, yin, ...
+                        win.data_.s, win.data_.e, win.data_.npix, v1, v2, v3, type, win);
+                    wout=d2d(win);
+                    wout.data_.s=sout;
+                    wout.data_.e=eout;
+                    wout.data_.npix=nout;
+                    wout.data_.p{1}=xout(:,1);
+                    wout.data_.p{2}=yout(1,:)';
                     %
                 else
                     %get bin boundaries in shoelace format
@@ -143,14 +144,13 @@ switch route
                     [xrright,yrright,srright,erright,nrright]=discard_lhs(xr,yr,sr,er,nr,...
                         win,v1,v2,v3);
                     %We must do shoelace rebinning on what is left. But first
-                    %we must make an output grid that covers all of the data.
+                    %we must make an output grid that covers all of the data_.
                     xinlo=min(min(xinright)); xrlo=min(min(xrright));
                     yinlo=min(min(yinright)); yrlo=min(min(yrright));
                     xinhi=max(max(xinright)); xrhi=max(max(xrright));
                     yinhi=max(max(yinright)); yrhi=max(max(yrright));
-                    xstep=win.data.p{1}(2)- win.data.p{1}(1);
-                    ystep=win.data.p{2}(2) - win.data.p{2}(1);
-                    getout=get(d2d_old(win));
+                    xstep=win.data_.p{1}(2)- win.data_.p{1}(1);
+                    ystep=win.data_.p{2}(2) - win.data_.p{2}(1);
                     xoutbin=[min([xinlo xrlo]):xstep:(max([xinhi xrhi])+xstep-eps)];%extra bit to avoid
                     %problems with rounding errors
                     youtbin=[min([yinlo yrlo]):ystep:(max([yinhi yrhi])+ystep-eps)];
@@ -166,22 +166,28 @@ switch route
                     [soutr,eoutr,noutr]=rebin_shoelace(xrright,yrright,srright,erright,...
                         nrright,xtmp,ytmp);
                     %this seems to have worked, although it is dreadfully slow.
-                    getout.p{1}=xoutbin; getout.p{2}=youtbin;
-                    sout=zeros(length(xoutbin)-1,length(youtbin)-1); eout=sout; nout=sout;
-                    soutrt=sout; eoutrt=eout; noutrt=nout;
-                    for i=1:(length(getout.p{2})-1)
-                        sout(:,i)=soutin((1:length(getout.p{1})-1)+(i-1)*(length(getout.p{1})-1));
-                        eout(:,i)=eoutin((1:length(getout.p{1})-1)+(i-1)*(length(getout.p{1})-1));
-                        nout(:,i)=noutin((1:length(getout.p{1})-1)+(i-1)*(length(getout.p{1})-1));
-                        soutrt(:,i)=soutr((1:length(getout.p{1})-1)+(i-1)*(length(getout.p{1})-1));
-                        eoutrt(:,i)=eoutr((1:length(getout.p{1})-1)+(i-1)*(length(getout.p{1})-1));
-                        noutrt(:,i)=noutr((1:length(getout.p{1})-1)+(i-1)*(length(getout.p{1})-1));
+
+                    sout=zeros(length(xoutbin)-1,length(youtbin)-1);
+                    eout=sout;
+                    nout=sout;
+                    soutrt=sout;
+                    eoutrt=eout;
+                    noutrt=nout;
+
+                    len_p1m1 = length(xoutbin) - 1;
+                    len_p2m1 = length(youtbin) - 1;
+
+                    for i=1:len_p2m1
+                        sout(:,i)=soutin((1:len_p1m1) + (i-1)*len_p1m1);
+                        eout(:,i)=eoutin((1:len_p1m1) + (i-1)*len_p1m1);
+                        nout(:,i)=noutin((1:len_p1m1) + (i-1)*len_p1m1);
+                        soutrt(:,i)=soutr((1:len_p1m1) + (i-1)*len_p1m1);
+                        eoutrt(:,i)=eoutr((1:len_p1m1) + (i-1)*len_p1m1);
+                        noutrt(:,i)=noutr((1:len_p1m1) + (i-1)*len_p1m1);
                     end
                     %
-    %                 nout(e1_old==0 & e2_old==0)=0;
-    %                 sout(e1_old==0 & e2_old==0)=0;
-    %                 eout(e1_old==0 & e2_old==0)=0;
-                    eout=eout./sout; eoutrt=eoutrt./soutrt;
+                    eout=eout./sout;
+                    eoutrt=eoutrt./soutrt;
                     eout(isnan(eout) | isinf(eout))=0;
                     eoutrt(isnan(eoutrt) | isinf(eoutrt))=0;
                     eout=eout+((eout==0).*1e16);
@@ -191,16 +197,19 @@ switch route
                     efinal=1./(1./eout + 1./eoutrt); efinal=efinal.*sfinal;
                     nfinal=nout+noutrt;
                     %
-                    getout.s=sfinal; getout.e=efinal; getout.npix=nfinal;
-                    getout.p{1}=getout.p{1}';
-                    getout.p{2}=getout.p{2}';
-                    getout.title=[getout.title,' SYMMETRISED '];
-                    wout=d2d_old(getout);
+                    wout=d2d(win);
+                    wout.data_.p{1}=xoutbin';
+                    wout.data_.p{2}=youtbin';
+
+                    wout.data_.s=sfinal;
+                    wout.data_.e=efinal;
+                    wout.data_.npix=nfinal;
+
+                    wout.data_.title=[getout.title,' SYMMETRISED '];
                 end
             else
                 error(mess);
             end
-            
         end
 end
 
