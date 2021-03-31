fe0f9e5e542c29a54170d6cab645c7ca8b046f57
Nick Battam <nicholas.battam@stfc.ac.uk>
2021-03-31 15:12:49 +0100

    Remove old SQW/DnD code
    Former-commit-id: 219c2f342d6a88967558afe7f75f8137dd959f1b

--- horace_core/@d0d_old/sech.m
+++
@@ -1,7 +0,0 @@
-function w = sech (w1)
-% Implement sech(w1) for objects
-%
-%   >> w = sech(w1)
-%
-
-w = unary_op_manager (w1, @sech_single);
--- horace_core/@d1d_old/asec.m
+++
@@ -1,7 +0,0 @@
-function w = asec (w1)
-% Implement asec(w1) for objects
-%
-%   >> w = asec(w1)
-%
-
-w = unary_op_manager (w1, @asec_single);
