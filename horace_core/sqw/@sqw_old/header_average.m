219c2f342d6a88967558afe7f75f8137dd959f1b
Nick Battam <nicholas.battam@stfc.ac.uk>
2021-03-31 15:12:49 +0100

    Remove old SQW/DnD code

--- horace_core/sqw/@sqw_old/header_average.m
+++
@@ -1,7 +0,0 @@
-function [header_ave, ebins_all_same]=header_average(sqw)
-% Get average header information from header field of sqw object
-%
-% *** Assumes that all the contributing spe files had the same lattice parameters and projection axes
-% This could be generalised later - but with repercussions in many routines
-
-[header_ave, ebins_all_same] = header_average(sqw.header);
--- horace_core/sqw/@sqw_old/sinh.m
+++
@@ -1,7 +0,0 @@
-function w = sinh (w1)
-% Implement sinh(w1) for objects
-%
-%   >> w = sinh(w1)
-%
-
-w = unary_op_manager (w1, @sinh_single);
