0349b3c938a998f728cf3e3606771bc093d187db
Toby Perring <toby.perring@stfc.ac.uk>
2021-04-12 12:11:04 +0100

    Updates to documentation for unary and binary operations

--- horace_core/@d1d/acosh.m
+++ horace_core/@d1d/acosh.m
@@ -1,19 +1,31 @@
 function w = acosh (w1)
-% Implement acosh(w1) for objects
+% Implements acosh(w1) for objects
 %
 %   >> w = acosh(w1)
 %
-% The input argument can be a scalar object or array of objects
+% Input:
+% ------
+%   w1          Input object or array of objects on which to apply the
+%               unary operator.
+%
+% Output:
+% -------
+%   w           Output object or array of objects.
 
 % -----------------------------------------------------------------------------
 % <#doc_def:>
 %   doc_dir = fullfile(fileparts(which('sigvar')),'_docify')
-%   doc_file = fullfile(doc_dir,'doc_unary.m')
 %
+%   doc_file_header = fullfile(doc_dir,'doc_unary_header.m')
+%   doc_file_IO = fullfile(doc_dir,'doc_unary_general_args_IO_description.m')
+%
+%   list_operator_arg = 0
 %   func_name = 'acosh'
 % -----------------------------------------------------------------------------
 % <#doc_beg:> binary_and_unary_ops
-%   <#file:> <doc_file>
+%   <#file:> <doc_file_header>
+%
+%   <#file:> <doc_file_IO> <list_operator_arg>
 % <#doc_end:>
 % -----------------------------------------------------------------------------
 
