8f19d1df8f6df9eae914cd7efbffe9d775d94ee1
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2021-05-07 16:09:00 +0100

    Fix tf_simulate/fit

--- _test/test_sqw_class/test_multifit_functions.m
+++ _test/test_sqw_class/test_multifit_functions.m
@@ -5,13 +5,13 @@ classdef test_multifit_functions < TestCase
     % on the new SQW class, and provide it with a meaningful name.
     % Only the *fit tests have been used; the dispersion, eval cases are
     % left in test_sqw_dnd_eval.
-    
+
     properties
         data_dir
         dnd_4_test
         sqw_4_test
     end
-    
+
     methods
         function obj=test_multifit_functions(name)
             if nargin<1
@@ -22,7 +22,7 @@ classdef test_multifit_functions < TestCase
             test_sqw_file = fullfile(obj.data_dir,'sqw_2d_2.sqw');
             obj.sqw_4_test = sqw(test_sqw_file); % the original of this line, read_sqw(test_sqw_file), did not work;
         end
-       
+
         function test_tobyfit(obj)
             %
             sample=IX_sample(true,[1,0,0],[0,1,0],'cuboid',[0.04,0.03,0.02]);
@@ -30,36 +30,26 @@ classdef test_multifit_functions < TestCase
                 set_instrument(obj.sqw_4_test, @maps_instrument,'-efix',800,'S');
             obj.sqw_4_test = ...
                 set_sample(obj.sqw_4_test,sample);
-            
-            
+
+
             kk = tobyfit(obj.sqw_4_test);
             kk = kk.set_fun (@obj.sqw_eval_tester);
             kk = kk.set_pin(1);
             kk = kk.set_bfun (@obj.funceval_tester2D);
             kk = kk.set_bpin (1);
-            
-            err_message = '';
-            try
-                ds = kk.simulate;
-                %[my_fitted_data,a] = kk.fit; % do not fit-- will not
-                %converge
-                failed = false;
-            catch ME
-                failed = true;
-                err_message = ME.message;
-            end
-            assertFalse(failed,err_message);
-            
+
+            ds = kk.simulate();
+
             sig = ds.data.s;
             pix = ds.data.pix;
             assertEqual(pix.signal(2),numel(sig)+1);
             assertEqual(pix.num_pixels + numel(sig),pix.signal(1));
-            
+
             assertEqual(sig(2),2);
             assertElementsAlmostEqual(sig(1),403.0462,'absolute',0.0001);
-            
+
         end
-       
+
         function test_fit_sqw_sqw(obj)
             %
             kk = multifit_sqw_sqw(obj.sqw_4_test);
@@ -67,29 +57,19 @@ classdef test_multifit_functions < TestCase
             kk = kk.set_pin(1);
             kk = kk.set_bfun (@obj.sqw_eval_tester);
             kk = kk.set_bpin (1);
-            
-            err_message = '';
-            try
-                ds = kk.simulate;
-                %[my_fitted_data,a] = kk.fit; % do not fit-- will not
-                %converge
-                failed = false;
-            catch ME
-                failed = true;
-                err_message = ME.message;
-            end
-            assertFalse(failed,err_message);
-            
+
+            ds = kk.simulate();
+
             sig = ds.data.s;
             pix = ds.data.pix;
             assertEqual(pix.signal(2),2);
             assertEqual(2*pix.num_pixels,pix.signal(1));
-            
+
             assertEqual(sig(2),2);
             assertElementsAlmostEqual(sig(1),386.0924,'absolute',0.0001);
-            
+
         end
-        
+
         %
         function test_fit_sqw(obj)
             %
@@ -98,59 +78,40 @@ classdef test_multifit_functions < TestCase
             kk = kk.set_pin(1);
             kk = kk.set_bfun (@obj.funceval_tester2D);
             kk = kk.set_bpin (1);
-            
-            err_message = '';
-            try
-                ds = kk.simulate;
-                %[my_fitted_data,a] = kk.fit; % do not fit-- will not
-                %converge
-                failed = false;
-            catch ME
-                failed = true;
-                err_message = ME.message;
-            end
-            assertFalse(failed,err_message);
-            
+
+            ds = kk.simulate();
+
             sig = ds.data.s;
             pix = ds.data.pix;
             assertEqual(pix.signal(2),numel(sig)+1);
             assertEqual(pix.num_pixels+numel(sig),pix.signal(1));
-            
+
             assertEqual(sig(2),2);
             assertElementsAlmostEqual(sig(1),403.0462,'absolute',0.0001);
-            
+
         end
-        
-        
+
+
         function test_fit_func(obj)
             kk = multifit (obj.sqw_4_test);
             kk = kk.set_fun (@obj.funceval_tester2D);
             kk = kk.set_pin(1);
             kk = kk.set_bfun (@obj.funceval_tester2D);
             kk = kk.set_bpin (1);
-            
-            err_message = '';
-            try
-                my_fitted_data = kk.simulate;
-                %[my_fitted_data,a] = kk.fit; % do not fit-- will not
-                %converge
-                failed = false;
-            catch ME
-                failed = true;
-                err_message = ME.message;
-            end
-            assertFalse(failed,err_message);
-            
+
+            my_fitted_data = kk.simulate();
+
             sig = my_fitted_data.data.s;
             assertEqual(sig(2),2);
             assertEqual(sig(1),2*numel(sig));
-            
+
         end
-        
-        
+
+
     end
+
     methods(Static)
-        function dis = sqw_disp_tester(h,k,l,par)
+        function dis = sqw_disp_tester(h,k,l,~)
             sz = size(h);
             if any(sz ~= size(k)) || any(sz ~=size(l))
                 error('SQW_EVAL:runtime_error','incorrect shape of input arrays');
@@ -162,8 +123,8 @@ classdef test_multifit_functions < TestCase
             end
             dis(1) = numel(h);
         end
-        
-        function dis = sqw_eval_tester(h,k,l,en,par)
+
+        function dis = sqw_eval_tester(h,k,l,en,~)
             sz = size(h);
             if any(sz ~= size(k)) || any(sz ~=size(l)) || any(sz ~= size(en))
                 error('SQW_EVAL:runtime_error','incorrect shape of input arrays');
@@ -175,8 +136,8 @@ classdef test_multifit_functions < TestCase
             end
             dis(1) = numel(h);
         end
-        
-        function dis = funceval_tester2D(x,en,par)
+
+        function dis = funceval_tester2D(x,en,~)
             sz = size(x);
             if any(sz ~= size(en))
                 error('FUNC_EVAL:runtime_error','incorrect shape of input arrays');
