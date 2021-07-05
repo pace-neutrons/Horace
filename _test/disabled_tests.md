4f781ab670ad6fdd17acf02b3a077ae2376b2fde
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-07-05 21:01:35 +0100

    Re pace-neutrons/Herbert#363 disabled another MPI test, which fails on Windows Jenkins

--- _test/disabled_tests.md
+++ _test/disabled_tests.md
@@ -7,6 +7,8 @@
   - test_job_submittion (Its not a disabled test, but a tester to debug job submission on a cluster)
 
 - test_mpi
-    - test_job_dispatcher_mpiexec (https://github.com/pace-neutrons/Herbert/issues/329)
+    - test_job_dispatcher_mpiexec  fails on windows Jenkins with Matlab 2019b (https://github.com/pace-neutrons/Herbert/issues/329)
         :test_job_with_logs_2workers
+        :test_job_with_logs_3workers
+        :test_job_fail_restart
   
--- _test/test_mpi/test_job_dispatcher_mpiexec.m
+++ _test/test_mpi/test_job_dispatcher_mpiexec.m
@@ -1,7 +1,7 @@
 classdef test_job_dispatcher_mpiexec < job_dispatcher_common_tests
-
+    
     methods
-
+        
         function this=test_job_dispatcher_mpiexec(name)
             if ~exist('name', 'var')
                 name = 'test_job_dispatcher_mpiexec';
@@ -9,7 +9,7 @@ classdef test_job_dispatcher_mpiexec < job_dispatcher_common_tests
             this = this@job_dispatcher_common_tests(name,'mpiexec_mpi');
             this.print_running_tests = true;
         end
-
+        
         function test_ClusterMPI_get_mpiexec_executable_exists(~)
             mpiexec_path = ClusterMPI.get_mpiexec();
             assertTrue(isfile(mpiexec_path));
@@ -19,15 +19,21 @@ classdef test_job_dispatcher_mpiexec < job_dispatcher_common_tests
             if is_jenkins && ispc
                 skipTest('test_job_with_logs_2workers skipped on Windows Jenkins due to launching instability');
             end
-            test_job_with_logs_2workers@job_dispatcher_common_tests(obj,varargin{:});            
+            test_job_with_logs_2workers@job_dispatcher_common_tests(obj,varargin{:});
         end
         function test_job_with_logs_3workers(obj,varargin)
             if is_jenkins && ispc
                 skipTest('test_job_with_logs_3workers skipped on Windows Jenkins due to launching instability');
             end
-            test_job_with_logs_3workers@job_dispatcher_common_tests(obj,varargin{:});            
+            test_job_with_logs_3workers@job_dispatcher_common_tests(obj,varargin{:});
+        end        
+        function test_job_fail_restart(obj,varargin)
+            if is_jenkins && ispc
+                skipTest('test_job_fail_restart skipped on Windows Jenkins due to launching instability');
+            end
+            test_job_fail_restart@job_dispatcher_common_tests(obj,varargin{:});
         end
-
-   end
-
+        
+    end
+    
 end
