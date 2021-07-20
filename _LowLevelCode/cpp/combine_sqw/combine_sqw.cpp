92eb128f035d4627c9a4493ab703d5d8e6096173
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-07-20 15:48:34 +0100

    Re #705 fixed omp switches (at least on windows)

--- _LowLevelCode/cpp/combine_sqw/combine_sqw.cpp
+++ _LowLevelCode/cpp/combine_sqw/combine_sqw.cpp
@@ -146,11 +146,7 @@ void combine_sqw(ProgParameters &param, std::vector<sqw_reader> &fileReaders, co
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
   if (nrhs == 0 && (nlhs == 0 || nlhs == 1)) {
-#ifdef _OPENMP
-        plhs[0] = mxCreateString(Horace::VERSION);
-#else
-        plhs[0] = mxCreateString(Horace::VER_NOOMP);
-#endif
+    plhs[0] = mxCreateString(Horace::VERSION);
     return;
   }
   //--------------------------------------------------------
--- _LowLevelCode/cpp/sort_pixels_by_bins/sort_pixels_by_bins.cpp
+++ _LowLevelCode/cpp/sort_pixels_by_bins/sort_pixels_by_bins.cpp
@@ -195,11 +195,7 @@ std::string  verify_index_array(const mxArray * pix_cell_array_ptr, bool &is_int
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     if (nrhs == 0 && (nlhs == 0 || nlhs == 1)) {
-#ifdef _OPENMP
         plhs[0] = mxCreateString(Horace::VERSION);
-#else
-        plhs[0] = mxCreateString(Horace::VER_NOOMP);
-#endif
         return ;
     }
 
