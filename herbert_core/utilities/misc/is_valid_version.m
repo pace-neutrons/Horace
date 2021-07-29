a0368c2cfad5351b39f7126cdd094b0a21a6a628
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-07-29 15:34:35 +0100

    Re pace-neutrons/Herbert#371 parsing mex version string according to changes to the version.h

--- herbert_core/utilities/misc/is_valid_version.m
+++ herbert_core/utilities/misc/is_valid_version.m
@@ -5,7 +5,8 @@ valid_version = false;
 if ~ischar(version_string)
     return;
 end
-
+version_string = split(version_string);
+version_string = version_string{1};
 version_regex = '^[0-9]+\.[0-9]+\.[0-9]+(\.[0-9a-z]+|)$';
 if regexp(version_string, version_regex)
     valid_version = true;
