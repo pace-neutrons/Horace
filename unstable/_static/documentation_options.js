46527177d567a015b81de8e93a80cc4ed6eb2cfc
pace-builder <pace.builder.stfc@gmail.com>
2021-10-13 02:07:07 +0000

    Document build from CI (3.6.0-linux-R2019b-c59694e)

--- unstable/_static/documentation_options.js
+++ unstable/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.6.0.41e8d11',
+    VERSION: '3.6.0.c59694e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
