4a2c5f5804ce4094a081472ad1753008de934a1d
cmarooney-stfc <christopher.marooney@stfc.ac.uk>
2021-10-17 09:33:29 +0100

    correct pr for errors 2

--- _test/test_sqw_class/test_sqw_constructor.m
+++ _test/test_sqw_class/test_sqw_constructor.m
@@ -18,94 +18,50 @@ methods
     end
 
         
-        function test_sqw_class_follows_expected_class_heirarchy(~)
-            sqw_obj = sqw();
-            
-            assertTrue(isa(sqw_obj, 'sqw'));
-            assertTrue(isa(sqw_obj, 'SQWDnDBase'));
-        end
-        
-        function test_default_constructor_returns_empty_instance(~)
-            sqw_obj = sqw();
-            
-            assertTrue(isa(sqw_obj, 'sqw'));
-            assertEqual(sqw_obj.main_header, struct([]));
-            assertEqual(sqw_obj.header, struct([]));
-            assertEqual(sqw_obj.detpar, struct([]));
-            assertEqual(sqw_obj.data.pix, PixelData());
-            assertEqual(numel(sqw_obj.data.pax), 0);
-        end
-        
-        function test_filename_constructor_returns_populated_class(obj)
-            sqw_obj = sqw(obj.test_sqw_1d_fullpath);
-            
-            % expected data populated from instance of test object
-            assertTrue(isa(sqw_obj, 'sqw'));
-            assertEqual(sqw_obj.main_header.nfiles, 85)
-            assertEqual(numel(sqw_obj.header), 85)
-            assertEqual(numel(sqw_obj.detpar.group), 36864);
-            assertEqual(numel(sqw_obj.data.pax), 1);
-            assertEqual(sqw_obj.data.pix.num_pixels, 100337);
-        end
-        
-        function test_filename_constructor_sets_pixel_page_size_if_passed(obj)
-            pagesize_pixels = 666; % test value
-            pagesize_bytes = pagesize_pixels * PixelData.DATA_POINT_SIZE * PixelData.DEFAULT_NUM_PIX_FIELDS;
-            
-            sqw_obj = sqw(obj.test_sqw_1d_fullpath, 'pixel_page_size', pagesize_bytes);
-            
-            assertTrue(isa(sqw_obj, 'sqw'));
-            assertTrue(sqw_obj.data.pix.num_pixels > pagesize_pixels);
-            assertEqual(sqw_obj.data.pix.num_pixels, 100337); % expected value from test file
-            assertEqual(sqw_obj.data.pix.page_size, pagesize_pixels);
-        end
-        
-        function test_filename_constructor_sets_all_data_default_pagesize(obj)
-            sqw_obj = sqw(obj.test_sqw_1d_fullpath);
-            
-            assertTrue(isa(sqw_obj, 'sqw'));
-            assertEqual(sqw_obj.data.pix.num_pixels, 100337); % expected value from test file
-            assertEqual(sqw_obj.data.pix.page_size, 100337);
-        end
-        
-        function test_copy_constructor_clones_object(obj)
-            sqw_obj = sqw(obj.test_sqw_1d_fullpath);
-            sqw_copy = sqw(sqw_obj);
-            
-            assertTrue(isa(sqw_obj, 'sqw'));
-            assertEqualToTol(sqw_copy, sqw_obj);
-        end
-        
-        function test_copy_constructor_returns_distinct_object(obj)
-            sqw_obj = sqw(obj.test_sqw_1d_fullpath);
-            sqw_copy = sqw(sqw_obj);
-            
-            sqw_copy.main_header.title = 'test_copy';
-            sqw_copy.header = struct([]);
-            sqw_copy.detpar.azim(1:10) = 0;
-            sqw_copy.data.dax = [2 1];
-            sqw_copy.data.pix.signal = 1;
-            
-            % changed data is not mirrored in initial
-            assertFalse(equal_to_tol(sqw_copy.main_header, sqw_obj.main_header));
-            assertFalse(equal_to_tol(sqw_copy.header, sqw_obj.header));
-            assertFalse(equal_to_tol(sqw_copy.detpar, sqw_obj.detpar));
-            assertFalse(equal_to_tol(sqw_copy.data, sqw_obj.data));
-            assertFalse(equal_to_tol(sqw_copy.data.pix, sqw_obj.data.pix));
-            
-            assertFalse(equal_to_tol(sqw_copy, sqw_obj));
-        end
-        
-        function test_save_load_returns_identical_object(obj)
-            tmp_filename=fullfile(tmp_dir, 'sqw_loadobj_test.mat');
-            cleanup_obj=onCleanup(@() delete(tmp_filename));
-            
-            sqw_obj = sqw(obj.test_sqw_1d_fullpath);
-            save(tmp_filename, 'sqw_obj');
-            from_file = load(tmp_filename);
-            assertEqualToTol(from_file.sqw_obj, sqw_obj);
-        end
-        
+    function test_sqw_class_follows_expected_class_heirarchy(~)
+        sqw_obj = sqw();
+
+        assertTrue(isa(sqw_obj, 'sqw'));
+        assertTrue(isa(sqw_obj, 'SQWDnDBase'));
+    end
+
+    function test_default_constructor_returns_empty_instance(~)
+        sqw_obj = sqw();
+
+        assertTrue(isa(sqw_obj, 'sqw'));
+        assertEqual(sqw_obj.main_header, struct([]));
+        assertTrue(isa(sqw_obj.my_header(), 'Experiment'));
+        empty_insts = IX_inst.empty;
+        assertEqual(sqw_obj.my_header().instruments, empty_insts);
+        assertEqual(sqw_obj.my_detpar(), struct([]));
+        assertEqual(sqw_obj.data.pix, PixelData());
+        assertEqual(numel(sqw_obj.data.pax), 0);
+    end
+
+    function test_filename_constructor_returns_populated_class(obj)
+        sqw_obj = sqw(obj.test_sqw_1d_fullpath);
+
+        % expected data populated from instance of test object
+        assertTrue(isa(sqw_obj, 'sqw'));
+        assertEqual(sqw_obj.main_header.nfiles, 85)
+        assertEqual(numel(sqw_obj.my_header().expdata), 85)
+        assertEqual(numel(sqw_obj.my_header().instruments), 85)
+        assertEqual(numel(sqw_obj.my_header().samples), 85)
+        assertEqual(numel(sqw_obj.detpar_x.group), 36864);
+        assertEqual(numel(sqw_obj.data.pax), 1);
+        assertEqual(sqw_obj.data.pix.num_pixels, 100337);
+    end
+
+    function test_filename_constructor_sets_pixel_page_size_if_passed(obj)
+        pagesize_pixels = 666; % test value
+        pagesize_bytes = pagesize_pixels * PixelData.DATA_POINT_SIZE * PixelData.DEFAULT_NUM_PIX_FIELDS;
+
+        sqw_obj = sqw(obj.test_sqw_1d_fullpath, 'pixel_page_size', pagesize_bytes);
+
+        assertTrue(isa(sqw_obj, 'sqw'));
+        assertTrue(sqw_obj.data.pix.num_pixels > pagesize_pixels);
+        assertEqual(sqw_obj.data.pix.num_pixels, 100337); % expected value from test file
+        assertEqual(sqw_obj.data.pix.page_size, pagesize_pixels);
     end
 
     function test_filename_constructor_sets_all_data_default_pagesize(obj)
@@ -129,17 +85,15 @@ methods
         sqw_copy = sqw(sqw_obj);
 
         sqw_copy.main_header.title = 'test_copy';
-        sqw_copy = sqw_copy.change_header(struct([]));
-        dtp = sqw_copy.my_detpar();
-        dtp.azim(1:10) = 0;
-        sqw_copy = sqw_copy.change_detpar(dtp);
+        sqw_copy = sqw_copy.change_header(Experiment());
+        sqw_copy.detpar_x.azim(1:10) = 0;
         sqw_copy.data.dax = [2 1];
         sqw_copy.data.pix.signal = 1;
 
         % changed data is not mirrored in initial
         assertFalse(equal_to_tol(sqw_copy.main_header, sqw_obj.main_header));
         assertFalse(equal_to_tol(sqw_copy.my_header(), sqw_obj.my_header()));
-        assertFalse(equal_to_tol(sqw_copy.my_detpar(), sqw_obj.my_detpar()));
+        assertFalse(equal_to_tol(sqw_copy.detpar_x, sqw_obj.detpar_x));
         assertFalse(equal_to_tol(sqw_copy.data, sqw_obj.data));
         assertFalse(equal_to_tol(sqw_copy.data.pix, sqw_obj.data.pix));
 
@@ -156,5 +110,6 @@ methods
         assertEqualToTol(from_file.sqw_obj, sqw_obj);
     end
 
+ 
 end
 end
