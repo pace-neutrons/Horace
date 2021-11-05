56236504810db8ac0a48e360f0a8a4de10ac8d93
Alex Buts <Alex.Buts@stfc.ac.uk>
2021-11-05 14:39:56 +0000

    Re #716 fixing failign tests on powder and performance

--- _test/test_sqw_pixels/test_pixels_equal.m
+++ _test/test_sqw_pixels/test_pixels_equal.m
@@ -187,7 +187,7 @@ classdef test_pixels_equal < TestCase & common_pix_class_state_holder
             sqw_copy = copy(obj.sqw_2d);
             
             num_reps = 5;
-            num_iters = 1;
+            num_iters = 5;
             
             f = @() equal_to_tol(sqw_copy, obj.sqw_2d);
             times_taken = benchmark_function(f, num_iters, num_reps);
