62ec852f3dafd88abeefeb621c2004879455f067
Duc Le <duc.le@stfc.ac.uk>
2022-01-11 15:23:49 +0000

    Remove docify-generated docstrings

--- herbert_core/utilities/classes/@sigvar/asin.m
+++ herbert_core/utilities/classes/@sigvar/asin.m
@@ -1,17 +1,4 @@
 function w = asin (w1)
-% Implements asin(w1) for objects
-%
-%   >> w = asin(w1)
-%
-% Input:
-% ------
-%   w1          Input object or array of objects on which to apply the
-%               unary operator.
-%
-% Output:
-% -------
-%   w           Output object or array of objects.
-
 % -----------------------------------------------------------------------------
 % <#doc_def:>
 %   doc_dir = fullfile(fileparts(which('sigvar')),'_docify')
--- herbert_core/utilities/classes/@sigvar/sech_single.m
+++ herbert_core/utilities/classes/@sigvar/sech_single.m
@@ -1,17 +1,4 @@
 function w = sech_single (w1)
-% Implements sech(w1) for a sigvar object
-%
-%   >> w = sech_single(w1)
-%
-% Input:
-% ------
-%   w1          Sigvar object. Scalar instance only (but signal and variance
-%               arrays can be scalar or multiple element).
-%
-% Output:
-% -------
-%   w           Output sigvar object.
-
 % -----------------------------------------------------------------------------
 % <#doc_def:>
 %   doc_dir = fullfile(fileparts(which('sigvar')),'_docify')
