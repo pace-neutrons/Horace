c2b12b561e6b8e6a940cb6c66b123b03241ba0e4
Chris Marooney <christopher.marooney@stfc.ac.uk>
2022-05-13 15:41:03 +0100

    extend unique_objects_container_design

--- _test/test_unique_objects/test_unique_objects.m
+++ _test/test_unique_objects/test_unique_objects.m
@@ -24,18 +24,18 @@ classdef test_unique_objects < TestCase
             assertTrue( isa(mi,'IX_inst_DGfermi') );
             
             % make a unique_objects_container (empty)
-            uoc = unique_objects_container();
+            uoc = unique_objects_container('type','{}');
             
             % add 3 identical instruments to the container
-            uoc.add(li);
-            uoc.add(li);
-            uoc.add(li);
+            uoc = uoc.add(li);
+            uoc = uoc.add(li);
+            uoc = uoc.add(li);
             % add 2 more instruments, identical to each other but not the
             % first 3
-            uoc.add(mi);
-            uoc.add(mi);
+            uoc = uoc.add(mi);
+            uoc = uoc.add(mi);
             % add another instrument same as the first 3
-            uoc.add(li);
+            uoc = uoc.add(li);
             
             % test that we put 6 instruments in the container
             assertEqual( numel(uoc.idx), 6);
@@ -77,16 +77,16 @@ classdef test_unique_objects < TestCase
             mi1 = merlin_instrument(180, 600, 'g');            
             mi2 = merlin_instrument(190, 700, 'g');  
             assertFalse( isequal(mi1,mi2) );
-            uoc = unique_objects_container();
-            nuix = uoc.add(mi1);
+            uoc = unique_objects_container('type','{}');
+            [uoc,nuix] = uoc.add(mi1);
             assertEqual( nuix, 1);
-            nuix = uoc.add(mi2);
+            [uoc,nuix] = uoc.add(mi2);
             assertEqual( nuix, 2);
-            nuix = uoc.add(mi1);
+            [uoc,nuix] = uoc.add(mi1);
             assertEqual( nuix, 3);
-            nuix = uoc.add(mi2);
+            [uoc,nuix] = uoc.add(mi2);
             assertEqual( nuix, 4);
-            nuix = uoc.add(mi2);
+            [uoc,nuix] = uoc.add(mi2);
             assertEqual( nuix, 5);
             assertEqual( numel(uoc.stored_objects), 2);
             assertEqual( numel(uoc.idx), 5);
@@ -99,17 +99,17 @@ classdef test_unique_objects < TestCase
             disp('NB This test WILL emit a warning');
             mi1 = merlin_instrument(180, 600, 'g'); 
             sm1 = IX_null_sample();
-            uoc = unique_objects_container();
-            uoc.add(mi1);
-            uoc.add(sm1);
+            uoc = unique_objects_container('type','{}');
+            uoc = uoc.add(mi1);
+            uoc = uoc.add(sm1);
             assertEqual( numel(uoc.stored_objects), 2);
             assertEqual( numel(uoc.idx), 2);
             assertEqual( mi1, uoc.get(1) );
             assertEqual( sm1, uoc.get(2) );
-            voc = unique_objects_container('baseclass','IX_inst');
-            nuix = voc.add(mi1);
+            voc = unique_objects_container('type','{}','baseclass','IX_inst');
+            [voc,nuix] = voc.add(mi1);
             assertTrue( nuix>0 );
-            nuix = voc.add(sm1);
+            [voc,nuix] = voc.add(sm1);
             assertFalse( nuix>0 );
             assertEqual( numel(voc.stored_objects), 1);
             assertEqual( numel(voc.idx), 1);
@@ -119,12 +119,12 @@ classdef test_unique_objects < TestCase
             disp('Test: test_change_serializer');
             mi1 = merlin_instrument(180, 600, 'g'); 
             mi2 = merlin_instrument(190, 700, 'g');  
-            uoc = unique_objects_container();
-            uoc.add(mi1);
-            uoc.add(mi2);
-            voc = unique_objects_container('convert_to_stream',@hlp_serialise);
-            voc.add(mi1);
-            voc.add(mi2);
+            uoc = unique_objects_container('type','{}');
+            uoc = uoc.add(mi1);
+            uoc = uoc.add(mi2);
+            voc = unique_objects_container('type','{}','convert_to_stream',@hlp_serialise);
+            voc = voc.add(mi1);
+            voc = voc.add(mi2);
             ie = isequal( voc.stored_hashes(1,:), uoc.stored_hashes(1,:) );
             assertFalse(ie);
             v1 = uint8(...
@@ -140,31 +140,33 @@ classdef test_unique_objects < TestCase
         function test_constructor_arguments(~)
             disp('Test: test_constructor_arguments');
             disp('NB This test WILL emit warningS');
+            f = @() unique_objects_container();
+            assertExceptionThrown(f, 'MATLAB:error:missingMessageArgument');
             mi1 = merlin_instrument(180, 600, 'g'); 
             sm1 = IX_null_sample();
-            uoc = unique_objects_container();
-            uoc.add(mi1);
-            uoc.add(sm1);
+            uoc = unique_objects_container('type','{}');
+            uoc = uoc.add(mi1);
+            uoc = uoc.add(sm1);
             assertEqual( numel(uoc.stored_objects), 2);
-            uoc = unique_objects_container('baseclass','IX_inst');
-            uoc.add(mi1);
-            uoc.add(sm1);
+            uoc = unique_objects_container('type','{}','baseclass','IX_inst');
+            uoc = uoc.add(mi1);
+            uoc = uoc.add(sm1);
             assertEqual( numel(uoc.stored_objects), 1);
             u1 = uint8(...
                 [122    85    30   186    79    64   138   166   121   219   196   239    36   104   116    22]...
                 );
             assertEqual( u1, uoc.stored_hashes(1,:) );
-            uoc = unique_objects_container('baseclass','IX_inst','convert_to_stream',@hlp_serialise);
-            uoc.add(mi1);
-            uoc.add(sm1);
+            uoc = unique_objects_container('type','{}','baseclass','IX_inst','convert_to_stream',@hlp_serialise);
+            uoc = uoc.add(mi1);
+            uoc = uoc.add(sm1);
             assertEqual( numel(uoc.stored_objects), 1);
             u1 = uint8(...
                 [124   197    72   173   189    40   141    89   154   200    43   138   160    63   243   121] ...
                 );
             assertEqual( u1, uoc.stored_hashes(1,:) );
-            uoc = unique_objects_container('convert_to_stream',@hlp_serialise,'baseclass','IX_inst');
-            uoc.add(mi1);
-            uoc.add(sm1);
+            uoc = unique_objects_container('type','{}','convert_to_stream',@hlp_serialise,'baseclass','IX_inst');
+            uoc = uoc.add(mi1);
+            uoc = uoc.add(sm1);
             assertEqual( numel(uoc.stored_objects), 1);
             u1 = uint8(...
                 [124   197    72   173   189    40   141    89   154   200    43   138   160    63   243   121] ...
