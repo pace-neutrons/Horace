8ed613a31ca52b053d1fe93d37b7d174cec4b201
Duc Le <duc.le@stfc.ac.uk>
2022-06-16 02:54:06 +0100

    Changes needed to pass CI tests
    * Add parallelise_multifit option to OptimalConfigInfo
    * Revert validate_ranges back to using block sizes not end indices

--- horace_core/utilities/validators/validate_ranges.m
+++ horace_core/utilities/validators/validate_ranges.m
@@ -1,22 +1,22 @@
-function validate_ranges(starts, ends)
+function validate_ranges(starts, bl_sizes)
 %%VALIDATE_RANGES ensure the given start and end indices are valid ranges
-% This function validates that the given starts and ends form valid ranges.
+% This function validates that the given starts and block sizes form valid ranges.
 % We check that the inputs have equal size, are vectors and that, for all i,
-% starts(i) <= ends(i).
+% block_sizes(i) > 0
 %
 %
-if any(size(starts) ~= size(ends))
+if any(size(starts) ~= size(bl_sizes))
     error( 'HORACE:validate_ranges:invalid_range', ...
            'Input arrays must have equal size, found [%s] and [%s].', ...
-           num2str(size(starts)), num2str(size(ends)) ...
+           num2str(size(starts)), num2str(size(bl_sizes)) ...
     );
 elseif ~isvector(starts)
     error( 'HORACE:validate_ranges:invalid_range', ...
         'Input arrays must be vectors, found size [%s].', ...
         num2str(size(starts)) ...
     );
-elseif any(starts > ends)
-    error('HORACE:validate_ranges:invalid_range', 'Invalid ranges, not all starts(i) <= ends(i).');
+elseif any(bl_sizes <= 0)
+    error('HORACE:validate_ranges:invalid_range', 'Invalid ranges, not all blocks(i) > 0.');
 end
 
 end
