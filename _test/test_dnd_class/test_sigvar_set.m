5afba6cd1e74ab74aea66b1d20d32922f8521748
Alex Buts <Alex.Buts@stfc.ac.uk>
2022-07-21 18:30:27 +0100

    Re #810 fixed test_sigvar_set

--- _test/test_dnd_class/test_sigvar_set.m
+++ _test/test_dnd_class/test_sigvar_set.m
@@ -8,50 +8,26 @@ classdef test_sigvar_set < TestCase
             obj = obj@TestCase('test_sigvar_set');
         end
 
-        function test_sigvar_set_raises_error_if_s_not_same_size_as_dnd_object(obj)
-            d2d_obj = d2d();
-            d2d_obj.s = zeros(3, 5);
-            d2d_obj.e = zeros(1,3);
-            sigvar_obj = sigvar(struct('s', [1, 2, 3], 'e', [44, 55, 66]));
-
-            try
-                d2d_obj.sigvar_set(sigvar_obj);
-                assertTrue(false, 'Expected exception not raised');
-            catch ME
-                assertEqual(ME.identifier, 'D2D:sigvar_set');
-                assertTrue(contains(ME.message, 'signal'));
-                assertTrue(contains(ME.message, num2str(size(d2d_obj.s))));
-                assertTrue(contains(ME.message, num2str(size(sigvar_obj.s))));
-            end
-        end
-
         function test_sigvar_set_raises_error_if_e_not_same_size_as_dnd_object(~)
-            ab = axes_block([0,1],[0,1],[0,0.5,1],[0,0.2,1]);
+            ab = axes_block([0,0.5,1],[0,1],[0,0.5,1],[0,1], ...
+                'single_bin_defines_iax',[false,true,true,true]);
             pr = ortho_proj();
             d2d_obj = d2d(ab,pr);
 
-            d2d_obj.s = zeros(3,5);
-            d2d_obj.e = zeros(3, 5);
             sigvar_obj = sigvar(struct('s', [1, 2, 3], 'e', [44, 55, 66]));
+            me = assertExceptionThrown(@()d2d_obj.sigvar_set(sigvar_obj), ...
+                'HORACE:DnDBase:invalid_argument');
 
-            try
-                d2d_obj.sigvar_set(sigvar_obj);
-                assertTrue(false, 'Expected exception not raised');
-            catch ME
-                assertEqual(ME.identifier, 'D2D:sigvar_set');
-                assertTrue(contains(ME.message, 'variance'));
-                assertTrue(contains(ME.message, num2str(size(d2d_obj.e))));
-                assertTrue(contains(ME.message, num2str(size(sigvar_obj.e))));
-            end
+            assertTrue(contains(me.message, 'size of npix array'));
+            assertTrue(contains(me.message, num2str(size(d2d_obj.e))));
+            assertTrue(contains(me.message, num2str(size(sigvar_obj.e))));
+
         end
         function test_sigvar_set_s_and_e_nopix_gives_zero(~)
-            ab = axes_block([0,1],[0,1],[0,0.5,1],[0,1]);
-            ab.single_bin_defines_iax(1)= false;
+            ab = axes_block([0,0.5,1],[0,1],[0,0.5,1],[0,1], ...
+                'single_bin_defines_iax',[false,true,true,true]);
             pr = ortho_proj();
             d2d_obj = d2d(ab,pr);
-
-            d2d_obj.s = zeros(2,3);
-            d2d_obj.e = zeros(2,3);
             d2d_obj.npix = zeros(2,3);
 
             sigvar_obj = sigvar(struct(...
@@ -67,14 +43,10 @@ classdef test_sigvar_set < TestCase
 
 
         function test_sigvar_set_updates_s_and_e_values(~)
-            ab = axes_block([0,1],[0,1],[0,0.5,1],[0,1]);
-            ab.single_bin_defines_iax(1)= false;
+            ab = axes_block([0,0.5,1],[0,1],[0,0.5,1],[0,1], ...
+                'single_bin_defines_iax',[false,true,true,true]);
             pr = ortho_proj();
             d2d_obj = d2d(ab,pr);
-
-            d2d_obj.s = zeros(2,3);
-            d2d_obj.e = zeros(2,3);
-            d2d_obj.npix = ones(2,3);
 
             sigvar_obj = sigvar(struct(...
                 's', [1, 2, 3; 4, 5, 6], ...
@@ -95,8 +67,8 @@ classdef test_sigvar_set < TestCase
 
             sigvar_obj = sigvar(struct('s', [1, 2, 3], 'e', [44, 55, 66]));
 
-            expected_s = [1, 0, 3];
-            expected_e = [44, 0, 66];
+            expected_s = [1, 0, 3]';
+            expected_e = [44, 0, 66]';
 
             result = d1d_obj.sigvar_set(sigvar_obj);
 
--- horace_core/sqw/@DnDBase/private/check_combo_arg_.m
+++ horace_core/sqw/@DnDBase/private/check_combo_arg_.m
@@ -4,10 +4,18 @@ function obj = check_combo_arg_(obj)
 sz = size(obj.s_);
 if any(sz ~= size(obj.e_))
     error('HORACE:DnDBase:invalid_argument', ...
-        'size of signal array different from size of error array')
+        'size of signal array: [%s] different from size of error array: [%s]', ...
+        num2str(sz),num2str(size(obj.e_)));
 end
 
 if any(sz ~= size(obj.npix_))
     error('HORACE:DnDBase:invalid_argument', ...
-        'size of npix array different from sizes of signal and error array')
+        'size of npix array: [%s] different from sizes of signal and error array: [%s]', ...
+        num2str(sz),num2str(size(obj.npix_)))
+end
+
+if any(sz ~=obj.axes.dims_as_ssize)
+    error('HORACE:DnDBase:invalid_argument', ...
+        'size of data arrays: [%s] different from the size of the grid, defined by axes: [%s]', ...
+        num2str(sz),num2str(obj.axes.dims_as_ssize) )
 end
