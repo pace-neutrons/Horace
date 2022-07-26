df43658a4053002ee80e4b376e5e0198331dd886
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2022-07-26 10:36:47 +0100

    Change alias from b to k to avoid PS name collision

--- tools/build_config/build.sh
+++ tools/build_config/build.sh
@@ -102,7 +102,7 @@ flags:
       Run the Horace build commands.
   -t, --test
       Run all Horace tests.
-  -B, --benchmark
+  -k, --benchmark
       Run all Horace benchmarks.
   -c, --configure
       Run cmake configuration stage
@@ -164,7 +164,7 @@ function main() {
     case $key in
         # flags
         -b|--build) build=$TRUE; shift ;;
-        -B|--benchmark) benchmark=$TRUE; shift;;
+        -k|--benchmark) benchmark=$TRUE; shift;;
         -t|--test) test=$TRUE; shift ;;
         -c|--configure) configure=$TRUE; shift;;
         -a|--analyze) analyze=$TRUE; shift ;;
