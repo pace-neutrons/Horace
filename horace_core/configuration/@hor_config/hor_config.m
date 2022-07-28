3f73b3fee7a09fceea5ca13bcb08493a9d82360d
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2022-07-28 11:03:01 +0100

    Misc cleanup

--- horace_core/configuration/@hor_config/hor_config.m
+++ horace_core/configuration/@hor_config/hor_config.m
@@ -187,7 +187,7 @@ classdef hor_config < config_base
             %>>is = hor_config_instance.wkdir_is_default;
             %
             work_dir  = config_store.instance().get_config_field('parallel_config','working_directory');
-            is = isempty(work_dir);
+            is = isempty(work_dir)
         end
 
         function hpcc = get.high_perf_config_info(~)
