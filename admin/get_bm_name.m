0ffa369296c6ec6d43d9dcb1bd3bfce94515f70b
Oli-Rest <olivierrestuccia@hotmail.co.uk>
2022-08-08 10:02:32 +0100

    fixing conflicts

--- admin/get_bm_name.m
+++ admin/get_bm_name.m
@@ -1,21 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 function out_file = get_bm_name()
     function_stack = dbstack;
     name = function_stack(2).name;
     out_file = [name, '.csv'];
-=======
-function [out_file, nDims, dataType, dataNum, nProcs] = get_bm_name()
-    function_stack = dbstack;
-    name = function_stack(2).name;
-    out_file = [name, '.csv'];
-    x = regexp(name, 'test_bm_([a-z_]+)_(\d)D_(small|medium|large)Data_(small|medium|large)(?:Number|Energy)_(\d+)procs', 'tokens');
-    [function_name, nDims, dataType, dataNum, nProcs] = x{1}{:};
->>>>>>> 89ccf4ee9 (Replace duplicated code (#833))
-=======
-function out_file = get_bm_name()
-    function_stack = dbstack;
-    name = function_stack(2).name;
-    out_file = [name, '.csv'];
->>>>>>> 8d4db5de5 (updating gen_data functions)
 end
