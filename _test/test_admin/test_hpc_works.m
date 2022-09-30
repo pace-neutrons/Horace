696d7ef9037203b27ef38f6366ba7c7e8375ccf2
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2022-09-30 11:55:17 +0100

    Alter test to reflect new HPC setup

--- _test/test_admin/test_hpc_works.m
+++ _test/test_admin/test_hpc_works.m
@@ -1,40 +1,41 @@
-classdef test_hpc_works< TestCase
-    % Testing default configuration manager, selecting
-    % configuration as function of a pc type
-    %
-    properties
-    end
+classdef test_hpc_works < TestCase
+    % Testing default configuration manager, selecting configuration as function of a pc type
+
     methods
-        %
+
         function this=test_hpc_works(name)
             if nargin<1
-                name = 'test_pc_spec_config';
+                name = 'test_hpc_works';
             end
             this = this@TestCase(name);
         end
-        %
+
         function test_load_config(obj)
-            pc = hpc_config;
+            pc = hpc_config();
             data_2restore = pc.get_data_to_store();
             clOb = onCleanup(@()set(pc,data_2restore));
             pc.saveable = false;
-            
-            [old_config,new_hpc_config]=hpc();
+
+            % Check HPC returns old object
+            [old_config,new_hpc_config] = hpc();
             old_dte = old_config.get_data_to_store();
-            assertEqual(old_dte,data_2restore);
-            
-            pc.build_sqw_in_parallel = false;
-            
+            assertEqual(old_dte, data_2restore);
+
+            % Check HPC off disables features
             hpc('off');
-            assertEqual(pc.build_sqw_in_parallel,false);
-            
-            new_hpc_config.build_sqw_in_parallel = false; % ensure this property is set to false
-            % on any machine, as we just set it to false for this config
+            assertFalse(pc.build_sqw_in_parallel);
+            assertFalse(pc.parallel_multifit);
+
+
+            % Check HPC reset returns recommended features
+            hpc('reset');
+            new_hpc_config.build_sqw_in_parallel = false;
+            new_hpc_config.parallel_multifit = false;
+
             new_config = pc.get_data_to_store();
             assertEqual(new_hpc_config,new_config);
         end
-        
-        
+
+
     end
 end
-
