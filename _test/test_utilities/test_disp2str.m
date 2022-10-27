026d43ff6f643907165ff52c979e867f55f32eb6
Alex Buts <Alex.Buts@stfc.ac.uk>
2022-10-27 22:58:28 +0100

    Re #796 fixed different disp results for different Matlab versions

--- _test/test_utilities/test_disp2str.m
+++ _test/test_utilities/test_disp2str.m
@@ -1,28 +1,31 @@
 classdef test_disp2str< TestCase
-    
+
     methods
         function obj = test_disp2str(~)
             obj@TestCase('test_disp2str');
         end
         function test_cell(~)
             pat = {1,2,3};
-            out = disp2str(pat);            
-            assertEqual(out,'{[1]}    {[2]}    {[3]}');
+            out = disp2str(pat);
+            if verLessThan('Matlab','9.10')
+                assertEqual(out,'[1]    [2]    [3]');
+            else
+                assertEqual(out,'{[1]}    {[2]}    {[3]}');
+            end
         end
-        
+
         function test_array(~)
             pat = [1,2,3];
-            out = disp2str(pat);            
+            out = disp2str(pat);
             assertEqual(out,'1     2     3');
         end
-        function test_whitespaces(~)
+        function test_whitespaces_removed(~)
 
             pat = 'abcdefg103040506';
 
             out = disp2str([' ',pat,'  ']);
             assertEqual(out, pat)
         end
-        
     end
-    
+
 end
