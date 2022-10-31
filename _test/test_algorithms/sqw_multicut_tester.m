3594401bc1a33428bff05a3682c1e5c266076d82
Alex Buts <Alex.Buts@stfc.ac.uk>
2022-10-31 14:28:25 +0000

    Re #796 fixed random failure issue with sqw_multicut_tester.m

--- _test/test_algorithms/sqw_multicut_tester.m
+++ _test/test_algorithms/sqw_multicut_tester.m
@@ -8,6 +8,7 @@ classdef sqw_multicut_tester < sqw
     methods
         function obj = sqw_multicut_tester(varargin)
             obj = obj@sqw(varargin{:});
+            obj.main_header.creation_date_defined = true;
         end
 
         function  wout = cut(obj, varargin)
