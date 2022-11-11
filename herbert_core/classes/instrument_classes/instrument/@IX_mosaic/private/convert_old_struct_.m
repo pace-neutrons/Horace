b3200047e9ded338f46203d0d6cd2fddf5737208
Alex Buts <Alex.Buts@stfc.ac.uk>
2022-11-11 13:22:51 +0000

    Re #797 checked that `test_multifit_horace_1:test_fit_multidimensional_dataset` has been fixed and warning removed

--- herbert_core/classes/instrument_classes/instrument/@IX_mosaic/private/convert_old_struct_.m
+++ herbert_core/classes/instrument_classes/instrument/@IX_mosaic/private/convert_old_struct_.m
@@ -9,6 +9,12 @@ if isfield(inputs(1),'class_version_') && inputs(1).class_version_ == 1
         'UniformOutput',false);
     cell_data = struct2cell(inputs);
     inputs = cell2struct(cell_data,new_fld_names);
-    inputs.mosaic_pdf_string = func2str(inputs.mosaic_pdf);
+    func_name = func2str(inputs.mosaic_pdf);
+    if strcmp(func_name,'UNKNOWN Function')
+        % v1 was using only @rand_mosaic_gaussian and the function have
+        % been moved from private to allow replacement so it is not recognized any more
+        func_name = 'rand_mosaic_gaussian';
+    end
+    inputs.mosaic_pdf_string = func_name;
     inputs = rmfield(inputs,{'class_version','mosaic_pdf'});        
 end
