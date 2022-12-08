afb104f166ad68269677b6613ff0f84098e6a091
Alex Buts <Alex.Buts@stfc.ac.uk>
2022-12-08 11:52:45 +0000

    Re #893 added prospective data_block class to keep block information

---
+++ horace_core/sqw/file_io/@data_block/data_block.m
@@ -0,0 +1,89 @@
+classdef data_block < serializable
+    %DATA_BLOCK describes the unit of binary data, which faccess_sqw_v4
+    %operates with.
+    %
+    %Block contains information about the block size, its location in the
+    %binary file and the ways of extracting these data from sqw/dnd object of
+    %interest or placing this object there
+
+    properties(Dependent)
+        name;
+        position;
+        size;
+    end
+    properties(Access=protected)
+        name_ ='';
+        position_=0;
+        size_ = 0;
+    end
+    %======================================================================
+    methods
+        function obj = put_block_data(obj,fid,sqw_obj)
+        end
+    end
+    %======================================================================
+    % CONSTRUCTOR and PROPERTY ACCESSORS
+    methods
+        function obj = data_block(varargin)
+            if nargin == 0
+                return;
+            end
+            fldNames = obj.saveableFields();
+            [obj,remains] = set_positional_and_key_val_arguments(obj,...
+                fldNames,false,varargin{:});
+            if ~isempty(remains)
+                error('HORACE:data_block:invalid_argument', ...
+                    'Unrecognized properties/values are provided to the block constructor %s',...
+                    disp2str(remains));
+            end
+        end
+        %
+        function nm = get.name(obj)
+            nm = obj.name_;
+        end
+        function obj = set.name(obj,val)
+            if ~(ischar(val)||isstring(val))
+                error('HORACE:data_block:invalid_argument', ...
+                    'Block name can be only string or character array. In fact its class is %s', ...
+                    class(val));
+            end
+            obj.name_ = val;
+        end
+        %
+        function pos = get.position(obj)
+            pos = obj.position_;
+        end
+        function obj = set.position(obj,val)
+            if ~(isscalar(val)&&isnumeric(val)&&val>=0)
+                error('HORACE:data_block:invalid_argument', ...
+                    'block position can be only non-negative number. It is %s',...
+                    disp2str(val));
+            end
+            obj.position_ = val;
+        end
+        %
+        function pos = get.size(obj)
+            pos = obj.size_;
+        end
+        function obj = set.size(obj,val)
+            if ~(isscalar(val)&&isnumeric(val)&&val>=0)
+                error('HORACE:data_block:invalid_argument', ...
+                    'block size can be only non-negative number. It is %s',...
+                    disp2str(val));
+            end
+            obj.size_ = val;
+        end
+    end
+    %======================================================================
+    % SERIALIZABLE INTERFACE
+    methods
+        function  ver  = classVersion(~)
+            % serializable fields version
+            ver = 1;
+        end
+        function flds = saveableFields(~)
+            flds = {'name','position','size'};
+        end
+        %------------------------------------------------------------------
+    end
+end
