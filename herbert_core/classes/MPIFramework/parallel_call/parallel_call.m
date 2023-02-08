18f7ecb91722b2ac878d5cb4fa4c273d8c25e18f
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-02-08 11:03:13 +0000

    Pass func through

--- herbert_core/classes/MPIFramework/parallel_call/parallel_call.m
+++ herbert_core/classes/MPIFramework/parallel_call/parallel_call.m
@@ -10,14 +10,14 @@ function varargout = parallel_call(func, args, varargin)
         w = args{1};
         args = args(2:end);
 
-        varargout{1} = parallel_sqw_eval(w, nWorkers, args);
+        varargout{1} = parallel_sqw_eval(func, w, nWorkers, args);
 
       case 'multifit_func_eval'
 
         w = args{1};
         args = args(2:end);
 
-        varargout{1} = parallel_mf_func_eval(w, nWorkers, args);
+        varargout{1} = parallel_mf_func_eval(func, w, nWorkers, args);
 
       otherwise
         error('HERBERT:parallel_call:invalid_argument', ...
