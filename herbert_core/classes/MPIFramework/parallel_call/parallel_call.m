82525d912d0ee062db74400573fda40b8f663eb7
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-02-13 13:18:41 +0000

    Attempt fix for mf tf

--- herbert_core/classes/MPIFramework/parallel_call/parallel_call.m
+++ herbert_core/classes/MPIFramework/parallel_call/parallel_call.m
@@ -9,15 +9,18 @@ function varargout = parallel_call(func, args, varargin)
 
         w = args{1};
         call_func = args{2};
-        args = args(3:end);
+        args = args(2:end);
 
         varargout{1} = parallel_sqw_eval(func, w, call_func, nWorkers, args);
 
       case 'multifit_func_eval'
 
+        % mf_func_eval signature:
+        %multifit_func_eval (wmask, xye, fun_wrap, bfun_wrap, pin_wrap, bpin_wrap,...
+        %    f_pass_caller, bf_pass_caller, pfin, p_info, output_type)
         w = args{1};
-        call_func = args{2};
-        args = args(3:end);
+        call_func = args{3};
+        args = args(2:end);
 
         varargout{1} = parallel_mf_func_eval(func, w, call_func, nWorkers, args);
 
