1cc8e91f1314c72625eb87123d9de2c2dae558ab
Alex Buts <Alex.Buts@stfc.ac.uk>
2023-02-23 19:26:50 +0000

    Re #952 minor comments

--- horace_core/sqw/axes_blocks/axes_block.m
+++ horace_core/sqw/axes_blocks/axes_block.m
@@ -2,7 +2,8 @@ classdef axes_block < ortho_axes
     % Transiend class, left for support of old data loading from .mat files
     % and v4 sqw binary files (if released with old axes_block)
     %
-    % The functionality have been moved to ortho_axes class
+    % The functionality of axes_block class have been moved to ortho_axes 
+    % class
     %
     methods
         function obj = axes_block(varargin)
