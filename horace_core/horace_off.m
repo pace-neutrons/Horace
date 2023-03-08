1b5a4526201b60f4bfcd7dc4053ea0b868cf4673
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2023-03-08 12:18:09 +0000

    Fix horace_install with herbert_off gone (#977)
    * Fix horace_install with herbert_off gone
    
    * Fix error reporting inability to find herbert_off

--- horace_core/horace_off.m
+++ horace_core/horace_off.m
@@ -11,24 +11,32 @@ function horace_off
 pths = horace_paths;
 horace_on_path = pths.get_folder('horace_on');
 herbert_on_path = pths.get_folder('herbert_on');
+pths.clear();
 
 warn_state=warning('off','all');    % turn of warnings (so don't get errors if remove non-existent paths)
 try
-    old_paths = [genpath(pths.horace), genpath(pths.herbert)];
+    old_paths = genpath(pths.horace);
     % make sure we are not removing the path to horace_on
     if ~isempty(horace_on_path)
         old_paths = strrep(old_paths,[horace_on_path,pathsep],'');
     end
+    rmpath(old_paths);
+catch
+    error('Problems removing "%s" and sub-directories from Matlab path',horace_on_path)
+end
+
+try
+    old_paths = genpath(pths.herbert);
     if ~isempty(herbert_on_path)
         old_paths = strrep(old_paths,[herbert_on_path,pathsep],'');
     end
     rmpath(old_paths);
-    warning(warn_state);    % return warnings to initial state
-    pths.clear();
 catch
-    warning(warn_state);    % return warnings to initial state if error encountered
-    error('Problems removing "%s" and sub-directories from Matlab path',horace_path)
+    error('Problems removing "%s" and sub-directories from Matlab path',herbert_on_path)
 end
+
+warning(warn_state);    % return warnings to initial state if error encountered
+
 % Make sure we're not removing any global paths
 addpath(getenv('MATLABPATH'));
 
