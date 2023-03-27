675da2c8d1fa906540a377dca1e817e651c2342a
abuts <Alex.Buts@stfc.ac.uk>
2023-03-27 10:08:50 +0100

    Re #846 formatting. Changed to restart a build failing due to Matlab issues

--- horace_core/sqw/coord_transform/@ortho_proj/private/build_from_old_data_struct_.m
+++ horace_core/sqw/coord_transform/@ortho_proj/private/build_from_old_data_struct_.m
@@ -10,7 +10,7 @@ if isfield(data_struct,'ulabel')
     data_struct.label = data_struct.ulabel;
 end
 if isfield(data_struct,'uoffset')
-    data_struct.offset = data_struct.uoffset;    
+    data_struct.offset = data_struct.uoffset;
 end
 use_u_to_rlu_transitional = isfield(data_struct,'u_to_rlu');
 
@@ -20,8 +20,8 @@ proj = proj.from_bare_struct(data_struct);
 %--------------------------------------------------------------------------
 % TODO: #892 this is compatibility function to support alignment.
 % This will change when alignment matrix is attached to pixels
-if use_u_to_rlu_transitional 
-    proj = proj.set_from_data_mat(data_struct.u_to_rlu(1:3,1:3),data_struct.ulen(1:3));    
+if use_u_to_rlu_transitional
+    proj = proj.set_from_data_mat(data_struct.u_to_rlu(1:3,1:3),data_struct.ulen(1:3));
 end
 if ~isempty(bmat_inv_ext)
     proj = proj.set_ub_inv_compat(bmat_inv_ext(1:3,1:3));
