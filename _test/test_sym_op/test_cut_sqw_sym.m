1100cf618ec9891f8f068abd42e615ee1b87a598
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-04-11 15:26:05 +0100

    Update test_cut_sqw_sym

--- _test/test_sym_op/test_cut_sqw_sym.m
+++ _test/test_sym_op/test_cut_sqw_sym.m
@@ -18,9 +18,9 @@ classdef test_cut_sqw_sym < TestCaseWithSave
         vbin2
         wbin2
         ebin2
-        
+
     end
-    
+
     methods
         % Constructor
         function this = test_cut_sqw_sym (name)
@@ -29,27 +29,27 @@ classdef test_cut_sqw_sym < TestCaseWithSave
                 name = 'test_cut_sqw_sym';
             end
             this@TestCaseWithSave(name)
-            
+
             % Get Horace log level
             this.log_level = get(hor_config,'log_level');
-            
+
             % Read in data
             this_path = fileparts(mfilename('fullpath'));
             this.data_source = fullfile(this_path,'test_cut_sqw_sym.sqw');
             this.data = read_horace(this.data_source);
-            
+
             % Cut projection and ranges etc
-            s100 = symop([1,0,0],[0,0,1],[1,1,0]);
-            sdiag= symop([1,1,0],[0,0,1],[1,1,0]);
+            s100 = SymopReflection([1,0,0],[0,0,1],[1,1,0]);
+            sdiag= SymopReflection([1,1,0],[0,0,1],[1,1,0]);
             this.sym = {sdiag,s100,[sdiag,s100]};
-            
+
             this.proj = ortho_proj([1,-1,0], [1,1,0]/sqrt(2), 'offset', [1,1,0], 'type', 'paa');
             range = [0,0.2];    % range of cut
             step = 0.01;        % Q step
             this.bin = [range(1)+step/2,step,range(2)-step/2];
             this.width = [-0.15,0.15];  % Width in Ang^-1 of cuts
             this.ebins = [105,0,115];
-            
+
             %%% New tests:
             % Read the stored pixel data
             this.data2_source = fullfile(this_path, 'test_sym_op.sqw');
@@ -65,34 +65,34 @@ classdef test_cut_sqw_sym < TestCaseWithSave
                   0  0  0  0  0  0  0  0  1  1 -1 -1
                   1  1 -1 -1  0  0  0  0  0  0  0  0];
             Ws = mat2cell(reshape(Ws,[3,3,12]),3,3,ones(12,1));
-            this.sym2 = squeeze(cellfun(@symop, Ws, 'UniformOutput', false));
+            this.sym2 = squeeze(cellfun(@Symop, Ws, 'UniformOutput', false));
             % setup projection and binning specifications
             this.proj2 = ortho_proj([1,0,0],[0,1,0]);
             this.ubin2 = [0, 0.05, 0.5];
             this.vbin2 = [-0.1, 0.1];
             this.wbin2 = [-0.1, 0.1];
             this.ebin2 = [-2, 2];
-            
-            
+
+
             % Tolerance
             this.tol_sp = [1e-6,1e-6];
-            
+
             % Save line - must be the last line
             this.save()
         end
-        
+
         %------------------------------------------------------------------------
         % Tests
-        %------------------------------------------------------------------------        
+        %------------------------------------------------------------------------
         %------------------------------------------------------------------------
         function test_cut_sym_with_pix (this)
             % Test symmetrisation, keeping pixels
-            
+
             skipTest("cut_sym needs modification to work with new cut #805")
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             w2sym = cut_sqw_sym (this.data_source, this.proj, this.bin,...
                 this.width, this.width, this.ebins, this.sym, '-pix');
             this.assertEqualToTolWithSave (w2sym, this.tol_sp,'ignore_str',1);
@@ -100,25 +100,25 @@ classdef test_cut_sqw_sym < TestCaseWithSave
         %------------------------------------------------------------------------
         function test_cut_sym_with_nopix (this)
             % Test symmetrisation, without keeping pixels
-            skipTest("cut_sym needs modification to work with new cut #805")            
+            skipTest("cut_sym needs modification to work with new cut #805")
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             d2sym = cut_sqw_sym (this.data_source, this.proj, this.bin,...
                 this.width, this.width, this.ebins, this.sym, '-nopix');
             this.assertEqualToTolWithSave (d2sym, this.tol_sp,'ignore_str',1);
         end
-        
+
         %------------------------------------------------------------------------
         function test_multicut_1 (this)
             % Test multicut capability for cuts that are adjacent
             % Note that the last cut has no pixels retained - a good test too!
-            skipTest("New dnd (d2d) not supported yet #878");            
+            skipTest("New dnd (d2d) not supported yet #878");
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             % Must use '-pix' to properly handle pixel double counting in general
             w1 = cut_sqw (this.data_source, this.proj, this.bin,...
                 this.width, this.width, [106,4,114,4], '-pix');
@@ -129,14 +129,14 @@ classdef test_cut_sqw_sym < TestCaseWithSave
                 w2(i) = tmp;
             end
             assertEqualToTol (w1, w2, this.tol_sp,'ignore_str',1)
-            
+
             % Save dnd only to save disk space
             d1=dnd(w1);
             this.assertEqualToTolWithSave (d1, this.tol_sp,'ignore_str',1);
             d2=dnd(w2);
             this.assertEqualToTolWithSave (d2, this.tol_sp,'ignore_str',1);
         end
-        
+
         %------------------------------------------------------------------------
         function test_multicut_2 (this)
             % Test multicut capability for cuts that are adjacent
@@ -145,7 +145,7 @@ classdef test_cut_sqw_sym < TestCaseWithSave
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             % Must use '-pix' to properly handle pixel double counting in general
             w1 = cut_sqw (this.data_source, this.proj, this.bin,...
                 this.width, this.width, [110,2,118,2], '-pix');
@@ -155,21 +155,21 @@ classdef test_cut_sqw_sym < TestCaseWithSave
                     this.width, this.width, 108+2*i+[-1,1], '-pix');
             end
             assertEqualToTol (w1, w2, this.tol_sp,'ignore_str',1)
-            
+
             % Save dnd only to save disk space
             d1=dnd(w1);
             this.assertEqualToTolWithSave (d1, this.tol_sp,'ignore_str',1);
             d2=dnd(w2);
             this.assertEqualToTolWithSave (d2, this.tol_sp,'ignore_str',1);
         end
-        
+
         %------------------------------------------------------------------------
         function test_multicut_3 (this)
-            % Test multicut capability for cuts that overlap adjacent cuts        
+            % Test multicut capability for cuts that overlap adjacent cuts
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             % Must use '-pix' to properly handle pixel double counting in general
             w1 = cut_sqw (this.data_source, this.proj, this.bin,...
                 this.width, this.width, [106,4,114,8], '-pix');
@@ -179,7 +179,7 @@ classdef test_cut_sqw_sym < TestCaseWithSave
                     this.width, this.width, 102+4*i+[-4,4], '-pix');
             end
             assertEqualToTol (w1, w2, this.tol_sp,'ignore_str',1)
-            
+
             % Save dnd only to save disk space
             d1=dnd(w1);
             this.assertEqualToTolWithSave (d1, this.tol_sp,'ignore_str',1);
@@ -191,11 +191,11 @@ classdef test_cut_sqw_sym < TestCaseWithSave
         function test_cut_sqw_sym_ptgr(this)
             % Test multiple overlapping symmetry related cuts, some of
             % which contribute zero pixels to the result.
-            skipTest("cut_sym needs modification to work with new cut #805")            
+            skipTest("cut_sym needs modification to work with new cut #805")
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             [c, s] = cut_sqw_sym(this.data2, this.proj2, ...
                 this.ubin2, this.vbin2, this.wbin2, this.ebin2, ...
                 this.sym2(2:end)); % skip the superfluous first (identity) operation
@@ -205,24 +205,24 @@ classdef test_cut_sqw_sym < TestCaseWithSave
         %
         function test_cut_with_pix (this)
             % Test a simple cut keeping pixels
-            
+
             w2 = cut_sqw (this.data_source, this.proj, this.bin,...
                 this.width, this.width, this.ebins, '-pix');
             this.assertEqualToTolWithSave (w2, this.tol_sp,'ignore_str',1);
         end
-        
+
         %------------------------------------------------------------------------
         function test_cut_with_nopix (this)
             % Test a simple cut without keeping pixels
             % Turn off output, but return to input value when exit or cntl-c
             finishup = onCleanup(@() set(hor_config,'log_level',this.log_level));
             set(hor_config,'log_level',-1);  % turn off output
-            
+
             d2 = cut_sqw (this.data_source, this.proj, this.bin,...
                 this.width, this.width, this.ebins, '-nopix');
             this.assertEqualToTolWithSave (d2, this.tol_sp,'ignore_str',1);
         end
-        
+
         %------------------------------------------------------------------------
         % Tests to add:
         % - cut from a file with no pixels, overlapping limits, outside limits
