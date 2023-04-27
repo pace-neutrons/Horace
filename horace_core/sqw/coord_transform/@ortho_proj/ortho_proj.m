88c7324ac0d5e65e3b007ec97fca1e0860ee2624
abuts <Alex.Buts@stfc.ac.uk>
2023-04-27 18:01:15 +0100

    Re #892 fixed tests for projection methods

--- horace_core/sqw/coord_transform/@ortho_proj/ortho_proj.m
+++ horace_core/sqw/coord_transform/@ortho_proj/ortho_proj.m
@@ -396,7 +396,7 @@ classdef ortho_proj<aProjectionBase
                 cur_axes_block,targ_proj,targ_axes_block)
             % get indexes of cells which may contributing into the cut.
             %
-            if ~(obj.do_generic||isempty(obj.ortho_ortho_transf_mat_))
+            if obj.convert_targ_to_source
                 contrib_ind= get_contrib_orthocell_ind_(obj,...
                     cur_axes_block,targ_axes_block);
             else
