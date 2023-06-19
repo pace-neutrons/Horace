80514e331b72444619272cd7bc129c09d99015c3
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-06-19 10:29:14 +0100

    Fix validate_horace and merge with validate_herbert

--- admin/validate_horace.m
+++ admin/validate_horace.m
@@ -1,37 +1,32 @@
 function err = validate_horace(varargin)
 % Run unit tests on Horace installation
 %
-%   >> validate_horace                 % Run full Horace validation
-%   >> validate_horace (foldname)       %  Run Horace validation on the single named folder
-%   >> validate_horace (foldname1, foldname2)  %  Run Horace validation on named folders
-%   >> validate_horace (foldname_cell)  %  Run Horace validation on the folders named
-%                                       % in a cell array of names
+%   >> validate_horace                             % Run full Horace validation
+%   >> validate_horace (foldname)                  %  Run Horace validation on the single named folder
+%   >> validate_horace (foldname1, foldname2)      %  Run Horace validation on named folders
+%   >> validate_horace (foldname_cell)             %  Run Horace validation on the folders named
+%                                                  % in a cell array of names
 %
-% In addition, one of more options are allowed fromt he following
+% In addition, one of more options are allowed from the following
 %
-%   >> validate_horace (...'-parallel') %  Enables parallel execution of unit tests
-%                                      % if the parallel computer toolbox is available
+%   >> validate_horace (...'-parallel')            %  Enables parallel execution of unit tests
+%                                                  % if the parallel computer toolbox is available
 %   >> validate_horace (...'-talkative')%  Prints output of the tests and
-%                                       % horace commands (log_level is set to default,
-%                                       % not quiet)
-%   >> validate_horace (...'-nomex')    %  Validate matlab code by forcefully
-%                                      % disabling mex even if mex files
-%                                       % are available
-%   >> validate_horace (...'-forcemex') %  Enforce use of mex files only. The
-%                                       % default otherwise for Horace to revert to
-%                                       % using matlab code.
-%   >> validate_horace (...'-nodisp_skipped') %  print only list of failed
-%                                       %        tests, ignoring skipped
-%   >> validate_horace (...'-exit_on_completion') % Exit Matlab when test suite ends.
+%                                                  % horace commands (log_level is set to default,
+%                                                  % not quiet)
+%   >> validate_horace (...'-nomex')               %  Validate matlab code by forcefully
+%                                                  % disabling mex even if mex files
+%                                                  % are available
+%   >> validate_horace (...'-forcemex')            %  Enforce use of mex files only. The
+%                                                  % default otherwise for Horace to revert to
+%                                                  % using matlab code.
+%   >> validate_horace (...'-nodisp_skipped')      %  print only list of failed
+%                                                  %        tests, ignoring skipped
+%   >> validate_horace (...'-exit_on_completion')  % Exit Matlab when test suite ends.
 % Exits with non-zero error code if any tests failed
-
-err = -1;
 
 if isempty(which('horace_init'))
     horace_on();
-    % clear any previous configurations stored before and start tests from
-    % default configuration
-    config_store.instance().clear_all('-files');
 end
 
 % Parse arguments
@@ -43,7 +38,7 @@ options = {'-parallel',  '-talkative',  '-nomex',  '-forcemex',...
     parse_char_options(varargin, options);
 
 if ~ok
-    error('VALIDATE_HORACE:invalid_argument', mess)
+    error('HORACE:validate_horace:invalid_argument', mess)
 end
 
 %==============================================================================
@@ -54,9 +49,26 @@ if ~isempty(test_folders)
     % clear any previous configurations stored before and start tests from
     % default configuration
     config_store.instance().clear_all('-files');
+
 else % no tests specified on command line - run them all
     test_folders = {...
-        'test_admin',...
+        ... % Ex herbert tests
+        'test_admin', ...
+        'test_xunit_framework', ...
+        'test_data_loaders', ...
+        'test_config', ...
+        'test_IX_classes', ...
+        'test_map_mask', ...
+        'test_multifit_herbert', ...
+        'test_utilities_herbert', ...
+        'test_serializers', ...
+        'test_instrument_classes', ...
+        'test_unique_objects_container', ...
+        'test_docify', ...
+        'test_geometry',...
+        'test_mpi_wrappers', ...
+        'test_mpi', ...
+        ... % Horace tests
         'test_algorithms',...
         'test_ascii_column_data', ...
         'test_change_crystal', ...
@@ -87,7 +99,9 @@ else % no tests specified on command line - run them all
         'test_TF_refine_crystal' ...
         };
 end
+
 system_tests = { ...
+    'test_mpi', ...
     'test_gen_sqw_workflow/test_gen_sqw_accumulate_sqw_mex', ...
     'test_gen_sqw_workflow/test_gen_sqw_accumulate_sqw_nomex', ...
     'test_gen_sqw_workflow/test_gen_sqw_accumulate_sqw_herbert', ...
@@ -97,9 +111,10 @@ system_tests = { ...
     'test_TF_let', ...
     'test_TF_refine_crystal' ...
     };
+
 if no_system
-    no_sys = ~ismember(test_folders,system_tests);
-    test_folders = test_folders(no_sys );
+    no_sys = ~ismember(test_folders, system_tests);
+    test_folders = test_folders(no_sys);
 end
 
 % Generate full test paths to unit tests
@@ -110,29 +125,38 @@ test_folders_full = fullfile(test_path, test_folders);
 
 hoc = hor_config();
 hpc = hpc_config();
+pcf = parallel_config();
 % (Validation must always return Horace and Herbert to their initial states, regardless
 %  of any changes made in the test routines)
 
 % On exit always revert to initial Horace and Herbert configurations
 % ------------------------------------------------------------------
-cur_horace_config = hoc.get_data_to_store(); % only get the public i.e. not sealed, fields
+initial_warn_state = warning();
+warning('off', 'MATLAB:class:DestructorError');
+
+% only get the public i.e. not sealed, fields
+cur_horace_config = hoc.get_data_to_store();
 cur_hpc_config = hpc.get_data_to_store();
+cur_par_config = pcf.get_data_to_store();
+
 % remove configurations from memory. Ensure only stored configurations are
 % stored
 clear config_store;
 
-
 % Create cleanup object (*** MUST BE DONE BEFORE ANY CHANGES TO CONFIGURATIONS)
 cleanup_obj = onCleanup(@() ...
-    validate_horace_cleanup(cur_horace_config, cur_hpc_config, {}));
+                        validate_horace_cleanup(cur_horace_config, ...
+                                                cur_hpc_config, ...
+                                                cur_par_config, ...
+                                                test_folders, ...
+                                                initial_warn_state));
 
 % Run unit tests
 % --------------
 % Set Horace and Herbert configurations to the defaults (but don't save)
 % (The validation should be done starting with the defaults, otherwise an error
 %  may be due to a poor choice by the user of configuration parameters)
-
-set(hoc,  'defaults');
+set(hoc, 'defaults');
 
 % Special unit tests settings.
 hoc.init_tests = true; % initialise unit tests
@@ -144,70 +168,77 @@ if talkative
 else
     hoc.log_level = -1; % turn off informational output
 end
+
+argi = {'-verbose'};
 if nodisp_skipped
-    argi = {'-verbose','-nodisp_skipped'};
-else
-    argi = {'-verbose'};
+    argi = [argi, '-nodisp_skipped'];
 end
 
 if parallel && license('checkout',  'Distrib_Computing_Toolbox')
     cores = feature('numCores');
-    if verLessThan('matlab',  '8.4')
-        if matlabpool('SIZE') == 0
-            
-            if cores > 12
-                cores = 12;
-            end
-            
-            matlabpool(cores);
-        end
-    else
-        if isempty(gcp('nocreate'))
-            if cores > 12
-                cores = 12;
-            end
-            parpool(cores);
-        end
+    cores = min(cores, 12);
+
+    if verLessThan('matlab',  '8.4') && matlabpool('SIZE') == 0
+        matlabpool(cores);
+    elseif isempty(gcp('nocreate'))
+        parpool(cores);
     end
-    
+
     test_ok = false(1, numel(test_folders_full));
-    
     time = bigtic();
-    
+
     parfor i = 1:numel(test_folders_full)
         test_ok(i) = runtests(test_folders_full{i}, argi{:})
+        % Run after each stage
+        validate_horace_cleanup(cur_horace_config, ...
+                                cur_hpc_config, ...
+                                cur_par_config, ...
+                                test_folders, ...
+                                initial_warn_state)
     end
-    
+
     bigtoc(time,  '===COMPLETED UNIT TESTS IN PARALLEL');
-    tests_ok = all(test_ok);
+
 else
+    test_ok = false(1, numel(test_folders_full));
     time = bigtic();
-    tests_ok = runtests(test_folders_full{:},  argi{:});
+
+    for i = 1:numel(test_folders_full)
+        test_ok(i) = runtests(test_folders_full{i}, argi{:})
+        % Run after each stage
+        validate_horace_cleanup(cur_horace_config, ...
+                                cur_hpc_config, ...
+                                cur_par_config, ...
+                                test_folders, ...
+                                initial_warn_state)
+    end
+
     bigtoc(time,  '===COMPLETED UNIT TESTS RUN ');
-    
+
 end
 
 close all
 clear config_store;
 
-if tests_ok
-    err = 0;
-end
+err = ~all(test_ok);
 
 if exit_on_completion
     exit(err);
 end
 
-%=================================================================================================================
-function validate_horace_cleanup(cur_horace_config, cur_hpc_config, test_folders)
-warn = warning('off',  'all'); % avoid warning on deleting non-existent path
-% Reset the configurations, and remove unit test folders from the path
+end
+
+function validate_horace_cleanup(cur_horace_config, cur_hpc_config, cur_par_config, test_folders, initial_warn_state)
 set(hor_config, cur_horace_config);
 set(hpc_config, cur_hpc_config);
+set(parallel_config, cur_par_config);
 
+warn = warning('off',  'all'); % avoid warning on deleting non-existent path
+% Reset the configurations, and remove unit test folders from the path
 % Clear up the test folders, previously placed on the path
 for i = 1:numel(test_folders)
     rmpath(test_folders{i});
 end
+warning(initial_warn_state);
 
-warning(warn);
+end
