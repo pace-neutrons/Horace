ce7e164869aae2e49ab3110c1c5180364aa7aadc
abuts <Alex.Buts@stfc.ac.uk>
2023-06-22 19:15:34 +0100

    Re #892 rewired uoffset to img_offset in old sqw files rather then offset as it has been written recently

--- horace_core/sqw/coord_transform/@ortho_proj/private/build_from_old_data_struct_.m
+++ horace_core/sqw/coord_transform/@ortho_proj/private/build_from_old_data_struct_.m
@@ -6,14 +6,13 @@ if isfield(data_struct,'ulabel')
     data_struct.label = data_struct.ulabel;
 end
 if isfield(data_struct,'uoffset')
-    data_struct.offset = data_struct.uoffset;
+    data_struct.img_offset = data_struct.uoffset;
 end
 if isfield(data_struct,'u_to_rlu_legacy')
     data_struct.u_to_rlu  = data_struct.u_to_rlu_legacy;
 end
 use_u_to_rlu_transitional =  isfield(data_struct,'u_to_rlu');
 
-proj.do_check_combo_arg = false;
 proj = proj.from_bare_struct(data_struct);
 
 %--------------------------------------------------------------------------
@@ -24,7 +23,5 @@ if use_u_to_rlu_transitional
     % correct transformation seems division from right. There is the difference
     u_transf = inv(data_struct.u_to_rlu(1:3,1:3))/bmatrix(proj.alatt,proj.angdeg);
     proj = proj.set_from_data_mat(u_transf,data_struct.ulen(1:3));
-else
-    proj = proj.check_combo_arg();
 end
 
