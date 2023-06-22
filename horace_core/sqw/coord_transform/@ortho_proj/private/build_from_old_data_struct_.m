abdc5e45ff5a033741ff00877620f8042d07035b
abuts <Alex.Buts@stfc.ac.uk>
2023-06-22 19:18:37 +0100

    Re #892 set up old uoffset more reliably

--- horace_core/sqw/coord_transform/@ortho_proj/private/build_from_old_data_struct_.m
+++ horace_core/sqw/coord_transform/@ortho_proj/private/build_from_old_data_struct_.m
@@ -5,9 +5,6 @@ function proj =  build_from_old_data_struct_(proj,data_struct,varargin)
 if isfield(data_struct,'ulabel')
     data_struct.label = data_struct.ulabel;
 end
-if isfield(data_struct,'uoffset')
-    data_struct.img_offset = data_struct.uoffset;
-end
 if isfield(data_struct,'u_to_rlu_legacy')
     data_struct.u_to_rlu  = data_struct.u_to_rlu_legacy;
 end
@@ -18,10 +15,13 @@ proj = proj.from_bare_struct(data_struct);
 %--------------------------------------------------------------------------
 % recover from old data, where u_to_rlu matrix is stored instead of
 % projection itself
-proj.do_check_combo_arg = true;
 if use_u_to_rlu_transitional
     % correct transformation seems division from right. There is the difference
     u_transf = inv(data_struct.u_to_rlu(1:3,1:3))/bmatrix(proj.alatt,proj.angdeg);
     proj = proj.set_from_data_mat(u_transf,data_struct.ulen(1:3));
+end
+% this has to be last operation, as it needs fully defined projection class
+if isfield(data_struct,'uoffset')
+    data_struct.img_offset = data_struct.uoffset;
 end
 
