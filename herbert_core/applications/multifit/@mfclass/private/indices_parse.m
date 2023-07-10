01b7fe6f1c79e27c947abcef7ff64ef693ee9761
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-07-10 10:00:47 +0100

    Respond to review

--- herbert_core/applications/multifit/@mfclass/private/indices_parse.m
+++ herbert_core/applications/multifit/@mfclass/private/indices_parse.m
@@ -1,15 +1,17 @@
 function ind = indices_parse (ind_in,ndatatot,str)
 % Check that a list of dataset indices has the correct format valid values
 %
+% This function checks that all indices are in the range 1
+%    to ndatatot, and that there are no repeated indices
+%
 %   >> ind = indices_parse (ind_in,ndatatot,str)
 %
 % Input:
 % ------
 %   ind_in      List of dataset indices (row vector)
-%               This function checks that all indices are in the range 1
-%              to ndatatot, and that there are no repeated indices
-%               Can be empty (i.e. []); this is valid even if ndatatot=0
-%               If 'all' then will be interpreted as all datasets
+%               Can be empty (i.e. []); this is valid even if ndatatot=0.
+%               If ind_in=='all' then 1:ndatatot will be returned unless ndatatot==0,
+%                  in which case [] will be returned.
 %
 %   ndatatot    Maximum permissible dataset index (can be 0)
 %
@@ -18,8 +20,8 @@ function ind = indices_parse (ind_in,ndatatot,str)
 %
 % Output:
 % -------
-%   idata       List of dataset indices (row vector). All elements will be
-%              in the range 1 to ndatatot, or will be []
+%   ind         List of dataset indices (row vector). All elements will be
+%                  in the range 1 to ndatatot, or will be []
 
 
 % Original author: T.G.Perring
@@ -36,7 +38,7 @@ elseif isnumeric(ind_in)
     if ~isrow(ind_in) || ~all(rem(ind_in,1) == 0) ||...
             any(ind_in < 1 | ind_in > ndatatot)
         error('HORACE:indices_parse:invalid_argument', ...
-              '%s indices must be a row of integers the range 1 - %d', str, ndatatot);
+              '%s indices must be a row of integers in the range 1 - %d', str, ndatatot);
 
     elseif numel(unique(ind_in)) ~= numel(ind_in)
         error('HORACE:indices_parse:invalid_argument', ...
@@ -52,4 +54,6 @@ elseif isnumeric(ind_in)
 else
     error('HORACE:indices_parse:invalid_argument', ...
           '%s indices must be numeric row vector or the character string ''all''', str);
+end
+
 end
