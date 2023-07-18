013469f0a7bd2b3e2af8f8989c9b33a4492ebac6
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-07-18 16:28:02 +0100

    File-back and reformat calculate_qw_pixels2

--- horace_core/sqw/@sqw/calculate_qw_pixels2.m
+++ horace_core/sqw/@sqw/calculate_qw_pixels2.m
@@ -1,7 +1,7 @@
 function qw=calculate_qw_pixels2(win)
 % Calculate qh,qk,ql,en for the pixels in an sqw dataset from the headers
 %
-%   >> qw=calculate_qw_pixels(win)
+%   >> qw = calculate_qw_pixels2(win)
 %
 % This method differs from calculate_qw_pixels because it recomputes the values
 % of momentum and energy from efix, emode and the detector information. This is
@@ -22,67 +22,79 @@ function qw=calculate_qw_pixels2(win)
 % *** assumes that all the contributing spe files had the same lattice parameters and projection axes
 % This could be generalised later - but with repercussions in many routines
 
-if numel(win)~=1
-    error('Only a single sqw object is valid - cannot take an array of sqw objects')
+if ~isscalar(win)
+    error('HORACE:calculate_qw_pixels2:invalid_argument', ...
+          'Only a single sqw object is valid - cannot take an array of sqw objects')
 end
 
-c=neutron_constants;
+c = neutron_constants;
 k_to_e = c.c_k_to_emev;
 
-irun = win.data.pix.run_idx';   % column vector
-idet = win.data.pix.detector_idx';   % column vector
-ien  = win.data.pix.energy_idx';   % column vector
+% as column vectors
+irun = win.pix.get_fields('run_idx')';
+idet = win.pix.get_fields('detector_idx')';
+ien = win.pix.get_fields('energy_idx')';
 
 if ~iscell(win.header)
-    header={win.header};
+
+    header = {win.header};
     emode = header.emode;
+
     efix = header.efix;
-    eps_lo=0.5*(header.en(1)+header.en(2));
-    eps_hi=0.5*(header.en(end-1)+header.en(end));
-    ne=numel(header.en)-1;
-    [~, ~, spec_to_rlu] = calc_proj_matrix (header.alatt, header.angdeg,...
-        header.cu, header.cv, header.psi, header.omega,...
-        header.dpsi, header.gl, header.gs);
+    eps_lo = 0.5*(header.en(1)+header.en(2));
+    eps_hi = 0.5*(header.en(end-1)+header.en(end));
+    ne = numel(header.en)-1;
+
+    [~, ~, spec_to_rlu] = calc_proj_matrix(header.alatt, header.angdeg,...
+                                           header.cu, header.cv, header.psi, header.omega,...
+                                           header.dpsi, header.gl, header.gs);
+
 else
     header = win.header;
-    emode = cellfun(@(x)(x.emode),header);
+    emode = cellfun(@(x) x.emode,header);
+
     if ~all(emode==emode(1))
-        error('Contributing runs to an sqw object must be all be direct geometry or all indirect geometry')
+        error('HORACE:calculate_qw_pixels2:invalid_argument', ...
+              'Contributing runs to an sqw object must be all be direct geometry or all indirect geometry')
     end
+
     emode = emode(1);
-    efix = cellfun(@(x)(x.efix),header);
-    eps_lo = cellfun(@(x)(0.5*(x.en(1)+x.en(2))),header);
-    eps_hi = cellfun(@(x)(0.5*(x.en(end-1)+x.en(end))),header);
-    ne = cellfun(@(x)(numel(x.en)-1),header);
+
+    efix = cellfun(@(x) x.efix,header);
+    eps_lo = cellfun(@(x) 0.5*(x.en(1)+x.en(2)),header);
+    eps_hi = cellfun(@(x) 0.5*(x.en(end-1)+x.en(end)),header);
+    ne = cellfun(@(x) numel(x.en)-1,header);
+
     spec_to_rlu = zeros(3,3,numel(header));
-    for i=1:numel(header)
+    for i = 1:numel(header)
         h = header{i};  % for clarity in next line
         [~, ~, spec_to_rlu(:,:,i)] = calc_proj_matrix (h.alatt, h.angdeg,...
             h.cu, h.cv, h.psi, h.omega, h.dpsi, h.gl, h.gs);
     end
 end
 
-eps=(eps_lo(irun).*(ne(irun)-ien)+eps_hi(irun).*(ien-1))./(ne(irun)-1);
-[~, detdcn] = spec_coords_to_det (win.detpar);
+eps = (eps_lo(irun) .* (ne(irun) - ien) + eps_hi(irun) .* (ien - 1)) ./ (ne(irun) - 1);
+[~, detdcn] = spec_coords_to_det(win.detpar);
 kfix = sqrt(efix/k_to_e);
 
-if emode==1
+switch emode
+  case 1
     ki = kfix(irun);
     kf = sqrt((efix(irun)-eps)/k_to_e);
-elseif emode==2
+  case 2
     ki = sqrt((efix(irun)+eps)/k_to_e);
     kf = kfix(irun);
-else
+  otherwise
     ki = kfix(irun);
     kf = ki;
 end
+
 qw = cell(1,4);
 qw(1:3) = calculate_q (ki, kf, detdcn(:,idet), spec_to_rlu(:,:,irun));
 qw{4} = eps;
 
+end
 
-
-%----------------------------------------------------------------------------------------
 function [d_mat, detdcn] = spec_coords_to_det (detpar)
 % Matrix to convert coordinates in spectrometer (or laboratory) frame into detector frame
 %
@@ -103,20 +115,23 @@ function [d_mat, detdcn] = spec_coords_to_det (detpar)
 % The detector frame is one with x axis along kf, y radially outwards. This is the
 % original Tobyfit detector frame.
 
-ndet=numel(detpar.x2);
-cp=reshape(cosd(detpar.phi),[1,1,ndet]);
-sp=reshape(sind(detpar.phi),[1,1,ndet]);
-cb=reshape(cosd(detpar.azim),[1,1,ndet]);
-sb=reshape(sind(detpar.azim),[1,1,ndet]);
+%% TODO: Investigate use of transform_pix_to_hkl
 
-d_mat=[             cp, cb.*sp, sb.*sp;...
-    -sp, cb.*cp, sb.*cp;...
-    zeros(1,1,ndet),    -sb,     cb];
+ndet = numel(detpar.x2);
 
-detdcn=[cp; cb.*sp; sb.*sp];
+cp = reshape(cosd(detpar.phi), [1,1,ndet]);
+sp = reshape(sind(detpar.phi), [1,1,ndet]);
+cb = reshape(cosd(detpar.azim), [1,1,ndet]);
+sb = reshape(sind(detpar.azim), [1,1,ndet]);
 
+d_mat = [cp, cb.*sp, sb.*sp;...
+         -sp, cb.*cp, sb.*cp;...
+         zeros(1,1,ndet), -sb, cb];
 
-%----------------------------------------------------------------------------------------
+detdcn = [cp; cb.*sp; sb.*sp];
+
+end
+
 function q = calculate_q (ki, kf, detdcn, spec_to_rlu)
 % Calculate qh,qk,ql for direct geometry instrument
 %
@@ -140,14 +155,12 @@ function q = calculate_q (ki, kf, detdcn, spec_to_rlu)
 %               i.e. q{1}=qh, q{2}=qk, q{3}=ql
 
 % Use in-place working to save memory (note: bsxfun not needed from 2016b an onwards)
-qtmp = bsxfun(@times,-kf',detdcn);      % -kf in spectrometer axes
+qtmp = -kf' .* detdcn;
 qtmp(1,:) = ki' + qtmp(1,:);            % qspec proper now
 qtmp = mtimesx_horace (spec_to_rlu,reshape(qtmp,[3,1,numel(ki)]));
 qtmp = squeeze(qtmp);
 
 % Package output
-q = cell(1,3);
-q{1} = qtmp(1,:)';
-q{2} = qtmp(2,:)';
-q{3} = qtmp(3,:)';
+num2cell(qtmp', 1);
 
+end
