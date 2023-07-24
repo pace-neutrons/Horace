75f489e180342b9ff57df9b9d63d02ae4058f42a
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2023-07-24 11:14:27 +0100

    Update compact to new sqw/dnd style (#1043)
    * Update compact to new sqw/dnd style
    
    * Apply suggestions from code review
    
    Co-authored-by: cmarooney-stfc <christopher.marooney@stfc.ac.uk>
    
    * Add missing tests
    
    * Update to use section; add abstract implementations in SQWDnDBase
    
    * Respond to review
    
    ---------
    
    Co-authored-by: cmarooney-stfc <christopher.marooney@stfc.ac.uk>

--- horace_core/sqw/@sqw/section.m
+++ horace_core/sqw/@sqw/section.m
@@ -1,6 +1,10 @@
 function wout = section (win,varargin)
 % Takes a section out of an sqw object
 %
+% This is essentially a cut from the DnD without recomputing bins
+%  and extracts the bins wholesale from the underlying object
+% This makes the process more efficient but does not allow transformation of projections
+%
 %   >> wout = section (win, [ax_1_lo, ax_1_hi], [ax_2_lo, ax_2_hi], ...)
 %
 % Input:
