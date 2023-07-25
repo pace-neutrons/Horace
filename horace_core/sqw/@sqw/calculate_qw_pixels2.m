4e99b98e4ef87ef36b1be0d46f0d24742c1e33f8
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2023-07-25 16:20:08 +0100

    Fix issues, refactor

--- horace_core/sqw/@sqw/calculate_qw_pixels2.m
+++ horace_core/sqw/@sqw/calculate_qw_pixels2.m
@@ -1,5 +1,5 @@
 function qw=calculate_qw_pixels2(win)
-% Calculate qh,qk,ql,en for the pixels in an sqw dataset from the headers
+% Calculate qh, qk, ql, en for the pixels in an sqw dataset from the headers
 %
 %   >> qw = calculate_qw_pixels2(win)
 %
@@ -36,62 +36,51 @@ idet = win.pix.get_fields('detector_idx')';
 ien = win.pix.get_fields('energy_idx')';
 
 if ~iscell(win.header)
-
     header = {win.header};
-    emode = header.emode;
-
-    efix = header.efix;
-    eps_lo = 0.5*(header.en(1)+header.en(2));
-    eps_hi = 0.5*(header.en(end-1)+header.en(end));
-    ne = numel(header.en)-1;
-
-    [~, ~, spec_to_rlu] = calc_proj_matrix(header.alatt, header.angdeg,...
-                                           header.cu, header.cv, header.psi, header.omega,...
-                                           header.dpsi, header.gl, header.gs);
-
 else
     header = win.header;
-    emode = cellfun(@(x) x.emode,header);
-
-    if ~all(emode==emode(1))
-        error('HORACE:calculate_qw_pixels2:invalid_argument', ...
-              'Contributing runs to an sqw object must be all be direct geometry or all indirect geometry')
-    end
-
-    emode = emode(1);
-
-    efix = cellfun(@(x) x.efix,header);
-    eps_lo = cellfun(@(x) 0.5*(x.en(1)+x.en(2)),header);
-    eps_hi = cellfun(@(x) 0.5*(x.en(end-1)+x.en(end)),header);
-    ne = cellfun(@(x) numel(x.en)-1,header);
-
-    spec_to_rlu = zeros(3,3,numel(header));
-    for i = 1:numel(header)
-        h = header{i};  % for clarity in next line
-        [~, ~, spec_to_rlu(:,:,i)] = calc_proj_matrix (h.alatt, h.angdeg,...
-            h.cu, h.cv, h.psi, h.omega, h.dpsi, h.gl, h.gs);
-    end
 end
 
-eps = (eps_lo(irun) .* (ne(irun) - ien) + eps_hi(irun) .* (ien - 1)) ./ (ne(irun) - 1);
+emode = cellfun(@(x) x.emode, header);
+
+if ~all(emode==emode(1))
+    error('HORACE:calculate_qw_pixels2:invalid_argument', ...
+          'Contributing runs to an sqw object must be all be direct geometry or all indirect geometry')
+end
+
+emode = emode(1);
+
+efix = cellfun(@(x) x.efix, header);
+eps_lo = cellfun(@(x) 0.5*(x.en(1)+x.en(2)), header);
+eps_hi = cellfun(@(x) 0.5*(x.en(end-1)+x.en(end)), header);
+n_en = cellfun(@(x) numel(x.en)-1, header);
+
+[~, ~, spec_to_rlu] = cellfun(...
+    @(h) calc_proj_matrix(h.alatt, h.angdeg, ...
+                          h.cu, h.cv, h.psi, ...
+                          h.omega, h.dpsi, h.gl, h.gs), header, 'UniformOutput', false);
+% Join in 3rd rank leading to n x n x nhead
+spec_to_rlu = cat(3, spec_to_rlu{:});
+
+eps_diff = (eps_lo(irun) .* (n_en(irun) - ien) + eps_hi(irun) .* (ien - 1)) ./ (n_en(irun) - 1);
 [~, detdcn] = spec_coords_to_det(win.detpar);
 kfix = sqrt(efix/k_to_e);
 
 switch emode
   case 1
     ki = kfix(irun);
-    kf = sqrt((efix(irun)-eps)/k_to_e);
+    kf = sqrt((efix(irun)-eps_diff)/k_to_e);
   case 2
-    ki = sqrt((efix(irun)+eps)/k_to_e);
+    ki = sqrt((efix(irun)+eps_diff)/k_to_e);
     kf = kfix(irun);
   otherwise
     ki = kfix(irun);
     kf = ki;
 end
 
-qw = cell(1,4);
-qw(1:3) = calculate_q (ki, kf, detdcn(:,idet), spec_to_rlu(:,:,irun));
-qw{4} = eps;
+qw = cell(1, 4);
+qw(1:3) = calculate_q (ki, kf, detdcn(:, idet), spec_to_rlu(:, :, irun));
+qw{4} = eps_diff;
 
 end
 
@@ -106,7 +95,7 @@ function [d_mat, detdcn] = spec_coords_to_det (detpar)
 %
 % Output:
 % -------
-%   d_mat       Matrix size [3,3,ndet] to take coordinates in spectrometer
+%   d_mat       Matrix size [3, 3, ndet] to take coordinates in spectrometer
 %              frame and convert in detector frame.
 %
 %   detdcn      Direction of detector in spectrometer coordinates ([3 x ndet] array)
@@ -119,21 +108,21 @@ function [d_mat, detdcn] = spec_coords_to_det (detpar)
 
 ndet = numel(detpar.x2);
 
-cp = reshape(cosd(detpar.phi), [1,1,ndet]);
-sp = reshape(sind(detpar.phi), [1,1,ndet]);
-cb = reshape(cosd(detpar.azim), [1,1,ndet]);
-sb = reshape(sind(detpar.azim), [1,1,ndet]);
+cp = reshape(cosd(detpar.phi), [1, 1, ndet]);
+sp = reshape(sind(detpar.phi), [1, 1, ndet]);
+cb = reshape(cosd(detpar.azim), [1, 1, ndet]);
+sb = reshape(sind(detpar.azim), [1, 1, ndet]);
 
 d_mat = [cp, cb.*sp, sb.*sp;...
          -sp, cb.*cp, sb.*cp;...
-         zeros(1,1,ndet), -sb, cb];
+         zeros(1, 1, ndet), -sb, cb];
 
 detdcn = [cp; cb.*sp; sb.*sp];
 
 end
 
 function q = calculate_q (ki, kf, detdcn, spec_to_rlu)
-% Calculate qh,qk,ql for direct geometry instrument
+% Calculate qh, qk, ql for direct geometry instrument
 %
 %   >> q = calculate_q (ki, kf, detdcn, spec_to_rlu)
 %
@@ -142,11 +131,11 @@ function q = calculate_q (ki, kf, detdcn, spec_to_rlu)
 %   ki          Incident wavevectors for each point [Column vector]
 %   kf          Final wavevectors for each point    [Column vector]
 %   detdcn      Array of unit vectors in the direction of the detectors
-%               Size is [3,npnt]
+%               Size is [3, npnt]
 %   spec_to_rlu Matrix to convert momentum in spectrometer coordinates to
 %               components in r.l.u.:
 %                   v_rlu = spec_to_rlu * v_spec
-%               Size is [3,3,npnt]
+%               Size is [3, 3, npnt]
 %
 % Output:
 % -------
@@ -156,11 +145,11 @@ function q = calculate_q (ki, kf, detdcn, spec_to_rlu)
 
 % Use in-place working to save memory (note: bsxfun not needed from 2016b an onwards)
 qtmp = -kf' .* detdcn;
-qtmp(1,:) = ki' + qtmp(1,:);            % qspec proper now
-qtmp = mtimesx_horace (spec_to_rlu,reshape(qtmp,[3,1,numel(ki)]));
+qtmp(1, :) = ki' + qtmp(1, :);            % qspec proper now
+qtmp = mtimesx_horace (spec_to_rlu, reshape(qtmp, [3, 1, numel(ki)]));
 qtmp = squeeze(qtmp);
 
 % Package output
-num2cell(qtmp', 1);
+q = num2cell(qtmp', 1);
 
 end
