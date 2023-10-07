a0b11f68a26b0bf45445958171f1003ca78586cd
abuts <Alex.Buts@stfc.ac.uk>
2023-10-07 07:45:45 +0100

    Re #1304 comments to  sqw_eval_pix.m

--- horace_core/sqw/@sqw/sqw_eval_pix.m
+++ horace_core/sqw/@sqw/sqw_eval_pix.m
@@ -2,8 +2,8 @@ function obj = sqw_eval_pix(obj, sqwfunc, ave_pix, pars, outfile)
 %==================================================================================================
 % SQW_EVAL_PIX
 %
-% Helper function for sqw eval executed on a pixel-less object (i.e. DnD or SQW with no pixels
-% Called by `sqw_eval_` defined in sqw/DnDBase
+% Helper function for sqw eval executed on a fill sqw object.
+% Called by `sqw_eval` defined in sqw/DnDBase
 %
 % Input:
 % ------
@@ -13,14 +13,15 @@ function obj = sqw_eval_pix(obj, sqwfunc, ave_pix, pars, outfile)
 %   sqwfunc    Handle to function that calculates S(Q,w)
 %   ave_pix    Boolean flag wither to apply function to averaged bin data
 %   pars       Arguments needed by the function.
-%   outfile    The file used for filebacking and output
+%   outfile    The file used for outputing filebacked result
 %
 %==================================================================================================
 
 %[obj, data] = obj.apply(sqwfunc, args, data, true); <- does not work
 eval_op = PageOp_sqw_eval();
+[eval_op,obj] = eval_op.init(obj,sqwfunc,pars,ave_pix);
 if ~isempty(outfile)
     eval_op.outfile = outfile;
 end
-[eval_op,obj] = eval_op.init(obj,sqwfunc,pars,ave_pix);
+
 obj = obj.apply_c(eval_op);
