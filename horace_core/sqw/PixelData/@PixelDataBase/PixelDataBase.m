9d66cf3d4ff10ce934ec6ebee5aa2d1ad277ed23
abuts <Alex.Buts@stfc.ac.uk>
2023-10-20 17:12:41 +0100

    Re #1325 Should fix cat

--- horace_core/sqw/PixelData/@PixelDataBase/PixelDataBase.m
+++ horace_core/sqw/PixelData/@PixelDataBase/PixelDataBase.m
@@ -743,11 +743,11 @@ classdef (Abstract) PixelDataBase < serializable
             [mean_signal, mean_variance,signal_msd] = ...
                 compute_bin_data_(obj, npix,pix_idx,calc_variance,calc_signal_msd);
         end
-        function obj = cat(varargin)
+        function out_obj = cat(obj,varargin)
             % Concatenate the given PixelData objects' pixels. This function performs
             % a straight-forward data concatenation.
             %
-            %   >> joined_pix = PixelDataBase.cat(pix_data1, pix_data2);
+            %   >> joined_pix = pix_data1.cat(pix_data1, pix_data2);
             %
             % Input:
             % ------
@@ -757,14 +757,18 @@ classdef (Abstract) PixelDataBase < serializable
             % -------
             %   obj         A PixelData object containing all the pixels in the inputted
             %               PixelData objects
+            %               The type of the object (filebacked or
+            %               memorybacked) will be defined by the type of
+            %               the first object to cat.
 
             % Take the dataclass of the first object.
-            if numel(varargin) == 1 && isa(varargin{1}, 'PixelDataBase')
-                obj = varargin{1};
+            if isempty(varargin)
+                out_obj = obj;
                 return;
             end
+            out_obj = copy(obj);
 
-            obj = varargin{1}.cat(varargin{:});
+            out_obj= out_obj.cat(varargin{:});
         end
     end
     %======================================================================
