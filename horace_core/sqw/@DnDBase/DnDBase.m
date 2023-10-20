0eac826831aeb5cbc0704906bd6d02e14c6fa466
abuts <Alex.Buts@stfc.ac.uk>
2023-10-20 21:08:53 +0100

    Re #1343 Added replicate to operations list

--- horace_core/sqw/@DnDBase/DnDBase.m
+++ horace_core/sqw/@DnDBase/DnDBase.m
@@ -153,6 +153,8 @@ classdef (Abstract)  DnDBase < SQWDnDBase & dnd_plot_interface
         w = sigvar_set(win, sigvar_obj);
         sz = sigvar_size(w);
         %------------------------------------------------------------------
+        wout = replicate (win, wref);
+        
         function obj=signal(~,varargin)
             error('HORACE:DnDBase:runtime_error',...
                 'Call to signal function is possible for sqw objects only')
