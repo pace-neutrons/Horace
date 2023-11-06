03b018cede4249c6733011cbfdf7ba284595fa69
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2023-11-06 10:13:00 +0000

    Add Matlab wrapper to allow legacy to new multifit (#1369)
    * Add Matlab wrapper to allow legacy to new multifit
    
    * Update admin/mf_leg_to_new.m
    
    Co-authored-by: Duc Le <duc.le@stfc.ac.uk>
    
    * Update admin/mf_leg_to_new.m
    
    Co-authored-by: Duc Le <duc.le@stfc.ac.uk>
    
    * Move to horace_core/admin
    
    * Respond to review
    
    ---------
    
    Co-authored-by: Duc Le <duc.le@stfc.ac.uk>

---
+++ horace_core/admin/mf_legacy_to_new.m
@@ -0,0 +1,31 @@
+function conv = mf_legacy_to_new(funcline)
+% MATLAB wrapper for python script to modernise multifit syntax
+%
+% >> mf_legacy_to_new("[wfit,fitdata]=multifit_sqw(my_new_cut,@sr122_xsec,pars,pfree,pbind,'list',1);")
+% ans =
+%
+%    kk = multifit(my_new_cut);
+%    kk = kk.set_fun(@sr122_xsec);
+%    kk = kk.set_pin(pars);
+%    kk = kk.set_free(pfree);
+%    kk = kk.set_bind(pbind);
+%    kk = kk.set_options('listing', 1);
+%    [wfit,fitdata] = kk.fit();
+%
+% Script to roughly convert from multifit_legacy to modern multifit syntax
+%
+% J. Wilkins 8-9-2023
+
+    pth = fullfile(horace_paths().root, 'tools');
+    if count(py.sys.path, pwd) == 0
+        insert(py.sys.path, int32(0), pth);
+    end
+
+    convstr = strip(char(py.mf_leg_to_new.convert_legacy_multifit(funcline)));
+    if nargout < 1
+        disp(convstr)
+    else
+        conv = convstr;
+    end
+
+end
