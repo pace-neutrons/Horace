e6b97b8e25bdcc70b2cd744de4957fcdf3b5dd23
abuts <Alex.Buts@stfc.ac.uk>
2023-11-17 11:53:04 +0000

    Re #1330 typo in cat code

--- horace_core/sqw/PixelData/@PixelDataBase/cat.m
+++ horace_core/sqw/PixelData/@PixelDataBase/cat.m
@@ -10,18 +10,18 @@ function pix_out = cat(varargin)
 %
 % Output:
 % -------
-%   obj         A PixelData object containing all the pixels in the inputted
+%   pix_out     A PixelData object containing all the pixels in the inputted
 %               PixelData objects
 %               The type of the object (filebacked or
 %               memorybacked) will be defined by the type of
 %               the first object to cat.
 
 % Take the dataclass of the first object.
-if isempty(varargin)
-    pix_out = obj;
+pix_out = copy(varargin{1});
+if numel(varargin) == 1
     return;
 end
-pix_out = copy(obj);
+
 page_op = PageOp_cat_pix();
 page_op = page_op.init(varargin{:});
 pix_out = PixelDataFileBacked.apply_op(pix_out,page_op);
