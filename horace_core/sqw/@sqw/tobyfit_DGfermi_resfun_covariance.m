6b9d11b769ab4dce22f24eedc6365bcdc0a2c5a6
tgperring <toby.perring@stfc.ac.uk>
2023-11-21 20:56:57 +0000

    Changes to tobyfit_*_resconv_* have also required changes to tobyfit_*_resfun_covariance as dq_mat is no longer held in the lookup structure returned by tobyfit_*_resconv_init as it is now computed on the fly within tobyfit_*_resconv. Have taken advantage of the need to make changes to unify some minor divergences between the the two tobyfit_*_resfun_covariance methods where the code should be identical.

--- horace_core/sqw/@sqw/tobyfit_DGfermi_resfun_covariance.m
+++ horace_core/sqw/@sqw/tobyfit_DGfermi_resfun_covariance.m
@@ -71,11 +71,20 @@ fermi_table = lookup.fermi_table;
 sample_table = lookup.sample_table;
 detector_table = lookup.detector_table;
 
+% Constants
+k_to_v = lookup.k_to_v;
+k_to_e = lookup.k_to_e;
+
 
 % Get covariance matricies
 % ------------------------
 for iw = 1:numel(win)
-    if iscell(win), wtmp = win{iw}; else, wtmp = win(iw); end
+    if iscell(win)
+        wtmp = win{iw};
+    else
+        wtmp = win(iw);
+    end
+    
     if all_pixels
         [irun,idet] = parse_pixel_indices (wtmp);
     else
@@ -84,9 +93,23 @@ for iw = 1:numel(win)
     npix = npix_arr(iw);
 
     % Simple pointers to items in lookup
+    x0=lookup.x0{iw};
+    xa=lookup.xa{iw};
+    x1=lookup.x1{iw};
+    thetam=lookup.thetam{iw};
+    angvel=lookup.angvel{iw};
+    ki = lookup.ki{iw};
     kf = lookup.kf{iw};
+    s_mat=lookup.s_mat{iw};
+    spec_to_rlu = lookup.spec_to_rlu{iw};
     dt = lookup.dt{iw};
 
+    [x2, ~, d_mat, f_mat] = detector_table.func_eval_ind (iw, irun, idet, @detector_info);
+    dq_mat = dq_matrix_DGfermi (ki(irun), kf,...
+        x0(irun), xa(irun), x1(irun), x2, thetam(irun), angvel(irun),...
+        s_mat(:,:,irun), f_mat, d_mat,...
+        spec_to_rlu(:,:,irun), k_to_v, k_to_e);
+    
     % Compute variances
     var_mod = (10^-6 * moderator_table.func_eval_ind(iw, irun, @pulse_width)).^2;
     cov_aperture = aperture_table.func_eval_ind(iw, irun, @covariance);
@@ -114,18 +137,14 @@ for iw = 1:numel(win)
     cov_x(11,11,:) = var_tbin;
 
     % Compute wavevector-energy covariance matrix in different dimensions
-    dq_mat = lookup.dq_mat{iw};
-    spec_to_rlu = lookup.spec_to_rlu{iw};
-
-
-    cov_hkle{iw} = transform_matrix (cov_x, dq_mat);
-    %TODO: Re #1040 this code is not consistent with generic projection:
+    %TODO: Re #1040 this code is not consistent with generic projections
     if ~isa(wtmp.data.proj,'line_proj')
         error('HORACE:sqw:not_implemented', ...
-            'resolution can not yet be calculated for any projection except linear projection')
+            'resolution cannot currently be calculated for any projection except linear projection')
     end
+    
+    cov_hkle{iw} = transform_matrix (cov_x, dq_mat);
     cov_proj{iw} = transform_matrix (cov_hkle{iw}, inv(wtmp.data.u_to_rlu));
-
     rlu_to_spec = invert_matrix (spec_to_rlu);
     rlu_to_spec4(1:3,1:3,:) = rlu_to_spec(:,:,irun);
     rlu_to_spec4(4,4,:) = 1;
@@ -143,13 +162,13 @@ end
 
 %=============================================================================
 function Cout = transform_matrix (C,B)
-% Compute the matrix product Cout = B * C * B' for nD matricies
+% Compute the matrix product Cout = B * C * B' for nD matrices
 % B can be a 2D matrix and dimension expansion is performed
 
 szC = size(C);
 szB = size(B);
 if numel(szC)==2 && numel(szB)==2
-    % Just do straighforward matlab matric multiplication
+    % Just do straightforward Matlab matrix multiplication
     Cout = B * C * B';
 else
     if numel(szB)==2
