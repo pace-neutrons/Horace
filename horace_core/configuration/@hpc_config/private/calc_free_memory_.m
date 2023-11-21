db042ab355b5ea6c1ad389327714a34cf9a3e4ae
abuts <Alex.Buts@stfc.ac.uk>
2023-11-21 13:41:47 +0000

    Re #1394 changes from review

--- horace_core/configuration/@hpc_config/private/calc_free_memory_.m
+++ horace_core/configuration/@hpc_config/private/calc_free_memory_.m
@@ -1,12 +1,13 @@
 function free_memory = calc_free_memory_()
 %CALC_FREE_MEMORY_ tries to evaluate the free memory available to
 %application by allocating sufficiently large chunk of memory and
-%identifying the size of this chunk from the moment when it becomes
-%impossible
+%identifying the size of this chunk until it is possible.
+%
+% Free memory assumed to be memory, which may be allocated.
 
 [~,free_memory]=sys_memory();
 ndata = floor(free_memory/8); % memory in bytes and I will be allocating doubles
-clOb = onCleanup(@()clear('data'));
+
 opt = struct('TolX',0.1);
 ndata = fzero(@heavi,ndata,opt);
 free_memory = floor(0.9*8*ndata);
@@ -22,4 +23,3 @@ catch
 end
 clear data;
 end
-
