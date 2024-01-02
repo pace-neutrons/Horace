7bbd9f445c00551d1b5e05b3e5da08e9ea1ad685
abuts <Alex.Buts@stfc.ac.uk>
2024-01-02 11:01:55 +0000

    Re #1344 sigvar_set implemented

---
+++ horace_core/sqw/page_operations/PageOp_sigvar_set.m
@@ -0,0 +1,32 @@
+classdef PageOp_sigvar_set < PageOpBase
+    % Single pixel page operation used by sigvar_set method
+    %
+
+    methods
+        function obj = PageOp_sigvar_set(varargin)
+            obj = obj@PageOpBase(varargin{:});
+            obj.op_name_ = 'sigvar_set';
+            obj.split_at_bin_edges = true;
+        end
+        function obj = init(obj,sqw_obj)
+            % sqw object here already have modified image so we use
+            % modified image to set pixels
+            obj  = init@PageOpBase(obj,sqw_obj);
+        end
+
+        function obj = apply_op(obj,~,npix_idx)
+            s = repelem(obj.img_.s(npix_idx(1):npix_idx(2)), obj.npix(npix_idx(1):npix_idx(2)));
+            e = repelem(obj.img_.e(npix_idx(1):npix_idx(2)), obj.npix(npix_idx(1):npix_idx(2)));
+            obj.page_data_(obj.signal_idx,:)   = s(:)';
+            obj.page_data_(obj.var_idx,:)      = e(:)';
+        end
+    end
+    methods(Access=protected)
+        function  does = get_changes_pix_only(~)
+            % pageOp calculates pixels only using image as source. No point
+            % of calculating image from pixels again as it would be in
+            % usual PageOp
+            does = true;
+        end
+    end
+end
