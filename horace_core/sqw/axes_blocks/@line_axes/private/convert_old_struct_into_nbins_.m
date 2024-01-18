1acb2148b03eade7e00230e0a2848a0d185d4ceb
abuts <Alex.Buts@stfc.ac.uk>
2024-01-18 13:02:55 +0000

    Re #1475 minor changes from review

--- horace_core/sqw/axes_blocks/@line_axes/private/convert_old_struct_into_nbins_.m
+++ horace_core/sqw/axes_blocks/@line_axes/private/convert_old_struct_into_nbins_.m
@@ -1,5 +1,5 @@
 function in = convert_old_struct_into_nbins_(in)
-% convert old axes_block structure, containing iint, p, iax etc 
+% convert old axes_block structure, containing iint, p, iax etc
 % into the axes_block ver 2 structure
 %
 
@@ -19,7 +19,7 @@ end
 in = rmfield(in,prop_to_convert);
 in.nbins_all_dims = nbins_all_dims;
 in.img_range = img_range;
-if isfield(in,'img_db_range') && ~isequal(in.img_db_range,PixelDataBase.EMPTY_RANGE_) 
+if isfield(in,'img_db_range') && ~isequal(in.img_db_range,PixelDataBase.EMPTY_RANGE_)
     % old format with img_db_range defined
     in.img_range = in.img_db_range;
 end
@@ -27,8 +27,6 @@ if isfield(in,'ulabel')
     in.label = in.ulabel;
     in = rmfield(in,'ulabel');
 end
-if all(nbins_all_dims == 1)
-    if isfield(in,'dax')
-        in = rmfield(in,'dax');
-    end
+if all(nbins_all_dims == 1) && isfield(in,'dax')
+    in = rmfield(in,'dax');
 end
