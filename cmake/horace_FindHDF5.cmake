d2dc161ef1a2bca96fdc366d52539522d32049fb
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-02-02 10:57:26 +0000

    Get HDF5 through glob rather than hard-coded reference

--- cmake/horace_FindHDF5.cmake
+++ cmake/horace_FindHDF5.cmake
@@ -14,8 +14,9 @@ module. You'll find the file bundled with your CMake isntallation.
 if(UNIX)
     set(HDF5_ROOT "${Horace_ROOT}/_LowLevelCode/external/HDF5_1.8.12_unix/")
     # On Linux, link to Matlab's version of HDF5
-    set(HDF5_hdf5_LIBRARY_RELEASE "${Matlab_LIBRARY_DIR}/libhdf5.so.8"
-        CACHE PATH "" FORCE)
+    file(GLOB HDF5_loc "${Matlab_LIBRARY_DIR}/libhdf5.so.*")
+    list(GET HDF5_loc 0 HDF5_loc)
+    set(HDF5_hdf5_LIBRARY_RELEASE "${HDF5_loc}" CACHE PATH "" FORCE)
 elseif(WIN32)
     set(HDF5_ROOT "${Horace_ROOT}/_LowLevelCode/external/HDF5_1.8.12_win/")
 endif()
