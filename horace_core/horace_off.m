2ea432398828ba9dce1402045cb132c28a1095e1
Alex Buts <Alex.Buts@stfc.ac.uk>
2024-02-09 12:20:19 +0000

    Re #1 fixing issue identified on IDAAAS and related to switch between different Horace versions

--- horace_core/horace_off.m
+++ horace_core/horace_off.m
@@ -10,12 +10,15 @@ function horace_off
 rootpath = fileparts(fileparts(which('horace_init')));
 on_path  = fileparts(which('horace_on')); %
 warn_state=warning('off','all');    % turn of warnings (so don't get errors if remove non-existent paths)
-if is_idaaas()
-    % clear idaaas control variables as they force default
-    % Horace to be initialized, and we want requested Horace to
-    % be initialized.
-    setenv('MATLAB_INIT_HORACE','');
-    setenv('MATLAB_INIT_MSLICE','');
+try
+    if is_idaaas()
+        % clear idaaas control variables as they force default
+        % Horace to be initialized, and we want requested Horace to
+        % be initialized.
+        setenv('MATLAB_INIT_HORACE','');
+        setenv('MATLAB_INIT_MSLICE','');
+    end
+catch
 end
 
 clear mex;
@@ -24,7 +27,7 @@ try
     % make sure we are not removing the path to horace_on
     if ~isempty(on_path)
         paths = strrep(paths,[on_path,pathsep],'');
-    end    
+    end
     rmpath(paths);
     warning(warn_state);    % return warnings to initial state
 catch
