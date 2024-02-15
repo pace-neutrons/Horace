2e8d63a7f4adabe7932e48aa2afa9a2763b27b8c
tgperring <toby.perring@stfc.ac.uk>
2024-02-15 09:28:32 +0000

    Change to documentation of sawtooth_iarray

--- herbert_core/utilities/maths/sawtooth_iarray.m
+++ herbert_core/utilities/maths/sawtooth_iarray.m
@@ -17,21 +17,7 @@ function ivout = sawtooth_iarray (n)
 % -------
 %   ivout   Output array: always a column vector with length sum(n(:)).
 %           It is constructed as:
-%               ivout [(1:n(1))'; (1:n(2))'; ...] 
-%           Non-integer values of n are rounded down to the next smaller
-%           integer, and any negative values are treated as zero.
-%           Note: if the output is empty then it is still a column with zero
-%           length i.e. has size = [0,1]
-%
-% EXAMPLES
-%   ivout = sawtooth_iarray(n)
-%
-%   n                 ivout
-% ----------------------------------------------------------------------
-%   0               zeros(0,1)      % column vector with zero length
-%   [2,3,1]         [1;2;1;2;3;1]
-%   [2,0,1]         [1;2;1]         % zero length middle sawtooth section
-%   [2.3,-0.7,3.99] [1;2;1;2;3]     % reals rounded downwards
+%               ivout [1;2;3..n(1); 1;2;3...n(2); ...] 
 
 
 n = n(:);
