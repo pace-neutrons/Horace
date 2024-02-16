7b43f13cda3da24bff4ed417a0ec701831aeb2d1
tgperring <toby.perring@stfc.ac.uk>
2024-02-16 14:41:57 +0000

    More response to comments

--- herbert_core/utilities/maths/sawtooth_iarray.m
+++ herbert_core/utilities/maths/sawtooth_iarray.m
@@ -26,12 +26,28 @@ if any(n<0) || any(round(n)~=n)
         'The elements of the input argument must all be nonnegative integer values')
 end
 
-n = n(n>0);
 if isempty(n)
+    % No values of n: empty column vector output
     ivout = zeros(0,1);
+    
 elseif isscalar(n)
+    % Single value of n: return is (1:n)'. Note: this is correct when n = 0 as
+    % well
     ivout = (1:n)';
+    
 else
+    % How the following two lines work:
+    % * First step is to create the array (1:sum(n(:))'. 
+    % * To get the required output, it is necessary to subtract n(1) from
+    % elements with indices (n(1)+1) to (n(1)+n(2)), then subtract
+    % (n(1)+n(2)) from the elements with indices (n(1)+n(2)+1) to
+    % (n(1)+n(2)+n(3)) etc. This can be achieved by feeding the cumulative sum
+    % of [0; n(:)] into the function repelem with repeat values n:
+    % - First elements are: 0  repeated n(1) times
+    % -        followed by: n(1)  repeated n(2) times
+    % -        followed by: n(1)+n(2)  repeated n(3) times
+    %                etc
+    % This algorithm works even when there are some or all n(i) == 0
     offset = cumsum([0;n]);
     ivout = (1:offset(end))' - repelem(offset(1:end-1), n);
 end
