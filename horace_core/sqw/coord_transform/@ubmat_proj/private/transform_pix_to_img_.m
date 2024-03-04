819b2359cab0a0760b266be93c0471d2beecdd81
abuts <Alex.Buts@stfc.ac.uk>
2024-03-04 16:35:11 +0000

    Re #1592 Template for ubmat_proj class copied from line_proj

---
+++ horace_core/sqw/coord_transform/@ubmat_proj/private/transform_pix_to_img_.m
@@ -0,0 +1,50 @@
+function pix_transf = transform_pix_to_img_(obj,pix_input,varargin)
+% Transform pixels expressed in crystal Cartesian coordinate systems
+% into image coordinate system
+%
+% Input:
+% pix_data -- [3xNpix] or [4xNpix] array of pix coordinates
+%             expressed in crystal Cartesian coordinate system
+%             or pixelData object containing this information.
+% Returns:
+% pix_out -- [3xNpix] or [4xNpix] array the pixels coordinates transformed
+%            into coordinate system, related to the image (e.g. hkl system)
+%
+
+if isa(pix_input,'PixelDataBase')
+    if pix_input.is_misaligned
+        pix_cc = pix_input.get_raw_data('q_coordinates');
+    else
+        pix_cc = pix_input.q_coordinates;
+    end
+
+    shift_ei = obj.offset(4) ~=0;
+
+    [pix_to_img, offset] = obj.get_pix_img_transformation(3, pix_input);
+
+    % transposed pix_to_image transformation, as the transformation is defined
+    % as column vectors and pixel_data here are also column vectors.
+    pix_transf = pix_to_img*(pix_cc - offset(:));
+
+    if shift_ei
+        ei = pix_input.dE - obj.offset(4);
+    else
+        ei = pix_input.dE;
+    end
+    pix_transf = [pix_transf;ei];
+
+else
+    % if pix_input is 4-d, this will use 4-D matrix and shift
+    % if its 3-d -- matrix is 3-dimensional and energy is not shifted
+    % anyway
+    ndim = size(pix_input, 1);
+
+    [pix_to_img,offset] = obj.get_pix_img_transformation(ndim,pix_input);
+
+    % transposed pix_to_image transformation, as the transformation is defined
+    % as column vectors and pixel_data here are also column vectors.
+    pix_transf= pix_to_img*(pix_input - offset(:));
+
+end
+
+end
