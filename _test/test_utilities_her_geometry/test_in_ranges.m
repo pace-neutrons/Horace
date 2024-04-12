056ca75d704e65af65b3a37aeb83dedba72ddb22
abuts <Alex.Buts@stfc.ac.uk>
2024-04-12 13:38:44 +0100

    Re #954 in_range function

---
+++ _test/test_utilities_her_geometry/test_in_ranges.m
@@ -0,0 +1,42 @@
+classdef test_in_ranges < TestCase
+    %
+    properties
+    end
+
+    methods
+        %--------------------------------------------------------------------------
+        function self = test_in_ranges(varargin)
+            if nargin>0
+                name = varargin{1};
+            else
+                name = 'test_in_ranges';
+            end
+            self = self@TestCase(name);
+        end
+
+        %--------------------------------------------------------------------------
+        function test_four_vectors_around(~)
+            box = [-1,-2,-3;1,2,3];
+            vec = zeros(3,4);
+            vec(1,2) = 2;
+            vec(3,3) = 3;
+            vec(:,4) = 5;
+
+            [in_details,in] = in_range(box,vec);
+
+            assertEqual(in,[1,-1,0,-1]);
+            assertEqual(in_details,[1,1,1;-1,1,1;1,1,0;-1,-1,-1]');
+        end
+
+        function test_one_vector_inside(~)
+            box = [-1,-2,-3;1,2,3];
+            vec = zeros(3,1);
+
+            [in_details,in] = in_range(box,vec);
+
+            assertEqual(in,1);
+            assertEqual(in_details,ones(3,1));
+        end
+        %--------------------------------------------------------------------------
+    end
+end
--- horace_core/sqw/@DnDBase/targ_range.m
+++ horace_core/sqw/@DnDBase/targ_range.m
@@ -1,4 +1,4 @@
-function range = targ_range(obj,targ_proj,varargin)
+function the_range = targ_range(obj,targ_proj,varargin)
 %TARG_RANGE calculate the full range of the image to be produced by target
 % projection from the current image,
 %
@@ -7,10 +7,10 @@ function range = targ_range(obj,targ_proj,varargin)
 % targ_proj  -- aProjectionBase class which defines target coordinate
 %               system where the range should be identified.
 % Optional:
-%  ranges_requested 
+%  ranges_requested
 %            -- four element logical array, where true states the requested
 %               range to identify. This is used for identifying the ranges
-%               of orthogonal dimensions (i.e. dE so that if only 
+%               of orthogonal dimensions (i.e. dE so that if only
 %
 % '-binning' if present, the method returns range as cellarray of binning
 %            parameters, i.e. the parameters which you would provide to cut
@@ -21,7 +21,7 @@ function range = targ_range(obj,targ_proj,varargin)
 %              if "-binning" is requested, this range is transformed into 4
 %              element cellarray, where each cell contains binning
 %              parameters in the form which provides initial binning range
-%            
+%
 %             when range_requested for an element of binning range is
 %             false, the range for this element is [-inf;inf] or corresponding
 %             cell in "-binning" mode is empty.
@@ -34,16 +34,17 @@ end
 if isempty(argi)
     ranges_requested = true(1,4);
 else
-    ranges_requested = logical(argi{1});    
+    ranges_requested = logical(argi{1});
     if numel(ranges_requested) ~= 4
-            error('HORACE:DnDBase:invalid_argument',...
-                'Requested range array needs to 4 elements. Actually, its size is :%s',...
+        error('HORACE:DnDBase:invalid_argument',...
+            'Requested range array needs to 4 elements. Actually, its size is :%s',...
             disp2str(size(range_requested)));
     end
 end
+the_range = repmat([-inf;inf],1,4);
 if ~exist('targ_proj','var') || isempty(targ_proj)
     % getting binning range which produced existing DnD object
-    range = obj.img_range;
+    the_range(:,ranges_requested) = obj.img_range(:,ranges_requested);
 else
     source_proj = obj.proj;
     if isstruct(targ_proj) % allow input to be a structure
@@ -61,26 +62,41 @@ else
         targ_proj.angdeg = source_proj.angdeg;
     end
     %
-    % cross-assign appropriate projections to enable possible optimizations
+    % cross-assign appropriate projections to allow
+    % "from_this_to_targ_coord" method and possible optimizations in
+    % source->target target->source transformations
     source_proj.targ_proj = targ_proj;
     targ_proj.targ_proj   = source_proj;
 
-    if isa(source_proj,class(targ_proj))
-        % if both projections relate to the same coordinate system, the min/max
-        % evaluation will be sufficient
-        cur_range = obj.img_range;
+    % can we deploy simple case of linear projections or curvelinear
+    % projections with the same centre?
+    cur_range = obj.img_range;
+    offset_diffr = source_proj.offset - targ_proj.offset;
+    if targ_proj.do_3D_transformation
+        is_zero_off  = any(abs(offset_diffr(1:3))<eps('single'));
+    else
+        is_zero_off  = any(abs(offset_diffr)<eps('single'));
+    end
+
+    if isa(source_proj,class(targ_proj)) && (is_zero_off || isa(source_proj,'LineProjBase'))
+        % if both projections relate to the same type of coordinate system,
+        % the min/max range evaluation will be trivial
         full_range = expand_box(cur_range(1,:),cur_range(2,:));
         full_targ_range = source_proj.from_this_to_targ_coord(full_range);
-        range = [min(full_targ_range,[],2),max(full_targ_range,[],2)]';
+        range = min_max(full_targ_range)';
     else
         % if not, analyse cut hull to understand what ranges can be
         % specified
-        range = search_for_range(obj,source_proj,ranges_requested);
+        targ_center = source_proj.transform_hkl_to_img(targ_proj.offset(:));
+        is_in       = in_range(cut_range,targ_center);
+
+        range = search_for_range(obj,source_proj,is_in,ranges_requested);
     end
+    the_range(:,ranges_requested) = range(:,ranges_requested);
 end
 if do_binning_range
     nsteps = obj.axes.nbins_all_dims;
-    range  = arrayfun(@build_binning,range(1,:),range(2,:),nsteps,'UniformOutput',false);
+    the_range  = arrayfun(@build_binning,the_range(1,:),the_range(2,:),nsteps,'UniformOutput',false);
 end
 
 function bin_range = build_binning(min_range,max_range,nsteps)
@@ -98,49 +114,51 @@ step = (max_range-min_range)/(nsteps);
 % axis binning parameters
 bin_range = [min_range+step/2,step,max_range-step/2];
 
-function range = search_for_range(obj,source_proj,ranges_requested)
+function out_range = search_for_range(obj,source_proj,in_ranges,ranges_requested)
 % find the maximal range, the current grid occupies in target coordinate
 % system
 %
-% The most primitive and pretty memory expensive search algorithm possible.
 %
-range = repmat([-inf;inf],1,4);
-[range0,dE_range] = transf_range(obj,source_proj,1);
+
+out_range = repmat([-inf;inf],1,4);
+nbins_per_dim = 10*ones(1,4);
+img_range = obj.axes.img_range;
+range0 = transf_range(source_proj,img_range,nbins_per_dim);
 if source_proj.do_3D_transformation
-    range(:,4) = dE_range;
+    out_range(:,4) = range0(:,4); % dE range is orthogonal to others and is not transformed
+    img_range = img_range(:,1:3);
+    nbins_per_dim = nbins_per_dim(1:3);
+    range0    = range0(:,1:3);
 end
 if sum(ranges_requested) == 1 && ranges_requested(4)
     return;
 end
 difr = 1;
-node_mult = 2;
-while difr>1.e-3 && node_mult<33 % node multiplier doubles number of points
+ic   = 1;
+while difr>1.e-3 && ic <= 5 % node multiplier doubles number of points
     % on each iteration step and goes 2,4,8,16,32. As the number of points increases on the hull,
     % it is still not too big number so memory requests are acceptable.
-    range = transf_range(obj,source_proj,node_mult);
+    nbins_per_dim   = nbins_per_dim*2;
+    range = transf_range(source_proj,img_range,nbins_per_dim);
     difr = calc_difr(range0,range);
     range0 = range;
-    node_mult = node_mult*2;
+    ic = ic+1;
 end
-if node_mult > 32
+if ic > 5 && difr>1.e-3
     warning('HORACE:targ_range', ...
         ['target range search algorithm have not converged after 5 iterations.\n', ...
         ' The default range identified for the cut may be inaccurate'])
 end
 %
 if source_proj.do_3D_transformation
-    range  = [range,dE_range];
+    out_range(:,1:3) = range;
 end
 
-function [loc_range,dE_range] = transf_range(obj,source_proj,multiplier)
+function loc_range = transf_range(source_proj,img_range,nbins_all_dims)
 % transfer axes hull into target coordinate system.
-if source_proj.do_3D_transformation
-    shell = obj.axes.get_bin_nodes('-3D','-hull',multiplier);
-    dE_range = obj.axes.img_range(:,4);
-else
-    shell = obj.axes.get_bin_nodes('-hull',multiplier);
-    dE_range = [];
-end
+
+shell = build_hull(img_range,nbins_all_dims);
+
 shell_transf = source_proj.from_this_to_targ_coord(shell);
 loc_range = min_max(shell_transf)';
 
