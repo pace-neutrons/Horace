6e655bec8b4d211d57866c49222f7d6da825f19b
abuts <Alex.Buts@stfc.ac.uk>
2024-05-31 14:41:54 +0100

    Re #1697 Revert commit on Incorrect usage of Horace-3 binning algorithm.
    This reverts commit bde757e1178729b6dfff765ee79e41b305fd1b90.

--- horace_core/sqw/coord_transform/@aProjectionBase/private/may_contribute_.m
+++ horace_core/sqw/coord_transform/@aProjectionBase/private/may_contribute_.m
@@ -11,8 +11,8 @@ function  [may_contributeND,may_contribute_dE] = may_contribute_(source_proj,...
 % build grid with points at bin edges for the target grid and bin centres
 % for reference grid
 if source_proj.do_3D_transformation_
-    [~,dEnodes] = targ_axes_block.get_bin_nodes('-3D','-ngrid','-halo');
-    [source_grid,baseEdges]  = source_axes_block.get_bin_nodes('-3D');
+    [targ_nodes,dEnodes] = targ_axes_block.get_bin_nodes('-3D','-ngrid','-halo');
+    [source_grid,baseEdges]  = source_axes_block.get_bin_nodes('-bin_centre','-3D');
     targ_grid_present = ones(size(dEnodes));
     targ_grid_present = nullify_edges(targ_grid_present,size(dEnodes));
     nodes_near = interp1(dEnodes,targ_grid_present,baseEdges,'linear',0);
@@ -23,11 +23,15 @@ if source_proj.do_3D_transformation_
     end
 else
     may_contribute_dE = [];
-    %targ_nodes = targ_axes_block.get_bin_nodes('-ngrid','-halo');
-    source_grid = source_axes_block.get_bin_nodes();
+    targ_nodes = targ_axes_block.get_bin_nodes('-ngrid','-halo');
+    source_grid = source_axes_block.get_bin_nodes('-bin_centre');
 end
-bin_range = targ_axes_block.img_range;
-bsize = source_axes_block.nbins_all_dims+1;
+% define unit signal on the edges of the target grid and zeros at "halo"
+% points surrounding the target grid
+szn = size(targ_nodes{1});
+targ_grid_present = ones(szn);
+targ_grid_present = nullify_edges(targ_grid_present,szn);
+
 % convert the coordinates of the bin centres of the reference grid into
 % the coordinate system of the target grid.
 conv_grid = source_proj.from_this_to_targ_coord(source_grid);
@@ -35,16 +39,17 @@ conv_grid = source_proj.from_this_to_targ_coord(source_grid);
 % find the presence of the reference grid centres within the target grid
 % cells. If reference grid is present its signal is higher then 0
 if source_proj.do_3D_transformation_
-
-    bin_inside = aProjectionBase.bin_inside(conv_grid,bsize(:,1:3),bin_range(:,1:3));
+    interp_ds = interpn(targ_nodes{1},targ_nodes{2},targ_nodes{3},targ_grid_present,...
+        conv_grid(1,:)',conv_grid(2,:)',conv_grid(3,:)', 'linear',0);
+    %targ_nodes = [targ_nodes{1};targ_nodes{2};targ_nodes{3}]; can to that
     % but:
     % Instead of combining target nodes, generate new without halo.
     % This may be more efficient as halo would include cells which do not
     % cotribute to final cut at all.
     targ_nodes = targ_axes_block.get_bin_nodes('-3D');
 else
-    bin_inside = aProjectionBase.bin_inside(conv_grid,bsize,bin_range);
-
+    interp_ds = interpn(targ_nodes{1},targ_nodes{2},targ_nodes{3},targ_nodes{4},targ_grid_present,...
+        conv_grid(1,:)',conv_grid(2,:)',conv_grid(3,:)',conv_grid(4,:)', 'linear',0);
     %targ_nodes = [targ_nodes{1};targ_nodes{2};targ_nodes{3};targ_nodes{4}];
     % see above -- better to generate new nodes.
     targ_nodes = targ_axes_block.get_bin_nodes(); % no halo
@@ -53,7 +58,7 @@ end
 % the case when source cells are larger and fully contain the cut cells
 targ_nodes = target_proj.from_this_to_targ_coord(targ_nodes);
 cell_dist = source_axes_block.bin_pixels(targ_nodes);
-may_contributeND = bin_inside(:) |cell_dist(:)>0;
+may_contributeND = interp_ds(:)>eps(single(1))|cell_dist(:)>0;
 
 
 function mat = nullify_edges(mat,sze)
