d3d044bd744af8f68832d708283ce81a95726189
abuts <Alex.Buts@stfc.ac.uk>
2024-07-10 16:10:00 +0100

    Re #1705 system test for replicate in parallel.

--- _test/test_gen_sqw_workflow/test_gen_sqw_accumulate_sqw_parpool.m
+++ _test/test_gen_sqw_workflow/test_gen_sqw_accumulate_sqw_parpool.m
@@ -47,7 +47,7 @@ classdef test_gen_sqw_accumulate_sqw_parpool <  ...
             %   test_multifit_horace_1_output.mat
             %
             % Reads previously created test data sets.
-            
+
             % constructor
             if ~exist('test_name','var')
                 test_name = mfilename('class');
@@ -57,7 +57,7 @@ classdef test_gen_sqw_accumulate_sqw_parpool <  ...
             if ispc() % TODO: this should be fixed
                 combine_algorithm  = 'mex_code';
             end
-            
+
             obj = obj@gen_sqw_common_config(-1,1,combine_algorithm,'parpool');
             obj = obj@gen_sqw_accumulate_sqw_tests_common(test_name,'parpool');
             obj.print_running_tests = true;
@@ -67,39 +67,86 @@ classdef test_gen_sqw_accumulate_sqw_parpool <  ...
             disp('Total time to run gen_sqw_accumulate_sqw_parpool code:')
             toc(obj.tc);
         end
-        
+        function test_replicate_in_parallel_and_serially(obj,varargin)
+            %-------------------------------------------------------------
+            if obj.skip_test
+                skipTest(fprintf('test_replicate_in_parallel_and_serially_%s is disabled',obj.test_pref));
+            end
+            if nargin> 1
+                % running in single test method mode.
+                obj.setUp();
+                clobS = onCleanup(@()obj.tearDown());
+            end
+            %-------------------------------------------------------------
+            clConf   = set_temporary_config_options('hor_config','delete_tmp',true);
+            clParConf = set_temporary_config_options('parallel_config','parallel_workers_number',2);            
+
+            wk_dir = obj.working_dir;
+
+            sqw_file_parallel=fullfile(wk_dir,'test_sqw_replicate_parallel.sqw');
+            sqw_file_serial  =fullfile(wk_dir,'test_sqw_replicate_serial.sqw');
+            clSqw=onCleanup(@()del_memmapfile_files(sqw_file_parallel,sqw_file_serial));
+
+            % --------------------------------------- Test accumulate_sqw
+            % ---------------------------------------
+
+            % Create some sqw files against which to compare the output of
+            % accumulate_sqw
+            % ---------------------------------------------------------------------------
+            [~,efix, emode, alatt, angdeg, u, v, psi, omega, dpsi, gl, gs]=unpack(obj);
+            spe = {obj.spe_file{1},obj.spe_file{1},obj.spe_file{4},obj.spe_file{4}};
+            sel  = 1:4;
+
+            [~,~,pix_range_ser]=gen_sqw (spe, '', sqw_file_parallel, efix(sel),...
+                emode, alatt, angdeg, u, v, psi(sel), omega(sel), dpsi(sel), gl(sel), gs(sel), ...
+                'replicate');
+
+            % Now do this serially ----------------------
+            parConfig = set_temporary_config_options('hpc_config','build_sqw_in_parallel',false);
+            [~,~,pix_range_par]=gen_sqw (spe, '', sqw_file_serial, efix(sel),...
+                emode, alatt, angdeg, u, v, psi(sel), omega(sel), dpsi(sel), gl(sel), gs(sel), ...
+                'replicate');
+
+            assertElementsAlmostEqual(pix_range_par,pix_range_ser);
+
+            sq_par = sqw(sqw_file_parallel);
+            sq_ser = sqw(sqw_file_serial);
+            assertEqualToTol(sq_par,sq_ser,'ignore_str',true,'tol',[1.e-8,1.e-8]);
+        end
+
+
         %------------------------------------------------------------------
         % Block of code to disable some tests for debugging Jenkins jobs
         function test_gen_sqw(obj,varargin)
-%             if is_jenkins && ispc
-%                 skipTest('Test disabled due to intermittent failure')
-%             end
+            %             if is_jenkins && ispc
+            %                 skipTest('Test disabled due to intermittent failure')
+            %             end
             test_gen_sqw@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
         end
         function test_accumulate_sqw14(obj,varargin)
-%             if is_jenkins && ispc
-%                 skipTest('Test disabled due to intermittent failure')
-%             end
+            %             if is_jenkins && ispc
+            %                 skipTest('Test disabled due to intermittent failure')
+            %             end
             test_accumulate_sqw14@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
         end
         function test_accumulate_and_combine1to4(obj,varargin)
-%             if is_jenkins && ispc
-%                 skipTest('Test disabled due to intermittent failure')
-%             end
+            %             if is_jenkins && ispc
+            %                 skipTest('Test disabled due to intermittent failure')
+            %             end
             test_accumulate_and_combine1to4@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
         end
         function test_accumulate_sqw1456(obj,varargin)
-%             if is_jenkins && ispc
-%                 skipTest('Test disabled due to intermittent failure')
-%             end
+            %             if is_jenkins && ispc
+            %                 skipTest('Test disabled due to intermittent failure')
+            %             end
             test_accumulate_sqw1456@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
         end
         function test_accumulate_sqw11456(obj,varargin)
-%             if is_jenkins && ispc
-%                 skipTest('Test disabled due to intermittent failure')
-%             end
+            %             if is_jenkins && ispc
+            %                 skipTest('Test disabled due to intermittent failure')
+            %             end
             test_accumulate_sqw11456@gen_sqw_accumulate_sqw_tests_common(obj,varargin{:});
         end
     end
-    
+
 end
