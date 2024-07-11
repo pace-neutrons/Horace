c6518a7b745da44788816caf8f5cb71eedeb3600
abuts <Alex.Buts@stfc.ac.uk>
2024-07-11 12:48:17 +0100

    Re #1700 replaced cerr error handling with Matlab exception

--- _LowLevelCode/cpp/mex_bin_plugin/bin_io_handler.cpp
+++ _LowLevelCode/cpp/mex_bin_plugin/bin_io_handler.cpp
@@ -1,96 +1,92 @@
 #include "bin_io_handler.hpp"
 
- void bin_io_handler::init(const fileParameters & fpar) {
+void bin_io_handler::init(const fileParameters& fpar) {
 
     this->filename = fpar.fileName;
     this->h_inout_sqw.open(this->filename.c_str(), std::ios::binary | std::ios::in | std::ios::out | std::ofstream::app);
     if (!this->h_inout_sqw.is_open()) {
         std::string err = "SQW_PIX_WRITER: Can not open target sqw file: " + fpar.fileName;
-        mexErrMsgTxt(err.c_str());
+        mexErrMsgIdAndTxt(this->MEX_ERR_ID.c_str(), err.c_str());
     }
 
-    this->last_pix_written   = 0;
+    this->last_pix_written = 0;
     this->pix_array_position = fpar.pix_start_pos;
-    this->nbin_position      = fpar.nbin_start_pos;
+    this->nbin_position = fpar.nbin_start_pos;
 }
 
- size_t bin_io_handler::get_file_size() {//in order to create bounds for seekp and seekg need size of file 
-	 std::streampos current_pos = this->h_inout_sqw.tellg();
-	 this->h_inout_sqw.seekg(0, std::ios::end);
-	 size_t size = this->h_inout_sqw.tellg();
-	 this->h_inout_sqw.seekg(current_pos);
-	 return size;
- }
- bool bin_io_handler::seek_within_bounds(std::streampos position) {
-	 size_t file_size = get_file_size();
-	 if (position < 0 || static_cast<size_t>(position) >= file_size) {
-		 std::cerr << "Error: Seek position " << position << " is out of bounds." << std::endl;
-		 this->h_inout_sqw.seekp(position);
-		 return false;
-	 }
-	 else {
+size_t bin_io_handler::get_file_size() {//in order to create bounds for seekp and seekg need size of file 
+    std::streampos current_pos = this->h_inout_sqw.tellg();
+    this->h_inout_sqw.seekg(0, std::ios::end);
+    size_t size = this->h_inout_sqw.tellg();
+    this->h_inout_sqw.seekg(current_pos);
+    return size;
+}
+bool bin_io_handler::seek_within_bounds(std::streampos position) {
+    size_t file_size = get_file_size();
+    if (position < 0 || static_cast<size_t>(position) >= file_size) {
+        std::stringstream buf;
+        buf << "Can not seek to the position: " << position << std::endl;
+        mexErrMsgIdAndTxt(this->MEX_ERR_ID.c_str(), buf.str().c_str());
+        return false;
+    }
+    else {
 
-		 this->h_inout_sqw.seekp(position);
+        this->h_inout_sqw.seekp(position);
 
-		 return true;
-	 }
- }
- /* write pixel metadata containing information about pixel width 
-    and number of pixels stored in pixels array
-    Inputs:
-    num_pixels -- number of pixes to be written in appropriate position in the file
-    */
- void bin_io_handler::write_pix_info(const size_t& num_pixels) {
-     size_t pix_info_position = this->pix_array_position - this->pixel_info_size;
-     uint32_t pix_width = uint32_t(this->pixel_width) ;
-     //this->h_inout_sqw.seekp(pix_info_position);
-	 //if (seek_within_bounds(pix_info_position)) {
-		 
-		 this->h_inout_sqw.write(reinterpret_cast<const char*>(&pix_width), sizeof(pix_width));
-		 this->h_inout_sqw.write(reinterpret_cast<const char*>(&num_pixels), sizeof(num_pixels));
-	// }
- }
+        return true;
+    }
+}
+/* write pixel metadata containing information about pixel width
+   and number of pixels stored in pixels array
+   Inputs:
+   num_pixels -- number of pixes to be written in appropriate position in the file
+   */
+void bin_io_handler::write_pix_info(const size_t& num_pixels) {
+    size_t pix_info_position = this->pix_array_position - this->pixel_info_size;
+    uint32_t pix_width = uint32_t(this->pixel_width);
 
- void bin_io_handler::read_pix_info(size_t& num_pixels, uint32_t& pix_width) {
-     size_t pix_info_position = this->pix_array_position - this->pixel_info_size;
-	 if (seek_within_bounds(pix_info_position)) {
-		 this->h_inout_sqw.seekg(pix_info_position);  // Changed to seekg for reading
-		 this->h_inout_sqw.read(reinterpret_cast<char*>(&pix_width), sizeof(pix_width));
-		 this->h_inout_sqw.read(reinterpret_cast<char*>(&num_pixels), sizeof(num_pixels));
-	 }
- }
+    this->h_inout_sqw.seekp(pix_info_position);
+    this->h_inout_sqw.write(reinterpret_cast<const char*>(&pix_width), sizeof(pix_width));
+    this->h_inout_sqw.write(reinterpret_cast<const char*>(&num_pixels), sizeof(num_pixels));
+    if (this->h_inout_sqw.fail()) {
+        std::stringstream buf;
+        buf << "Error writing pixel metadata at position: " << pix_info_position << std::endl;
+        mexErrMsgIdAndTxt(this->MEX_ERR_ID.c_str(), buf.str().c_str());
+
+    }
+
+
+}
+
+void bin_io_handler::read_pix_info(size_t& num_pixels, uint32_t& pix_width) {
+    size_t pix_info_position = this->pix_array_position - this->pixel_info_size;
+    if (seek_within_bounds(pix_info_position)) {
+        this->h_inout_sqw.seekg(pix_info_position);  // Changed to seekg for reading
+        this->h_inout_sqw.read(reinterpret_cast<char*>(&pix_width), sizeof(pix_width));
+        this->h_inout_sqw.read(reinterpret_cast<char*>(&num_pixels), sizeof(num_pixels));
+    }
+}
 
 
 /* Write chunk on pixels stored in write buffer */
-void bin_io_handler::write_pixels(const char* buffer, size_t num_pixels) { 
+void bin_io_handler::write_pixels(const char* buffer, size_t num_pixels) {
     size_t pix_pos = pix_array_position + last_pix_written * this->pixel_width;
-	//this->h_inout_sqw.seekp(pix_pos);
-	//seek_within_bounds(pix_pos);
-	if (seek_within_bounds(pix_pos)) {
-		size_t length = num_pixels * this->pixel_width;
-		this->h_inout_sqw.write(buffer, length);
-		this->last_pix_written += length;
-	}
-	else {
-	size_t length = num_pixels * this->pixel_width;
-	this->h_inout_sqw.write(buffer, length);
-	this->last_pix_written += length;
+    //this->h_inout_sqw.seekp(pix_pos);
+    //seek_within_bounds(pix_pos);
+    if (seek_within_bounds(pix_pos)) {
+        size_t length = num_pixels * this->pixel_width;
+        this->h_inout_sqw.write(buffer, length);
+        this->last_pix_written += length;
+    }
+    else {
+        size_t length = num_pixels * this->pixel_width;
+        this->h_inout_sqw.write(buffer, length);
+        this->last_pix_written += length;
     }
 
 
 }
 
-void set_properties(const  std::string& fileName, const size_t num_pixels(), const size_t pix_start_pos(), const size_t pix_width()) {
-	
-	fileParameters input;
-	
-		input.fileName = "/_test/file_for_metadata.bin";
-		input.pix_start_pos = 98;
-		input.pixel_width = 36;
-		
-	
-	//bin_io_handler(input);
-}
 //
 
 
--- _LowLevelCode/cpp/mex_bin_plugin/bin_io_handler.hpp
+++ _LowLevelCode/cpp/mex_bin_plugin/bin_io_handler.hpp
@@ -25,35 +25,36 @@
 /* Class responsible for writing block of pixels on HDD */
 class bin_io_handler {
 public:
-	bin_io_handler() :
-		last_pix_written(0), pix_array_position(12),pixel_width(36),nbin_position(0) {}
-	
-	//bin_io_handler() = default;
+    bin_io_handler() :
+        last_pix_written(0), pix_array_position(12),pixel_width(36),nbin_position(0) {}
+    
+    //bin_io_handler() = default;
 
     void init(const fileParameters &fpar);
     void write_pixels(const char * const buffer, const size_t n_pix_to_write);
-	void write_pix_info(const size_t& num_pixels);
-	
+    void write_pix_info(const size_t& num_pixels);
+    
     void read_pix_info(size_t& num_pixels, uint32_t& pix_width);
-	size_t get_file_size();
-	bool seek_within_bounds(std::streampos position);
+    size_t get_file_size();
+    bool seek_within_bounds(std::streampos position);
     
     ~bin_io_handler();
 
-	// size of the fields containing pixel information, e.g. pixel_width (in bytes)
-	// and number of pixels written on disk
-	const size_t pixel_info_size{ 12 };
+    // size of the fields containing pixel information, e.g. pixel_width (in bytes)
+    // and number of pixels written on disk
+    const size_t pixel_info_size{ 12 };
+    const std::string MEX_ERR_ID{ "HORACE:bin_io_handler:invalid_argument" };
 private:
     //exchange_buffer &Buff;
-	
+    
     std::string filename;
-	std::fstream h_inout_sqw;
-	size_t last_pix_written;
-	size_t pix_array_position;
-	size_t pixel_width;
-	size_t nbin_position;
-	
-	
+    std::fstream h_inout_sqw;
+    size_t last_pix_written;
+    size_t pix_array_position;
+    size_t pixel_width;
+    size_t nbin_position;
+    
+    
     std::vector<float> pix_buffer{};
     
 
