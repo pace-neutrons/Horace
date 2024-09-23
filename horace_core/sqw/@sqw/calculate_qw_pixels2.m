c4d3465b76d6aa84968aa047bed8af45801c417c
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-09-23 15:03:53 +0100

    Fix issues with calculate_qw_pixels2 for recent horace

--- horace_core/sqw/@sqw/calculate_qw_pixels2.m
+++ horace_core/sqw/@sqw/calculate_qw_pixels2.m
@@ -36,8 +36,11 @@ irun = idx(1,:)';
 idet = idx(2,:)';
 ien = idx(3,:)';
 
+remap = containers.Map(unique(irun), 1:numel(win.header));
+irun = arrayfun(@(x) remap(x), irun);
+
 if ~iscell(win.header)
-    header = {win.header};
+    header = num2cell(win.header)';
 else
     header = win.header;
 end
@@ -56,15 +59,17 @@ eps_lo = cellfun(@(x) 0.5*(x.en(1)+x.en(2)), header);
 eps_hi = cellfun(@(x) 0.5*(x.en(end-1)+x.en(end)), header);
 n_en = cellfun(@(x) numel(x.en)-1, header);
 
+
 [~, ~, spec_to_rlu] = cellfun(...
     @(h) calc_proj_matrix(h.alatt, h.angdeg, ...
                           h.cu, h.cv, h.psi, ...
                           h.omega, h.dpsi, h.gl, h.gs), header, 'UniformOutput', false);
+
 % Join in 3rd rank leading to n x n x nhead
 spec_to_rlu = cat(3, spec_to_rlu{:});
 
 eps_diff = (eps_lo(irun) .* (n_en(irun) - ien) + eps_hi(irun) .* (ien - 1)) ./ (n_en(irun) - 1);
-[~, detdcn] = spec_coords_to_det(win.detpar);
+detdcn = spec_coords_to_det(win.detpar);
 kfix = sqrt(efix/k_to_e);
 
 switch emode
@@ -79,13 +84,15 @@ switch emode
     kf = ki;
 end
 
-qw = cell(1, 4);
-qw(1:3) = calculate_q (ki, kf, detdcn(:, idet), spec_to_rlu(:, :, irun));
+qw = cell(4, 1);
+qw(1:3) = calculate_q(ki, kf, detdcn(:, idet), spec_to_rlu(:, :, irun));
 qw{4} = eps_diff;
+qw = cat(2, qw{:})';
+qw = win.data.proj.transform_hkl_to_pix(qw);
 
 end
 
-function [d_mat, detdcn] = spec_coords_to_det (detpar)
+function detdcn = spec_coords_to_det (detpar)
 % Matrix to convert coordinates in spectrometer (or laboratory) frame into detector frame
 %
 %   >> d_mat = spec_coords_to_det (detpar)
@@ -107,50 +114,17 @@ function [d_mat, detdcn] = spec_coords_to_det (detpar)
 
 %% TODO: Investigate use of transform_pix_to_hkl
 
-ndet = numel(detpar.x2);
+detdcn = {};
+for i=1:detpar.n_objects
+    ndet = numel(detpar{i}.x2);
 
-cp = reshape(cosd(detpar.phi), [1, 1, ndet]);
-sp = reshape(sind(detpar.phi), [1, 1, ndet]);
-cb = reshape(cosd(detpar.azim), [1, 1, ndet]);
-sb = reshape(sind(detpar.azim), [1, 1, ndet]);
+    cp = cosd(detpar{i}.phi);
+    sp = sind(detpar{i}.phi);
+    cb = cosd(detpar{i}.azim);
+    sb = sind(detpar{i}.azim);
 
-d_mat = [cp, cb.*sp, sb.*sp;...
-         -sp, cb.*cp, sb.*cp;...
-         zeros(1, 1, ndet), -sb, cb];
-
-detdcn = [cp; cb.*sp; sb.*sp];
-
+    detdcn{i} = [cp, cb.*sp, sb.*sp];
 end
 
-function q = calculate_q (ki, kf, detdcn, spec_to_rlu)
-% Calculate qh, qk, ql for direct geometry instrument
-%
-%   >> q = calculate_q (ki, kf, detdcn, spec_to_rlu)
-%
-% Input:
-% ------
-%   ki          Incident wavevectors for each point [Column vector]
-%   kf          Final wavevectors for each point    [Column vector]
-%   detdcn      Array of unit vectors in the direction of the detectors
-%               Size is [3, npnt]
-%   spec_to_rlu Matrix to convert momentum in spectrometer coordinates to
-%               components in r.l.u.:
-%                   v_rlu = spec_to_rlu * v_spec
-%               Size is [3, 3, npnt]
-%
-% Output:
-% -------
-%   q           Components of momentum (in rlu) for each point
-%               [Cell array of column vectors]
-%               i.e. q{1}=qh, q{2}=qk, q{3}=ql
-
-% Use in-place working to save memory (note: bsxfun not needed from 2016b an onwards)
-qtmp = -kf' .* detdcn;
-qtmp(1, :) = ki' + qtmp(1, :);            % qspec proper now
-qtmp = mtimesx_horace (spec_to_rlu, reshape(qtmp, [3, 1, numel(ki)]));
-qtmp = squeeze(qtmp);
-
-% Package output
-q = num2cell(qtmp', 1);
-
+detdcn = cat(1, detdcn{:})';
 end
