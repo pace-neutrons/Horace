d134296531402418154cd7075390e471cae7c6d4
abuts <Alex.Buts@stfc.ac.uk>
2024-09-27 15:36:47 +0100

    Re #1753 disable error for failing cpp_communicator in check_horace_mex.m

--- horace_core/admin/check_horace_mex.m
+++ horace_core/admin/check_horace_mex.m
@@ -1,4 +1,4 @@
-function [rez, n_errors, can_use_mex_4_combine] = check_horace_mex()
+function [rez, n_errors, can_use_mex_4_combine,can_use_MPI] = check_horace_mex()
 % function checks if horace mex files are compiled correctly and returns
 % their version string.
 %
@@ -54,6 +54,7 @@ rez = cell(numel(functions_name_list), 1);
 
 n_errors=0;
 can_use_mex_4_combine = true;
+can_use_MPI = true;
 for i=1:numel(functions_name_list)
     try
         rez{i}=[functions_name_list{i},functions_handle_list{i}()];
@@ -63,6 +64,8 @@ for i=1:numel(functions_name_list)
         if contains(functions_name_list{i},'combine_sqw') % provide special treatment
             % for combine_sqw function
             can_use_mex_4_combine=false;
+        elseif contains(functions_name_list{i},'cpp_communicator')
+            can_use_MPI = false;
         else
             n_errors=n_errors+1;
         end
