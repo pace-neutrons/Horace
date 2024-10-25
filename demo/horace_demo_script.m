4fc5562e1259b36201525ca0a2fc240c4979f19e
abuts <Alex.Buts@stfc.ac.uk>
2024-10-25 11:33:39 +0100

    Re #1762 modifying spinW model to obtain reference point, which allows fit convergence. Add test script which highlight difference between analytical and spinW models.

--- demo/horace_demo_script.m
+++ demo/horace_demo_script.m
@@ -20,6 +20,7 @@ par_file=fullfile(indir,'4to1_124.par'); % detector parameters (positions) file.
 %                                       % stored in nxspe files.
 sqw_file=fullfile(indir,'fe_demo.sqw'); % let's place output sqw file into
 %                                       % init directory.
+% after generation, sqw file becomes the source of further analysis
 data_source =sqw_file;
 
 
@@ -130,24 +131,25 @@ plot(wdiff);
 %==================================
 
 cc2a=cut_sqw(sqw_file,proj,[-3,0.05,3],[-3,0.05,3],[-0.1,0.1],[180,220]);
+plot(cc2a)
+keep_figure;
 
 % Simulate a model for S(Q,w):
 % (This is an example where the user can provide the spectral weight as a
 % function of (vector) Q and energy)
 w_sqw=sqw_eval(cc2a,@demo_FM_spinwaves,[250 0 2.4 10 5]);
-plot(w_sqw)
-keep_figure;
 %Looks vaguely like the data.
 
+% Simulation parameters are 300 0 2 10 2 and we try to recover it
 %Do a fit. We'll fit parameters 1, 3 and 5 in our model, but leave 2 and 4
 %fixed (2nd vector is list of free parameters). We will
 %also have a backgound function (specified separately). The (optional) list
 %argument gives a verbose output during the fitting process
-J = 250;     % Exchange interaction in meV
+J = 300;     % Exchange interaction in meV
 D = 0;       % Single-ion anisotropy in meV
-gam = 0.5;   % Intrinsic linewidth in meV (inversely proportional to excitation lifetime)
+gam  = 2;   % Intrinsic linewidth in meV (inversely proportional to excitation lifetime)
 temp = 10;  % Sample measurement temperature in Kelvin
-amp = 5;  % Magnitude of the intensity of the excitation (arbitrary units)
+amp  = 2;  % Magnitude of the intensity of the excitation (arbitrary units)
 
 kk = multifit_sqw (cc2a);
 kk = kk.set_fun (@demo_FM_spinwaves);
@@ -172,17 +174,17 @@ try
     fefm.genlattice('lat_const',[2.87 2.87 2.87],'angled',[90 90 90],'sym','I m -3 m');
     fefm.addatom('r',[0 0 0],'S',2.5,'label', 'MFe3');
     fefm.gencoupling();
-    fefm.addmatrix('label','J','value',1);
+    fefm.addmatrix('label','J','value',-1);
     fefm.addcoupling('mat','J','bond',1);
 
     fefm.addmatrix('value',diag([0 0 -1]),'label','D');
     fefm.addaniso('D');
     fefm.genmagstr('mode','direct','S',[0,0; 0,0;1,1]);
 
-    cpars = {'mat', {'J', 'D(3,3)'}, 'hermit', false, 'optmem', 1, 'useFast', true, 'resfun', 'sho', 'formfact', true};
+    cpars = {'mat', {'J', 'D(3,3)'}, 'hermit', false, 'optmem', 1, 'useFast', true, 'resfun', 'sho', 'formfact', false};
 
     kk = multifit_sqw (cc2a);
-    kk = kk.set_fun (@fefm.horace_sqw, {[300 0 2 10 2], cpars{:}});
+    kk = kk.set_fun (@fefm.horace_sqw, {[50 0 2 10 0.1], cpars{:}});
 
     %kk = kk.set_pin ({[250 0 2.4 10 5],fefm}); %input parameters
     kk = kk.set_free ([1 0 1 0 1]); %fitting parameters 1,3 and 5
