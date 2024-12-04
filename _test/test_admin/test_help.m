09c1953694b28e302f0500acaaea15d4539cab8c
abuts <Alex.Buts@stfc.ac.uk>
2024-12-04 13:38:20 +0000

    Re #1780 fixing unit test for lower Matlab versions

--- _test/test_admin/test_help.m
+++ _test/test_admin/test_help.m
@@ -13,9 +13,14 @@ classdef test_help< TestCase
 
         end
         function test_help_works(~)
+            if matlab_version_num() < 9.05
+                ref_str = 'Reference page in Doc Center';
+            else
+                ref_str = 'Documentation for test_help';
+            end
             ref_str = sprintf('%s\n%s\n%s',...
                 'Verify if help which uses docify works regardless of Matlab version',...
-                'Documentation for test_help',...
+                ref_str,...
                 'doc test_help');
             actual_help = disp2str(help('test_help'));
 
