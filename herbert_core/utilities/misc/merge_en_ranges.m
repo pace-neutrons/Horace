a62835f0028ac9ae9b0d87c36e652b16f35e38af
abuts <Alex.Buts@stfc.ac.uk>
2025-02-12 16:29:20 +0000

    Re #1152 Algorithms of selecting energy transfers for detectors. Incomplete

---
+++ herbert_core/utilities/misc/merge_en_ranges.m
@@ -0,0 +1,15 @@
+function en = merge_en_ranges(en,unique_en_run_idx,ien,irun,unique_inst_run_idx)
+%MERGE_EN_RANGES Identify maximal enery transfer range, corresponding to
+% each bunch of unique detectors. Return cellarray of possible enery transfers for each
+% bunch of runs with unique detectors.
+%
+% Inputs:
+% en                  -- cellarray of unique energy transfer ranges
+% unique_en_run_idx   -- cellarray (size of en) of run indices with 
+%                        each cell contains array of run indices, contribuing
+%                        into correspondent en cell
+% ien                 -- array of size irun, containing numbers of en cells
+%                        which should be included in reslut
+% 
+
+end
--- horace_core/sqw/@sqw/calculate_qw_pixels2.m
+++ horace_core/sqw/@sqw/calculate_qw_pixels2.m
@@ -12,7 +12,7 @@ function qw=calculate_qw_pixels2(win,coord_in_rlu,return_matrix)
 % Input:
 % ------
 %  win          -- Input sqw object
-% Optional: 
+% Optional:
 %  coord_in_rlu -- default true. Returns pixel coordinates in reciprocal
 %                  lattice units (projection onto rotated hkl coordinate
 %                  system). If false, return pixel coordinates in Crystal
@@ -35,13 +35,13 @@ function qw=calculate_qw_pixels2(win,coord_in_rlu,return_matrix)
 
 if ~isscalar(win)
     error('HORACE:calculate_qw_pixels2:invalid_argument', ...
-          'Only a single sqw object is valid - cannot take an array of sqw objects')
+        'Only a single sqw object is valid - cannot take an array of sqw objects')
 end
 if nargin<2
     coord_in_rlu = true;
     return_matrix= false;
 elseif nargin<3
-    return_matrix= false;    
+    return_matrix= false;
 end
 
 % as column vectors
@@ -50,8 +50,8 @@ irun = idx(1,:)';
 idet = idx(2,:)';
 ien  = idx(3,:)';
 
-% if we want possible change in alatt during experiment, go to 
-% 
+% if we want possible change in alatt during experiment, go to
+%
 alatt = win.data.alatt;
 angdeg = win.data.angdeg;
 
@@ -60,47 +60,56 @@ ix_exper = experiment.expdata;
 remapper = win.experiment_info.runid_map;
 %
 % convert run_id in pixels into number of IX_experiment, corresponding to
-% this pixel. Now irun represent number of IX_experiment in Experiment 
+% this pixel. Now irun represent number of IX_experiment in Experiment
 % class or number of transformation matrix in list of all transformations
 % (spec_to_rlu)
 irun     = remapper.get_values_for_keys(irun,true); % retrieve experiment numbers which corresponds to pix run_id;
 
-if coord_in_rlu % coordinates in hkl
+if coord_in_rlu % coordinates in rlu, lattice is aligned with beam in
+    % a direction specified in IX_experiment
     n_matrix = 3;
 else % coordinates in Crystan Cartesian
-    n_matrix = 1;    
+    n_matrix = 1;
 end
+% energies:
+% incident/analysis
+efix = experiment.get_efix();
+% energy transfers:
+[en,unique_en_run_idx]   = experiment.get_en_transfer(true,true);
+% unique detectors. Wider then unique instrumens, so use detectors
+all_det = experiment.detector_arrays;
+[unique_det, unique_inst_run_idx] = all_det.get_unique_objects_and_indices(true);
+n_unique_det = numel(unique_inst_run_idx);
+
+% identify maximal enery transfer range, corresponding to each bunch of unique
+% detectors
+en = merge_en_ranges(en,unique_en_run_idx,ien,irun,unique_inst_run_idx); % return 
+% cellarray of possible enery transfers for each bunch of runs with unique detectors.
+
+det_dir = cell(1,n_unique_det);
+for i=1:n_unique_det
+    selected = irun == unique_inst_run_idx{i};
+    idet_4_run = idet(selected);
+    det_dir(i) = unique_det{i}.calc_detdcn(idet_4_run);
+end
+
+
 % obtain transformation matrices to convert each run's dector positions
 % into common coordinate system related to crystal (hkl or crystal
 % Cartesian)
 spec_to_rlu  = arrayfun(...
     @(ex) calc_proj_matrix(ex,alatt, angdeg,n_matrix), ix_exper, 'UniformOutput', false);
 
-% energies
-efix = experiment.get_efix();
-[en,n_unique_en_idx]   = experiment.get_en_transfer(true);
-% 
-all_det = experiment.detector_arrays;
-[unique_det, run_idx] = all_det.get_unique_objects_and_indices(true);
-
-n_unique_det = numel(run_idx);
-det_dir = cell(1,n_unique_det);
-for i=1:n_unique_det 
-    selected = irun == run_idx{i};
-    idet_4_run = idet(selected);
-    det_dir(i) = unique_det{i}.calc_detdcn(idet_4_run);
-end
 
 
-
-% 
+%
 % % Join in 3rd rank leading to n x n x n_experiments
 % spec_to_rlu = cat(3, spec_to_rlu{:});
-% 
+%
 % eps_diff = (eps_lo(irun) .* (n_en(irun) - ien) + eps_hi(irun) .* (ien - 1)) ./ (n_en(irun) - 1);
 % detdcn = spec_coords_to_det(win.detpar);
 % kfix = sqrt(efix/k_to_e);
-% 
+%
 % switch emode
 %   case 1
 %     ki = kfix(irun);
@@ -112,7 +121,7 @@ end
 %     ki = kfix(irun);
 %     kf = ki;
 % end
-% 
+%
 % qw = cell(4, 1);
 % qw(1:3) = calculate_q(ki, kf, detdcn(:, idet), spec_to_rlu(:, :, irun));
 % qw{4} = eps_diff;
