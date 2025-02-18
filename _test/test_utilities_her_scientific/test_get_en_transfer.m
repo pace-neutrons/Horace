b6d09c0f5e3d2da9a507f650911d73f032935934
abuts <Alex.Buts@stfc.ac.uk>
2025-02-18 20:25:21 +0000

    Re #1152 simplified get_en_transfer to use the same code as get_efix

--- _test/test_utilities_her_scientific/test_get_en_transfer.m
+++ _test/test_utilities_her_scientific/test_get_en_transfer.m
@@ -30,8 +30,8 @@ classdef test_get_en_transfer< TestCase
 
             [uen,lidx] = get_en_transfer(id,true,true);
 
-            assertEqual(uen{1},0.5*(en1(1:end-1)+en1(2:end)));
-            assertEqual(uen{2},0.5*(en2(1:end-1)+en2(2:end)));            
+            assertEqual(uen,{0.5*(en1(1:end-1)+en1(2:end)), ...
+                0.5*(en2(1:end-1)+en2(2:end))});
             assertEqual(lidx,{[1:5,9,10],[6,7,8]});
         end
         
@@ -51,7 +51,7 @@ classdef test_get_en_transfer< TestCase
 
             assertEqual(uen{1},0.5*(en1(1:end-1)+en1(2:end)));
             assertEqual(uen{2},0.5*(en2(1:end-1)+en2(2:end)));            
-            assertEqual(gidx,[1,6]);
+            assertEqual(gidx,{1,6});
         end
         
         function test_single_unique_en_lidx(~)
@@ -63,7 +63,7 @@ classdef test_get_en_transfer< TestCase
 
             [uen,lidx] = get_en_transfer(id,true,true);
 
-            assertEqual(uen{1},0.5*(en(1:end-1)+en(2:end)));
+            assertEqual(uen,{0.5*(en(1:end-1)+en(2:end))});
             assertEqual(lidx,{1:5});
         end
         
@@ -76,8 +76,8 @@ classdef test_get_en_transfer< TestCase
 
             [uen,gidx] = get_en_transfer(id,true,false);
 
-            assertEqual(uen{1},0.5*(en(1:end-1)+en(2:end)));
-            assertEqual(gidx,1);
+            assertEqual(uen,{0.5*(en(1:end-1)+en(2:end))});
+            assertEqual(gidx,{1});
         end
     end
 end
--- herbert_core/utilities/scientific/get_en_transfer.m
+++ herbert_core/utilities/scientific/get_en_transfer.m
@@ -1,4 +1,4 @@
-function [en_tot,unique_idx]  = get_en_transfer(exper,bin_centre,get_lidx)
+function [en_transf,unique_idx]  = get_en_transfer(exper,bin_centre,get_lidx)
 %GET_EN_TRANSFER Return cellarray of energy transfer arrays, contributed
 % into the array of IX_experiments.
 %
@@ -13,11 +13,14 @@ function [en_tot,unique_idx]  = get_en_transfer(exper,bin_centre,get_lidx)
 %               these bins rather than their bin boundaries of these
 %               bunches
 % get_lidx  --  If true, return cellarray of local indices,
-%               if false, arry of unique indices.
+%               if false, array of unique indices.
 %
 % Returns:
-% en       -- cellarray of energy transfer arrays, present in experiment
-%             containing only unique energy transfer arrays.
+% en       -- cellarray of energy transfer arrays, one per each element of
+%             input exper array.
+%             If two arguments are requested, this cellarray contains
+%             unique energy transfer values only
+%
 %
 % unique_idx  depending on get_lidx false or true
 %       -- either (get_lidx == false)
@@ -29,37 +32,21 @@ function [en_tot,unique_idx]  = get_en_transfer(exper,bin_centre,get_lidx)
 %             single unique bunch of energy transfers
 
 n_runs = numel(exper);
-en_tot = cell(1,n_runs);
-normas = zeros(1,n_runs);
+en_transf = cell(1,n_runs);
 for i=1:n_runs
-    en_tot{i} = exper(i).get_en(bin_centre);
-    normas(i) = norm(en_tot{i}); % kind of simplified hash for energy transfer array. 
-    % May think to replace to build_hash for reliability
+    en_transf{i} = exper(i).get_en(bin_centre);
 end
-[~,idx] = sort(normas);
-en_sorted = en_tot(idx);
-unique_idx = ones(1,n_runs);
-n_unique = 1;
-for i=2:n_runs
-    if isequal(en_sorted{i},en_sorted{i-1})
-        unique_idx(i) = n_unique;
-        continue;
-    end
-    n_unique = n_unique+1;
-    unique_idx(i) = n_unique;
-end
-ridx          = 1:n_runs;
-[~,ridx]      = sort(ridx(idx));
-unique_idx    = unique_idx(ridx);
-[un_idx,ia,ic]= unique(unique_idx);
-en_tot = en_tot(ia);
-if get_lidx
-    n_unique = numel(un_idx);
-    lidx = 1:n_runs;
-    unique_idx = cell(1,n_unique);
+if nargout>1
+    Diff =[1.e-8,1.e-8];
+    unique_idx = calc_eq_indices(en_transf,Diff);
+    n_unique = numel(unique_idx);
+    ent_selected = cell(1,n_unique);
     for i=1:n_unique
-        unique_idx{i} = lidx(ic==i);
+        first_unique = unique_idx{i}(1);
+        if ~get_lidx
+            unique_idx{i} = first_unique;
+        end
+        ent_selected{i} = en_transf{first_unique};
     end
-else
-    unique_idx = ia(:)';
+    en_transf = ent_selected;
 end
