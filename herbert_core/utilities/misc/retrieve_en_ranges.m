e5c116443efc0fca35edd87953a0d0fa9d37c0f0
abuts <Alex.Buts@stfc.ac.uk>
2025-02-27 15:47:02 +0000

    Re #1152 Formally working but in fact something wrong

--- herbert_core/utilities/misc/retrieve_en_ranges.m
+++ herbert_core/utilities/misc/retrieve_en_ranges.m
@@ -1,4 +1,4 @@
-function [efix_out,en_tr_out,en_tr_idx_minmax] = retrieve_en_ranges(efix_info,en_tr_info,udet_info,run_id,en_id)
+function [efix_out,en_tr_out,en_tr_idx] = retrieve_en_ranges(efix_info,en_tr_info,udet_info,run_id,en_id)
 %RETRIEVE_EN_RANGES Identify maximal enery transfer range, corresponding to
 % each bunch of unique detectors. Return cellarray of possible enery transfers for each
 % bunch of runs with unique detectors.
@@ -6,28 +6,29 @@ function [efix_out,en_tr_out,en_tr_idx_minmax] = retrieve_en_ranges(efix_info,en
 % Inputs:
 % efix_info            -- compact_array of unique fixed energy values (arrays
 %                         for indirect instrument)
-% ent                  -- compact_array of unique energy transfer arrays
+% en_tr_info           -- compact_array of unique energy transfer arrays
 % udet_info            -- compact array of detector arrays contributed into
 %                         experiment
 %
+% run_id              -- array of runidices contributing into result
 % ien                 -- array of size run_id, containing numbers of en cells
 %                        which should be included in reslut.
-% run_id              -- array of runidices contributing into
+
 %
 % Returns:
-% efix_out            -- compact_array of energy transfers, contributed
-%                        into
-%                        each unique instrument
-% en_tr_out          -- cellarray containing arrays of energy indices,
+% efix_out            -- cellarray of compact_arrays of incident energies
+%                        (analyzed energies for indorect),
+%                        contributed into each unique instrument.
+% en_tr_out           -- cellarray  of compact_array of energy transfers,
 %                        each array contributing to a unique run.
-% en_tr_idx_minmax   -- cellarray of minmax values of energy transfer
-%                        indices contributed into runs
+% en_tr_idx        --    cellarray of arrys of nergy transfer indices
+%                        contributed into runs
 
 n_unique_instruments = udet_info.n_unique;
 
 en_tr_out = cell(1,n_unique_instruments);
 efix_out  = cell(1,n_unique_instruments);
-en_tr_idx_minmax = cell(1,n_unique_instruments);
+en_tr_idx = cell(1,n_unique_instruments);
 
 %
 % select only energy transfers which corresponds to indices specified as
@@ -36,21 +37,21 @@ en_tr_idx_minmax = cell(1,n_unique_instruments);
 % Reorder input arrays according to the runs performed at for every unique
 % instrument
 for i=1:n_unique_instruments
-    inst_runs     = udet_info.nunq_idx{i};
+    inst_runs     = udet_info.nonunq_idx{i};
     efix_out{i}   = efix_info.get_subobj(inst_runs);
-    en_tr         = en_tr_info.get_subobj(inst_runs);    
-    % found energy indices, which may contribute into this run
-    contributed_runs = ismember(run_id,en_tr.nonunq_idx()); 
-    idx_ent_present   = unique(en_id(contributed_runs));
-    min_max_etr_idx = cell(1,en_tr.n_unique);
+    en_tr         = en_tr_info.get_subobj(inst_runs);
+    % found energy indices, which may contribute into run with this
+    % insrument
+    contributed_runs = ismember(run_id,en_tr.nonunq_idx{i});
+    idx_ent_present  = unique(en_id(contributed_runs));
+    etr_idx          = cell(1,en_tr.n_unique);
     for j = 1:en_tr.n_unique
-        ent_i        = en_tr_info.uniq_val{j};        % unique energy transfer values
-        idx_valid    = idx_ent_present<=numel(ent_i); % en transfer indices may contribute to this unique values        
+        ent_i        = en_tr.unique_val{j};           % unique energy transfer values
+        idx_valid    = idx_ent_present<=numel(ent_i); % en transfer indices may contribute to this unique values
         idx_ent_j    = idx_ent_present(idx_valid);    % indices which may contribute into run
-        min_max_etr_idx{j} = min_max(idx_ent_j(:)');
-        en_tr_info.uniq_val{j} = ent_i(idx_ent_j);
+        etr_idx{j}   = idx_ent_j(:)';
+        en_tr.unique_val{j}  = ent_i(idx_ent_j);
     end
-    en_tr_idx_minmax{i} = min_max_etr_idx;
+    en_tr_idx{i} = etr_idx;
     en_tr_out{i} = en_tr;
 end
-
