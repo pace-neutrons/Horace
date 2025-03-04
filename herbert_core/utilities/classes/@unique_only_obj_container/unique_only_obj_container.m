15e5f44d0d12978dceb027b64d4cbaf029f79582
abuts <Alex.Buts@stfc.ac.uk>
2025-03-04 16:33:32 +0000

    Re #1152 bug (typo) in unique_only_obj_container.m

--- herbert_core/utilities/classes/@unique_only_obj_container/unique_only_obj_container.m
+++ herbert_core/utilities/classes/@unique_only_obj_container/unique_only_obj_container.m
@@ -264,7 +264,7 @@ classdef unique_only_obj_container < ObjContainersBase
                 nuidx = varargin{1};
                 self.check_if_range_allowed(nuidx);
                 uidx = self.lidx_(nuidx);
-                if isscalar(numel(uidx))
+                if isscalar(uidx)
                     uo = self.unique_objects_{uidx};
                 else % this makes {a:b}  behave like (a:b).
                     % TODO: May be should be modified and extended.
