c9709eed5cffc208b6338d988ed15b820d9d302d
abuts <Alex.Buts@stfc.ac.uk>
2025-03-06 14:52:55 +0000

    Re #1776 extract check_combo_arg code into separate function

--- horace_core/sqw/coord_transform/@kf_sphere_proj/kf_sphere_proj.m
+++ horace_core/sqw/coord_transform/@kf_sphere_proj/kf_sphere_proj.m
@@ -226,32 +226,15 @@ classdef kf_sphere_proj<sphere_proj
             %
             %   >> obj = check_combo_arg(w)
             %
-            % Throws HORACE:CurveProjBase:invalid_argument with the message
-            % suggesting the reason for failure if the inputs are incorrect
-            % w.r.t. each other.
+            % Throws 'HORACE:CurveProjBase:invalid_argument' or
+            % 'HORACE:kf_sphere_proj:not_implemented' depending on the
+            % issue containing the message suggesting the reason for
+            % failure if the inputs are inconsistent with each other.
             %
             % Normalizes input vectors to unity and constructs the
             % transformation to new coordinate system when operation is
             % successful
-            obj = check_combo_arg@sphere_proj(obj);
-            if any(abs(obj.offset_)>eps('double'))
-                error('HORACE:kf_sphere_proj:not_implemented',[ ...
-                    'non-zero offset is not implemented for this type of projection.\n' ...
-                    'There are doubts that it should be implemented at all'])
-            end
-            if obj.emode_ == 1
-                obj.ki_mod_ = sqrt(obj.Ei_/neutron_constants('c_k_to_emev')); % incident
-                obj.ki_ = [obj.ki_mod_;0;0];
-            elseif obj.emode_==2
-                error('HORACE:kf_sphere_proj:not_implemented', ...
-                    'kf_sphere_proj is not yet implemented for emode %d', ...
-                    obj.emode_)
-                %obj.ki_mod_=sqrt((efix+eps(:))/neutron_constants('c_k_to_emev')); % [ne x 1]
-            else
-                error('HORACE:kf_sphere_proj:not_implemented', ...
-                    'kf_sphere_proj is not implemented for emode %d', ...
-                    obj.emode_)
-            end
+            obj = check_combo_arg_(obj);
         end
     end
     methods(Static)
