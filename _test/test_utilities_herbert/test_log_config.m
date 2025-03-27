7eab85429ee5263d35e0a4d087636e039a3d28e4
abuts <Alex.Buts@stfc.ac.uk>
2025-03-27 11:57:45 +0000

    Re #1776 typo in test_log_config

--- _test/test_utilities_herbert/test_log_config.m
+++ _test/test_utilities_herbert/test_log_config.m
@@ -61,7 +61,7 @@ classdef test_log_config< TestCase
             clOb = set_temporary_config_options(tc,'info_log_print_time',2);
             tc = tc.init_adaptive_logging();
             pause(2);
-            [tc,time] = tc.adapt_logging(1);
+            [tc,time] = tc.adapt_logging(2);
             assertEqualToTol(time,2,'tol',[0.1,0.1]);
             slr = tc.info_log_split_ratio;
             assertEqual(slr,2);
