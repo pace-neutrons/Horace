bc361072ce236f9e4ff0ac07ada8fecb90a7c733
abuts <Alex.Buts@stfc.ac.uk>
2025-04-16 17:33:28 +0100

    Re #1854 finished AxesBlockBase.init_accumulators method

--- horace_core/sqw/axes_blocks/@AxesBlockBase/AxesBlockBase.m
+++ horace_core/sqw/axes_blocks/@AxesBlockBase/AxesBlockBase.m
@@ -813,6 +813,10 @@ classdef AxesBlockBase < serializable
             % obj     -- initialized instance of AxesBlockBase class
             % n_accum -- number of accumulator arrays to initialize.
             %            may be 1 or 3 (2 transformed to 3)
+            % force_3D-- if true, return only 3-dimensional
+            %            accumulator arrays ignoring last (energy transfer)
+            %            dimension.
+            %
             % Returns:   Depending on n_accum, 1 or 3 empty arrays
             %            if n_accum == 1, two other arrays are empty
             % npix    -- empty npix array, used to accumulate pixels present
@@ -822,16 +826,7 @@ classdef AxesBlockBase < serializable
             % e       -- empty error array used to accumulate pixels variance
             %            in a bin (size of obj.dims_as_ssize)
             %
-            if force_3D
-            else
-                npix = zeros(obj.dims_as_ssize());
-                if n_accum == 1
-                    s = []; e=[];
-                else
-                    s = zeros(obj.dims_as_ssize());
-                    e = zeros(obj.dims_as_ssize());
-                end
-            end
+            [npix,s,e] = init_accumulators_(obj,n_accum,force_3D);
         end
     end
     %======================================================================
