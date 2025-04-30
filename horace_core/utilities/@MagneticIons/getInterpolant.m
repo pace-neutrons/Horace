420d58b22a42f1c72a06c635cbac6d61b7117e46
abuts <Alex.Buts@stfc.ac.uk>
2025-04-30 13:59:40 +0100

    Re #1862 reverted to intial J definition

--- horace_core/utilities/@MagneticIons/getInterpolant.m
+++ horace_core/utilities/@MagneticIons/getInterpolant.m
@@ -7,7 +7,7 @@ function [J0_ff,varargout] = getInterpolant(IonName)
 %>> Usage:
 %
 %>>[J0_ff,J2_ff]=MagneticIons.getInterpolant('Fe0')
-% or 
+% or
 %>>[J0_ff,J2_ff,J4_ff]=MagneticIons.getInterpolant('Fe0')
 % or
 %>>[J0_ff,J2_ff,J4_ff,J6_ff]=MagneticIons.getInterpolant('Fe3')
@@ -23,18 +23,18 @@ function [J0_ff,varargout] = getInterpolant(IonName)
 %
 %e.g.:
 %>>FF=J0_ff(Q2).^2+J2_ff(Q2).^2;
-%  where Q2==Q.*Q and J0_ff calculates magnetic form factor in zero and
-%  J2_ff -- dipole approximation correspondingly
+%  where Q2==Q.*Q (without 4*pi^2 factor)  and J0_ff calculates magnetic
+%  form factor in zero and J2_ff -- dipole approximation correspondingly
 %
 %
-par = self.IonParMap_(IonName);
+par = MagneticIons.IonParMap_(IonName);
 
 %ion            A      a     B      b     C      c     D
-%J0_ff = @(x2)((A*exp(-a*x2)+B*exp(-b*x2)+C*exp(-c*x2)+D)).^2;
+%J0_ff = @(x2)((A*exp(-a*x2)+B*exp(-b*x2)+C*exp(-c*x2)+D));
 
-J0_ff = @(x2)((par(1,1)*exp(-par(1,2)*x2)+par(1,3)*exp(-par(1,4)*x2)+par(1,5)*exp(-par(1,6)*x2)+par(1,7))).^2;
+J0_ff = @(x2)((par(1,1)*exp(-par(1,2)*x2)+par(1,3)*exp(-par(1,4)*x2)+par(1,5)*exp(-par(1,6)*x2)+par(1,7)));
 if nargout>1
     for i=1:nargout-1
-        varargout{i}=@(x2)(((par(i+1,1)*exp(-par(i+1,2)*x2)+par(i+1,3)*exp(-par(i+1,4)*x2)+par(i+1,5)*exp(-par(i+1,6)*x2)+par(i+1,7)).*x2)).^2;
+        varargout{i}=@(x2)(((par(i+1,1)*exp(-par(i+1,2)*x2)+par(i+1,3)*exp(-par(i+1,4)*x2)+par(i+1,5)*exp(-par(i+1,6)*x2)+par(i+1,7)).*x2));
     end
 end
