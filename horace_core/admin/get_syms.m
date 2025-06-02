1edda766d46516407e03681673e69aa40af8ebc1
Duc Le <duc.le@stfc.ac.uk>
2025-06-02 10:29:34 +0100

    Address review comment

--- horace_core/admin/get_syms.m
+++ horace_core/admin/get_syms.m
@@ -1,8 +1,12 @@
 function [info, sym] = get_syms(arg)
 % MATLAB wrapper for python script to generate crystal symmetries
 %
-% >> get_syms('P3_212')
-% >> get_syms(13)
+% >> [info, symop] = get_syms('P3_212')
+% >> [info, symop] = get_syms(13)
+%
+% where `info` is a string of information on the spacegroup and
+% `symop` is an array of symmetry operations which can be used by 
+% symmetrise_sqw or cut_sqw.
 %
 % Script to roughly convert from multifit_legacy to modern multifit syntax
 %
@@ -15,6 +19,9 @@ function [info, sym] = get_syms(arg)
 
     outpy = py.get_syms.main(arg);
     info = string(outpy(1));
+    % outpy(2) is a string which begins "sym = {..."
+    % evalc evaluates it in the current workspace, giving a variable "sym"
+    % which is returned to the user as the second output variable.
     evalc(string(outpy(2));
     if nargout < 2
         disp(info)
