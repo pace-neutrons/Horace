ce3a5ee10ae2304fd28ac9dcd0bb280b1b45d3ae
abuts <Alex.Buts@stfc.ac.uk>
2025-07-09 14:50:42 +0100

    Re #896 (b) finished mode 6 (bin pixels non-sorted, return cell idx)

--- _test/test_mex_nomex/test_bin_pixels_at_AxesBlock_mex_nomex.m
+++ _test/test_mex_nomex/test_bin_pixels_at_AxesBlock_mex_nomex.m
@@ -20,6 +20,117 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             [~,n_errors] = check_horace_mex();
             obj.no_mex = n_errors > 0;
         end
+        %==================================================================
+        function performance_mex_nomex_mode6_sort_and_uid(obj)
+            if obj.no_mex
+                skipTest('Can not test mex code to check binning against mex');
+            end
+            % this will recover existing configuration after test have been
+            % finished and temporary mex/nomex values will be set within
+            % the loop.
+            clObHor = set_temporary_config_options(hor_config, 'use_mex', false,'log_level',-1);
+            %
+            AB = AxesBlockBase_tester('nbins_all_dims',[50,20,50,20], ...
+                'img_range',[0,0,0,0;1,0.8,1,0.8]);
+
+            n_points = 20000000;
+            n_repeats = 5;
+            npix_nomex = []; s_nomex = [];e_nomex=[];uniqId_nom = [];
+            npix_mex   = []; s_mex = [];  e_mex=[];uniqId_mex = [];
+
+            t_nomex = zeros(1,n_repeats);
+            t_mex  = zeros(1,n_repeats);
+            disp("*** Mex/nomex performance mode6 (bin pixels + unique runid + return idx):")
+            for i= 1:n_repeats
+                fprintf('.')
+                pix_data = rand(9,n_points);
+                ids = 500+floor(100*rand(1,n_points));
+                pix_data(PixelDataBase.field_index('run_idx'),:) = ids;
+
+                pix = PixelDataMemory(pix_data);
+                coord = pix.coordinates;
+                config_store.instance.set_value('hor_config','use_mex',false);
+                t1 = tic();
+                [npix_nomex,s_nomex,e_nomex,pix_ok_nom,uniqId_nom,pix_idx_nom] = AB.bin_pixels(coord,npix_nomex,s_nomex,e_nomex,pix,uniqId_nom);
+                t_nomex(i) = toc(t1);
+                fprintf('.')
+
+                config_store.instance.set_value('hor_config','use_mex',true);
+
+                t1 = tic();
+                [npix_mex,s_mex,e_mex,pix_ok_mex,uniqId_mex,pix_idx_mex] = AB.bin_pixels(coord,npix_mex,s_mex,e_mex,pix,uniqId_mex);
+                t_mex(i) = toc(t1);
+
+                assertEqual(uint32(uniqId_nom),uniqId_mex);
+                assertEqual(uint64(pix_idx_nom),pix_idx_mex);
+
+                assertEqual(npix_nomex,npix_mex)
+                assertEqualToTol(s_nomex,s_mex,'tol',[1.e-12,1.e-12])
+                assertEqualToTol(e_nomex,e_mex,'tol',[1.e-12,1.e-12])
+                assertEqualToTol(pix_ok_nom,pix_ok_mex,'tol',[1.e-12,1.e-12])
+            end
+            tav_mex = sum(t_mex)/n_repeats;
+            tav_nom = sum(t_nomex)/n_repeats;
+            fprintf( ...
+                '\n*** time of first step,    nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
+                t_nomex(1),t_mex(1),t_nomex(1)/t_mex(1));
+            fprintf( ...
+                '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
+                tav_nom,tav_mex,tav_nom/tav_mex);
+            % REFERENCE DATA: ndw2671
+            %*** Mex/nomex performance mode6 (bin pixels + unique runid + return idx):
+            %*** time of first step,    nomex:  3.8(sec)  mex:  1.3(sec); Acceleration :  2.6-3
+            %*** Average time per step, nomex:  3.3(sec)  mex:  1.4(sec); Acceleration :  2.4
+        end
+
+        function test_bin_pixels_mex_nomex_mode6_nosort_multipage(obj)
+            if obj.no_mex
+                skipTest('Can not test mex code to check binning against mex');
+            end
+            AB = AxesBlockBase_tester('nbins_all_dims',[10,1,20,1], ...
+                'img_range',[0,0,0,0;0.8,0.8,1,0.8]);
+            pix_coord1 = rand(9,20);
+            pix_id = [10,10,11,11,7, 5,5,5,10,10];
+            pix_coord1(PixelDataBase.field_index('run_idx'),:) = [pix_id,pix_id];
+
+            pix_coord2 = rand(9,10);
+            pix_id = [10,11,7,11,7, 8,5,5,10,10];
+            pix_coord2(PixelDataBase.field_index('run_idx'),:) = pix_id;
+            pix1 = PixelDataMemory(pix_coord1);
+            pix2 = PixelDataMemory(pix_coord2);
+
+            clObHor = set_temporary_config_options(hor_config, 'use_mex', false);
+
+            npix_nom = [];   s_nom    = [];   e_nom    = [];
+            in_coord = pix1.coordinates;
+            [npix_nom,s_nom,e_nom,pix_ok_nom1,uniq_id1_nom,pix_idx_nom1] = AB.bin_pixels(in_coord,npix_nom,s_nom,e_nom,pix1);
+            in_coord = pix2.coordinates;
+            [npix_nom,s_nom,e_nom,pix_ok_nom2,uniq_id2_nom,pix_idx_nom2] = AB.bin_pixels(in_coord,npix_nom,s_nom,e_nom,pix2,uniq_id1_nom);
+            assertEqual(size(npix_nom),[10,20]);
+
+            clear clObHor
+            clObHor = set_temporary_config_options(hor_config, 'use_mex', true);
+
+            npix_mex = []; s_mex    = [];   e_mex    = [];
+
+            in_coord = pix1.coordinates;
+            [npix_mex,s_mex,e_mex,pix_ok_mex1,uniq_id1_mex,pix_idx_mex1] = AB.bin_pixels(in_coord,npix_mex,s_mex,e_mex,pix1);
+            assertEqual(pix_ok_nom1,pix_ok_mex1);
+            assertEqual(uint64(pix_idx_nom1),pix_idx_mex1);
+            assertEqual(uint32(uniq_id1_nom),uniq_id1_mex)
+            in_coord = pix2.coordinates;
+            [npix_mex,s_mex,e_mex,pix_ok_mex2,uniq_id2_mex,pix_idx_mex2] = AB.bin_pixels(in_coord,npix_mex,s_mex,e_mex,pix2,uniq_id1_mex);
+            assertEqual(size(npix_nom),[10,20]);
+
+            assertEqual(npix_mex,npix_nom);
+            assertEqualToTol(s_mex,s_nom);
+            assertEqualToTol(e_mex,e_nom);
+            assertEqual(uint64(pix_idx_nom2),pix_idx_mex2);
+            assertEqual(uint32(uniq_id2_nom),uniq_id2_mex)
+
+            assertEqual(pix_ok_nom2,pix_ok_mex2);
+        end
+
         function test_bin_pixels_mode6_nosort(obj)
             if obj.no_mex
                 skipTest('Can not test mex code to bin pixels in mode 5');
@@ -44,12 +155,84 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             assertEqual(size(npix_mex),[10,30]);
 
             assertEqual(uint32(unique_runid_nom),unique_runid_mex)
-            assertEqual(uint64(pix_id_nom),pix_id_mex)            
+            assertEqual(uint64(pix_id_nom),pix_id_mex)
             assertEqual(npix_mex,npix_nom);
             assertEqual(s_mex,s_nom);
             assertEqual(e_mex,e_nom);
             assertEqualToTol(pix_ok_nom,pix_ok_mex);
-        end        
+        end
+
+        function test_bin_pixels_inputs_mode6_twice(obj)
+            if obj.no_mex
+                skipTest('Can not test mex code to check binning parameters');
+            end
+            clObHor = set_temporary_config_options(hor_config, 'use_mex', true);
+            clWarn  = set_temporary_warning('off','HORACE:test_warning','HORACE:bin_pixels_c:internal_accumulator_reset');
+
+            AB = AxesBlockBase_tester('nbins_all_dims',[10,20,30,40], ...
+                'img_range',[-1,-2,-3,-10;1,2,3,40]);
+            pix_coord = rand(9,10);
+            pix_id = [10,10,11,11,7, 5,5,5,10,10];
+            pix_coord(PixelDataBase.field_index('run_idx'),:) = pix_id;
+
+            pix = PixelDataMemory(pix_coord);
+
+            in_coord = pix.coordinates;
+            [npix,s,e,pix_ok,unique_id,pix_img_idx,out_data] = AB.bin_pixels(in_coord,[],[],[],pix,[],'-test_mex_inputs');
+
+            assertEqual(pix_ok.data,out_data.pix_ok_data);
+            assertEqual(pix_ok.data,pix_coord);
+            assertEqual(pix_ok.data_range,out_data.pix_ok_data_range);
+            % range matrix have been allocated and probably contains zeros
+            % but this is not guaranteed.
+            assertEqual(size(out_data.pix_ok_data_range),[2,9]);
+            assertTrue(isempty(unique_id));
+            assertTrue(isa(unique_id,'uint32'));
+            assertTrue(isempty(pix_img_idx));
+            assertTrue(isa(pix_img_idx,'uint64'));
+
+
+            assertEqual(size(npix),[10,20,30,40]);
+            assertEqual(npix,zeros(10,20,30,40));
+            assertEqual(s,npix);
+            assertEqual(e,npix);
+            assertEqual(out_data.coord_in,in_coord);
+            assertEqual(out_data.pix_candidates,pix_coord);
+
+            npix(2) = 10;
+            s(1) = 10;
+            e(3) = 10;
+            pix_coord2 = rand(9,20);
+            pix_coord2(PixelDataBase.field_index('run_idx'),:) = [pix_id,pix_id];
+            pix = PixelDataMemory(pix_coord2);
+
+            warning('HORACE:test_warning','warning to clear warning cache')
+            in_coord = pix.coordinates;
+            [npix_out,s_out,e_out,pix_ok,pix_id,pix_img_idx,out_data] = AB.bin_pixels(in_coord,npix,s,e,pix,pix_id,'-test_mex_inputs');
+            [~,wid] = lastwarn;
+            assertEqual(wid,'HORACE:bin_pixels_c:internal_accumulator_reset');
+
+            assertEqual(pix_ok.data,out_data.pix_ok_data);
+            assertEqual(pix_ok.data,pix_coord2);
+            assertEqual(pix_ok.data_range,out_data.pix_ok_data_range);
+            % range matrix have been allocated and probably contains zeros
+            % but this is not guaranteed.
+            assertEqual(size(out_data.pix_ok_data_range),[2,9]);
+
+            assertTrue(isa(unique_id,'uint32'));
+            assertEqual(pix_id,uint32([5,7,10,11]));
+            assertTrue(isempty(pix_img_idx));
+            assertTrue(isa(pix_img_idx,'uint64'));
+
+
+            assertEqual(size(npix_out),[10,20,30,40]);
+            assertEqual(npix,npix_out);
+            assertEqual(s,s_out);
+            assertEqual(e,e_out);
+
+            assertEqual(out_data.coord_in,in_coord);
+            assertEqual(out_data.pix_candidates,pix_coord2);
+        end
 
         function test_return_inputs_mex_mode6_nosort_2D(obj)
             % bin pixels and sort pixels, input/output parameters
@@ -75,7 +258,7 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             assertTrue(isempty(unique_id));
             assertTrue(isa(unique_id,'uint32'));
             assertTrue(isempty(pix_idx));
-            assertTrue(isa(pix_idx,'uint64'));            
+            assertTrue(isa(pix_idx,'uint64'));
 
 
             assertEqual(pix_ok.data,out_data.pix_ok_data);
@@ -97,7 +280,7 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             assertTrue(isempty(out_data.alignment_matr));
             assertEqual(out_data.pix_candidates,pix.data);
             assertTrue(out_data.check_pix_selection);
-            assertEqual(out_data.pix_img_idx,pix_idx);            
+            assertEqual(out_data.pix_img_idx,pix_idx);
 
         end
 
@@ -121,7 +304,7 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
 
             t_nomex = zeros(1,n_repeats);
             t_mex  = zeros(1,n_repeats);
-            disp("*** Mex/nomex performance mode6 (bin and sort pixels + unique runid):")
+            disp("*** Mex/nomex performance mode5 (bin and sort pixels + unique runid):")
             for i= 1:n_repeats
                 fprintf('.')
                 pix_data = rand(9,n_points);
@@ -157,8 +340,12 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
                 tav_nom,tav_mex,tav_nom/tav_mex);
+            % REFEFENCE Data NDW2671
+            %*** Mex/nomex performance mode5 (bin and sort pixels + unique runid):
+            %*** time of first step,    nomex:  5.8(sec)  mex:  2.1(sec); Acceleration :  2.7
+            %*** Average time per step, nomex:  5.7(sec)  mex:  2.1(sec); Acceleration :  2.7
         end
-        
+
         function test_bin_pixels_mex_nomex_mode5_multipage(obj)
             if obj.no_mex
                 skipTest('Can not test mex code to check binning against mex');
@@ -383,6 +570,9 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '\n*** time of first step : %4.2g(sec)  av time per step: %4.2g(sec)\n', ...
                 t_mex(1),tav_mex);
+            % REFERENCE Data ndw2671
+            %*** Mex performance mode4 (bin and sort pixels):
+            %*** time of first step :  1.9(sec)  av time per step:  1.8(sec)
         end
 
         function performance_mex_nomex_mode4_and_align(obj)
@@ -441,7 +631,10 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
                 tav_nom,tav_mex,tav_nom/tav_mex);
-
+            % REFERENCE Data ndw2671
+            %*** Mex/nomex performance mode4 (bin and sort pixels applying alignment):
+            %*** time of first step,    nomex:  5.9(sec)  mex:  1.7(sec); Acceleration :  3.4
+            %*** Average time per step, nomex:  6.1(sec)  mex:  1.9(sec); Acceleration :  3.2
         end
 
         function performance_mex_nomex_mode4(obj)
@@ -463,7 +656,7 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
 
             t_nomex = zeros(1,n_repeats);
             t_mex  = zeros(1,n_repeats);
-            disp("*** Mex/nomex performance mode5 (bin and sort pixels):")
+            disp("*** Mex/nomex performance mode4 (bin and sort pixels):")
             for i= 1:n_repeats
                 fprintf('.')
                 pix_data = rand(9,n_points);
@@ -494,6 +687,10 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
                 tav_nom,tav_mex,tav_nom/tav_mex);
+            % REFERENCE Data ndw2671
+            %*** Mex/nomex performance mode4 (bin and sort pixels):
+            %*** time of first step,    nomex:  5.4(sec)  mex:  1.7(sec); Acceleration :  3.1
+            %*** Average time per step, nomex:  5.4(sec)  mex:  1.8(sec); Acceleration :    3
         end
 
         function test_bin_pixels_mex_nomex_mode4_multipage0Dim(obj)
@@ -789,7 +986,10 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
                 tav_nom,tav_mex,tav_nom/tav_mex);
-
+            % REFERENCE Data ndw2671
+            %*** Mex/nomex performance mode3 (binning cellarrays of data over coordinate frame):
+            %*** time of first step,    nomex:  2.2(sec)  mex:  0.7(sec); Acceleration :  3.2
+            %*** Average time per step, nomex:  2.2(sec)  mex: 0.69(sec); Acceleration :  3.3
         end
 
         function test_bin_pixels_mex_nomex_mode3_3Dmultipage(obj)
@@ -1007,7 +1207,7 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
 
             t_nomex = zeros(1,n_repeats);
             t_mex  = zeros(1,n_repeats);
-            disp("*** Mex/nomex performance mode3 (bin pixels only):")
+            disp("*** Mex/nomex performance mode2 (bin pixels + sig_err):")
             for i= 1:n_repeats
                 fprintf('.')
                 pix_data = rand(9,n_points);
@@ -1037,6 +1237,10 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
                 tav_nom,tav_mex,tav_nom/tav_mex);
+            % REFERENCE Data ndw2671
+            %*** Mex/nomex performance mode2 (bin pixelsbin pixels + sig_err):
+            %*** time of first step,    nomex:  3.4(sec)  mex: 0.68(sec); Acceleration :    5
+            %*** Average time per step, nomex:  3.4(sec)  mex: 0.71(sec); Acceleration :  4.8
         end
 
         function test_bin_pixels_mex_nomex_mode2_2Dmultipage(obj)
@@ -1220,7 +1424,7 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
 
             t_nomex = zeros(1,n_repeats);
             t_mex  = zeros(1,n_repeats);
-            disp("*** Mex/nomex performance mode0:")
+            disp("*** Mex/nomex performance mode0 (bin coord, calc npix):")
             for i= 1:n_repeats
                 fprintf('.')
                 in_coord = rand(4,n_points);
@@ -1246,7 +1450,10 @@ classdef test_bin_pixels_at_AxesBlock_mex_nomex < TestCase
             fprintf( ...
                 '*** Average time per step, nomex: %4.2g(sec)  mex: %4.2g(sec); Acceleration : %4.2g\n', ...
                 tav_nom,tav_mex,tav_nom/tav_mex);
-
+            % REFERENCE Data ndw2671
+            %*** Mex/nomex performance mode0 (bin coord, calc npix):
+            %*** time of first step,    nomex:  1.4(sec)  mex: 0.61(sec); Acceleration :  2.3
+            %*** Average time per step, nomex:  1.4(sec)  mex: 0.62(sec); Acceleration :  2.2
         end
 
         function test_bin_pixels_mex_nomex_mode0_3D_1Dmultipage_single(obj)
