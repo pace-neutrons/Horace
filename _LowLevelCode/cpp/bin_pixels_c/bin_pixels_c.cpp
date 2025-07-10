500cd73eb1f158f6c0c53d3445da2dda84130df7
abuts <Alex.Buts@stfc.ac.uk>
2025-07-10 10:14:16 +0100

    Re #896 (b) modify input analysis code to retrieve all parameters from MATLAB rather then selected, which certainly change.

--- _LowLevelCode/cpp/bin_pixels_c/bin_pixels_c.cpp
+++ _LowLevelCode/cpp/bin_pixels_c/bin_pixels_c.cpp
@@ -163,11 +163,10 @@ void parse_inputs(mxArray* plhs[], mxArray const* prhs[], std::unique_ptr<class_
 
     auto bin_arg_ptr = bin_arg_holder->class_ptr;
     if (bin_arg_ptr->new_binning_arguments_present(prhs)) {
-        bin_arg_ptr->parse_bin_inputs(prhs[in_arg::param_struct]);
         force_update = true;
-    } else {
-        bin_arg_ptr->parse_changed_bin_inputs(prhs[in_arg::param_struct]);
     }
+    bin_arg_ptr->parse_bin_inputs(prhs[in_arg::param_struct]);
+
     bin_arg_ptr->check_and_init_accumulators(plhs, prhs,force_update);
     return;
 };
