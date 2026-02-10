0c679bb41a81c58c6cb456921ca17d886b52070a
abuts <Alex.Buts@stfc.ac.uk>
2026-02-10 12:34:19 +0000

    Re #1822 more tolerance errors

--- _test/test_sym_op/test_symop.m
+++ _test/test_sym_op/test_symop.m
@@ -47,7 +47,7 @@ classdef test_symop < TestCase
             pix_cc = lp.transform_img_to_pix(transf_pix);
 
             sym_pix = refl.transform_pix(pix,{},true(1,size(pix,2)),true);
-            assertEqualToTol(pix_cc , sym_pix);
+            assertEqualToTol(pix_cc , sym_pix,'tol',1.e-14);
         end
         function test_symop_transform_projection_eq_symop_100(~)
             refl = SymopReflection([1,0,0],[0,0,1]);
@@ -59,7 +59,7 @@ classdef test_symop < TestCase
             pix_cc = lp.transform_img_to_pix(transf_pix);
 
             sym_pix = refl.transform_pix(pix,{},true(1,size(pix,2)),true);
-            assertEqual(pix_cc , sym_pix);
+            assertEqualToTol(pix_cc , sym_pix,'tol',1.e-14);
         end
 
         function test_symop_create_identity(~)
